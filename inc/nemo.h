f8cae117d73bf0e5a61fc155ddb15632c5ab2fd0
Peter Teuben <teuben@gmail.com>
1990-10-11 02:28:29 +0000

    import NEMO V3

---
+++ inc/nemo.h
@@ -0,0 +1,18 @@
+/*
+ *  A silly little header file for lazy programmers
+ *  Should be enough for any standard Nemo program
+ *  Does assume the presence of the whole Nemo kernel,
+ *  hence the IO core program cannot use this
+ */
+
+#ifndef _nemo_h
+#define _nemo_h
+
+#include <stdinc.h>		/* also does <math.h> */
+#include <getparam.h>
+#include <vectmath.h>
+
+#include <filestruct.h>
+#include <history.h>
+
+#endif
