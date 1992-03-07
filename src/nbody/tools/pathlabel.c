08fbffe23c9a88d0878270007ce32297285d5df3
Peter Teuben <teuben@gmail.com>
1992-03-07 17:03:30 +0000

    import NEMO V3

---
+++ src/nbody/tools/pathlabel.c
@@ -0,0 +1,170 @@
+/*
+ * PATHLABEL.C: label particles according to position along parametric path.
+ *
+ *	xx-xxx-xx  0.0 -- JEB
+ *	20-feb-92  0.1 removed dex() - which is defined in the NEMO kernel PJT
+ *	 7-mar-92  0.2 fixed pathw string bug  & happy gcc2.0	           pjt
+ */
+
+#include <stdinc.h>
+#include <getparam.h>
+#include <vectmath.h>
+#include <filestruct.h>
+
+#include <snapshot/snapshot.h>
+#include <snapshot/body.h>
+#include <snapshot/get_snap.c>
+#include <snapshot/put_snap.c>
+
+string defv[] = {
+    "in=???\n			Input file",
+    "out=???\n			Output file",
+    "pathx=sin(TWO_PI*q)\n	X(q) coordinate of Path",
+    "pathy=cos(TWO_PI*q)\n	Y(q) coordinate of Path",
+    "pathw=0.1\n		--never used--",
+    "npnts=128\n		Number of points",
+    "VERSION=0.2\n		7-mar-92 PJT",
+    NULL,
+};
+
+string usage = "convert ASCII N-body file to binary format";
+
+
+Body *btab = NULL;
+int nbody;
+real tsnap = 0.0;
+
+rproc pathx, pathy;
+real pathw;
+int npnts;
+
+
+void loadfuns();	/* dummy local math loader */
+
+void
+nemo_main()
+{
+    stream instr, outstr;
+    rproc compile_func();
+    void pathlabel();
+    int snapbits;
+
+    instr = stropen(getparam("in"), "r");
+    get_history(instr);
+    outstr = stropen(getparam("out"), "w");
+    put_history(outstr);
+    pathx = compile_func(getparam("pathx"), "q");
+    pathy = compile_func(getparam("pathy"), "q");
+    pathw = getdparam("pathw");
+    npnts = getiparam("npnts");
+    get_snap(instr, &btab, &nbody, &tsnap, &snapbits);
+    if (snapbits & PhaseSpaceBit == 0)
+	error("%s: no phasespace info\n", getargv0());
+    pathlabel();
+    snapbits |= AuxBit;
+    put_snap(outstr, &btab, &nbody, &tsnap, &snapbits);
+}
+
+/*
+ * COMPILE_FUNC: workhorse routine which compiles a real function of
+ * a real variable, loads the object module, and returns a pointer.
+ */
+
+#include <filefn.h>
+#include <loadobj.h>
+
+bool havesyms = FALSE;		/* TRUE if symbols have been loaded */
+int funcmpld = 0;	       	/* count of functions compiled */
+
+rproc 
+compile_func(expr, var)
+string expr;			/* C expression for function */
+string var;			/* name of argument variable */
+{
+    char path[64], file[64], func[64], cmmd[128];
+    stream cdstr;
+    proc findfn(), result;
+
+    if (! havesyms)
+	mysymbols(getargv0());
+    havesyms = TRUE;
+    fprintf(stderr, "[compile_func: invoking cc]\n");
+    sprintf(path, "/tmp/cf_%d", getpid());
+    sprintf(file, "%s.c", path);
+    cdstr = fopen(file, "w");
+    sprintf(func, "_cf_%d", ++funcmpld);
+    fprintf(cdstr, "#include <stdinc.h>\n#include <math.h>\n");
+    fprintf(cdstr, "real %s(%s)\n", &func[1], var);
+    fprintf(cdstr, "real %s;\n", var);
+    fprintf(cdstr, "{\n    return (%s);\n}\n", expr);
+    fclose(cdstr);
+    sprintf(cmmd, "cc -o %s.o -c %s.c", path, path);
+    if (system(cmmd) != 0)
+	error("compile_func in %s: cant %s\n", getargv0(), cmmd);
+    sprintf(file, "%s.o", path);
+    loadobj(file);
+    sprintf(cmmd, "rm %s.c %s.o", path, path);
+    if (system(cmmd) != 0)
+	error("compile_func in %s: cant %s\n", getargv0(), cmmd);
+    result = findfn(func);
+    if (result == NULL)
+	error("compile_func in %s: cant find %s\n", getargv0(), func);
+    return ((rproc) result);
+}
+
+/*
+ * LOADFUNS: this curious function is never called.  However, its mere
+ * existence is sufficient to fool ld into loading the math functions.
+ */
+
+void
+loadfuns()
+{
+    real cbrt(), sqrt(), qbe(), sqr();
+    real sin(), cos(), asin(), acos();
+    real tan(), atan(), atan2();
+    real exp(), dex(), log(), log10(), pow();
+    real fabs(), floor(), ceil(), rint();
+
+    (void) cbrt(1.0);
+    (void) sqrt(1.0);
+    (void) qbe(1.0);
+    (void) sqr(1.0);
+    (void) sin(1.0);
+    (void) cos(1.0);
+    (void) asin(1.0);
+    (void) acos(1.0);
+    (void) tan(1.0);
+    (void) atan(1.0);
+    (void) atan2(1.0, 1.0);
+    (void) exp(1.0);
+    (void) dex(1.0);
+    (void) log(1.0);
+    (void) log10(1.0);
+    (void) pow(1.0, 1.0);
+    (void) fabs(1.0);
+    (void) floor(1.0);
+    (void) ceil(1.0);
+    (void) rint(1.0);
+}
+
+void
+pathlabel()
+{
+    int j, i;
+    real q, px, py, dx, dy, dij;
+
+    for (j = 0; j <= npnts; j++) {
+	q = ((real) j) / ((real) npnts);
+	px = (pathx)(q);
+	py = (pathy)(q);
+	if (j%8==0) dprintf(1,"q, px, py = %f, %f, %f\n", q, px, py);
+	for (i = 0; i < nbody; i++) {
+	    dx = Pos(&btab[i])[0] - px;
+	    dy = Pos(&btab[i])[1] - py;
+	    dij = sqrt(dx*dx + dy*dy);
+	    if (j == 0 || dij < Aux(&btab[i]))
+		Aux(&btab[i]) = dij;
+	}
+    }
+}
