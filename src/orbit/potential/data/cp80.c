91d59fa97f045e79f55f48352f6b0d7400cd0fd2
Peter Teuben <teuben@gmail.com>
1993-11-04 01:24:29 +0000

    import NEMO V3

---
+++ src/orbit/potential/data/cp80.c
@@ -0,0 +1,103 @@
+/*
+ * cp80.c:   Used by Contopoulos & Papayanopoulos (1980, AA 92, 33)
+ *           in their study of orbits in barred galaxies
+ *           (See also Barbanis & Woltjer, 1967)
+ *
+ *  V = V_0 + V_1
+ *      Where the axisymetric part (V_0) is the Isochrone model
+ *      and a cos(2*phi) perturbation V_1=eps.sqrt(r).(16-r)cos(2*phi)
+ *
+ *	7-mar-92   happy gcc2.0				pjt
+ *	  oct-93   get_pattern
+ *
+ *
+ */
+
+/*CTEX
+ *    {\bf potname=cp80
+ *	 potpars={\it $\Omega,\epsilon$}}
+ *
+ *  Contopoulos \& Papayannopoulos (1980, A\&A, 92,33)
+ *  used this potential
+ *  in the study of orbits in barred galaxies. Note that their
+ *  ``bar'' is oriented along the Y-axis, and an axis ratio is not
+ *  well defined. The potential used is given by adding an
+ *  axisymmetric component to a m=2 fourier component:
+ *  $$
+ *     \Phi = \Phi_1 + \Phi_2
+ *  $$
+ *  where $\Phi_1$ is the Isochrone potential with unit scalelength and
+ *  mass, and $\Phi_2$ the Barbanis \& Woltjer (1965) potential:
+ *  $$
+ *        \Phi_1 = - { 1 \over { (1 + \sqrt{1+r^2})}}
+ *  $$
+ *  and
+ *  $$
+ *         \Phi_2 = \epsilon  r (16-r) cos(2\phi)
+ *  $$
+ */
+ 
+#include <stdinc.h>
+
+local double omega = 0.05;         /* pattern speed */
+local double eps = 0.00001;        /* 'bar' perturbation: 0.001 is strong! */
+
+/*  Following two are not considered parameters in the model, but fixed */
+/*  yet in the code they are treated as parameters                      */
+local double iso_mass = 1.0;
+local double iso_radius = 1.0;
+
+local double iso_radius2;
+
+double sqrt(), sqr();
+
+void inipotential (npar, par, name)
+    int    *npar;
+    double par[];
+    char *name;
+{
+    int n;
+
+    n = *npar;
+    if (n>0) omega = par[0];
+    if (n>1) eps = par[1];
+    if (n>2) warning("CP80 potential: only 2 parameters used");
+
+    iso_radius2 = sqr(iso_radius);
+
+    dprintf (0,"INIPOTENTIAL CP80 [2D version]\n");
+    dprintf (1,"  Parameters : Pattern Speed=%g  eps=%g\n",omega,eps);
+    par[0] = omega;
+}
+    
+void potential (ndim,pos,acc,pot,time)
+    int    *ndim;
+    double pos[], acc[], *pot, *time;
+{
+    double a, tmp, rad2, rad, rads, ft, fr, cosp, sinp, costp, sintp;
+
+    rad2 = sqr(pos[0]) + sqr(pos[1]);
+
+/* Isochrone */
+    a=sqrt(rad2+iso_radius2);
+    *pot = -iso_mass / (iso_radius + a);
+    tmp = *pot / (a * (iso_radius + a));
+    acc[0] = tmp*pos[0];
+    acc[1] = tmp*pos[1];
+    if (*ndim>2) acc[2] = 0.0;
+    if (rad2==0.0) return;
+
+/* Barbanis & Woltjer's bar: */
+    rad = sqrt(rad2);
+    rads = sqrt(rad);
+    costp=(sqr(pos[0])-sqr(pos[1]))/rad2;
+    sintp=2*pos[0]*pos[1]/rad2;
+    cosp=pos[0]/rad;
+    sinp=pos[1]/rad;
+
+    *pot += eps*rads*(16-rad)*costp;
+    fr = eps*(16-3*rad)*costp/rads;	/* (neg of) radial and */
+    ft = 2*eps*(rad-16)*sintp/rads;	/* tangential force */
+    acc[0] -= fr*cosp - ft*sinp;
+    acc[1] -= fr*sinp + ft*cosp;
+}
