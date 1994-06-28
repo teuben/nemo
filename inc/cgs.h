b18cf048d90136bd98a21b64d597e0728dbf492d
Peter Teuben <teuben@gmail.com>
1994-06-28 22:21:07 +0000

    import NEMO V3

---
+++ inc/cgs.h
@@ -0,0 +1,18 @@
+/* 
+ *  Some fundamental physical constants - cgs units
+ *
+ *  See also: "The Fundamental Physical Constants" by E. Richard
+ *     Cohen and Barry N. Taylor (PHYICS TODAY, August 1989).
+ */
+
+
+#define G_CGS 6.67e-8		/* gravitional constant */
+#define Msol_CGS 1.99e33	/* solar mass in grams */
+#define mH_CGS 1.67e-24		/* mass of hydrogen atom in grams */
+#define c_CGS 299792458000.0    /* speed of light [cm/sec] */
+#define k_CGS 1.380658e-16      /* Boltzmann  [erg/deg] */
+#define h_CGS 6.6260755e-27     /* Planck [erg-sec] */
+
+#define KPCCM 3.085678e+21	/* kiloparsec in centimeters */
+#define GYRSEC 3.155693e+16	/* gigayear in seconds */
+
