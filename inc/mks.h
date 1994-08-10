73059338d6878e7ef3368214084c2d08fb0450d7
Peter Teuben <teuben@gmail.com>
1994-08-10 16:03:15 +0000

    import NEMO V3

---
+++ inc/mks.h
@@ -0,0 +1,21 @@
+/* 
+ *  Some fundamental physical constants - mks units
+ *
+ *  See also:
+ *  "The Fundamental Physical Constants" by
+ *      E. Richard Cohen and Barry N. Taylor
+ *          (PHYICS TODAY, August 1989, Austus 1994)
+ */
+
+
+#define c_MKS 299792458.0       /* speed of light [m/s] */
+#define k_MKS 1.380658e-23      /* Boltzmann  [J/K] */
+#define h_MKS 6.6260755e-34     /* Planck [J-s] */
+#define G_MKS 6.67259e-11       /* Newton's Gravitational Constant "G" [N m^2/kg^2] */
+
+#define M_SOLAR   1.989e30	/* Mass of Sun [kg] */
+#define L_SOLAR   3.827e26 	/* Luminosity of Sun [W] */
+#define R_SOLAR	  6.96e8	/* Radius of Sun [m] */
+#define M_EARTH   5.974e24	/* Mass of earth [kg] */
+#define AU	  1.496e11      /* Astronomical Unit [m]  */
+#define PC        3.0856e16     /* Parsec [m] */
