deeb4b77769e1dd8d9b5600e1f50634000f02869
Peter Teuben <teuben@gmail.com>
1994-11-22 03:57:08 +0000

    Initial revision

---
+++ src/kernel/misc/nemomain.cc
@@ -0,0 +1,38 @@
+/*
+ *	NEMOMAIN:  standard way a nemoprogram starts and finishes
+ *	by calling your main program 'nemo_main' instead of
+ *	'main' you get more of the user interface done
+ *	transparently
+ *              (1) calls 'initparam' to set up program
+ *              (2) calls program 'nemo_main'
+ *              (3) calls 'finiparam' to finish program
+ *              (4) return normal status to shell
+ *
+ *      20-nov-93  added options MAIN_ symbol to fool the fortran compiler
+ *	21-nov-94  correct C++ version for C++ main
+ */
+
+#include <stdinc.h>
+#include <getparam.h>
+
+extern string defv[];		/* defv MUST be defined in user program ! */
+extern string usage;		/* One line description of the program    */
+
+extern void nemo_main(void);	/* this must be be a C++ nemo_main too !! */
+
+void main(int argc,char *argv[])
+{
+    if(argv[argc] != NULL) 
+       warning("Your C compiler seems to have an old-style (short) argv[]");
+    initparam(argv,defv);		/* start  */
+    nemo_main();			/* call program */
+    finiparam();			/* end */
+    exit(0);                            /* return normal status to shell */
+}
+
+#if defined(MAIN)
+MAIN_()
+{
+  error("MAIN_ called; some fortran inconsistency");
+}
+#endif
