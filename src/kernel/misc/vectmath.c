45449a68d4695caa736981d9d9f3864d9781665e
Peter Teuben <teuben@gmail.com>
1995-01-24 07:18:33 +0000

    Initial revision

---
+++ src/kernel/misc/vectmath.c
@@ -0,0 +1,67 @@
+/*
+ * VECTMATH.C: source code for vector/matrix operations.
+ * Joshua Barnes  1 December 1986  Princeton, NJ.
+ *	29-jul-92	convex fix		PJT
+ *	22-jan-95	ansi prototypes - functions return real ????
+ */
+
+#include <stdinc.h>
+
+#ifndef SINGLEPREC
+#  define _dotvp	_dotvp_d
+#  define _absv		_absv_d
+#  define _distv	_distv_d
+#  define _tracem	_tracem_d
+#else
+#  define _dotvp	_dotvp_f
+#  define _absv		_absv_f
+#  define _distv	_distv_f
+#  define _tracem	_tracem_f
+#endif
+
+double _dotvp(real *v, real *u, int n)
+{
+    real s;
+
+    s = 0.0;
+    while (--n >= 0)
+	s += (*v++) * (*u++);
+    return (s);
+}
+
+double _absv(real *v, int n)
+{
+    real s;
+
+    s = 0.0;
+    while (--n >= 0) {
+	s += (*v) * (*v);
+	v++;
+    }
+    return (sqrt(s));
+}
+
+double _distv(real *v, real *u, int n)
+{
+    real d, s;
+
+    s = 0.0;
+    while (--n >= 0) {
+	d = (*v++) - (*u++);
+	s += d * d;
+    }
+    return (sqrt(s));
+}
+
+double _tracem(real *p, int n)
+{
+    double s;
+    register int i;
+
+    s = 0.0;
+    for (i = n; --i >= 0; ) {
+	s += (*p);
+	p += (n + 1);			/* next diag. element */
+    }
+    return (s);
+}
