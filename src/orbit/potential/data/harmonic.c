7ff02ba6b41a5f3361dca99ecbe8b1e169dc4b86
Peter Teuben <teuben@gmail.com>
1995-03-27 17:12:03 +0000

    import NEMO V3

---
+++ src/orbit/potential/data/harmonic.c
@@ -0,0 +1,53 @@
+/*
+ * harmonic.c - harmonic (constant density) potential
+ *
+ *	7-mar-92  happy gcc2.0		pjt
+ *	  oct-93  get_pattern		pjt
+ */
+
+/*CTEX
+ * {\bf potname=harmonic
+ *       potpars={\it $\Omega,\omega_x^2,\omega_z^2,\omega_z^2$}}
+ *
+ *
+ * Harmonic potential
+ * $$
+ *        \Phi = {1 \over 2} \omega_x^2 x^2
+ *             + {1 \over 2} \omega_y^2 y^2
+ *             + {1 \over 2} \omega_z^2 z^2
+ * $$
+ */
+ 
+
+#include <stdinc.h>
+
+static double omega = 0.0;           /* just put to zero until implemented */
+static double h[3] = {1.0,1.0,1.0};  /* default parameters harmonic potential */
+
+#define SQR(x)  ((x)*(x))
+
+void inipotential (int *npar, double *par, string name)
+{
+    int i;
+
+    if (*npar>0) omega = par[0];
+    for (i=1; i<(*npar); i++)
+       h[i-1] = sqr(par[i]);
+
+    dprintf (1,"INI_POTENTIAL Harmonic\n");
+    dprintf (1,"  Parameters : Pattern Speed = %f\n",omega);
+    dprintf (1,"  wx^2,wy^2,wz^2= %f %f %f\n",h[0],h[1],h[2]);
+    par[0] = omega;
+}
+
+void potential (int *ndim,double *pos,double *acc,double *pot,double *time)
+{
+        int    i;
+        
+        *pot = 0.0;
+        for (i=0; i<*ndim; i++) {
+                (*pot) += h[i] * SQR(pos[i]);
+                acc[i] = -h[i] * pos[i];
+        }
+        *pot *= 0.5;
+}
