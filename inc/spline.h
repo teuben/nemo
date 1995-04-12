e6f3de8dc92725bdaed64dc2daf981c2c1cdca9c
Peter Teuben <teuben@gmail.com>
1995-04-12 16:00:29 +0000

    Initial revision

---
+++ inc/spline.h
@@ -0,0 +1,11 @@
+/*
+ * SPLINE.C: functions to evaluate cubic spline approximations.
+ */
+
+extern void spline(real *, real *, real *, int);
+extern real seval(real, real *, real *, real *, int);
+extern real spldif(real, real *, real *, real *, int);
+extern real spldif2(real, real *, real *, real *, int);
+
+
+
