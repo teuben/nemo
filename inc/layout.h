97e63e4f0fdf92100303a5b5511edbfaba55a327
Peter Teuben <teuben@gmail.com>
1995-04-13 03:37:56 +0000

    import NEMO V3

---
+++ inc/layout.h
@@ -0,0 +1,48 @@
+/*
+ * layout.h:  definitions for the YAPP layout interpreter 
+ */
+
+#ifndef _layout_h
+#define _layout_h
+
+/* 
+ * pl_id:  enumeration of all valid YAPP commands 
+ */
+
+typedef enum { End, Swap, Xscale, Yscale, Ltype, Line, Move,    /* Valid Yapp */
+               Point, Circle, Cross, Box, Just, Text, Flush,
+               Frame, Init, Stop, NOP} pl_id;
+
+
+/*
+ * pl_par:     union to hold any type of parameter we support
+ */
+
+typedef union pl_par {      /* hold a function parameters */
+    char   c;
+    int    i;
+    float  f;
+    double d;
+    real   r;
+    char  *s;
+} pl_par;
+
+#define MAXPAR  5
+
+/*
+ * plcommand:   a structure holding the pl_id of the calling function,
+ *              and a list of par's, up to MAXPAR.
+ */
+
+typedef struct plcommand {  /* a YAPP command: name and parameters */
+	pl_id  id;
+        pl_par pars[MAXPAR];
+        struct plcommand *next;
+} plcommand;
+
+
+plcommand *pl_fread(string file);
+int        pl_lread(string line, plcommand *p);
+void       pl_exec (plcommand *p);
+
+#endif
---
+++ src/kernel/misc/log2.c
@@ -0,0 +1,11 @@
+/*
+ * LOG2: log to the base two.
+ * WHY AINT THIS STANDARD? WHATS *W*R*O*N*G* WITH PEOPLE?
+ */
+#include <math.h>
+
+double log2(double x)
+{
+
+    return (log(x) / 0.693147180559945);	/* normalize by ln(2)       */
+}
