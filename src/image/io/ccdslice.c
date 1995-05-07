799c9aa55bbeabed232f9d5d176f4d7514e7dfb1
Peter Teuben <teuben@gmail.com>
1995-05-07 04:09:05 +0000

    import NEMO V3

---
+++ src/image/io/ccdslice.c
@@ -0,0 +1,140 @@
+/* 
+ *	CCDSLICE: take slices from a cube
+ *
+ *	 6-may-95  V1.0 created 	Peter Teuben
+ */
+
+
+#include <stdinc.h>
+#include <getparam.h>
+#include <vectmath.h>
+
+#include <image.h>
+
+string defv[] = {	/* keywords + help string for user interface */
+    "in=???\n       Input filename (image)",
+    "out=???\n      Output filename (image)",
+    "zvar=z\n       Slice variable (x,y,z)",
+    "zrange=\n      Slices to select (1..n<zvar>)",
+    "VERSION=1.0\n  6-may-95 PJT",
+    NULL,
+};
+
+string usage="takes slices from a cube";
+
+#ifndef MAXPLANE
+#define MAXPLANE 2048
+#endif
+
+#define X_SLICE 0
+#define Y_SLICE 1
+#define Z_SLICE 2
+
+nemo_main()
+{
+    imageptr iptr=NULL, optr=NULL;
+    stream instr, outstr;
+    int planes[MAXPLANE], mode, i;
+    string zvar;
+    int nx, ny, nz;
+
+    instr = stropen (getparam("in"),"r");	/* get stream */
+    read_image (instr,&iptr);               /* read image */
+    strclose(instr);                        /* close image file */
+
+    zvar = getparam("zvar");
+    if (streq(zvar,"x")) {
+        mode = X_SLICE;
+        nx = Ny(iptr);
+        ny = Nz(iptr);
+        nz = Nx(iptr);
+    } else if (streq(zvar,"y")) {
+        mode = Y_SLICE;
+        nx = Nx(iptr);
+        ny = Nz(iptr);
+        nz = Ny(iptr);
+    } else if (streq(zvar,"z")) {
+        mode = Z_SLICE;
+        nx = Nx(iptr);
+        ny = Ny(iptr);
+        nz = Nz(iptr);
+    } else 
+        error("Illegal slice axis %s, must be x,y,z",zvar);
+
+
+    if (hasvalue("zrange")) {
+        nz = nemoinpi(getparam("zrange"),planes,MAXPLANE);
+        if (nz<1) 
+            error("Bad syntax %d for zrange=%s",nz,getparam("zrange"));
+    } else {
+        for (i=0; i<nz; i++)
+            planes[i] = i+1;
+    }
+    create_cube(&optr,nx,ny,nz);
+    slice(iptr,optr,mode,planes);
+    
+    outstr = stropen(getparam("out"),"w");
+    write_image(outstr,optr);
+    strclose(outstr);
+}
+
+slice(imageptr i, imageptr o, int mode, int *planes)
+{
+    int x, y, z, iz;
+
+    if (mode==X_SLICE) {
+        for(iz=0; iz<Nz(o); iz++) {
+            z = planes[iz]-1;
+            if (z<0 || z>Nx(i)) 
+                error("%d: illegal plane in x, max is %d",z,Nx(i));
+            for (y=0; y<Ny(o); y++)
+            for (x=0; x<Nx(o); x++)
+                CubeValue(o,x,y,iz) = CubeValue(i,z,x,y);
+        }
+        Namex(o) = Namey(i);
+        Namey(o) = Namez(i);
+        Namez(o) = Namex(i);
+        Xmin(o) = Ymin(i);
+        Ymin(o) = Zmin(i);
+        Zmin(o) = Xmin(i);
+        Dx(o) = Dy(i);
+        Dy(o) = Dz(i);
+        Dz(o) = Dx(i);
+    } else if (mode==Y_SLICE) {
+        for(iz=0; iz<Nz(o); iz++) {
+            z = planes[iz]-1;
+            if (z<0 || z>Ny(i)) 
+                error("%d: illegal plane in y, max is %d",z,Ny(i));
+            for (y=0; y<Ny(o); y++)
+            for (x=0; x<Nx(o); x++)
+                CubeValue(o,x,y,iz) = CubeValue(i,x,z,y);
+        }
+        Namex(o) = Namex(i);
+        Namey(o) = Namez(i);
+        Namez(o) = Namey(i);
+        Xmin(o) = Xmin(i);
+        Ymin(o) = Zmin(i);
+        Zmin(o) = Ymin(i);
+        Dx(o) = Dx(i);
+        Dy(o) = Dz(i);
+        Dz(o) = Dy(i);
+    } else if (mode==Z_SLICE) {
+        for(iz=0; iz<Nz(o); iz++) {
+            z = planes[iz]-1;
+            if (z<0 || z>Nz(i)) 
+                error("%d: illegal plane in z, max is %d",z,Nz(i));
+            for (y=0; y<Ny(o); y++)
+            for (x=0; x<Nx(o); x++)
+                CubeValue(o,x,y,iz) = CubeValue(i,x,y,z);
+        }
+        Namex(o) = Namex(i);
+        Namey(o) = Namey(i);
+        Namez(o) = Namez(i);
+        Xmin(o) = Xmin(i);
+        Ymin(o) = Ymin(i);
+        Zmin(o) = Zmin(i);
+        Dx(o) = Dx(i);
+        Dy(o) = Dy(i);
+        Dz(o) = Dz(i);
+    }
+}
