2b7b420e990650c63b68b59e5bb44ad36e247d5a
Peter Teuben <teuben@gmail.com>
1995-05-23 23:43:55 +0000

    Initial revision

---
+++ src/nbody/evolve/aarseth/tools/fname.h
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------
+** MACRO FNAME for any fortran callable routine name.
+**
+**  This macro prepends, appends, or does not modify a name
+**  passed as a macro parameter to it based on the FNAME_PRE_UNDERSCORE,
+**  FNAME_POST_UNDERSCORE macros set for a specific system.
+**
+**---------------------------------------------------------------*/
+#if defined(FNAME_PRE_UNDERSCORE) && defined(FNAME_POST_UNDERSCORE)
+#ifdef __STDC__
+#   define FNAME(x)     _##x##_
+#else
+#   define FNAME(x)     _/**/x/**/_
+#endif
+#endif
+#if defined(FNAME_PRE_UNDERSCORE) && !defined(FNAME_POST_UNDERSCORE)
+#ifdef __STDC__
+#   define FNAME(x)     _##x
+#else
+#   define FNAME(x)     _/**/x
+#endif
+#endif
+#if !defined(FNAME_PRE_UNDERSCORE) && defined(FNAME_POST_UNDERSCORE)
+#ifdef __STDC__
+#   define FNAME(x)     x##_
+#else
+#   define FNAME(x)     x/**/_
+#endif
+#endif
+#if !defined(FNAME_PRE_UNDERSCORE) && !defined(FNAME_POST_UNDERSCORE)
+#   define FNAME(x)     x
+#endif
+
