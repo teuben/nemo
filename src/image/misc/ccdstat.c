992fa96e9a81108b1cf7bb4fc70517ff7b35da27
Peter Teuben <teuben@gmail.com>
1995-09-17 17:04:31 +0000

    import NEMO V3

---
+++ src/image/misc/ccdstat.c
@@ -0,0 +1,82 @@
+/* 
+ * CCDSTAT
+ *
+ *	 4-oct-88  V1.0 created
+ *	 5-nov-93  V1.1 nemo V2, and using moment.h
+ *      16-sep-95  V1.2 added min/max
+ *
+ */
+
+#include <stdinc.h>
+#include <getparam.h>
+#include <filestruct.h>
+#include <image.h>
+#include <moment.h> 
+
+string defv[] = {
+    "in=???\n       Input image filename",
+    "min=\n         Minimum overrride",
+    "max=\n         Maximum overrride",
+    "VERSION=1.2\n  16-sep=95 PJT",
+    NULL,
+};
+
+string usage="basic statistics of an image";
+
+string	infile;	        		/* file names */
+stream  instr;				/* file streams */
+
+imageptr iptr=NULL;			/* will be allocated dynamically */
+
+
+int    nx,ny,nz,nsize;			/* actual size of map */
+double xmin,ymin,zmin,dx,dy,dz;
+double size;				/* size of frame (square) */
+double cell;				/* cell or pixel size (square) */
+
+
+
+nemo_main()
+{
+    int  i, j, k;
+    real x, xmin, xmax, mean, sigma, skew, kurt;
+    Moment m;
+    bool Qmin, Qmax;
+    
+    instr = stropen (getparam("in"), "r");
+    read_image (instr,&iptr);
+    strclose(instr);
+
+    nx = Nx(iptr);	
+    ny = Ny(iptr);
+    nz = Nz(iptr);
+    Qmin = hasvalue("min");
+    if (Qmin) xmin = getdparam("min");
+    Qmax = hasvalue("max");
+    if (Qmax) xmax = getdparam("max");
+
+
+    ini_moment(&m,4);
+    for (i=0; i<nx; i++) {
+      for (j=0; j<ny; j++) {
+        for (k=0; k<nz; k++) {
+            x =  CubeValue(iptr,i,j,k);
+            if (Qmin && x<xmin) continue;
+            if (Qmax && x>xmax) continue;
+            accum_moment(&m,x,1.0);
+        }
+      }
+    }
+    nsize = nx * ny * nz;
+    
+    mean = mean_moment(&m);
+    sigma = sigma_moment(&m);
+    skew = skewness_moment(&m);
+    kurt = kurtosis_moment(&m);
+          
+    printf ("Min=%f  Max=%f\n",min_moment(&m), max_moment(&m));
+    printf ("Mean and dispersion  : %f %f\n",mean,sigma);
+    printf ("Skewness and kurtosis: %f %f\n",skew,kurt);
+    printf ("%d/%d out-of-range points discarded\n",nsize-n_moment(&m), nsize);
+        
+}
