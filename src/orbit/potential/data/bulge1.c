4e16284fde511aa68a98eac947bcd77f5b9f141b
Peter Teuben <teuben@gmail.com>
1996-03-05 05:00:29 +0000

    Initial revision

---
+++ src/orbit/potential/data/bulge1.c
@@ -0,0 +1,93 @@
+/*
+ * bulge1:	homogeneous bulge with certain axis ratio
+ *
+ */
+
+/*CTEX
+ *  {\bf potname=bulge1
+ *	 potpars={\it $\Omega,M,R,b/a$}}
+ */
+ 
+ 
+#include <stdinc.h>			/* standard Nemo include */
+
+static double omega = 0.0;	     /* just put to zero until implemented */
+static double bumass = 1.0;
+static double rbul = 1.0;
+static double axirat = 1.0;
+
+static double ecc, rbul2;
+
+#define G  1.0
+
+void inipotential (npar, par, name)
+int    *npar;
+double par[];
+char *name;
+{
+    int i;
+
+    if (*npar>0)
+        omega = par[0];
+    if (*npar>1)
+        bumass = par[1];
+    if (*npar>2)
+        rbul = par[2];
+    if (*npar>3)
+        axirat = par[3];
+    if (*npar>4)
+        warning("Skipped potential parameters beyond 4");
+
+    dprintf (1,"INI_POTENTIAL Homogenous oblate bulge %s\n",name);
+    dprintf (1,"  Parameters : Pattern Speed = %f (should be forced 0.0)\n",omega);
+    dprintf (1,"  mass = %f   length = %f   axirat = %f\n",bumass, rbul, axirat);
+
+    if (axirat < 0.0) axirat = 1.0;
+    if (axirat > 1.0) axirat = 1.0/axirat;
+    if (axirat == 1.0)
+        ecc = 0.0;
+    else 
+        ecc = sqrt(1.0 - sqr(axirat));
+    rbul2 = sqr(rbul);
+
+    dprintf(2,"  ecc = %f\n",ecc);
+}
+
+
+void potential (ndim,pos,acc,pot,time)
+int    *ndim;
+double pos[], acc[], *pot,*time;
+{
+    int    i;
+    double r,x,f;
+        
+    for (i=0, r=0; i<*ndim; i++)    /* find radius */
+        r += sqr(pos[i]);
+    dprintf(3,"r = %f",sqrt(r));
+    if (ecc>0.0) {
+        if (*ndim>2 && pos[2] != 0.0) 
+    	    warning("bulge1	doesn't work outside xy-plane");
+        r = sqrt(r);
+        x = ecc / MAX(1.0, r/rbul);
+        f = r * 3*G*bumass * (asin(x)-x*sqrt(1-x*x)) /
+                    (2*rbul*rbul*rbul*ecc*ecc*ecc);
+    } else {
+        if (r>rbul2) {                 /* outside bulge */
+            f = G * bumass / r;        /* Good old Newton */
+            r = sqrt(r);
+        } else {                       /* inside bulge */
+            r = sqrt(r);
+            f = G * bumass / (rbul*rbul*rbul) * r;
+        }
+    }
+    if (r>0.0) {
+        for (i=0; i<*ndim; i++) 
+            acc[i] = -(pos[i]/r) * f;        /* radial force to cartesian */
+    } else 
+        for (i=0; i<*ndim; i++) 
+            acc[i] = 0.0;
+    *pot = 0.0;
+    dprintf(3," f = %f acc=%f %f %f\n",f,acc[0],acc[1],acc[2]);
+    
+}
+
---
+++ src/orbit/potential/data/mestel.c
@@ -0,0 +1,65 @@
+/*
+ * mestel.c: Mestel disk - only valid in the XY plane
+ *
+ *	nov-90  created		PJT
+ *	mar-92  happy gcc2.0	PJT
+ *	oct-93  get_pattern     PJT
+ */
+
+/*CTEX
+ *  {\bf potname=mestel
+ *       potpars={\it $\Omega,M,R$}}
+ */
+ 
+
+#include <stdinc.h>
+ 
+local double omega = 0.0;           /* just put to zero until implemented */
+local double mestel_mass = 1.0;
+local double mestel_radius = 1.0;
+
+local double vc2;
+
+
+void inipotential (npar, par, name)
+int    *npar;
+double par[];
+char *name;
+{
+    int n;
+
+    n = *npar;
+    if (n>0) omega = par[0];
+    if (n>1) mestel_mass = par[1];
+    if (n>2) mestel_radius = par[2];
+    if (n>3) warning("Mestel disk potential: only 3 parameters used");
+
+    dprintf (1,"INIPOTENTIAL Mestel disk %s\n",name);
+    dprintf (1,"  Parameters : Pattern Speed = %f\n",omega);
+    dprintf (1,"  mass, radius, q = %f %f\n", mestel_mass,mestel_radius);
+    vc2 = mestel_mass/mestel_radius;
+    par[0] = omega;
+}
+    
+void potential (ndim,pos,acc,pot,time)
+int    *ndim;
+double pos[], acc[], *pot, *time;
+{
+    double tmp, r, r2;
+    int    i, n;
+
+    n = MIN(*ndim,2);
+    for (i=0, r2=0.0; i<n; i++)
+        r2 += sqr(pos[i]);
+    if (r2==0.0) {
+        *pot = 0.0;         /* a lie */
+        for (i=0; i<n; i++)
+            acc[i] = 0.0;
+    } else {
+        r=sqrt(r2);
+        *pot = vc2 * log(r);        /*** ??? dimensionality ??? ***/
+        tmp = -vc2 / r2;
+        for (i=0; i<n; i++)
+            acc[i] = tmp*pos[i];
+    }
+}
