d300d427fbfdcbec6f0cce914c558b59276b9682
Peter Teuben <teuben@gmail.com>
1996-09-16 19:53:47 +0000

    Initial revision

---
+++ src/kernel/misc/layout.c
@@ -0,0 +1,236 @@
+/* 
+ *
+ * layout: ascii interpreter of basic YAPP commands
+ *      20-oct-92       original version                pjt
+ *	22-jan-95	ansi prototypes			pjt
+ *	12-apr-95	fixed potential stack problem, no more ARGS
+ *      16-sep-96  0.1  added init= keyword             pjt
+ *
+ */
+
+#include <stdinc.h>
+#include <yapp.h>
+#include <strlib.h>
+#include <ctype.h>
+#include <layout.h>
+
+local struct www {                  /*  List of all basic YAPP commands */
+    pl_id id;                       /* YAPP function id; see layout.h */
+    char *command;                  /* name of YAPP function command */
+    char *args;                     /* type encoding, one char for each par */
+}  www[] = {
+    Swap,   "swap",     "", 
+    Xscale, "xscale",   "rr",
+    Yscale, "yscale",   "rr",
+    Ltype,  "ltype",    "ii",
+    Line,   "line",     "rr",
+    Move,   "move",     "rr",
+    Point,  "point",    "rrr",
+    Circle, "circle",   "rrr",
+    Cross,  "cross",    "rrr",
+    Box,    "box",      "rrr",
+    Just,   "just",     "i",
+    Text,   "text",     "srrrr",
+    Flush,  "flush",    "",
+    Frame,  "frame",    "",
+    Init,   "init",     "srrrr",
+    Stop,   "stop",     "",
+    NOP,    "#",        "",
+    NOP,    NULL,       NULL,
+};
+
+local string *split(string);
+
+
+/*
+ * PL_FREAD: read a text file with layout commands, and return
+ *           a pointer to a linked list of plcommand's that can
+ *           be executed at later times.
+ */
+
+plcommand *pl_fread(string file)
+{
+    stream fp=stropen(file,"r");
+    char line[128];
+    int n;
+    plcommand *p, *start;
+
+    start = (plcommand *) allocate(sizeof(plcommand));  /* alloc first */
+    start->id = End;
+    p = start;
+
+    while (fgets(line,128,fp)) {        /* read all lines in file */
+	dprintf(1,"%s",line);
+        if (line[0] == '#') continue;
+        if (pl_lread(line,p)>0) {
+            p->next = (plcommand *) allocate(sizeof(plcommand));
+            p = p->next;
+            p->id = End;
+        }
+    }
+    strclose(fp);
+    return start;
+}
+
+/*
+ * PL_LREAD:  read one line, parse it into a plcommand
+ *            returns the number of arguments ('argc'), including
+ *            the command itself.
+ *          
+ */
+
+int pl_lread(string line, plcommand *p)
+{
+    int i, j, n;
+    char *cp;
+    string *sp;
+
+    sp = split(line);
+    n = xstrlen(sp,sizeof(string))-2;   /* number of cmd arguments */
+
+    p->id = NOP;               /* Default is unknown command (NOP) */
+    p->next = NULL;                 /* and make sure no more known */
+
+    for (i=0; www[i].command; i++)      /* find command from table */
+        if (streq(www[i].command,sp[0])) {            /* if found: */
+            dprintf(1,"pl_parse: %s\n",sp[0]);
+            cp = www[i].args;
+            if (strlen(cp) != n) {
+                warning("pl_parse: Need %d arguments for %s",n,sp[0]);
+                break;
+            }
+            p->id = www[i].id;
+            for (j=0; *cp; j++, cp++)
+                switch(*cp) {
+                  case 'r': p->pars[j].r = atof(sp[j+1]);   break;
+                  case 'i': p->pars[j].i = atoi(sp[j+1]);   break;
+                  case 's': p->pars[j].s = scopy(sp[j+1]);  break;
+                  default:  warning("type %c not implemented",*cp);
+                }
+            break;
+        }
+    if (!www[i].command)
+        warning("Command %s not understood ?\n",sp[0]);
+    return n+1;
+}
+
+/* 
+ * PL_EXEC:    execute a linked list of plcommand's through your
+ *              current yapp implementation
+ */
+
+void pl_exec(plcommand *p)
+{	
+    for(;;) {                       /* loop over all commands */
+        if (p==NULL) break;         /* See if end of linked list */
+        if (p->id == End) break;    /* or 'End' also signifies end of work */
+	switch(p->id) {             /* execute proper yapp id code */
+            case Swap:   plswap();
+			 break;
+            case Xscale: plxscale(p->pars[0].r,p->pars[1].r);
+			 break;
+            case Yscale: plyscale(p->pars[0].r,p->pars[1].r);
+			 break;
+            case Ltype:  plltype(p->pars[0].i,p->pars[1].i);
+                         break;
+	    case Line:   plline(p->pars[0].r,p->pars[1].r);
+			 break;
+	    case Move:   plmove(p->pars[0].r,p->pars[1].r);
+			 break;
+	    case Point:	 plpoint(p->pars[0].r,p->pars[1].r);
+			 break;
+            case Circle: plcircle(p->pars[0].r,p->pars[1].r,p->pars[2].r);
+			 break;
+            case Cross:  plcross(p->pars[0].r,p->pars[1].r,p->pars[2].r);
+			 break;
+            case Box:    plbox(p->pars[0].r,p->pars[1].r,p->pars[2].r);
+			 break;
+            case Just:   pljust(p->pars[0].i);
+			 break;
+            case Text:   pltext(p->pars[0].s,p->pars[1].r,p->pars[2].r,
+                                             p->pars[3].r,p->pars[4].r);
+			 break;
+	    case Flush:  plflush();
+			 break;
+            case Frame:  plframe();
+			 break;
+            case Init:   plinit(p->pars[0].s, p->pars[1].r, p->pars[2].r,
+                                              p->pars[3].r, p->pars[4].r);
+                         break;
+            case Stop:   plstop();
+			 break;
+            case NOP:    break;
+	    default: 	 warning("pl_exec: unknown id code %d",p->id);
+		 	 break;
+	}
+        p = p->next;                /* point to the next plcommand */
+    } /* for(;;) */
+}
+
+#define MAXWORD 256
+
+#define skipspace(c)    while(isspace(*c)) {c++;}
+#define skipword(c)     while(*c && !isspace(*c)) {c++;}
+#define skipstring(c)   while(*c && *c!='"') {c++;}
+
+local string *split(string line)
+{
+    char *cp=line;
+    int n=0;
+    permanent string sp[MAXWORD+1];
+
+    for(;;) {
+        skipspace(cp);
+        if (*cp==0) {
+            break;
+        } else if (*cp=='"') {
+            cp++;
+            sp[n++] = cp;
+            skipstring(cp);
+            if (*cp=='"') {
+                *cp=0;
+                cp++;
+            } else {        /* bad end of line: forgot to close " */
+                warning("Badly formed string: %s",sp[n-1]);
+                break;
+            }
+        } else {
+            sp[n++] = cp;
+            skipword(cp);
+            if (*cp==0) break;   /* legal end of line */
+            *cp=0;
+            cp++;
+        }
+    }
+    sp[n] = NULL;
+    dprintf(1,"Command %s: argc=%d\n",sp[0],n);
+    return sp;
+}
+
+
+
+#ifdef TOOLBOX
+
+#include <getparam.h>
+
+string defv[] = {
+    "in=???\n       Input yapp layout file",
+    "init=f\n       Add plinit() before calling pl_exec()?",
+    "stop=f\n       Add plstop() after calling pl_exec()?",
+    "VERSION=0.1\n  16-sep-96 PJT",
+    NULL,
+};
+
+string usage="YAPP interpreter";
+
+nemo_main()
+{
+    if (getbparam("init"))
+        plinit("***",0.0,20.0,0.0,20.0);
+    pl_exec(pl_fread(getparam("in")));
+    if (getbparam("stop"))
+        plstop();
+    
+}
+
+#endif
