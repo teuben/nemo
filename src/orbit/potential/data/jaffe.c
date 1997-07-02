ab05c550173b42770b1ab035b675f4de31a8ba2d
Peter Teuben <teuben@gmail.com>
1997-07-02 18:11:37 +0000

    Initial revision

---
+++ src/orbit/potential/data/jaffe.c
@@ -0,0 +1,67 @@
+/*
+ *  Jaffe:		Jaffe model
+ *		jun-97	documented Dehnen
+ *
+ */
+
+/*CTEX
+ *	{\bf potname=jaffe
+ *       potpars={\it $\Omega,M,r_c$}}
+ *
+ * Jaffe potential (BT, pp.237, see also MNRAS 202, 995 (1983))),
+ * special $\gamma=2$ case of Dehnen potential.
+ *
+ * $$
+ *    \Phi = - {  M \over r_c}  \ln{
+ *                         \left( { r \over {r_c + r} } \right) }
+ * $$
+ */
+ 
+ 
+#include <stdinc.h>                     /* standard Nemo include */
+
+local double omega = 0.0;           /* just put to zero until implemented */
+local double hmass = 1.0;	/* total mass */
+local double a = 1.0;		/* scale lenght */
+
+local double vc;
+
+void inipotential (int *npar, double *par, string name)
+{
+    if (*npar>0)   omega = par[0];
+    if (*npar>1)   hmass = par[1];
+    if (*npar>2)   a = par[2];
+    if (*npar>3)   warning("Skipped potential parameters beyond 3");
+
+    dprintf (1,"INI_POTENTIAL Jaffe potential\n");
+    dprintf (1,"  Parameters : Pattern Speed = %f\n",omega);
+    dprintf (1,"  mass = %f   scalelength = %f\n",hmass, a);
+
+    vc = hmass/a;       /* velocity near center */
+
+    par[0] = omega;
+
+}
+
+void potential (int *ndim,double *pos,double *acc,double *pot,double *time)
+{
+    int    i;
+    double r2,r,f;
+        
+    for (i=0, r2=0; i<*ndim; i++)              /* radius - squared */
+        r2 += sqr(pos[i]);
+
+    if (r2==0.0) {
+        *pot = 0.0;			/* a lie though */
+        for (i=0; i<*ndim; i++)
+            acc[i] = 0.0;
+    } else {
+        r = sqrt(r2);                        /* radius */
+        f = 1.0/(r+a);                  /* temporary storage */
+        *pot = vc * log(r*f);                 /* returned potential */
+        f *= hmass/r2;
+        for (i=0; i<*ndim; i++)
+            acc[i] = -pos[i]*f;        /* radial force to cartesian */
+    }
+}
+
---
+++ src/scripts/ctex.awk
@@ -0,0 +1,33 @@
+#! /bin/nawk -f
+#
+#  extract CTEX comments from C source code...
+#  Each paragraph should be structured as follows:
+#
+#	/*CTEX
+#	 *  bla bla bla (tex source)
+#	 */
+#
+#  Usage:
+#	ctex.awk file.c > file.tex
+#
+#  See also the 'ctex' shell scripts, which combines various ctex.awk
+#  calls into one (la)tex file
+
+BEGIN {
+  on = 0;
+  printf("%% File: %s\n",ARGV[1]);
+}
+
+{
+  if ($1 == "*" && on) {
+    for (i=2; i<=NF; i++)
+        printf("%s ",$i);
+    printf("\n");
+  } else if ($1 == "/*CTEX") {
+    printf("%% CTEX Line: %d\n",NR);
+    on = 1;
+  } else if ($1 == "*/") {
+    printf("\n");
+    on=0;
+  }
+}  
