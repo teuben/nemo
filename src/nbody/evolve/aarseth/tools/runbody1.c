5e6e63be2c8473207258ded20a3b333be0e7c32e
Peter Teuben <teuben@gmail.com>
1998-03-06 17:25:00 +0000

    import NEMO V3

---
+++ src/nbody/evolve/aarseth/tools/runbody1.c
@@ -0,0 +1,210 @@
+/*
+ *  MAIN.C: NEMO driver for nbody1
+ *	
+ *  4-mar-98    cloned off runbody2, added KZ_ parameters     PJT
+ *
+ *
+ */
+
+#include <stdinc.h>
+#include <getparam.h>
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+string defv[] = {
+    "in=\n            input file (optional - see nbody= ) ",
+    "outdir=???\n     output run directory (required)",
+
+    "nbody=???\n      Total particle number (<= NMAX).",
+    "nfix=1\n         Output frequency of data save or binaries; KZ(3 & 6)",
+    "nrand=0\n        Random number sequence skip",
+    "nrun=1\n         Run identification index",
+
+    "eta=0.02\n       Time-step parameter for irregular force polynomial",
+    "deltat=0.25\n    Output time interval in units of the crossing time",
+    "tcrit=2.0\n      Termination time in units of the crossing time",
+    "qe=0.00002\n     Energy tolerance (restart if DE/E > 5*QE & KZ(2) > 1)",
+    "eps=0.05\n       Potential softening parameter",
+
+
+    "kz=1,2,0,0,0,1,0,0,0,0,1\n  Non-zero options for alternative paths (see below)\n"
+      " 1  COMMON save on unit 1 if TCOMP > CPU or if TIME > TCRIT.\n"
+      " 2  COMMON save on unit 2 at output (=1); restart if DE/E > 5*QE (=2).\n"
+      " 3  Basic data written to unit 3 at output time (frequency NFIX).\n"
+      " 4  Initial conditions on unit 4 (=1: output; =2: input).\n"
+      " 5  Initial conditions (=0: uniform & isotropic; =1: Plummer).\n"
+      " 6  Output of significant binaries.\n"
+      " 7  Output of movie frames on unit 7.\n"
+      " 8  Generation of two subsystems (merger experiment).\n"
+      " 9  Individual bodies printed at output time (MIN(5**KZ9,N)).\n"
+      "10  No scaling of initial conditions.\n"
+      "11  Modification of ETA by tolerance QE.\n"
+      "12  Initial parameters for binary orbit.\n"
+      "13  Escaper removal (R > 2*RTIDE; RTIDE = 10*RSCALE).\n"
+      "14  Adjustment of coordinates & velocities to c.m. condition.\n",
+
+    "alphas=2.3\n     Power-law index for initial mass function",
+    "body1=5.0\n      Maximum particle mass before scaling",
+    "bodyn=1.0\n      Minimum particle mass before scaling",
+
+    "q=0.0\n          Virial ratio (q=0.5 for virial equilibrium)",
+    "vxrot=0.0\n      XY-velocity scaling factor (> 0 for solid-body rotation)",
+    "vzrot=0.0\n      Z-velocity scaling factor (not used if VXROT = 0)",
+    "rbar=1.0\n       Virial radius in pc (for scaling to physical units)",
+    "zmbar=1.0\n      Mean mass in solar units",
+
+    "xcm=\n           Displacement for subsystem (routine SCALE; KZ 17)",
+    "ecc=\n           Eccentricity of relative motion for subsystem (ECC =< 1)",
+
+    "kstart=1\n       Running mode (1=new 2=restart 3,4,5=restart w/ new par",
+    "tcomp=40.0\n     Maximum allowed running time (minutes)",
+
+    "VERSION=1.0\n    5-mar-98 PJT",
+    NULL,
+};
+
+string usage="N-body code";
+
+#define KZ_MAX  15
+
+
+#define KZ_OUT   3
+#define KZ_INI   4
+#define KZ_MOV   7
+#define KZ_MER   8
+#define KZ_BIN  12
+#define KZ_COM  14
+
+nemo_main()
+{
+    int nbody, nfix, nrand, nrun, kstart;
+    real eta, deltat, tcrit, qe, eps, tcomp;
+    int k, nkz, kz[KZ_MAX];
+    real alphas, body1, bodyn;
+    real q, vxrot, vzrot, rbar, zmbar;
+    string exefile = "nbody1";
+    string parfile = "nbody1.in";
+    string rundir = getparam("outdir");
+    string infile;
+    char fullname[256], runcmd[256];
+    stream datstr, histr;
+
+    kstart = getiparam("kstart");
+    tcomp =  getdparam("tcomp");
+
+    nbody = getiparam("nbody");
+    nfix = getiparam("nfix");
+    nrand = getiparam("nrand");
+    nrun = getiparam("nrun");
+
+    eta = getdparam("eta");
+    deltat = getdparam("deltat");
+    tcrit = getdparam("tcrit");
+    qe = getdparam("qe");
+    eps = getdparam("eps");
+
+    nkz = nemoinpi(getparam("kz"),kz,KZ_MAX);
+    if (nkz < 0) error("%d Syntax error kz=%s",nkz,getparam("kz"));
+    for (k=nkz; k<KZ_MAX; k++) kz[k]=0;
+    for (k=0; k<KZ_MAX; k++) dprintf(1,"%d ",kz[k]);
+    dprintf(1,"\n");
+
+    alphas = getdparam("alphas");
+    body1 = getdparam("body1");
+    bodyn = getdparam("bodyn");
+
+    q = getdparam("q");
+    vxrot = getdparam("vxrot");
+    vzrot = getdparam("vzrot");
+    rbar = getdparam("rbar");
+    zmbar = getdparam("zmbar");
+
+    make_rundir(rundir);
+
+    sprintf(fullname,"%s/%s",rundir,parfile);
+    datstr = stropen(fullname,"w");    
+
+    /*  New Run */
+
+  if (kstart == 1) {
+
+    fprintf(datstr,"%d %g\n",kstart,tcomp);
+    fprintf(datstr,"%d %d %d %d\n",nbody,nfix,nrand,nrun);
+    fprintf(datstr,"%g %g %g %g %g\n",eta,deltat,tcrit,qe,eps);
+
+    for (k=0; k<KZ_MAX; k++) fprintf(datstr,"%d ",kz[k]);
+    fprintf(datstr,"\n");
+
+    if (kz[KZ_INI-1] != 2) {
+        fprintf(datstr,"%g %g %g\n",alphas,body1,bodyn);
+        if (hasvalue("in")) warning("Also gave in=, will be ignored");
+    } else
+    	if (!hasvalue("in")) warning("Did not supply in=");
+    fprintf(datstr,"%g %g %g %g %g\n",q,vxrot,vzrot,rbar,zmbar);
+
+    if (kz[KZ_BIN-1] > 0)
+        fprintf(datstr,"%g %g\n",getdparam("xcm"), getdparam("ecc"));
+    strclose(datstr);
+
+    goto_rundir(rundir);
+    histr = stropen("history","w");
+    put_history(histr);
+    strclose(histr);
+
+    if (hasvalue("in")) {
+	infile = getparam("in");
+	if (*infile == '-') {		/* do something special for pipes */
+            sprintf(runcmd,"stou4 %s",infile);
+	} else if (*infile == '/') {	/* regular file */
+            sprintf(runcmd,"stou4 %s nbody=%d",infile,nbody);
+        } else
+            error("in=%s must be an absolute pathname or special file",infile);
+        dprintf(0,"%s\n",runcmd);
+        if (system(runcmd)) error("Error converting input data");
+    }
+
+    sprintf(runcmd,"%s < %s",exefile,parfile);
+    run_program(runcmd);
+  } else {
+    error("kstart=%d not yet supported",kstart);
+  }
+}
+
+
+goto_rundir(string name)
+{
+    if (chdir(name))
+        error("Cannot change directory to %s",name);
+}
+
+make_rundir(string name)
+{
+    if (mkdir(name, 0755))
+        warning("Run directory %s already exists",name);
+}
+
+run_program(string cmd)
+{
+    system(cmd);
+}
+
+
+/*
+ *	Order of input lines in "nbody1.in" for a new run (KSTART=1)
+ *
+ *          variables                   condition               where
+ *  ----------------------------    -------------------
+ *  KSTART TCOMP                                            nbody1.f    MAIN
+ *  Nbody NFIX NRAND NRUN                                   input.f     INPUT
+ *  ETA DELTAT TCRIT QE EPS                                 input.f     INPUT
+ *  KZ(1..15)                                               input.f     INPUT
+ *      ALPHAS BODY1 BODYN          KZ(4).NE.2              data.f      DATA
+ *      SEMI ECC                    KZ(12).NE.0             data.f      DATA
+ *  Q VXROT VZROT RBAR ZMBAR                                scale.f     SCALE
+ *      NFRAME DELTAF               KZ(7).GT.0              scale.f     SCALE
+ *      XCM ECC                     KZ(8).GT.0              subsys.f    SUBSYS
+ *
+ */
+ 
