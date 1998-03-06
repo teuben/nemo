71d2937f4b37a17e001716df543861a2591fa93f
Peter Teuben <teuben@gmail.com>
1998-03-06 15:47:05 +0000

    import NEMO V3

---
+++ src/nbody/evolve/aarseth/nbody2/source/params.h
@@ -0,0 +1,11 @@
+*       NBODY2 parameters.
+*       ------------------
+*
+      PARAMETER  (NMAX=2500,LMAX=80)
+*
+*
+*       ----------------------------------------
+*       NMAX    Maximum number of bodies.
+*       LMAX    Maximum size of neighbour lists.
+*       ----------------------------------------
+*
