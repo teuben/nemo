297429d2137b8996dffec133387fb161ecf7a212
Peter Teuben <teuben@gmail.com>
2000-01-27 21:07:32 +0000

    Initial revision

---
+++ src/nbody/evolve/aarseth/nbody0/nbody00.c
@@ -0,0 +1,354 @@
+/* NBODY00.C: handedited f2c translated version of NBODY0.F 
+ *
+ * 	S.J. Aarseth's Standard N-Body Program (nbody0) 
+ *
+ * 	Adapted from the source code in Binney & Tremaine's (1987)  
+ *       book 'Galactic Dynamics',  
+ *         by Peter Teuben - June '89:  
+ *       - all variables to be declared and a choice of 'real' or  
+ *         'double precision'  
+ * 	- all I/O is done through subroutines in order to allow easy  
+ * 	  interface with development different systems, e.g. NEMO89.  
+ * 	- PARAMETERS for NMAX and NDIM via an include file (see Makefile)  
+ * 	  Note that NDIM should NEVER be reset from 3, the code does  
+ * 	  not handle 2D stuff (yet).  
+ *
+ *          jun-89   Created  
+ * 	 23-jan-90   Back to double precision  - PJT  
+ * 	  5-apr-90   started to declare all variables - 	PJT  
+ *       14-nov-91   finished(!) declaring all variables;  
+ *                   tested using IMPLICIT NONE and compile with -u  
+ *       15-nov-91   Connection Machine testing in nbody0.fcm file  
+ *	 10-may-92   f2c conversion - manually optimized
+ *                   body index (i,j) are 0 based
+ *                   dim index (k) is 0 based
+ *	 11-feb-98   Fixed array index for f2dot(k)  a(16)->a(15)
+ *	  6-jan-00   new F77_FUNC macros, renamed to nbody00.c since
+ *		     compiled name could clash with nbody0.f
+ *       21-jan-00   added debug output and reset= option
+ */
+
+
+#define NMAX 512
+#define NDIM 3
+
+/* extract X, Y and Z components of a 3-vector */
+#define X(a,i)	(a[i*NDIM])
+#define Y(a,i)  (a[i*NDIM+1])
+#define Z(a,i)  (a[i*NDIM+2])
+
+#define SQR(x)  ((x)*(x))
+
+/* Table of constant values */
+
+static int c_nmax = NMAX;
+
+#include <nemo.h>
+#include "proto.h"
+
+
+  /*    extern int outbods_(), inbods_(), inpars_(), outene_();  */
+  /*    local  int nbody0_() */
+
+extern double cputime();
+
+
+/* *********************************************************************** */
+void nbody0(void)
+{
+    /* Initialized data */
+    double time = 0.0, tnext = 0.0, e0;
+    int nsteps = 0, makesure, nout = 0, nreset;
+    double time0, time1=0.0;        /* counters for my timing/debugging */
+
+    /* System (f2c) generated locals */
+    double d__1, d__2, d__3, d__4, d__5, d__6;
+
+    /* Local variables */
+    double fdot[NDIM*NMAX], body[NMAX], step[NMAX], 
+           f1dot[NDIM], f2dot[NDIM], f3dot[NDIM], 
+           x0dot[NDIM*NMAX], a[17], f[NDIM*NMAX],
+           e, s, x[NDIM*NMAX], tcrit, 
+           d1[NDIM*NMAX], d2[NDIM*NMAX], d3[NDIM*NMAX], f1[NDIM], 
+	   t0[NMAX], t1[NMAX], t2[NMAX], t3[NMAX], x0[NDIM*NMAX], 
+	   dt, deltat, dt1, dt2, dt3, eta, eps2, t1pr, t2pr, t3pr;
+    int    i, j, k, n, itmp, reset;
+
+
+    inpars(&c_nmax, &n, &eta, &deltat, &tcrit, &eps2, &reset);
+    inbods(&n, body, x0, x0dot);
+
+/*           obtain total forces and first derivative for each body */
+    for (i = 0; i < n; ++i) {
+	for (k = 0; k < NDIM; ++k) {
+	    f[k+i*3] = 0.0;
+	    fdot[k+i*3] = 0.0;
+	    d2[k+i*3] = 0.0;
+	    d3[k+i*3] = 0.0;
+	}
+	for (j = 0; j < n; ++j) {
+	    if (j == i) continue;
+	    for (k = 0; k < NDIM; ++k) {
+		a[k    ] = x0[k+j*3] - x0[k+i*3];
+		a[k + 3] = x0dot[k+j*3] - x0dot[k+i*3];
+	    }
+	    d__1 = a[0];
+	    d__2 = a[1];
+	    d__3 = a[2];
+	    a[6] = 1.0 / (SQR(d__1) + SQR(d__2) + SQR(d__3) + eps2);
+	    a[7] = body[j] * a[6] * sqrt(a[6]);
+	    a[8] = (a[0]*a[3] + a[1]*a[4] + a[2]*a[5]) * 3.0 * a[6];
+	    for (k = 0; k < NDIM; ++k) {
+		f[k+i*3] += a[k] * a[7];
+		fdot[k+i*3] += (a[k + 3] - a[k] * a[8]) * a[7];
+	    }
+	}
+    } /* for(i) */
+/*              form second and third derivative */
+    for (i = 0; i < n; ++i) {
+	for (j = 0; j < n; ++j) {
+	    if (i == j) continue;
+	    for (k = 0; k < NDIM; ++k) {
+		a[k    ] = x0[k+j*3] - x0[k+i*3];
+		a[k + 3] = x0dot[k+j*3] - x0dot[k+i*3];
+		a[k + 6] = f[k+j*3] - f[k+i*3];
+		a[k + 9] = fdot[k+j*3] - fdot[k+i*3];
+	    }
+	    d__1 = a[0];
+	    d__2 = a[1];
+	    d__3 = a[2];
+	    a[12] = 1.0 / (SQR(d__1) + SQR(d__2) + SQR(d__3) + eps2);
+	    a[13] = body[j] * a[12] * sqrt(a[12]);
+	    a[14] = (a[0]*a[3] + a[1]*a[4] + a[2]*a[5]) * a[12];
+	    d__1 = a[3];
+	    d__2 = a[4];
+	    d__3 = a[5];
+	    d__4 = a[14];
+	    a[15] = (SQR(d__1) + SQR(d__2) + SQR(d__3) + 
+	             a[0]*a[6] + a[1]*a[7] + a[2]*a[8]) * a[12] + SQR(d__4);
+	    d__1 = a[14];
+	    a[16] = ((a[3]*a[6] + a[4]*a[7] + a[5]*a[8]) * 9 +
+		     (a[0]*a[9] + a[1]*a[10] + a[2]*a[11]) * 3) * 
+		    a[12] + a[14] * (a[15] * 9.0 - SQR(d__1) * 12.);
+	    for (k = 0; k < NDIM; ++k) {
+		f1dot[k] = a[k + 3] - a[14] * 3 * a[k];
+		f2dot[k] = (a[k + 6] - a[14] * 6 * f1dot[k] - a[15] * 
+			3 * a[k]) * a[13];
+		f3dot[k] = (a[k + 9] - a[15] * 9 * f1dot[k] - a[16] * 
+			a[k]) * a[13];
+		d2[k+i*3] += f2dot[k];
+		d3[k+i*3] = d3[k+i*3] + f3dot[k] - a[14] * 9 * f2dot[k];
+	    }
+	}
+    } /* for (i) */
+
+/*             initialize integration steps and convert to force diffences */
+    for (i = 0; i < n; ++i) {
+	d__1 = X(f,i);
+	d__2 = Y(f,i);
+	d__3 = Z(f,i);
+	d__4 = X(d2,i);
+	d__5 = Y(d2,i);
+	d__6 = Z(d2,i);
+        step[i] = sqrt(eta * sqrt( 
+          (SQR(d__1)+SQR(d__2)+SQR(d__3)) / (SQR(d__4)+SQR(d__5)+SQR(d__6)) ) );
+	t0[i] = time;
+	t1[i] = time - step[i];
+	t2[i] = time - step[i] * 2;
+	t3[i] = time - step[i] * 3;
+	for (k = 0; k < NDIM; ++k) {
+	    d1[k+i*3] = (d3[k+i*3] * step[i] / 6 - 
+	                         d2[k+i*3] * 0.5) * step[i] + 
+	                         fdot[k+i*3];
+	    d2[k+i*3] = d2[k+i*3] * 0.5 - d3[k+i*3] * 0.5 * step[i];
+	    d3[k+i*3] /= 6;
+	    f[k+i*3] *= 0.5;
+	    fdot[k+i*3] /= 6;
+	}
+    } /* for (i) */
+
+/* ----------------------------------------------------------------------- */
+/* ==============> entry point when major output + diagnostics to be done */
+
+/*              energy check and output */
+L100:
+    e = 0.0;
+    for (i = 0; i < n; ++i) {
+	dt = tnext - t0[i];
+	for (k = 0; k < NDIM; ++k) {
+	    f2dot[k] = d3[k+i*3] * (t0[i] - t1[i] + 
+	        (t0[i] - t2[i])) + d2[k+i*3];
+	    x[k+i*3] = ((((d3[k+i*3] * 0.05 * dt + f2dot[k] / 12.0) * dt  +
+		    fdot[k+i*3]) * dt + f[k+i*3]) * dt + 
+		    x0dot[k+i*3]) * dt + x0[k+i*3];
+	    a[k] = (((d3[k+i*3] * 0.25 * dt + f2dot[k] /
+		     3.0) * dt + fdot[k+i*3] * 3.0) * dt 
+		    + f[k+i*3] * 2.0) * dt + x0dot[k+i*3];
+	}
+	itmp=i+1;
+	outbods(&body[i], &x[i*3], a, &step[i], &itmp);
+	d__1 = a[0];
+	d__2 = a[1];
+	d__3 = a[2];
+	e += 0.5 * body[i] * (SQR(d__1) + SQR(d__2) + SQR(d__3));
+    }
+    for (i = 0; i < n; ++i) {
+	for (j = 0; j < n; ++j) {
+	    if (j == i) continue;
+	    d__1 = X(x,i) - X(x,j);
+	    d__2 = Y(x,i) - Y(x,j);
+	    d__3 = Z(x,i) - Z(x,j);
+	    e -= body[i] * 0.5 * body[j] / 
+		    sqrt(SQR(d__1) + SQR(d__2) + SQR(d__3) + eps2);
+	}
+    }
+    if (nout ==0) e0 = e;
+    nout++;
+    outene(&tnext, &nsteps, &e);
+    if (time > tcrit) {
+        printf("Time spent in searching for next advancement: %g\n",
+                time1*60.0);
+        printf("Energy conservation: %g / %g = %g\n",e-e0,e0,(e-e0)/e);
+	if (reset) 
+            printf("Time resets needed %d times / %d dumps\n",nreset,nout);
+        return;
+    } 
+    tnext += deltat;
+    makesure = 1;
+
+
+/* ----------------------------------------------------------------------- */
+/*   ===============>  Normal entry point for next timstep */
+
+
+/*      find next body (i) to be advanced and set new time 
+ *      this linear search takes about 5% of the total CPU
+ *      see sift() to speed up this process 
+ */
+L200:
+    time0=cputime();
+    time = 1e10;
+    for (j = 0; j < n; ++j) {
+	if (time > t0[j] + step[j]) {
+	    i = j;
+	    time = t0[j] + step[j];
+	}
+    }
+    time1 += (cputime()-time0);
+
+    /* however, the time needs to be reset if the next datadump is
+     * coming up earlier than the next particle. Why? Because
+     * if we don't this next particle will then be integrated beyond
+     * tnext ??  When Sverre and I were discussing this in January 2000,
+     * two solutions were proposed:
+     */
+#if 1
+                                            /* Peter : only do it after dump */
+    dprintf(3,"time=%g i=%d\n",time,i);
+    if (makesure) {
+        dprintf(2,"TIME?: %g %g\n",time,tnext);
+        if (reset && (time > tnext)) {
+            dprintf(1,"RESET: %g > %g\n",time,tnext);
+	    time = tnext;
+	    nreset++;
+        }
+        makesure=0;
+    }
+#else
+                                            /* Sverre : do it always */
+	/* bug: integrates too far */
+    if (time > tnext) {
+            dprintf(1,"RESET: %g > %g\n",time,tnext);
+	    time = tnext;
+	    nreset++;
+    }
+#endif
+    
+/*              predict all coordinates to first order in force derivative */
+    for (j = 0; j < n; ++j) {
+	s = time - t0[j];
+	X(x,j) = ((X(fdot,j) * s + X(f,j)) * s + X(x0dot,j)) * s + X(x0,j);
+	Y(x,j) = ((Y(fdot,j) * s + Y(f,j)) * s + Y(x0dot,j)) * s + Y(x0,j);
+	Z(x,j) = ((Z(fdot,j) * s + Z(f,j)) * s + Z(x0dot,j)) * s + Z(x0,j);
+    }
+/*              include 2nd and 3rd order and obtain the velocity */
+    dt = time - t0[i];
+    for (k = 0; k < NDIM; ++k) {
+	f2dot[k] = d3[k+i*3] * (t0[i] - t1[i] + (t0[i]
+		 - t2[i])) + d2[k+i*3];
+	d__1 = dt, d__1 *= d__1;
+	x[k+i*3] = (d3[k+i*3] * 0.05 * dt + f2dot[k]
+		 / 12.) * SQR(d__1) + x[k+i*3];
+	x0dot[k+i*3] = (((d3[k+i*3] * 0.25 * dt + f2dot[k] / 3.0) * 
+	        dt + fdot[k+i*3] * 3.0) * 
+		dt + f[k+i*3] * 2.0) * dt + x0dot[k+i*3];
+	f1[k] = 0.0;
+    }
+/*              obtain current force on i-th body */
+    for (j = 0; j < n; ++j) {
+	if (j == i) continue;
+	d__1 = a[0] = X(x,j) - X(x,i);
+	d__2 = a[1] = Y(x,j) - Y(x,i);
+	d__3 = a[2] = Z(x,j) - Z(x,i);
+	a[3] = 1.0 / (SQR(d__1) + SQR(d__2) + SQR(d__3) + eps2);
+	a[4] = body[j] * a[3] * sqrt(a[3]);
+	f1[0] += a[0] * a[4];
+	f1[1] += a[1] * a[4];
+	f1[2] += a[2] * a[4];
+    }
+
+/*              set time intervals for new difference and update the times */
+    dt1 = time - t1[i];
+    dt2 = time - t2[i];
+    dt3 = time - t3[i];
+    t1pr = t0[i] - t1[i];
+    t2pr = t0[i] - t2[i];
+    t3pr = t0[i] - t3[i];
+    t3[i] = t2[i];
+    t2[i] = t1[i];
+    t1[i] = t0[i];
+    t0[i] = time;
+/*              form new differences and include 4th order semi-iterative  */
+    for (k = 0; k < NDIM; ++k) {
+	a[k    ] = (f1[k   ] -  f[k+i*3 ] * 2.0) / dt;
+	a[k + 3] = (a[k    ] - d1[k+i*3]) / dt1;
+	a[k + 6] = (a[k + 3] - d2[k+i*3]) / dt2;
+	a[k + 9] = (a[k + 6] - d3[k+i*3]) / dt3;
+	d1[k+i*3] = a[k    ];
+	d2[k+i*3] = a[k + 3];
+	d3[k+i*3] = a[k + 6];
+	f1dot[k] = t1pr * t2pr * t3pr * a[k + 9];
+	f2dot[k] = (t1pr * t2pr + t3pr * (t1pr + t2pr)) * a[k + 9];
+	f3dot[k] = (t1pr + t2pr + t3pr) * a[k + 9];
+	d__1 = dt, d__2 = d__1;
+	x0[k+i*3] = (((a[k + 9] * dt / 30. + f3dot[k] * 
+		0.05) * dt + f2dot[k] / 12.) * dt + f1dot[k] / 6.) 
+		* (d__2 * SQR(d__1)) + x[k+i*3];
+	d__1 = dt;
+	x0dot[k+i*3] = (((a[k + 9] * 0.2 * dt + f3dot[k] * 
+		0.25) * dt + f2dot[k] / 3.0) * dt + f1dot[k] * 0.5) * 
+		SQR(d__1) + x0dot[k+i*3];
+    }
+/*           scale F and FDOT by factorials and set new integration step  */
+    for (k = 0; k < NDIM; ++k) {
+	f[k+i*3] = f1[k] * 0.5;
+	fdot[k+i*3] = ((d3[k+i*3] * dt1 + d2[k+i*3]) *
+		 dt + d1[k+i*3]) / 6.0;
+	f2dot[k] = (d3[k+i*3] * (dt + dt1) + d2[k+i*3]) * 2.0;
+    }
+    d__1 = f1[0];
+    d__2 = f1[1];
+    d__3 = f1[2];
+    d__4 = f2dot[0];
+    d__5 = f2dot[1];
+    d__6 = f2dot[2];
+    step[i] = sqrt(eta * sqrt( 
+       (SQR(d__1)+SQR(d__2)+SQR(d__3)) / (SQR(d__4)+SQR(d__5)+SQR(d__6)) ) );
+    ++nsteps;
+    if (time - tnext >= 0.0) {
+	goto L100;
+    } else {
+	goto L200;
+    }
+} /* nbody0 */
+
---
+++ src/nbody/reduc/snapcmp.c
@@ -0,0 +1,355 @@
+/*
+ * SNAPCMP.C: compare two N-body snapshots.
+ *
+ *	22-jan-90   V1.2					JEB
+ *	25-nov-90   V1.3 helpvec, allocate from library 	PJT
+ *	23-mar-94   V1.3a
+ *	29-mar-94   V1.4 process all snapshots - added time to output  pjt
+ *	27-jan-00   V1.4b  more usefule error messages                 pjt
+ */
+
+#include <stdinc.h>
+#include <getparam.h>
+#include <filestruct.h>
+#include <history.h>
+#include <vectmath.h>
+#include <yapp.h>
+#include <axis.h>
+#include <snapshot/snapshot.h>
+#include <snapshot/body.h>
+#include <snapshot/get_snap.c>
+
+string defv[] = {			/* DEFAULT INPUT PARAMETERS         */
+    "in1=???\n				  first input file name",
+    "in2=???\n				  second input file name",
+    "time1=all\n			  time selected from 1st file",
+    "time2=all\n			  time selected from 2nd file",
+    "obs=r\n				  observable used in comparison",
+    "diffpart=true\n			  difference particles first",
+    "relative=false\n			  scale difference by ref value",
+#if HISTOGRAM
+    "nbins=8\n				  number of bins in histogram",
+    "xrange=0.0:1.0\n			  range of values to histogram",
+    "xlabel=\n				  label for X axis; default is obs ",
+    "ymax=\n				  if given, use for max N value",
+    "ylabel=N\n				  label for Y axis",
+#endif
+#if SCATTERPLOT
+    "xvar=\n				  indp var to plot; default is obs",
+    "xrange=0.0:2.0\n			  range of values to display",
+    "xlabel=\n				  X axis label; default is xvar",
+    "yrange=0.0:1.0\n			  range of delta values to display",
+    "ylabel=delta\n			  label for Y axis",
+#endif
+#if HISTOGRAM || SCATTERPLOT
+    "nxticks=3\n			  number of tickmarks along X axis",
+    "nyticks=3\n			  number of tickmarks along Y axis",
+    "xbox=2.5:17.5\n			  extent of graph in x direction",
+    "ybox=5.0:15.0\n			  extent of graph in y direction",
+    "formal=false\n			  if true, make publication plot",
+#endif
+    "VERSION=1.4b\n			  27-jan-00 PJT",
+    NULL,
+};
+
+string usage = "compare two N-body snapshots";
+
+rproc obsfunc;
+bool diffpart;
+bool relative;
+
+#if HISTOGRAM || SCATTERPLOT
+
+real xrange[2], yrange[2];
+real xbox[2], ybox[2];
+real xtrans(real), ytrans(real);
+
+#endif
+
+
+local real *snapcmp(Body*, Body*, int, real);
+local int cmpreal(real*, real*);
+local void printquart(real*, int, real);
+
+extern rproc btrtrans(string);
+
+void nemo_main()
+{
+    stream instr1, instr2;
+    string time1, time2, file1, file2;
+    Body *btab1 = NULL, *btab2 = NULL;
+    int nbody1, bits1, nbody2, bits2;
+    real tsnap1 = 0.0, tsnap2 = 0.0, *result;
+
+    file1 = getparam("in1");
+    file2 = getparam("in2");
+    instr1 = stropen(file1, "r");
+    get_history(instr1);
+    instr2 = stropen(file2, "r");
+    get_history(instr2);
+    time1 = getparam("time1");
+    time2 = getparam("time2");
+    obsfunc = btrtrans(getparam("obs"));
+    diffpart = getbparam("diffpart");
+    relative = getbparam("relative");
+    for(;;) {
+      do {
+	if (!get_snap_by_t(instr1, &btab1, &nbody1, &tsnap1, &bits1, time1))
+		break;
+        dprintf(1,"snap1: reading time=%g\n",tsnap1);
+      } while (bits1 == TimeBit);
+      do {
+	if (!get_snap_by_t(instr2, &btab2, &nbody2, &tsnap2, &bits2, time2))
+		break;
+        dprintf(1,"snap2: reading time=%g\n",tsnap2);
+      } while (bits2 == TimeBit);
+      if (bits1 == 0 && bits2 == 0)
+        break;
+      else if (bits1 == 0)
+    	error("no more snapshots found in %s at t=%g",file1,tsnap2);
+      else if (bits2 == 0)
+    	error("no more snapshots found in %s at t=%g",file2,tsnap1);
+      if (nbody1 != nbody2)
+	error("%s = %d, %d different", NobjTag, nbody1, nbody2);
+      if (tsnap1 != tsnap2)
+	warning("times = %f, %f are different", tsnap1, tsnap2);
+      if (bits1 != bits2)
+	warning("bits = 0x%x, 0x%x are different", bits1, bits2);
+      result = snapcmp(btab1, btab2, nbody1, tsnap1);
+#if STATISTICS
+      printquart(result, nbody1, tsnap1);
+#endif
+#if HISTOGRAM
+      histogramplot(result, nbody1);
+#endif
+#if SCATTERPLOT
+      snapcmpplot(result, btab1, nbody1, tsnap1);
+#endif
+    }
+}
+
+local real *snapcmp(Body *btab1, Body *btab2, int nbody, real tsnap)
+{
+    real *result, oref, odiff;
+    Body *bp1, *bp2, dbody;
+    int i;
+
+    result = (real *) allocate(nbody * sizeof(real));
+    for (i = 0; i < nbody; i++) {
+	bp1 = btab1 + i;
+	bp2 = btab2 + i;
+	oref = (*obsfunc)(bp1, tsnap, i);
+	if (diffpart) {
+	    Mass(&dbody) = Mass(bp1) - Mass(bp2);
+	    SUBV(Pos(&dbody), Pos(bp1), Pos(bp2));
+	    SUBV(Vel(&dbody), Vel(bp1), Vel(bp2));
+	    Phi(&dbody) = Phi(bp1) - Phi(bp2);
+	    SUBV(Acc(&dbody), Acc(bp1), Acc(bp2));
+	    Aux(&dbody) = Aux(bp1) - Aux(bp2);
+	    Key(&dbody) = Key(bp1) - Key(bp2);
+	    odiff = (*obsfunc)(&dbody, tsnap, i);
+	} else
+	    odiff = oref - (*obsfunc)(bp2, tsnap, i);
+	if (relative)
+	    odiff = odiff / oref;
+	result[i] = odiff;
+    }
+    return result;
+}
+
+#if STATISTICS
+
+local void printquart(real result[], int nbody, real tsnap)
+{
+    qsort(result, nbody, sizeof(real), cmpreal);
+    printf("%g %g %g %g %g %g\n",
+           tsnap,
+	   result[0],
+	   result[nbody/4],
+	   result[nbody/2-1],
+	   result[3*nbody/4-1],
+	   result[nbody-1]);
+}
+
+local int cmpreal(real *ap, real *bp)
+{
+    return (*ap < *bp ? -1 : *ap > *bp ? 1 : 0);
+}
+
+#endif
+
+#if HISTOGRAM
+
+histogramplot(result, nbody)
+real result[];
+int nbody;
+{
+    int nbins, *hgram, *histogram(), i;
+    string xlabel, ylabel;
+    real x, y;
+
+    nbins = getiparam("nbins");
+    setrange(xrange, getparam("xrange"));
+    hgram = histogram(result, nbody, nbins, xrange);
+    fitrange(yrange, getiparam("nyticks"), hgram, nbins);
+    plinit("", 0.0, 20.0, 0.0, 20.0);
+    xlabel = getparam(streq(getparam("xlabel"), "") ? "obs" : "xlabel");
+    ylabel = getparam("ylabel");
+    axisplot(xlabel, ylabel);
+    for (i = 1; i <= nbins; i++) {
+	x = xrange[0] + (i - 1.0) * (xrange[1] - xrange[0]) / nbins;
+	y = hgram[i];
+	if (i == 1)
+	    plmove(xtrans(x), ytrans(y));
+	else
+	    plline(xtrans(x), ytrans(y));
+	x = xrange[0] + i * (xrange[1] - xrange[0]) / nbins;
+	plline(xtrans(x), ytrans(y));
+    }
+    plstop();
+}
+
+int *histogram(values, nvals, nbins, range)
+real values[];
+int nvals;
+int nbins;
+real range[];
+{
+    int *hgram, i, j;
+    real x;
+
+    hgram = (int *) allocate((nbins + 2) * sizeof(int));
+    for (j = 0; j <= nbins+1; j++)
+	hgram[j] = 0;
+    for (i = 0; i < nvals; i++) {
+	x = (values[i] - range[0]) / (range[1] - range[0]);
+	if (0.0 <= x && x < 1.0)
+	    j = 1 + floor(nbins * x);
+	else
+	    j = (x < 0.0 ? 0 : nbins + 1);
+	hgram[j]++;
+    }
+    return (hgram);
+}
+
+fitrange(range, nyticks, hgram, nbins)
+real range[];
+int nyticks;
+int hgram[];
+int nbins;
+{
+    int hmax, i, hstep;
+
+    if (streq(getparam("ymax"), "")) {		/* derive ymax from hgram[] */
+	hmax = 0;
+	for (i = 1; i <= nbins; i++)
+	    hmax = MAX(hmax, hgram[i]);
+	hstep = 5;
+	while ((nyticks + 0.5) * hstep < hmax)
+	    hstep = hstep + 5;
+	range[0] = 0.0;
+	range[1] = (nyticks + 1) * hstep;
+    } else {					/* use value supplied       */
+	range[0] = 0.0;
+	range[1] = getdparam("ymax");
+    }
+}
+
+#endif
+
+#if SCATTERPLOT
+
+snapcmpplot(result, btab, nbody, tsnap)
+real result[];
+Body *btab;
+int nbody;
+real tsnap;
+{
+    rproc indfunc;
+    string xlabel, ylabel;
+    int nxticks, nyticks, i;
+    real x, y;
+    Body *bp;
+
+    if (! streq(getparam("xvar"), "")) {
+	indfunc = btrtrans(getparam("xvar"));
+	xlabel = getparam(streq(getparam("xlabel"), "") ? "xvar" : "xlabel");
+    } else {
+	indfunc = obsfunc;
+	xlabel = getparam(streq(getparam("xlabel"), "") ? "obs" : "xlabel");
+    }
+    setrange(xrange, getparam("xrange"));
+    setrange(yrange, getparam("yrange"));
+    ylabel = getparam("ylabel");
+    plinit("", 0.0, 20.0, 0.0, 20.0);
+    axisplot(xlabel, ylabel);
+    for (i = 0, bp = btab; i < nbody; i++, bp++) {
+	x = xtrans((*indfunc)(bp, tsnap, i));
+	y = ytrans(result[i]);
+	if (xbox[0] <= x && x <= xbox[1] &&
+	      ybox[0] <= y && y <= ybox[1])
+	    plpoint(x, y);
+    }
+    plstop();
+}
+
+#endif
+
+#if HISTOGRAM || SCATTERPLOT
+
+axisplot(xlabel, ylabel)
+string xlabel, ylabel;
+{
+    int nxticks, nyticks;
+
+    setrange(xbox, getparam("xbox"));
+    setrange(ybox, getparam("ybox"));
+    nxticks = getiparam("nxticks");
+    nyticks = getiparam("nyticks");
+    if (getbparam("formal")) {
+	formalaxis = TRUE;
+	xaxisvar.labdn = 0.44;
+	xaxisvar.szlab = 0.40;
+	yaxisvar.numdn = 0.24;
+	yaxisvar.labdn = 0.24;
+	yaxisvar.szlab = 0.40;
+    }
+    xaxis(xbox[0], ybox[0], xbox[1] - xbox[0],
+	  xrange, - nxticks, xtrans, xlabel);
+    xaxis(xbox[0], ybox[1], xbox[1] - xbox[0],
+	  xrange, - nxticks, xtrans, NULL);
+    yaxis(xbox[0], ybox[0], ybox[1] - ybox[0],
+	  yrange, - nyticks, ytrans, ylabel);
+    yaxis(xbox[1], ybox[0], ybox[1] - ybox[0],
+	  yrange, - nyticks, ytrans, NULL);
+}
+
+setrange(rval, rexp)
+real rval[];
+string rexp;
+{
+    char *cptr;
+
+    cptr = strchr(rexp, ':');
+    if (cptr != NULL) {
+        rval[0] = atof(rexp);
+	rval[1] = atof(cptr+1);
+    } else {
+        rval[0] = 0.0;
+	rval[1] = atof(rexp);
+    }
+}
+
+real xtrans(real x)
+{
+    return (xbox[0] + (xbox[1] - xbox[0]) *
+	      (x - xrange[0]) / (xrange[1] - xrange[0]));
+}
+
+real ytrans(real y)
+{
+    return (ybox[0] + (ybox[1] - ybox[0]) *
+	      (y - yrange[0]) / (yrange[1] - yrange[0]));
+}
+
+#endif
