8ae909f574f9e21ff5a4c26428d8900568bfad6c
Peter Teuben <teuben@gmail.com>
2000-11-29 02:27:01 +0000

    Initial revision

---
+++ src/nbody/evolve/hackcode/hackcode1/code.c
@@ -0,0 +1,186 @@
+/*
+ * CODE.C: hierarchical N-body code.
+ *	
+ * updates:
+ * 	7-jul-89  V1.1   changed names of 'options'         (PJT)
+ *     26-may-91  V1.2   fixed IMAX in defs.h for Cray      (PJT)
+ *     18-nov-91  V1.2a  malloc -> allocate                 (PJT)
+ *     15-aug-92  V1.2b  nemo_main such to return 0 to shell(PJT)
+ *     18-jan-94  V1.3   srandom -> set_xrandom()	     pjt 
+ *     28-nov-00  documented a memory leak
+ */
+
+#include "code.h"
+
+string defv[] = {		/* DEFAULT PARAMETER VALUES */
+
+    /* file names for structured binary input/output */
+    "in=\n			  snapshot of initial conditions ",
+    "out=\n			  stream of output snapshots ",
+
+    /* file names for saving/restoring program state */
+    "restart=\n			  input state and set controls ",
+    "continue=\n		  input state and continue run ",
+    "save=\n			  output state as code runs ",
+
+    /* params used only if "in", "restart" and "continue" not given */
+    "nbody=128\n		  number of particles to generate ",
+    "seed=123\n			  random number generator seed ",
+    "cencon=false\n		  centrally concentrated system ",
+
+    /* params to control N-body integration */
+    "freq=32.0\n		  fundamental integration frequency ",
+    "eps=0.05\n			  usual potential softening ",
+    "tol=1.0\n			  cell subdivision tolerence ",
+    "fcells=1.0\n		  cell allocation parameter ",
+    "options=mass,phase\n	  misc. control options ",
+
+    "tstop=2.0\n		  time to stop integration ",
+    "freqout=4.0\n		  major data-output frequency ",
+    "minor_freqout=32.0\n	  minor data-output frequency ",
+
+    "debug=false\n		  turn on debugging messages ",
+    "VERSION=1.3\n		  18-jan-94 PJT",
+    NULL,
+};
+
+string usage = "hierarchical N-body code";
+
+string headline = "Hack code";	/* default id for run */
+
+nemo_main()
+{
+    startrun();					/* set params, input data   */
+    initoutput();				/* begin system output      */
+    while (tnow < tstop + 0.1/freq)		/* while not past tstop     */
+	stepsystem();				/*   advance N-body system  */
+    stopoutput();				/* finish up output         */
+}
+
+/*
+ * STARTRUN: startup hierarchical N-body code.
+ */
+
+startrun()
+{
+    string restfile, contfile;
+    bool scanopt();
+
+    infile = getparam("in");			/* set I/O file names       */
+    outfile = getparam("out");
+    restfile = getparam("restart");
+    contfile = getparam("continue");
+    savefile = getparam("save");
+    options = getparam("options");		/* set control options      */
+    debug = getbparam("debug");
+    if (*contfile != NULL)			/* resume interrupted run   */
+	restorestate(contfile);
+    else if (*restfile != NULL) {		/* resume w/ new parameters */
+	restorestate(restfile);
+	/* NOTE: someday, I will have a way to tell which, if any, of these *
+	 * parameters are actually input from the command line, and only    *
+	 * change them.  ANY NON-DEFAULT ARGS MUST BE SPECIFIED AT RESTART. */
+	eps = getdparam("eps");			/*   get modified params    */
+	tol = getdparam("tol");
+	options = getparam("options");		/*   restorestate overwrite */
+	fcells = getdparam("fcells");
+	tstop = getdparam("tstop");
+	freqout = getdparam("freqout");
+	minor_freqout = getdparam("minor_freqout");
+	if (scanopt(options, "new_tout")) {	/*   reset output times?    */
+	    tout = tnow + 1 / freqout;		/*     offset from present  */
+	    minor_tout = tnow + 1 / minor_freqout;
+	}
+    } else {					/* start new calculation    */
+	if (*infile != NULL)			/*   was data file given?   */
+	    inputdata(infile);			/*     read inital data     */
+	else {					/*   make initial conds?    */
+	    nbody = getiparam("nbody");		/*     get nbody parameter  */
+	    if (nbody < 1)			/*     is value absurd?     */
+		error("startrun: absurd nbody\n");
+	    set_xrandom(getiparam("seed"));	/*     set random generator */
+	    testdata(getbparam("cencon"));	/*     make test model      */
+	}
+	freq = getdparam("freq");		/*   get various parameters */
+	eps = getdparam("eps");
+	tol = getdparam("tol");
+	fcells = getdparam("fcells");
+	tstop = getdparam("tstop");
+	freqout = getdparam("freqout");
+	minor_freqout = getdparam("minor_freqout");
+	nstep = 0;				/*   start counting steps   */
+	minor_tout = tout = tnow;		/*   schedule first output  */
+	SETVS(rmin, -2.0);			/*   init box scaling       */
+	rsize = -2.0 * rmin[0];
+    }
+}
+
+/*
+ * TESTDATA: generate initial conditions for test runs.
+ * NOTE: Should really make a Plummer Model! 
+ */
+
+testdata(cencon)
+bool cencon;				/* make concentrated system */
+{
+    vector cmr, cmv;
+    register bodyptr p;
+
+    headline = "Hack code: test data";		/* supply default headline  */
+    tnow = 0.0;					/* pos, vel set at t = 0    */
+    bodytab = (bodyptr) allocate(nbody * sizeof(body));      /* MEMORY LEAK */
+    CLRV(cmr);					/* init cm pos, vel         */
+    CLRV(cmv);
+    for (p = bodytab; p < bodytab+nbody; p++) {	/* loop over particles      */
+	Type(p) = BODY;				/*   tag as a body          */
+	Mass(p) = 1.0 / nbody;			/*   set masses equal       */
+	pickvec(Pos(p), cencon);		/*   pick position          */
+	ADDV(cmr, cmr, Pos(p));
+	pickvec(Vel(p), FALSE);			/*   pick velocity          */
+	ADDV(cmv, cmv, Vel(p));
+    }
+    DIVVS(cmr, cmr, (real) nbody);		/* normalize cm coords      */
+    DIVVS(cmv, cmv, (real) nbody);
+    for (p = bodytab; p < bodytab+nbody; p++) {	/* loop over particles      */
+	SUBV(Pos(p), Pos(p), cmr);		/*   offset by cm coords    */
+	SUBV(Vel(p), Vel(p), cmv);
+    }
+}
+
+/*
+ * STEPSYSTEM: advance N-body system one time-step.
+ */
+
+stepsystem()
+{
+    real dthf, dt;
+    register bodyptr p;
+    vector acc1, dacc, dvel, vel1, dpos;
+
+    dt = 1.0 / freq;				/* get basic time-step      */
+    dthf = 0.5 * dt;				/* and basic half-step      */
+    maketree(bodytab, nbody);			/* load bodies into tree    */
+    nfcalc = n2bcalc = nbccalc = 0;		/* zero interaction counts  */
+    for (p = bodytab; p < bodytab+nbody; p++) { /* loop over particles      */
+	SETV(acc1, Acc(p));			/*   save old acceleration  */
+	hackgrav(p);				/*   compute new acc for p  */
+	nfcalc++;				/*   count force calcs      */
+	n2bcalc += n2bterm;			/*   and 2-body terms       */
+	nbccalc += nbcterm;			/*   and body-cell terms    */
+	if (nstep > 0) {			/*   if past first step?    */
+	    SUBV(dacc, Acc(p), acc1);		/*     use change in accel  */
+	    MULVS(dvel, dacc, dthf);		/*     to make 2nd order    */
+	    ADDV(Vel(p), Vel(p), dvel);		/*     correction to vel    */
+	}
+    }
+    output();					/* do major or minor output */
+    for (p = bodytab; p < bodytab+nbody; p++) {	/* loop advancing bodies    */
+	MULVS(dvel, Acc(p), dthf);		/*   use current accel'n    */
+	ADDV(vel1, Vel(p), dvel);		/*   find vel at midpoint   */
+	MULVS(dpos, vel1, dt);			/*   find pos at endpoint   */
+	ADDV(Pos(p), Pos(p), dpos);		/*   advance position       */
+	ADDV(Vel(p), vel1, dvel);		/*   advance velocity       */
+    }
+    nstep++;					/* count another mu-step    */
+    tnow = tnow + dt;				/* finally, advance time    */
+}
---
+++ src/nbody/evolve/hackcode/hackcode1/load.c
@@ -0,0 +1,238 @@
+/*
+ * LOAD.C: routines to create body-tree.
+ * Public routines: maketree().
+ *
+ *	4-nov-91  added decl. intcoord() for _trace_
+ *	18-nov-91 malloc -> allocate
+ *	21-may-92 extra forward decl for SGI
+ *	24-mar-94 ansi
+ *	 4-mar-96 removed redundant (bad prototype) floor() definition
+ *      28-nov-00 fixed bad index bug in printf() - documented a leak
+ */
+
+#include "defs.h"
+
+local cellptr ctab = NULL;	/* cells are allocated from here */
+local int ncell, maxcell;	/* count cells in use, max available */
+
+/* local forward declarations: */
+local expandbox();
+local loadtree();
+local bool intcoord();
+local int subindex();
+local hackcofm();
+local cellptr makecell();
+
+/*
+ * MAKETREE: initialize tree structure for hack force calculation.
+ */
+
+maketree(btab, nbody)
+bodyptr btab;			/* array of bodies to build into tree */
+int nbody;			/* number of bodies in above array */
+{
+    register bodyptr p;
+
+    if (ctab == NULL) {				/* first time through?      */
+	maxcell = fcells * nbody;		/*   typ. need: 0.5 nbody   */
+	ctab = (cellptr) allocate(maxcell * sizeof(cell));   /* NEVER FREED */
+						/*   allocate cell space    */
+    }
+    ncell = 0;					/* reset cells in use       */
+    troot = NULL;				/* deallocate current tree  */
+    for (p = btab; p < btab+nbody; p++)		/* loop over all bodies     */
+	if (Mass(p) != 0.0) {			/*   only load massive ones */
+	    expandbox(p);			/*     expand root to fit   */
+	    loadtree(p);			/*     insert into tree     */
+	}
+    hackcofm(troot);				/* find c-of-m coordinates  */
+}
+
+/*
+ * EXPANDBOX: enlarge cubical "box", salvaging existing tree structure.
+ */
+
+local expandbox(p)
+bodyptr p;                      /* body to be loaded */
+{
+    bool intcoord();
+    int k, xtmp[NDIM], xmid[NDIM], subindex();
+    vector rmid;
+    cellptr makecell(), newt;
+
+    while (! intcoord(xtmp, Pos(p))) {		/* expand box (rarely)      */
+        if (debug)
+            printf("expandbox: expanding box\n");
+        ADDVS(rmid, rmin, 0.5 * rsize);         /*   find box midpoint      */
+        for (k = 0; k < NDIM; k++)              /*   loop over dimensions   */
+            if (Pos(p)[k] < rmid[k])            /*     is p left of mid?    */
+                rmin[k] -= rsize;               /*       extend to left     */
+        rsize = 2.0 * rsize;                    /*   double length of box   */
+        if (debug)
+            printf("\t   rmin = [%8.4f,%8.4f,%8.4f]\trsize = %8.4f\n",
+                   rmin[0], rmin[1], rmin[2], rsize);
+        if (troot != NULL) {                     /*   repot existing tree?   */
+            newt = makecell();                  /*     create new root cell */
+            assert(intcoord(xmid, rmid));	/*     locate old root cell */
+            k = subindex(xmid, IMAX >> 1);      /*     find old tree index  */
+            Subp(newt)[k] = troot;               /*     graft old on new     */
+	    if (debug)
+		printf("expandbox: old root goes in subcell %d\n", k);
+            troot = (nodeptr) newt;              /*     plant new tree       */
+        }
+    }
+}
+
+/*
+ * LOADTREE: descend tree and insert particle.
+ */
+
+local loadtree(p)
+bodyptr p;			/* body to load into tree */
+{
+    int l, xp[NDIM], xq[NDIM], subindex();
+    nodeptr *qptr;
+    cellptr c, makecell();
+    bool intcoord();
+
+    assert(intcoord(xp, Pos(p)));		/* form integer coords      */
+    l = IMAX >> 1;				/* start with top bit       */
+    qptr = &troot;				/* start with tree root     */
+    while (*qptr != NULL) {			/* loop descending tree     */
+	if (debug)
+	    printf("loadtree: descending tree  l = %o\n", l);
+	assert(l != 0);				/*   dont run out of bits   */
+	if (Type(*qptr) == BODY) {		/*   reached a "leaf"?      */
+	    if (debug)
+		printf("loadtree: replacing body with cell\n");
+	    c = makecell();			/*     alloc a new cell     */
+	    assert(intcoord(xq, Pos(*qptr)));	/*     get integer coords   */
+	    Subp(c)[subindex(xq, l)] = *qptr;	/*     put body in cell     */
+	    *qptr = (nodeptr) c;		/*     link cell in tree    */
+	}
+	qptr = &Subp(*qptr)[subindex(xp, l)];	/*   move down one level    */
+	l = l >> 1;				/*   and test next bit      */
+    }
+    if (debug)
+	printf("loadtree: installing body  l = %o\n", l);
+    *qptr = (nodeptr) p;			/* found place, store p     */
+}
+
+/*
+ * INTCOORD: compute integerized coordinates.
+ * Returns: TRUE unless rp was out of bounds.
+ */
+
+local bool intcoord(xp, rp)
+int xp[NDIM];			/* integerized coordinate vector [0,IMAX) */
+vector rp;			/* real coordinate vector (system coords) */
+{
+    register int k;
+    bool inb;
+    double xsc;
+
+    if (debug)
+        printf("intcoord: rp = [%8.4f,%8.4f,%8.4f]\n", rp[0], rp[1], rp[2]);
+    inb = TRUE;					/* use to check bounds      */
+    for (k = 0; k < NDIM; k++) {		/* loop over dimensions     */
+        xsc = (rp[k] - rmin[k]) / rsize;        /*   scale to range [0,1)   */
+        if (0.0 <= xsc && xsc < 1.0)            /*   within unit interval?  */
+            xp[k] = floor(IMAX * xsc);          /*     then integerize      */
+        else                                    /*   out of range           */
+            inb = FALSE;                        /*     then remember that   */
+    }
+    if (debug)
+        printf("\t  xp = [%8x,%8x,%8x]\tinb = %d\n", xp[0], xp[1], xp[2], inb);
+    return (inb);
+}
+
+/*
+ * SUBINDEX: determine which subcell to select.
+ */
+
+local int subindex(x, l)
+int x[NDIM];		        /* integerized coordinates of particle */
+int l;			        /* current level of tree */
+{
+    register int i, k;
+
+    if (debug)
+        printf("subindex: x = [%8x,%8x,%8x]\tl = %8x\n", x[0],x[1],x[2],l);
+    i = 0;                                      /* sum index in i           */
+    for (k = 0; k < NDIM; k++)                  /* check each dimension     */
+        if (x[k] & l)                           /*   if beyond midpoint     */
+            i += NSUB >> (k + 1);               /*     skip over subcells   */
+    if (debug)
+        printf("          returning %d\n", i);
+    return (i);
+}
+
+/*
+ * HACKCOFM: descend tree finding center-of-mass coordinates.
+ */
+
+local hackcofm(q)
+register nodeptr q;             /* pointer into body-tree */
+{
+    register int i;
+    register nodeptr r;
+    static vector tmpv, dr;
+    static real drsq;
+    static matrix drdr, Idrsq, tmpm;
+
+    if (Type(q) == CELL) {                      /* is this a cell?          */
+        Mass(q) = 0.0;                          /*   init total mass        */
+        CLRV(Pos(q));				/*   and c. of m.           */
+        for (i = 0; i < NSUB; i++) {            /*   loop over subcells     */
+            r = Subp(q)[i];
+            if (r != NULL) {                    /*     does subcell exist?  */
+                hackcofm(r);                    /*       find subcell cm    */
+                Mass(q) += Mass(r);             /*       sum total mass     */
+                MULVS(tmpv, Pos(r), Mass(r));   /*       find moment        */
+                ADDV(Pos(q), Pos(q), tmpv);     /*       sum tot. moment    */
+            }
+        }
+        DIVVS(Pos(q), Pos(q), Mass(q));         /*   rescale cms position   */
+#ifdef QUADPOLE
+	CLRM(Quad(q));				/*   init. quad. moment     */
+	for (i = 0; i < NSUB; i++) {		/*   loop over subnodes     */
+	    r = Subp(q)[i];
+	    if (r != NULL) {			/*     does subnode exist?  */
+		SUBV(dr, Pos(r), Pos(q));	/*       displacement vect. */
+		OUTVP(drdr, dr, dr);		/*       outer prod. of dr  */
+		DOTVP(drsq, dr, dr);		/*       dot prod. dr * dr  */
+		SETMI(Idrsq);			/*       init unit matrix   */
+		MULMS(Idrsq, Idrsq, drsq);	/*       scale by dr * dr   */
+		MULMS(tmpm, drdr, 3.0);		/*       scale drdr by 3    */
+		SUBM(tmpm, tmpm, Idrsq);	/*       form quad. moment  */
+		MULMS(tmpm, tmpm, Mass(r));	/*       of cm of subnode,  */
+		if (Type(r) == CELL)		/*       if subnode is cell */
+		    ADDM(tmpm, tmpm, Quad(r));	/*         use its moment   */
+		ADDM(Quad(q), Quad(q), tmpm);	/*       add to qm of cell  */
+	    }
+	}
+#endif
+    }
+}
+
+/*
+ * MAKECELL: allocation routine for cells.
+ */
+
+local cellptr makecell()
+{
+    register cellptr c;
+    register int i;
+
+    if (ncell >= maxcell)
+	error("makecell: need more than %d cells; increase fcells\n",
+	      maxcell);
+    c = ctab + ncell;
+    ncell++;
+    if (debug)
+	printf("cell %d allocated at address 0%o\n", ncell, (int) c);
+    Type(c) = CELL;
+    for (i = 0; i < NSUB; i++)
+	Subp(c)[i] = NULL;
+    return (c);
+}
