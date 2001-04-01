bd0920a5bba70b161aa0e228aa306b5a0d221865
Peter Teuben <teuben@gmail.com>
2001-04-01 19:28:06 +0000

    import NEMO V3

---
+++ src/nbody/trans/hackdens/density.c
@@ -0,0 +1,228 @@
+/*
+ * DENSITY.C: routines to compute local density.
+ * Public routines: hackden().
+ *
+ *      18-jul-92  PJT  replaced many if(debug)printf(...) by dprintf(1,...)
+ *	 1-apr-01  PJT  compiler warnings
+ */
+
+#include "defs.h"
+
+local void walksub(real, nodeptr, vector, real, real *, int *);
+local bool subdivp(real, vector, real);
+
+real directden(p, nb, dis, ra, base, nbody)
+    bodyptr p;
+    int nb;
+    real dis;
+    real *ra;
+    bodyptr base;
+    int nbody;
+{
+    double sqrt();
+    int total, i;
+    bodyptr q;
+    real rn, nbr, den;
+    real distcount();
+    vector disp;
+    for(i=0, q=base; i<nbody; i++, q++){
+	SUBV(disp, Pos(p), Pos(q));
+	DOTVP(ra[i], disp, disp);
+    }
+    rn=distcount(ra, nbody, nb);
+    nbr = nb-2.0;
+    den=nbr/(rn*sqrt(rn)*FRTHRD_PI);
+#ifdef DEBUG
+    dprintf(0,"Directden= %f\n", den);
+#endif
+    return (den);
+}
+
+real hackden(p, nb, dis, newdis, ra)
+    bodyptr p;
+    int nb;
+    real dis;
+    real *newdis;
+    real *ra;
+{
+    double sqrt(), pow();
+    int total;
+    real rn, nbr, den;
+    real distcount();
+    real dis0, dismax;
+#ifdef DEBUG
+    dprintf(0,"Hackden: nb, dis: %d %f\n", nb, dis);
+#endif    
+    total=0; dis0=0; dismax=1.1e30;
+    while(total < nb || total > nb*10){
+	hackcount(p, dis, ra, &total);
+	if(total < nb) {
+	   dis0=dis;
+	   if(dismax < 1e30){
+	      dis=(dismax+dis0)*0.5;
+	   }else{
+	      dis=dis*1.5;
+	   }
+	}
+	if(total > nb*10){
+	   dismax=dis;
+	   dis=(dis+dis0)*0.5;
+	}
+#ifdef DEBUG
+	dprintf(0,"nb, newdis: %d %f\n", total, dis);
+#endif
+
+     }
+    nbr=nb;
+    *newdis=1.5*dis*pow(nbr/total,0.333333);
+#ifdef DEBUG
+    dprintf(0,"Hackcount returns: %d\n", total);
+#endif	
+    rn=distcount(ra, total, nb);
+#ifdef DEBUG
+    dprintf(0,"distcount returns: %f\n", rn);
+#endif
+    nbr = nb-2.0;
+    den=nbr/(rn*sqrt(rn)*FRTHRD_PI);
+#ifdef DEBUG
+    dprintf(0,"Hackden= %f\n", den);
+#endif
+    return (den);
+}
+
+real distcount(ra ,total, nb)
+    real *ra;
+    int total;
+    int nb;
+{
+    register int i,j;
+    register real tmp;
+    int jmin;
+#ifdef DEBUG
+    dprintf(0,"distcount: distances--");
+    for(i=0; i<total; i++)dprintf(0," %f", ra[i]);
+    puts("");
+#endif    
+    for(i=0; i<nb; i++){
+	tmp=1e20;
+	for(j=i; j<total; j++){
+	    if(ra[j] < tmp){
+		tmp=ra[j];
+		jmin=j;
+	    }
+	}
+	ra[jmin]=ra[i];
+	ra[i]=tmp;
+    }
+#ifdef DEBUG
+    dprintf("after sort: distances--");
+    for(i=0; i<nb; i++)dprintf(0," %f", ra[i]);
+    puts("");
+#endif    
+    return(ra[nb-1]);
+}
+/*
+ * HACKCOUNT: count the number of particles in a given radius.
+ */
+
+local bodyptr pskip;			/* body to skip in force evaluation */
+local vector pos0;			/* point to evaluate field at */
+
+hackcount(p, dis, ra, total)
+bodyptr p;
+real dis;
+real *ra;
+int * total;
+{
+    pskip = p;					/* exclude p from f.c.      */
+    SETV(pos0, Pos(p));				/* set field point          */
+    hackwalk(dis, ra, total);			/* recursively compute      */
+}
+
+/*
+ * HACKWALK: walk the tree opening cells too close to a given point.
+ */
+
+
+hackwalk(dis, ra, total)
+real dis;
+real *ra;
+int * total;
+{
+    vector croot;
+    int i;
+    for(i=0; i<NDIM; i++)croot[i]=rmin[i]+rsize*0.5;
+    *total=0;
+    walksub(dis, troot, croot, rsize, ra, total);
+}
+
+/*
+ * WALKSUB: recursive routine to do hackwalk operation.
+ */
+
+local void walksub(dis, p, cpos, d, ra, total)
+real dis;			        /* critical displacement */
+register nodeptr p;                     /* pointer into body-tree */
+vector cpos;			        /* geometoric center of the node */
+real d;                                 /* size of box  */
+real * ra;			/* array to store distances to */
+				/* particles within sphere */
+int *total;			/* number of particles in the sphere */
+{
+    bool subdivp();
+    register nodeptr *pp;
+    register int i,j;
+    register int k;
+    real offset, r2;
+    vector cpossub, disp;
+    offset = d*0.25;
+    dprintf(1,"walksub: p = %o  d = %f\n", p, d);
+    if (Type(p) == BODY){
+	r2=0.0;
+	SUBV(disp, Pos(p), pos0);               /* compute displacement     */
+	DOTVP(r2, disp, disp);                  /* and find dist squared    */
+	if(r2 < dis*dis){
+	    *total +=1;
+	    ra[*total-1]=r2;
+	}
+    }else if (subdivp(dis, cpos, d)) {          /* should p be opened?      */
+        pp = & Subp(p)[0];                      /*   point to sub-cells     */
+        for (k = 0; k < NSUB; k++) {            /*   loop over sub-cells    */
+	    for(i=NDIM-1, j=1; i>=0; i--, j*=2){
+		if(j&k){
+		    cpossub[i]=cpos[i]+offset;
+		}else{
+		    cpossub[i]=cpos[i]-offset;
+		}
+	    }
+            if (*pp != NULL)                    /*     does this one exist? */
+                walksub(dis,*pp, cpossub, d*0.5, ra, total);
+            pp++;                               /*     point to next one    */
+        }
+    }
+}
+
+/*
+ * SUBDIVP: decide if a node should be opened.
+ * true if need to subdivide
+ */
+
+local bool subdivp(dis, cpos, d)
+real dis;			        /* critical separation  */
+vector cpos;			        /* geometrical center of the node */
+real d;                                 /* size of cell squared */
+{
+    int i;
+    vector dr;
+    real drsq, lcrit;
+    SUBV(dr, cpos, pos0);                     /* compute displacement     */
+    for (i=0; i<3; i++){
+	if(ABS(dr[i]) > dis+d*0.5){
+	    return (0);
+	}
+    }
+    DOTVP(drsq, dr, dr);                        /* and find dist squared    */
+    lcrit= dis + 0.875*d;	                /* critical separation */
+    lcrit = lcrit*lcrit;
+    return (drsq < lcrit);                /* use geometrical rule     */
+}
---
+++ src/nbody/trans/hackdens/hackdens.c
@@ -0,0 +1,231 @@
+/*
+ * HACKDENS.C: hackcode local density calculation tool.
+ *
+ *	7-jul-89  V1.1   updated with get_history()	PJT
+ *     23-oct-90  V1.2   helpvec			PJT
+ *     18-jul-92  V1.3   replaced many if(debug)printf(...) by dprintf(1,...)
+ *			 Also nemo_main() and usage	PJT
+ *	1-apr-01      a  compiler warning
+ */
+
+#include "defs.h"
+#include <getparam.h>
+#include <filestruct.h>
+
+#include <archaic/snapshot.h>
+
+string defv[] = {		/* DEFAULT INPUT PARAMETERS */
+    "in=???\n			  input file with mass coordinates ",
+    "out=\n			  output file with f.c. results ",
+    "neib=6\n			  number of neibours to define local density ",
+    "rneib=0.1\n		  initial guess for neighbour sphere radius ",
+    "write_at_phi=t\n		  flag to write density in place of Phi ",
+    "rsize=4.0\n		  side-length of initial box",
+    "rmin=\n			  lower left corner of initial box",
+    "options=phase,mass\n	  misc. control options {phase, mass}",
+    "fcells=0.9\n		  cell/body allocation ratio ",
+    "verbose=f\n		  flag to print # of particles finished ",
+    "VERSION=1.3a\n		  1-apr-01 PJT",
+    NULL,
+};
+
+string usage = "hackcode local density calculation tool";
+
+nemo_main()
+{
+    inputdata();			/* input mass and test data */
+    dencalc();				/* find force at test pos */
+    outresult();			/* write snap with results */
+}
+
+bodyptr massdata;		/* array of mass points */
+int nmass;			/* number of mass points */
+
+bodyptr testdata;		/* array of test points */
+int ntest;			/* number of test points */
+
+inputdata()
+{
+    string input;
+    stream instr;
+
+    input = getparam("in");
+    instr = stropen(input, "r");
+    readsnapshot(&massdata, &nmass, instr);	/* read mass coord data */
+    strclose(instr);
+    testdata = massdata;
+    ntest = nmass;
+}
+
+real tsnap;
+
+readsnapshot(btab_ptr, nobj_ptr, instr)
+bodyptr *btab_ptr;				/* gets particle array */
+int *nobj_ptr;					/* gets number of bodies */
+stream instr;					/* stream to read from */
+{
+    int nobj, cs, i;
+    real *mbuf, *mp, *pbuf, *pp;
+    bodyptr bp;
+
+    get_history(instr);
+
+    get_set(instr, SnapShotTag);
+    get_set(instr, ParametersTag);
+    if (get_tag_ok(instr, TimeTag)) {
+	get_data_coerced(instr, TimeTag, RealType, &tsnap, 0);
+    }else{
+	tsnap=0.0;
+    }
+    get_data(instr, NobjTag, IntType, &nobj, 0);
+    if (nobj < 1)
+	error("readsnapshot: %s = %d  is absurd\n", NobjTag, nobj);
+    get_tes(instr, ParametersTag);
+    get_set(instr, ParticlesTag);
+    get_data(instr, CoordSystemTag, IntType, &cs, 0);
+    if (cs != CSCode(Cartesian, NDIM, 2))
+	error("readsnapshot: cannot handle %s = %d\n", CoordSystemTag, cs);
+    mbuf = mp = (real *) malloc(nobj * sizeof(real));
+    pbuf = pp = (real *) malloc(nobj * 2 * NDIM * sizeof(real));
+    if (mp == NULL || pp == NULL)
+	error("readsnapshot: not enuf memory for buffers\n");
+    get_data_coerced(instr, MassTag, RealType, mbuf, nobj, 0);
+    get_data_coerced(instr, PhaseSpaceTag, RealType, pbuf, nobj, 2, NDIM, 0);
+    get_tes(instr, ParticlesTag);
+    get_tes(instr, SnapShotTag);
+    *btab_ptr = bp = (bodyptr) malloc(nobj * sizeof(body));
+    if (bp == NULL)
+	error("readsnapshot: not enuf memory for bodies\n");
+    for (i = 0; i < nobj; i++) {
+	Type(bp) = BODY;
+	Mass(bp) = *mp++;
+	SETV(Pos(bp), pp);
+	pp += NDIM;
+	SETV(Vel(bp), pp);
+	pp += NDIM;
+	bp++;
+    }
+    free(mbuf);
+    free(pbuf);
+    *nobj_ptr = nobj;
+}
+
+real *dendata;			/* local density */
+
+int n2btot, nbctot;		/* body-body, body-cell interactions */
+
+real cputree, cpufcal;		/* CPU time to build tree, compute forces */
+
+dencalc()
+{
+    real hackden(), directden();
+    real *pp, *work, rneib, newrneib;
+    int neibnum;
+    double cputime(), cpubase, atof();
+    string *burststring(), *rminxstr;
+    int xstrlen(), i, ibody;
+    bodyptr bp;
+    bool verbose;
+
+    verbose=getbparam("verbose");
+    rneib=getdparam("rneib");
+    neibnum=getiparam("neib")+1;
+#ifdef DEBUG
+    dprintf(0,"neib=%d rneib=%f\n", neibnum, rneib);
+#endif    
+    rsize = getdparam("rsize");
+    rminxstr = burststring(getparam("rmin"), ", ");
+    if (xstrlen(rminxstr, sizeof(string)) < NDIM) {
+	SETVS(rmin, - rsize / 2.0);
+    } else
+	for (i = 0; i < NDIM; i++)
+	    rmin[i] = atof(rminxstr[i]);
+    dprintf(0,"initial rsize: %8f    rmin: %8f  %8f  %8f\n",
+	   rsize, rmin[0], rmin[1], rmin[2]);
+    fcells = getdparam("fcells");
+    dendata = pp = (real *) malloc(ntest * sizeof(real));
+    work = (real *) malloc(ntest * sizeof(real));
+    if (pp == NULL||work==NULL){
+	error("forcecalc: not enuf memory for results\n");
+    }
+    cpubase = cputime();
+    maketree(massdata, nmass);
+    cputree = cputime() - cpubase;
+    dprintf(0,"  final rsize: %8f    rmin: %8f  %8f  %8f\n",
+	   rsize, rmin[0], rmin[1], rmin[2]);
+    cpubase = cputime();
+    n2btot = nbctot = 0;
+    ibody=0;
+    for (bp = testdata; bp < testdata+ntest; bp++) {
+#if 0
+	dprintf(0,"DirectDen= %f\n", directden(bp, neibnum, rneib, work,
+					    testdata, ntest));
+#endif	
+	*pp = hackden(bp, neibnum, rneib, &newrneib, work);
+	rneib=0.95*rneib+0.05*newrneib;
+	pp++;
+	ibody++;
+	if(verbose && ibody%100==0)dprintf(0," %d rn=%f\n", ibody,rneib);
+    }
+    cpufcal = cputime() - cpubase;
+}
+
+stream outstr;
+
+outresult()
+{
+    string out;
+
+    out = getparam("out");
+    if (*out != 0) {
+	outstr = stropen(out, "w");
+	put_history(outstr);
+	writesnapshot();			/* output testdata results */
+	strclose(outstr);
+    }
+}
+
+writesnapshot()
+{
+    real *mbuf, *mp, *pspbuf, *pspp;
+    bodyptr bp;
+    int cs = CSCode(Cartesian, NDIM, 2);
+    string options = getparam("options");
+    bool scanopt();
+
+    mbuf = mp = (real *) malloc(ntest * sizeof(real));
+    pspbuf = pspp = (real *) malloc(ntest * 2 * NDIM * sizeof(real));
+    if (mbuf == NULL || pspbuf == NULL)
+	error("writesnapshot: not enuf memory for buffers\n");
+    for (bp = testdata; bp < testdata+ntest; bp++) {
+	*mp++ = Mass(bp);
+	SETV(pspp, Pos(bp));
+	pspp += NDIM;
+	SETV(pspp, Vel(bp));
+	pspp += NDIM;
+    }
+    put_set(outstr, SnapShotTag);
+    put_set(outstr, ParametersTag);
+    put_data(outstr, NobjTag, IntType, &ntest, 0);
+    put_data(outstr, TimeTag, RealType, &tsnap, 0);
+    put_tes(outstr, ParametersTag);
+    put_set(outstr, ParticlesTag);
+    put_data(outstr, CoordSystemTag, IntType, &cs, 0);
+    if (scanopt(options, "mass"))
+	put_data(outstr, MassTag, RealType, mbuf, ntest, 0);
+    if (scanopt(options, "phase"))
+	put_data(outstr, PhaseSpaceTag, RealType, pspbuf, ntest, 2, NDIM, 0);
+    if(getbparam("write_at_phi")){
+	put_data(outstr, PotentialTag, RealType, dendata, ntest, 0);
+    }else{
+	put_data(outstr, "Density", RealType, dendata, ntest, 0);
+    }
+    put_tes(outstr, ParticlesTag);
+    put_set(outstr, DiagnosticsTag);
+    put_data(outstr, "cputree", RealType, &cputree, 0);
+    put_data(outstr, "cpucal", RealType, &cpufcal, 0);
+    put_tes(outstr, DiagnosticsTag);
+    put_tes(outstr, SnapShotTag);
+    free(mbuf);
+    free(pspbuf);
+}
