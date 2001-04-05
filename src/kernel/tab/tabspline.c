38106e3892b0d1b4580cd8e52bf3f0bd2fe1516d
Peter Teuben <teuben@gmail.com>
2001-04-05 15:35:36 +0000

    cleanup, added keyword format= to tabspline

--- src/kernel/tab/tabspline.c
+++ src/kernel/tab/tabspline.c
@@ -3,6 +3,7 @@
  *
  *   16-apr-98   2.0 : split off from TOOLBOX in spline.c  (kernel/misc)    pjt
  *   22-jan-01   2.1 : allow x= and y= to be arrays, for Mousumi Das	    pjt
+ *   5-apr-01	 2.2 : added format=					    pjt
  *
  *   TODO:
  *      - spline vs. linear
@@ -19,7 +20,8 @@ string defv[] = {
     "x=\n           For these X's, find Y",
     "y=\n           For these Y's, find X",
     "n=0\n          Which one to find (0=all)",
-    "VERSION=2.1\n  24-jan-00 PJT",
+    "format=%g\n    Output format",
+    "VERSION=2.2\n  5-apr-01 PJT",
     NULL,
 
 };
@@ -35,6 +37,8 @@ nemo_main()
     real *coldat[2], *xdat, *ydat, xmin, xmax, ymin, ymax;
     real fy[MAXZERO], fx[MAXZERO], xp[MAXDATA], yp[MAXDATA], x, y;
     stream instr;
+    string fmt1;
+    char fmt[100];
     int i, j, n, nx, ny, nmax, izero, nzero = getiparam("n");
     bool Qx, Qy;
 
@@ -48,12 +52,16 @@ nemo_main()
     colnr[1] = getiparam("ycol");
     Qx = hasvalue("x");
     Qy = hasvalue("y");
+    fmt1 = getparam("format");
+    sprintf(fmt,"%s %s\n",fmt1,fmt1);
+    dprintf(1,"Using format=\"%s\"\n",fmt);
+
 
     instr = stropen(getparam("in"),"r");
     n = get_atable(instr,2,colnr,coldat,nmax);
 
     for(i=0; i<n; i++) {
-        dprintf(2,"%g %g\n",xdat[i],ydat[i]);
+        dprintf(2,fmt,xdat[i],ydat[i]);
         if (i==0) {
             xmin = xmax = xdat[0];
             ymin = ymax = ydat[0];
@@ -79,7 +87,7 @@ nemo_main()
                 if (nzero==0 || izero < nzero) {
                     fx[izero] = ydat[i-1] +
                         (x-xdat[i-1])*(ydat[i]-ydat[i-1])/(xdat[i]-xdat[i-1]);
-                    printf("%g %g\n",x,fx[izero]);
+                    printf(fmt,x,fx[izero]);
                 }
                 izero++;
             }
@@ -101,7 +109,7 @@ nemo_main()
                 if (nzero==0 || izero < nzero) {
                     fy[izero] = xdat[i-1] +
                         (y-ydat[i-1])*(xdat[i]-xdat[i-1])/(ydat[i]-ydat[i-1]);
-                    printf("%g %g\n",fy[izero],y);
+                    printf(fmt,fy[izero],y);
                 }
                 izero++;
             }
