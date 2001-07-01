8d0eee8385e8e9e9b25c29421fd8b899d8658bcf
Peter Teuben <teuben@gmail.com>
2001-07-01 13:59:23 +0000

    martelsnap's matrix was never transposed.... now data look ok. added some manual pages comments

--- src/nbody/io/martelsnap.c
+++ src/nbody/io/martelsnap.c
@@ -3,6 +3,8 @@
  *    19-mar-99    written
  *    30-jun-01    optional swap, in case endianisms are different
  *                 also added coordsystem
+ *     1-jul-01    1.1a ieck, data  are not stored (pos,vel) per particle
+ *                 but first all X's, then all Y's, etc.etc.
  */
  
 #define SinglePrec
@@ -20,7 +22,7 @@ string defv[] = {
     "nbody=262144\n Maximum number of particles",
     "headline=\n    Random verbiage",
     "swap=f\n       Swap bytes in each float if endianism differs",
-    "VERSION=1.1\n  30-jun-01 pjt",
+    "VERSION=1.1a\n 1-jul-01 pjt",
     NULL,
 };
 
@@ -31,10 +33,10 @@ extern void bswap(void *vdat, int len, int cnt);
 void nemo_main()
 {
     stream instr, outstr;
-    real *fbuf, tsnap;
+    real *fbuf, *fnewbuf, tsnap;
     bool Qswap = getbparam("swap");
     long nread;
-    int nbody, nmax;
+    int nbody, nmax, i,j;
     int cs = CSCode(Cartesian, NDIM, 2);
 
     nmax = getiparam("nbody");
@@ -51,11 +53,16 @@ void nemo_main()
         nread = unfread(instr, (char *)fbuf, nmax*6*sizeof(real));
 	if (Qswap) bswap((char *)fbuf, sizeof(real), nread/sizeof(real));
         if (nread<1) break;
+	fnewbuf = (real *) allocate(nread*sizeof(real));
         nbody = nread/6/sizeof(real);
         if (nread % (6*sizeof(real)))
             error("nbody=%d does not divide into %s",nbody,6*sizeof(real));
 	dprintf(1,"Found %d particles\n",nbody);
-
+	for (i=0; i<6; i++) {
+	  for (j=0; j<nbody; j++) {
+	    fnewbuf[i+j*6] = fbuf[j+i*nbody];
+	  }
+	}
         tsnap = 0.0;
         put_set(outstr,SnapShotTag);
         put_set(outstr,ParametersTag);
@@ -64,9 +71,10 @@ void nemo_main()
         put_tes(outstr,ParametersTag);
         put_set(outstr,ParticlesTag);
 	put_data(outstr,CoordSystemTag, IntType, &cs, 0);
-        put_data(outstr,PhaseSpaceTag,RealType,fbuf,nbody,2,NDIM,0);
+        put_data(outstr,PhaseSpaceTag,RealType,fnewbuf,nbody,2,NDIM,0);
         put_tes(outstr,ParticlesTag);
         put_tes(outstr,SnapShotTag);
+	free(fnewbuf);
     }
     free(fbuf);
     strclose(instr);
