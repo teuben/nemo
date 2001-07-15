db2c421fc85fcfc0e0c6321aa010c5065b080698
Peter Teuben <teuben@gmail.com>
2001-07-15 22:36:15 +0000

    closing up to 3.0.6

--- src/nbody/io_nemo/io_nemo.h
+++ src/nbody/io_nemo/io_nemo.h
@@ -1,20 +1,43 @@
-/* --------------------------------------------------------- *\
-|* io_nemo.h :
+/* -------------------------------------------------------------- *\
+|* io_nemo.h :	JC LAMBERT	 22-Oct-97	 V1.0
 |*
-\* --------------------------------------------------------- */
+|* V1.0  : created
+|*
+|* Header file for IO_NEMO library
+|*
+\* -------------------------------------------------------------- */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/* -------------------------------------------------------------- *\
+|* Extern Shared variables declared in "io_nemo_dmp.c" file
+\* -------------------------------------------------------------- */
 
-char * allocate_pointer(char *, int );
+/* variables for reading */
+ extern char * io_in[MAXIO]; 
+ extern FILE * instr[MAXIO];
+ extern bool   read_one[MAXIO];
 
-void reajust_ptr();
+/* variables for writing */
+ extern char * io_out[MAXIO]; 
+ extern FILE * outstr[MAXIO];
+ extern bool   save_one[MAXIO];
 
-int io_nemo(char * , char * , ...);
+/* snapshot variables */
+ extern char * pos,
+             * vel,
+             * pot,
+             * acc,
+             * mass,
+             * timu,
+             * selt,
+             * selp;
 
-int close_io_nemo(char * );
+ extern int  * nbody;
 
-#ifdef __cplusplus
-}
-#endif
+
+/* flags */
+ extern int N_io, T_io, M_io, X_io, V_io, P_io, A_io,
+           F_dim, S_io, R_io, C_io, I_io, ST_io, SP_io ; /*MODIFIED*/
+
+/* -------------------------------------------------------------- *\
+|* End of io_nemo.h
+\* -------------------------------------------------------------- */
