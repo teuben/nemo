d6af21aee7c1e1f1909c5f251fa102e44a4951e9
Peter Teuben <teuben@gmail.com>
2001-12-09 04:33:08 +0000

    declared this file  not-in-use

--- src/image/fits/ccd_cfitsio.c
+++ src/image/fits/ccd_cfitsio.c
@@ -1,23 +1,26 @@
 /*
  * wrapper so that NEMO can call cfitsio, since both are using <fitsio.h>
+ *
+ *   - this file is not used yet, using <cfitsio/fitsio.h> is the current
+ *     proposed solution
  */
 
+#include <stdio.h>
 #include <fitsio.h>
 
 
 static fitsfile *fptr;
-
-
 
 void ccd_cfitsio_open(char *filename)
 {
   int status;
   int bitpix = USHORT_IMG;
   long naxis = 2;
-  long naxes[2] = {128, 128}
+  long naxes[2] = {128, 128};
 
   if (fits_create_file(&fptr, filename, &status))
     printf("%s\n",status);
 
   if (fits_create_img(fptr, bitpix, naxis, naxes, &status))
     printf("%s\n",status);
+}
