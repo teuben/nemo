5aa5fcee0e26ba8cee033577caf47706b1aa66c9
Amar Amarnath <amar@astro.umd.edu>
2001-12-28 18:56:26 +0000

    Changed include file to point to "stdinc.h" instead of "stdiinc.h"

--- inc/exception.h
+++ inc/exception.h
@@ -1,16 +1,30 @@
 #ifndef	NEMO_EXCEP
 #define NEMO_EXCEP
 
+#include <stdinc.h>
+
+// marks point of return from exception handler
 extern int BeginBlock (void);
+
+// turns off exception handling for the block begun
+// with the last BeginBlock; does cleanup of memory and streams
 extern int EndBlock (void);
 
+// Marks allocated memory as resource to be freed when
+// error occurs
 extern int RegisterPointer (void * ptr);
+
+// Unmarks resource - for example, when it is freed
 extern int UnRegisterPointer (void * ptr);
 
 extern int RegisterStream (stream fptr);
 extern int UnRegisterStream (stream fptr);
 
+// Raises exception when irretrievable error occurs
+// Transfers control to exception handling mechanism
 extern void RaiseException (int errNumber);
+
+// Restores context after exception handling is done
 extern void RestoreUserContext (void);
 
 // extern void FreeAllNemoResources (void);
