36ac65722ceafef9d0c68eaf114002cbfb64a37a
Peter Teuben <teuben@gmail.com>
2002-02-20 04:11:16 +0000

    added all=, m=

--- src/image/trans/ccdfill.c
+++ src/image/trans/ccdfill.c
@@ -4,6 +4,7 @@
  *	10-dec-93 	written	- very quick and dirty      pjt
  *	14-mar-95	protos's
  *	22-feb-97	1.1 added bad=				pjt
+ *      19-fe-02        1.2 added all=, m=			pjt
  *                      
  */
 
@@ -19,7 +20,9 @@ string defv[] = {
 	"out=???\n      Output image file",
 	"n=1\n		Number of neighbor cells on all sides to use",
 	"bad=0.0\n	Value of a bad pixel to be patched",
-	"VERSION=1.1\n  22-feb-97 PJT",
+	"all=f\n        Force all points?",
+	"m=3\n          Minimum number of neighbor pixels needed",
+	"VERSION=1.2\n  19-feb-02 PJT",
 	NULL,
 };
 
@@ -27,20 +30,26 @@ string usage = "patch up holes in an image";
 
 
 local void ini_fit(void), accum_fit(int, int, real);
-local bool good_fit(void);
+local bool good_fit(int);
 local real best_fit(void);
+
+#define NLSQ  3
+
 
 void nemo_main(void)
 {
     stream   instr, outstr;
-    int      n, nx, ny, nz;        /* size of scratch map */
+    int      m, n, nx, ny, nz;        /* size of scratch map */
     int      ngood, ntry;
     int      i,j, di, dj;
     imageptr iptr=NULL, iptr1=NULL;      /* pointer to images */
     real     crit = getdparam("bad");
-
+    bool     Qall = getbparam("all");
+    
     instr = stropen(getparam("in"), "r");
     n = getiparam("n");
+    m = getiparam("m");
+    if (m < NLSQ) error("Cannot choose m=%d < %d",m,NLSQ);
 
     read_image( instr, &iptr);
 
@@ -60,7 +69,7 @@ void nemo_main(void)
     ntry = ngood = 0;
     for (j=0; j<ny; j++) {                  /* loop over image and patch */
     	for (i=0; i<nx; i++) {
-            if (MapValue(iptr1,i,j) == crit) {      /* try and patch up */
+            if (MapValue(iptr1,i,j) == crit || Qall) {   /* try and patch up */
                 ntry++;
                 ini_fit();
                 for (dj=-n; dj<=n; dj++) {
@@ -71,7 +80,7 @@ void nemo_main(void)
                             accum_fit(di,dj,MapValue(iptr1,i+di,j+dj));
                     }
                 }
-                if (good_fit()) {
+                if (good_fit(m)) {
                     MapValue(iptr,i,j) = best_fit();
                     ngood++;
                 } 
@@ -83,7 +92,6 @@ void nemo_main(void)
 }
 
 
-#define NLSQ  3
 
 local int nsum;
 local real lmat[NLSQ*NLSQ], lvec[NLSQ], lsol[NLSQ], la[NLSQ+1];
@@ -104,9 +112,9 @@ local void accum_fit(int di, int dj, real val)
     lsq_accum(NLSQ,lmat,lvec,la,1.0);
 }
 
-local bool good_fit(void)
+local bool good_fit(int m)
 {
-    return nsum>=NLSQ;
+  return nsum>=m;
 }
 
 local real best_fit(void)
