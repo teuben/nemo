1b319c52eb661cf08e1490b232b82b148b950934
Peter Teuben <teuben@gmail.com>
2002-08-10 05:58:51 +0000

    initial support for output WCS contruction

--- src/image/fits/fitsglue.c
+++ src/image/fits/fitsglue.c
@@ -9,6 +9,7 @@
  *     12-feb-99      b changed for new fts_cdata 		PJT
  *	5-apr-01      c increased default MAXIN			pjt
  *     20-jun-01   1.3  removed the blocking= keyword           pjt
+ *     10-aug-02   1.4  copy over WCS of the first file         pjt
  *
  */
 
@@ -22,13 +23,14 @@ string defv[] = {			/* Standard NEMO keyword+help */
     "naxis3=0\n            Preset NAXIS3 here, or let program scan first",
     "compact=f\n           Compact (move) dummy axes to the end",
     "inlist=\n             optional nemoinp(1) list expression for in=",
-    "VERSION=1.3\n         20-jun-01 PJT",
+    "wcs=f\n               try and copy a reasonably WCS from input to output",
+    "VERSION=1.4\n         10-aug-02 PJT",
     NULL,
 };
 
 string usage = "catenate fits images into a fitscube";
 
-#include "fits.h"
+#include <fits.h>
 
 extern string *burststring(string,string);
 
@@ -38,17 +40,31 @@ extern string *burststring(string,string);
 
 local string minmax[] = { "DATAMIN", "DATAMAX", NULL };
 
+typedef struct fts_wcs {
+  int naxis;
+  double crval;
+  double crpix;
+  double cdelt;
+  char ctype[32];
+  char cunit[32];  /* optional */
+} fts_wcs;
+
+void fts_iniwcs(int, fts_wcs *);
+void fts_addwcs(struct fits_header *,int ,fts_wcs *);
+void fts_setwcs(struct fits_header *,int ,fts_wcs *);
+
 void nemo_main()
 {
     stream instr, outstr;
     int    i,j, n, planesize, totsize;
     int    zbufsiz, ninfiles, naxis, naxis1, naxis2, naxis3, naxisn[3], ntmp;
-    bool   Qmultiple, Qcompact = getbparam("compact");
+    bool   Qmultiple, Qcompact = getbparam("compact"), Qwcs = getbparam("wcs");
     struct fits_header fh, fh_out;
     string outname, *innames;
     char   *buffer, *zbuf, fname[128];
     int    inlist[MAXIN];
     double datamin, datamax;
+    fts_wcs wcs[3];
 
     innames = burststring(getparam("in"),", ");
     ninfiles = xstrlen(innames,sizeof(string)) - 1;
@@ -71,6 +87,7 @@ void nemo_main()
 
     Qmultiple = getbparam("multiple");
     naxis3 = getiparam("naxis3");
+    fts_iniwcs(3,wcs);
 
     if (naxis3 < 1) {
       for (j=0; j<ninfiles; j++) {        /* scan all files for compatibility */
@@ -79,6 +96,7 @@ void nemo_main()
             fts_zero(&fh);
             n = fts_rhead(&fh,instr);       /* read header */
             if (n<0) break;                 /* done reading */
+	    if (Qwcs) fts_addwcs(&fh,3,wcs);
             if (naxis3 < 1) {        /* first time around */
                 naxis  = fh.naxis;
                 naxis1 = fh.naxisn[0];
@@ -112,7 +130,7 @@ void nemo_main()
     outstr = stropen(outname,"w");
 
     fts_zero(&fh_out);
-    for (j=0; j<ninfiles; j++) {
+    for (j=0; j<ninfiles; j++) {              /* now accumulate data */
         instr = stropen(innames[j],"r");
         for (;;) {
             fts_zero(&fh);
@@ -143,7 +161,8 @@ void nemo_main()
                             fh_out.naxisn[i-1] = ntmp;
                         }
                     }
-                }
+                } 
+		if (Qwcs) fts_setwcs(&fh_out,3,wcs);
                 fts_whead(&fh_out,outstr);
             }
             fts_cdata(&fh,instr,outstr,FALSE,FALSE);
@@ -154,4 +173,67 @@ void nemo_main()
         strclose(instr);
     } /* for (j) */
     strclose(outstr);
+}
+
+/* 
+ * this WCS stuff should be absorbed into fits.c
+ */
+
+void fts_iniwcs(int ndim, fts_wcs *wcs)
+{
+  int i;
+
+  for (i=0; i<ndim; i++)
+    wcs[i].naxis = 0;
+}
+
+void fts_addwcs(struct fits_header *fh,int ndim,fts_wcs *wcs)
+{
+  int i;
+
+  if (wcs[0].naxis == 0) {  /* first time around */
+    if (fh->naxis > 3) error("Cannot handle > 3D data yet");
+    for (i=0; i < fh->naxis; i++) {
+      wcs[i].naxis = fh->naxisn[i];
+      wcs[i].crpix = fh->crpixn[i];   /* need to check if they exist */
+      wcs[i].crval = fh->crvaln[i];
+      wcs[i].cdelt = fh->cdeltn[i];
+      strcpy(wcs[i].ctype, fh->ctypen[i]);
+    }
+    if (wcs[2].naxis == 0) wcs[2].naxis = 1;
+    dprintf(0,"fts_addwcs: Starting at %d x %d x %d [%s %s]\n",
+	    wcs[0].naxis, wcs[1].naxis, wcs[2].naxis,
+	    wcs[0].ctype, wcs[1].ctype);
+  } else {                  /* accumulate */
+    if (fh->naxis > 2) {
+      wcs[2].naxis += fh->naxisn[2];      
+    } else {
+      wcs[2].naxis++;
+    }
+    dprintf(0,"fts_addwcs: Continue to %d x %d x %d\n",
+	    wcs[0].naxis, wcs[1].naxis, wcs[2].naxis);
+  }
+}
+
+void fts_setwcs(struct fits_header *fh,int ndim,fts_wcs *wcs)
+{
+  int i,n;
+
+  n = fh->naxis;
+  dprintf(0,"fts_setwcs: naxis=%d\n",n);
+  if (n>3) error("Cannot do > 3D yet");
+  fh->crvaln = (double *) allocate(n*sizeof(double));
+  fh->crpixn = (double *) allocate(n*sizeof(double));
+  fh->cdeltn = (double *) allocate(n*sizeof(double));
+  fh->ctypen = (char **) allocate(n*sizeof(char *));
+
+  for (i=0; i<n; i++) {
+    if (fh->naxisn[i] != wcs[i].naxis) 
+      warning("fts_setwcs: mismatch at axis %d: %d != %d",
+	      i+1,fh->naxisn[i],wcs[i].naxis);
+    fh->crvaln[i] = wcs[i].crval;
+    fh->crpixn[i] = wcs[i].crpix;
+    fh->cdeltn[i] = wcs[i].cdelt;
+    fh->ctypen[i] = wcs[i].ctype;
+  }
 }
