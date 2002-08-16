7f2f734b5933d4bae04bae195bbecafbd8c035e7
Peter Teuben <teuben@gmail.com>
2002-08-16 00:14:49 +0000

    better WCS definition

--- src/orbit/potential/data/ccd.c
+++ src/orbit/potential/data/ccd.c
@@ -7,33 +7,45 @@
  *		   working on them, and will hence move away (escape)   PJT
  *	   oct-93  get_pattern
  *	 4-mar-96  fixed VERSION to CCD_VERSION
- *      19-jul-02  cleaned up the code a bit, provided scale factor     PJT
+ *      19-jul-02  V2.0 cleaned up the code a bit, provided potential scale factor     PJT
+ *      13-aug-02  V3.0 reworked WCS issues due to ported FITS files from e.g. miriad's potfft
  *
  *  Note: astronomical images often have Dx<0 (Right Ascension increases
  *        to the left)....
+ * 
+ *  ToDo:  potential is just given from the nearest(?) pixel, no interpolation done yet
  *
  */
 
 /*CTEX
  *  {\bf potname=ccd
- *	 potpars={\it $\Omega,I_scale,X_o,Y_o$}
+ *	 potpars={\it $\Omega,I_scale,Xcen,Ycen,Dx,Dy$}
  *	 potfile={\it image(5NEMO)}}
+ *
+ *  The lower left pixel of an image in NEMO is defined as (0,0), with WCS values
+ *  Xmin,Ymin derived from the header. If (Xcen,Ycen) are used, these are the 
+ *  0-based pixel coordinates of the center pixel. If (Dx,Dy) are used, these are
+ *  the pixel separations.
+ *  To aid astronomical images where Dx<0, these are interpreted as positive.
  */
 
 #include <stdinc.h>
 #include <filestruct.h>
 #include <image.h>
 
-#define CCD_VERSION "ccd V2.0 17-jul-02"
+#define CCD_VERSION "ccd V3.0 13-aug-02"
 
 local double   omega = 0.0;
 local double   iscale = 1.0;
 local stream   potstr = NULL;
 local imageptr iptr = NULL;
-local double   idx, idy, xmin, ymin, xmax, ymax, dx, dy;
+local double   idx, idy, xmin, ymin, xmax, ymax;
+local bool     Qcen,Qdel;
 local int      nx, ny;
-local double   xoff=0.0;       /* central pixel shift w.r.t. Xmin.Ymin */
-local double   yoff=0.0;       /* in pixel coordinates */
+local double   xcen=0.0;       /* central pixel; where (0,0) is the lower left pixel */
+local double   ycen=0.0;
+local double   dx=1;           /* pixel steps */
+local double   dy=1;
 
 
 void inipotential (int *npar, double par[], char *name)
@@ -41,15 +53,27 @@ void inipotential (int *npar, double par[], char *name)
     int n;
 
     n = *npar;
-    if (n>0)  omega = par[0];
-    if (n>1)  iscale = par[1];
-    if (n>2)  xoff = par[2];
-    if (n>3)  yoff = par[3];
-    if (n>4)  warning("inipotential(ccd): npar=%d only 4 parameter accepted",n);
+    if (n>0)  omega = par[0];                /* standard pattern speed */
+    if (n>1)  iscale = par[1];               /* scaling factor applied to potential */
+    if (n>2)  {                              /* alternate definition of center pixel */
+      xcen = par[2];
+      if (n>3)  ycen = par[3];
+      else ycen = xcen;
+    }
+    if (n>4)  {                              /* alternate definition of pixel size */
+      dx = par[4];
+      if (n>5)  dy = par[5];
+      else dy = dx;
+    }
+    if (n>6)  warning("inipotential(ccd): npar=%d only 6 parameter accepted",n);
+
+    /* set some easy to use booleans */
+    Qcen = n>2;
+    Qdel = n>4;
 
     dprintf(1,"INIPOTENTIAL: %s: %s\n",CCD_VERSION,name);
-    dprintf(1,"  Parameters:  Omega=%g iscale=%g xoff,yoff=%g,%g\n",
-	    omega,iscale,xoff,yoff);
+    dprintf(1,"  Parameters:  Omega=%g iscale=%g xcen,ycen=%g,%g dx,dy=%g,%g\n",
+	    omega,iscale,xcen,ycen,dx,dy);
 
     potstr = stropen (name, "r");          /* open the image */
     read_image (potstr,&iptr);              /* read the image */
@@ -60,14 +84,21 @@ void inipotential (int *npar, double par[], char *name)
 	MapValue(iptr,i,j) =  MapValue(iptr,i,j) * iscale;
     }
 
-    dx = Dx(iptr);
-    dy = Dy(iptr);
-    idx = 1.0/dx;
-    idy = 1.0/dy;
-    xmin = Xmin(iptr);
-    ymin = Ymin(iptr);
     nx = Nx(iptr);
     ny = Ny(iptr);
+    if (!Qdel) {
+      dx = Dx(iptr);
+      dy = Dy(iptr);
+    }
+    idx = 1.0/dx;
+    idy = 1.0/dy;
+    if (!Qcen) {
+      xmin = Xmin(iptr);
+      ymin = Ymin(iptr);
+    } else {
+      xmin = -xcen*dx;
+      ymin = -ycen*dy;
+    }
 
     if (idx != idy) {
         if (idx == -idy && xmin == -ymin) {     /* try and patch it */
@@ -79,12 +110,12 @@ void inipotential (int *npar, double par[], char *name)
     }
     if (idx<0) warning("1/Dx=%f",idx);
     if (idy<0) warning("1/Dy=%f",idy);
-    xmax = xmin + nx * dx;
+    xmax = xmin + nx * dx;              /* pixel centers */
     ymax = ymin + ny * dy;
     dprintf(1,"Offset and scale factors: xmin,ymin,1/dx,1/dy=%f %f %f %f\n",
             xmin,ymin,idx,idy);
-    dprintf(1,"Formal X-range: %g %g\n",xmin - dx/2,xmax + dx/2);
-    dprintf(1,"Formal Y-range: %g %g\n",ymin - dy/2,ymax + dy/2);
+    dprintf(1,"Formal full pixel X-range: %g %g\n",xmin-0.5*dx,xmax+0.5*dx);
+    dprintf(1,"Formal full pixel Y-range: %g %g\n",ymin-0.5*dy,ymax+0.5*dy);
 
     par[0] = omega;
 }
@@ -94,14 +125,14 @@ void potential(int *ndim,double *pos,double *acc,double *pot,double *time)
     double x, y, dx, x3, x4, x8, dy, y3, y4, y8;
     int ix, iy;
 
-    x = (pos[0]-xmin)*idx + 0.5 + xoff;    /* fractional cell index   0..nx */
-    y = (pos[1]-ymin)*idy + 0.5 + yoff;
+    x = (pos[0]-xmin)*idx + 0.5;    /* fractional cell index   0..nx */
+    y = (pos[1]-ymin)*idy + 0.5;
     ix = (int) floor(x);            /* cell index:   0 .. nx-1 */
     iy = (int) floor(y);
     dx = x-ix-0.5;    x3=3*dx;    x4=4*dx;    x8=2*x4;
     dy = y-iy-0.5;    y3=3*dy;    y4=4*dy;    y8=2*y4;
 
-    dprintf(5,"x,y,ix,iy=%f %f %d %d\n",x,y,ix,iy);
+
 #if 0
     if (ix<1) ix=1;         /* kludge for border cell particles ??*/
     if (ix>nx-2) ix=nx-2
@@ -137,6 +168,7 @@ void potential(int *ndim,double *pos,double *acc,double *pot,double *time)
                +( 2   +y4)*MapValue(iptr,ix  ,iy+1)
 	       +( 2+x3+y4)*MapValue(iptr,ix+1,iy+1) )/12.0*idy;
     acc[2] = 0.0;
+    dprintf(5,"x,y,ix,iy,ax,ay=%f %f %d %d %g %g\n",x,y,ix,iy,acc[0],acc[1]);
 }
 
 
