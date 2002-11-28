15a6f754c9ea9b0a2040312915e26979857e27ac
Peter Teuben <teuben@gmail.com>
2002-11-28 04:35:46 +0000

    split deriv into delta, added logic=

--- src/kernel/tab/tabclip.c
+++ src/kernel/tab/tabclip.c
@@ -21,6 +21,7 @@ string defv[] = {                /* DEFAULT INPUT PARAMETERS */
     "ycol=2\n		 y coordinate column",
     "deriv=\n            Clipping if abs(derivative) exceeds this value",
     "eta=\n              Max deviation of local 2nd order polynomial allowed",
+    "logic=and\n         Use AND or OR",
     "nmax=100000\n       Hardcoded allocation space, if needed for piped data",
     "comment=f\n         Keep clipped points as commented lines?",
     "VERSION=0.3\n	 27-nov-02 PJT",
@@ -43,6 +44,8 @@ local bool *ok;                         /* masking array */
 local int    npt;			/* actual number of data points */
 local int    nmax;			/* lines to allocate */
 
+local bool Qand;
+
 
 /****************************** START OF PROGRAM **********************/
 
@@ -60,15 +63,23 @@ nemo_main()
 
 setparams()
 {
-    input = getparam("in");             /* input table file */
-    output = getparam("out");             /* input table file */
-    xcol = getiparam("xcol");
-    ycol = getiparam("ycol");
+  string  logic = getparam("logic");
 
-    nmax = nemo_file_lines(input,getiparam("nmax"));
-    dprintf(1,"Allocated %d lines for table\n",nmax);
-    x = (real *) allocate(sizeof(real) * (nmax+1));   /* X data */
-    y = (real *) allocate(sizeof(real) * (nmax+1));   /* Y data */
+  input = getparam("in");             /* input table file */
+  output = getparam("out");             /* input table file */
+  xcol = getiparam("xcol");
+  ycol = getiparam("ycol");
+  if (streq(logic,"and"))
+    Qand = TRUE;
+  else if (streq(logic,"or"))
+    Qand = FALSE;
+  else 
+    error("logic %s must be 'and' or 'or'",logic);
+
+  nmax = nemo_file_lines(input,getiparam("nmax"));
+  dprintf(1,"Allocated %d lines for table\n",nmax);
+  x = (real *) allocate(sizeof(real) * (nmax+1));   /* X data */
+  y = (real *) allocate(sizeof(real) * (nmax+1));   /* Y data */
 }
 
 read_data()
@@ -154,6 +165,55 @@ deriv_data()
   dprintf(0,"Min/Max for deriv = %g %g (dmin=%g)\n",d_min,d_max,dmin);
 }
 
+delta_data()
+{
+  int    i;
+  real d1, d2, d1a, d2a, dmin = getdparam("deriv");
+  real d_min, d_max;
+  bool first = TRUE;
+
+  for (i=0; i<npt; i++) {                  /* loop over all points */
+    if (i>1 ) {
+      d1 = (y[i]-y[i-1])/(x[i]-x[i-1]);
+      if (first) {
+	d_min = d_max = ABS(d1);
+	first = FALSE;
+      }
+    } else
+      d1 = dmin+1;
+    if (i>2 )
+      d1a = (y[i]-y[i-2]);
+    else
+      d1a = 0;
+    if (i<npt-2)
+      d2a = y[i]-y[i+2];
+    else
+      d2a = 0.;
+    if (i<npt-1) {
+      d2 = (y[i]-y[i+1])/(x[i]-x[i+1]);
+      if (first) {
+	d_min = d_max = ABS(d1);
+	first = FALSE;
+      }
+    } else
+      d2 = dmin+1;
+    d1 = ABS(d1);  
+    d2 = ABS(d2);  
+    d1a = ABS(d1a);
+    d2a = ABS(d2a);
+    if (d1 < dmin) {
+      d_min = MIN(d_min,d1);
+      d_min = MIN(d_min,d2);
+    }
+    if (d2 < dmin) {
+      d_max = MAX(d_max,d1);
+      d_max = MAX(d_max,d2);}
+    if (d1 > dmin || d2 > dmin || d1a > dmin || d2a > dmin) ok[i] = FALSE;
+    dprintf(1,"DERIV: %d %g %g %g %g %g %g %g %g %d\n",i,y[i],y[i-1],dmin,dmin,d1,d2,d1a,d2a,ok[i]);
+  }
+  dprintf(0,"Min/Max for deriv = %g %g (dmin=%g)\n",d_min,d_max,dmin);
+}
+
 /*
  * fit a local polynomial to the 3 points defined by 0,1,2
  * and compute the 'velocity' and 'acceleration'
@@ -185,12 +245,18 @@ eta_data()
   bool first = TRUE;
   dprintf(0,"ETA: Using eta=%g\n",eta);
 
+  /* can't do much about the first 3 and last 3 points .... */
+
   for (i=3; i<npt-3; i++) {                  /* loop over all points */
     d1 = get_av(i-3,i-2,i-1,i);
     d2 = get_av(i+3,i+2,i+1,i);
     d1 = ABS(d1-y[i]);
     d2 = ABS(d2-y[i]);
-    if (d1 > eta && d2 > eta) ok[i] = FALSE;
+    if (Qand) {
+      if (d1 > eta && d2 > eta) ok[i] = FALSE;
+    } else {
+      if (d1 > eta || d2 > eta) ok[i] = FALSE;
+    }
     dprintf(1,"ETA: %d %g %g %g %g\n",i,x[i],y[i],d1,d2);
   }
 }
