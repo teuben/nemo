21573f5f5304d6e4085479a1e0c28ff65ad14fc0
Peter Teuben <teuben@gmail.com>
2002-12-12 16:06:41 +0000

    changed name to core123, otherwise local version was used.... since you cannot overload names already existing in rotcurshape's main()

--- src/image/rotcur/shape/core1.c
+++ src/image/rotcur/shape/core1.c
@@ -1,10 +1,18 @@
 #include <nemo.h>
 
-real rotcur_core(real r, int n, real *p, real *d)
+static int first = 1;
+
+real rotcur_core123(real r, int n, real *p, real *d)
 {
   real x = r / p[1];
   d[0] = x/(1+x);
   d[1] = -p[0]*d[0]/(p[1]*(1+x));
+#if 1
+  if (first) {
+    dprintf(0,"First time in rotcur_core\n");
+    first = 0;
+  }
+#endif
   return p[0] * d[0];
 }
 
