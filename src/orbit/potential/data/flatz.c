f5736ab6f7e405c4f3c2fe2a2622323389481673
Peter Teuben <teuben@gmail.com>
2003-02-13 06:52:34 +0000

    first submission, project for Rob Olling

---
+++ src/orbit/potential/data/flatz.c
@@ -0,0 +1,71 @@
+/*
+ * flatz.c: potential & forces as defined by a rotation curve
+ *            that is linear to (r0,v0) and flat thereafter
+ *          and quasi harmonic in Z
+ *
+ *	29-dec-01	derived from rotcur
+ *      13-feb-03       derived from rotcur0
+ */
+
+/*CTEX
+ *  {\bf potname=flatz
+ *       potpars={\it $\Omega$},r0,v0,s,h
+ *	 
+ */
+ 
+
+#include <stdinc.h>
+#include <spline.h>
+#include <table.h>
+
+local double omega = 0.0;
+local double r0  = 1.0;
+local double v0 = 1.0;
+local double s = 1.0;
+local double h = 0.2;
+local double h2;
+
+void inipotential (int *npar, double *par, string name)
+{
+    int n;
+
+    n = *npar;
+    if (n>0) omega = par[0];
+    if (n>1) r0    = par[1];
+    if (n>2) v0    = par[2];
+    if (n>3) s     = par[3];
+    if (n>4) h     = par[4];
+    if (n>5) warning("Rotcur potential: only 5 parameters usable");
+    
+    dprintf (1,"INIPOTENTIAL FlatZ potential %s\n",name);
+    dprintf (1,"  Parameters : Pattern Speed = %f\n",omega);
+    dprintf (1,"  R0 = %g  V0 = %g S = %g  H = %g\n", r0, v0, s, h);
+
+    par[0] = omega;     /* return pattern speed again */
+    h2 = h*h;
+}
+    
+void potential (int *ndim, double *pos,double *acc,double *pot,double *time)
+{
+    real r, r2, v, f;
+    int    i;
+
+    for (i=0, r2=0.0; i<2; i++)
+        r2 += sqr(pos[i]);
+    r=sqrt(r2);
+
+    if (r < r0)
+        v = (r/r0)*v0;
+    else
+        v = v0;
+
+    if (r > 0)
+	f = sqr(v/r);
+    else
+    	f = 0;
+
+    *pot = 0.0;             /* no potentials... for now */
+    acc[0] = -f*pos[0]; 
+    acc[1] = -f*pos[1]; 
+    acc[2] = -s*pos[2]/sqrt(pos[2]+h2);
+}
