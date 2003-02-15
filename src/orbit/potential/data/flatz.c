5fd1dc50788cc4a2c81d665250c22cb9be361464
Peter Teuben <teuben@gmail.com>
2003-02-15 22:33:37 +0000

    buglet fix by Rob Olling

--- src/orbit/potential/data/flatz.c
+++ src/orbit/potential/data/flatz.c
@@ -5,6 +5,7 @@
  *
  *	29-dec-01	derived from rotcur
  *      13-feb-03       derived from rotcur0; toy project Rob Olling
+ *      15-feb-03       RPO, buglet fixed and documentation added
  */
 
 /*CTEX
@@ -19,10 +20,10 @@
 #include <table.h>
 
 local double omega = 0.0;
-local double r0  = 1.0;
-local double v0 = 1.0;
-local double s = 1.0;
-local double h = 0.2;
+local double r0    = 1.0;  /* radius at which RC changes from linear to flat */
+local double v0    = 1.0;  /* rotation speed beyond r0                       */
+local double s     = 1.0;  /* columndensity of matter (constant with r)      */
+local double h     = 0.2;  /* scaleheight of the vertical mass distribution  */
 local double h2;
 
 void inipotential (int *npar, double *par, string name)
@@ -35,14 +36,14 @@ void inipotential (int *npar, double *par, string name)
     if (n>2) v0    = par[2];
     if (n>3) s     = par[3];
     if (n>4) h     = par[4];
-    if (n>5) warning("Rotcur potential: only 5 parameters usable");
+    if (n>5) warning("FlatZ potential: only 5 parameters usable");
     
     dprintf (1,"INIPOTENTIAL FlatZ potential %s\n",name);
     dprintf (1,"  Parameters : Pattern Speed = %f\n",omega);
     dprintf (1,"  R0 = %g  V0 = %g S = %g  H = %g\n", r0, v0, s, h);
 
     par[0] = omega;     /* return pattern speed again */
-    h2 = h*h;
+    h2     = h*h;
 }
     
 void potential (int *ndim, double *pos,double *acc,double *pot,double *time)
@@ -64,8 +65,8 @@ void potential (int *ndim, double *pos,double *acc,double *pot,double *time)
     else
     	f = 0;
 
-    *pot = 0.0;             /* no potentials... for now */
+    *pot   =  0.0;             /* no potentials... for now */
     acc[0] = -f*pos[0]; 
     acc[1] = -f*pos[1]; 
-    acc[2] = -s*pos[2]/sqrt(pos[2]+h2);
+    acc[2] = -s*pos[2]/sqrt(pos[2]*pos[2]+h2);    /* --> 1/sqrt(z^2+h^2) */
 }
