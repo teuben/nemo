677ba8afa540f9779eb15678570cb1d968828bcc
Peter Teuben <teuben@gmail.com>
2003-06-11 22:14:04 +0000

    finalized 0.1 release

--- src/kernel/tab/tabslide.c
+++ src/kernel/tab/tabslide.c
@@ -1,7 +1,15 @@
 /*
- * TABSLIDE:    slide through a large dataset
+ * TABSLIDE:    slide through a large ascii table and do some work
  *
  *      11-jun-03   V0.1   template for Vanessa              PJT
+ *
+ * some performance numbers on a P4/1.6GHz
+ * zcat 5body0.dat.gz | sum                                 10"
+ * zcat 5body0.dat.gz | wc                                  56"
+ * zcat 5body0.dat.gz | awk 'END{print NF,NR}'              35"
+ * zcat 5body0.dat.gz | awk '{sum+=$1}END{print sum,NF,NR}' 40"
+ * zcat 5body0.dat.gz | tabslide -                          34"
+ * zcat 5body0.dat.gz | tabslide -  nmax=100000             35"
  */
 
 #include <nemo.h>
@@ -16,24 +24,25 @@ string defv[] = {
 
 string usage="template sliding through large ascii datasets";
 
+#ifndef MAXCOL
 #define MAXCOL 16
+#endif
 
-typedef struct column {
-    int maxdat;     /* allocated length of data */          /* not used */
-    int ndat;       /* actual length of data */             /* not used */
-    real *dat;      /* pointer to data */
-    int colnr;      /* column number this data came from */ /* not used */
-} a_column;
-
-int nxcol, nycol, xcolnr[MAXCOL];
-a_column            xcol[MAXCOL];
-
+real *coldat[MAXCOL];
+int colnr[MAXCOL];
 
 stream instr, outstr;       /* input / output file */
 
-
 int    nmax;                /* allocated space */
-int    npt;                 /* actual number of points from table */
+int    ntot;                /* total number of rows processed */
+int    npt;                 /* number of rows in the buffer */
+int    ncols;               /* number of colums */
+
+void setparams(void);
+int read_data(void);
+void process_data(void);
+
+real sum = 0.0;             /* some work related variables */
 
 
 /****************************** START OF PROGRAM **********************/
@@ -41,43 +50,64 @@ int    npt;                 /* actual number of points from table */
 nemo_main()
 {
     setparams();
-    read_data();
+    while (read_data())
+      process_data();
+    dprintf(0,"\nProcessed %d lines\n",ntot);
+    dprintf(0,"Sum = %g\n",sum);
 }
 
+/*
+ *  get user interface and initialize variables 
+ */
 
-setparams()
+void setparams(void)
 {
-    string inname = getparam("in");
-    nmax = nemo_file_lines(inname,getiparam("nmax"));
-    if (nmax<0) error("Error opening %s",inname);
-    if (nmax==0) error("No data?");
-    instr = stropen (inname,"r");
+  int i;
+  string inname = getparam("in");
 
-    nxcol = nemoinpi(getparam("xcol"),xcolnr,MAXCOL);
-    if (nxcol<0) error("Illegal xcol= nxcol=%d",nxcol);
+  nmax = nemo_file_lines(inname,getiparam("nmax"));
+  if (nmax<0) error("Error opening %s",inname);
+  if (nmax==0) error("No data?");
+  instr = stropen (inname,"r");
+  
+  ncols = nemoinpi(getparam("xcol"),colnr,MAXCOL);
+  if (ncols<0) error("Illegal xcol= ncol=%d",ncols);
+  
+  for (i=0; i<ncols; i++)
+    coldat[i] =  (real *) allocate(nmax * sizeof(real));
 }
 
-
-read_data()
-{
-    real *coldat[2*MAXCOL+1];
-    int colnr[2*MAXCOL+1], ncols = 0, i, j, ntot=0;
+/* 
+ * read a number of columns, and return number read, 0 for end of file
+ */
 
-    for (i=0; i<nxcol; i++) {
-        coldat[ncols] = xcol[i].dat = (real *) allocate(nmax * sizeof(real));
-        colnr[ncols] = xcolnr[i];        
-        ncols++;
+int read_data(void)
+{
+  int i;
+
+  npt = get_atable(instr,ncols,colnr,coldat,nmax);
+  if (npt < 0) {
+    npt = -npt;
+  } 
+  ntot += npt;
+  dprintf(0,".",ntot);
+  return npt;
+}
+
+/*
+ * do some work, notice all variables are essentially global
+ * not the worlds best style of programming 
+ */
+
+void process_data(void)
+{
+  int i, j;
+
+  for (i=0; i<ncols; i++) {   /* loop over all col's */
+    for (j=0; j<npt; j++) {   /* loop over all row's */
+      sum += coldat[i][j];  /* just sum the data   */
+      dprintf(1,"%d %d %g\n",i,j,coldat[i][j]);
     }
-
-    for (;;) {
-      npt = get_atable(instr,ncols,colnr,coldat,nmax);
-      if (npt < 0) {
-        npt = -npt;
-      } else if (npt==0) 
-	break;
-      ntot += npt;
-      dprintf(0,"Processed %d\n",ntot);
-    } 
-	
+  }
 }
 
