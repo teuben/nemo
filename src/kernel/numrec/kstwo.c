4cb812d9dc9bc9282a23ed9328f49796b9f2c68c
Peter Teuben <teuben@gmail.com>
2003-08-11 00:29:55 +0000

    indexing problem... the usual

--- src/kernel/numrec/kstwo.c
+++ src/kernel/numrec/kstwo.c
@@ -18,8 +18,8 @@ void kstwo(real data1[], unsigned long n1, real data2[], unsigned long n2,
         en2=n2;
         *d=0.0;
         while (j1 < n1 && j2 < n2) {
-                if ((d1=data1[j1]) <= (d2=data2[j2])) fn1=j1++/en1;
-                if (d2 <= d1) fn2=j2++/en2;
+                if ((d1=data1[j1]) <= (d2=data2[j2])) fn1=(++j1)/en1;
+                if (d2 <= d1) fn2=(++j2)/en2;
                 if ((dt=fabs(fn2-fn1)) > *d) *d=dt;
         }
         en=sqrt(en1*en2/(en1+en2));
