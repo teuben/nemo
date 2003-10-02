94aca157831fec280f6ed6dd8e68d8a1c7757218
Peter Teuben <teuben@gmail.com>
2003-10-02 20:59:05 +0000

    add cumulatives, and switch array order

--- src/tutor/table/mktable.c
+++ src/tutor/table/mktable.c
@@ -1,4 +1,4 @@
-/*   MKTABLE:    create an interesting table with simple C math
+/*   MKTABLE:    create an interesting table 
  *
  *   compile and link with e.g.
  *
@@ -10,33 +10,65 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MAXCOL      16
+#define MAXCOL       6
 #define MAXROW    1000
-#define MAXLINELEN 256
+
+/*
+ * normalize all elements from an array to the array length
+ */
+
+void normalize(double *a, int n)
+{
+  int i;
+
+  for (i=0; i<n; i++)
+    a[i] /= n;            /* same as:    a[i] = a[i]/n   */
+}
+
+/*
+ *  start of main program
+ */
 
 int main(int argc, char *argv[])
 {
-  int i, j, nxcol, npt;
-  int do_sqrt, do_log, do_exp;
-  double x[MAXROW], y[MAXROW][MAXCOL];
+  int i, j, npt;
+  double x[MAXROW], y[MAXCOL][MAXROW], sum0, sum1, sum2;
 
-  npt = 100;
-  do_sqrt = 1;
-  do_log  = 1;
-  do_exp  = 1;
+  npt = 100;         /* set the number of points */
 
-  for (i=0; i<npt; i++) {
-    x[i] = (double)i / (double)npt;
-    y[i][0] = sqrt(x[i]);
-    y[i][1] = log(x[i]);
-    y[i][2] = exp(x[i]);
+  if (npt >= MAXROW) {      /* make sure program has enough array space */
+    printf("npt=%d too large -or- MAXROW=%d too small\n",npt,MAXROW);
+    exit(1);
   }
 
+  sum0 = sum1 = sum2 = 0.0;         /* set to 0 */
 
+  for (i=0; i<npt; i++) {               /* compute and accumulate */
+    x[i] = (double)(i+1) / (double)npt;
+    y[0][i] = sqrt(x[i]);
+    y[1][i] = log(x[i]);
+    y[2][i] = exp(x[i]);
+    sum0 += y[0][i];
+    sum1 += y[1][i];
+    sum2 += y[2][i];
+    y[3][i] = sum0;
+    y[4][i] = sum1;
+    y[5][i] = sum2;
 
-  for (i=0; i<npt; i++)
-    printf("%g %g %g %g\n", x[i], y[i][0], y[i][1], y[i][2]);
+  }
 
+  normalize(y[3],npt);             /* normalize the cumulatives */
+  normalize(y[4],npt);
+  normalize(y[5],npt);
+
+  for (i=0; i<npt; i++) {                              /* print out */
+    printf("%g", x[i]);                                        /* x */
+    printf(" %g %g %g", y[0][i], y[1][i], y[2][i]);    /* functions */
+    printf(" %g %g %g", y[3][i], y[4][i], y[5][i]);  /* cumulatives */
+    printf("\n");
+  }
+
+  return 0;     /* must return an 'int' */
 
 }
 
