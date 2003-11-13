0c0e8365e452034889d4d3ddbd50f93f847e4b7e
Peter Teuben <teuben@gmail.com>
2003-11-13 23:22:12 +0000

    improved

--- src/tutor/table/tabread.c
+++ src/tutor/table/tabread.c
@@ -1,17 +1,15 @@
-/*
- * TABREAD: read a table, for the tutorial
+/*   TABREAD: read an ASCII table in NEMO , for the tutorial
  * 
  *      17-sep-2003     created                    PJT     
- *
  */
 
 #include <nemo.h>
 
-string defv[] = {                /* DEFAULT INPUT PARAMETERS */
-    "in=???\n            Input file name (table)",
-    "xcol=1,2\n		 x coordinate column(s) to read",
-    "nmax=100000\n       Hardcoded allocation space, if needed for piped data",
-    "VERSION=1.0\n	 17-sep-03 PJT",
+string defv[] = {
+    "in=???\n         Input file name (table)",
+    "xcol=1,2\n	      x coordinate column(s) to read (1=first)",
+    "nmax=100000\n    Hardcoded allocation space, for piped data",
+    "VERSION=1.0\n    19-sep-03 PJT",
     NULL
 };
 
@@ -26,34 +24,31 @@ nemo_main()
   string input;
   stream instr;
 
-  input = getparam("in");                  /* input table filename */
-
-  nmax = nemo_file_lines(input,getiparam("nmax"));
+  input = getparam("in");                             /* input table filename */
+  nmax = nemo_file_lines(input,getiparam("nmax"));             /* count lines */
   dprintf(0,"Allocated %d lines for table\n",nmax);
-  instr = stropen(input,"r");
 
-  nxcol = nemoinpi(getparam("xcol"),colnr,MAXCOL);
+  nxcol = nemoinpi(getparam("xcol"),colnr,MAXCOL);   /* colum numbers to read */
   if (nxcol < 1) error("Error parsing xcol=%s",getparam("xcol"));
 
   dprintf(0,"Reading %d column(s): ",nxcol);
-  for (j=0; j<nxcol; j++) {
+  for (j=0; j<nxcol; j++) {          /* loop allocating space for each column */
     dprintf(0," %d",colnr[j]);
     coldat[j] = (real *) allocate(sizeof(real)*nmax);
   }
   dprintf(0,"\n");
 
-  npt = get_atable(instr,nxcol,colnr,coldat,nmax);    /* get data */
+  instr = stropen(input,"r");                                    /* open file */
+  npt = get_atable(instr,nxcol,colnr,coldat,nmax);                /* get data */
   if (npt < 0) {
     npt = -npt;
     warning("Could only read first set of %d data",npt);
   }
   dprintf(0,"Found %d lines in table\n",npt);
   
-  /* find global min and max in all data */
-
-  xmin = xmax =  coldat[0][0];
-  for (i=0; i<npt; i++) {     
-    for (j=0; j<nxcol; j++) {
+  xmin = xmax =  coldat[0][0];          /* set the min/max to the first data */
+  for (j=0; j<nxcol; j++) {                        /*  loop over all columns */
+    for (i=0; i<npt; i++) {                      /* and loop over all points */
       xmax=MAX(coldat[j][i],xmax);
       xmin=MIN(coldat[j][i],xmin);
     }
