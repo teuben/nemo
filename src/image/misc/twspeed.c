540a839fb2057c153159e9c3a53ffcc743e2ea3f
Peter Teuben <teuben@gmail.com>
2003-11-18 14:38:25 +0000

    new

---
+++ src/image/misc/twspeed.c
@@ -0,0 +1,92 @@
+/* 
+ * TWSPEED
+ *
+ *	30-mar-03  V1.0 resurrected from pspeed as twspeed
+ *
+OLD::
+./apus1/teuben/nemo/usr/pjt/image/pspeed.c
+./apus1/teuben/mars/teuben/d/image/pspeed.c
+./apus1/nemo/usr/pjt/image/pspeed.c
+ */
+
+#include <stdinc.h>
+#include <getparam.h>
+#include <filestruct.h>
+#include <image.h>
+#include <moment.h> 
+
+string defv[] = {
+    "vel=???\n       Input mean velocity image",
+    "xin=\n       Input mean position image",
+    "den=???\n       Input density image",
+    "pa=90\n         *** Position Angle of disk",
+    "center=\n       *** Center of galaxy (map-center)",
+    "step=1\n        Slit width in pixels",
+    "VERSION=1.0\n   30-mar-03 PJT",
+    NULL,
+};
+
+string usage="TW pattern speed of an object";
+
+stream  vinstr, xinstr, instr;
+
+imageptr viptr=NULL;			/* will be allocated dynamically */
+imageptr xiptr=NULL;			/* will be allocated dynamically */
+imageptr iptr=NULL;			/* will be allocated dynamically */
+
+
+int    nx,ny,nz,nsize;			/* actual size of map */
+double xmin,ymin,zmin,dx,dy,dz;
+double size;				/* size of frame (square) */
+double cell;				/* cell or pixel size (square) */
+
+
+
+nemo_main()
+{
+  int  i, j, k, dir;
+  real sum0, sum1, sum2, sum11,sum00, xmean;
+
+    
+  vinstr = stropen (getparam("vel"), "r");         /* velocity map */
+  read_image (vinstr,&viptr);
+  strclose(vinstr);
+
+  if (hasvalue("xin")) {
+    xinstr = stropen (getparam("xin"), "r");      /* optional Xmean map */
+    read_image (xinstr,&xiptr);
+    strclose(xinstr);
+  } else
+    xinstr = NULL;
+  
+  instr = stropen (getparam("den"), "r");         /* density map */
+  read_image (instr,&iptr);
+  strclose(instr);
+
+  
+  nx = Nx(iptr);	
+  ny = Ny(iptr);
+  xmin = Xmin(iptr);
+  ymin = Ymin(iptr);
+  dx = Dx(iptr);
+  dy = Dy(iptr);
+  
+  for (j=0; j<ny; j++) {     /* loop over all lines parallel to the major axis */
+    sum0 = sum1 = sum2 = sum11 = sum00 = 0;
+    for (i=0; i<nx; i++) {
+      sum00 += MapValue(iptr,i,j);
+      sum11 += (xmin + i*dx) * MapValue(iptr,i,j);
+    }
+    xmean = sum11/sum00;
+    for (i=0; i<nx; i++) {          /* integrate TW quantaties */
+      sum0 += MapValue(iptr,i,j);     /* integrate DEN map */
+      sum1 += MapValue(viptr,i,j);    /* integrate VEL map */
+      if (xiptr)
+	sum2 += MapValue(xiptr,i,j);    /* compute on the fly from 'iptr' map */
+      else
+	sum2 += xmean;
+    }
+    if (sum0 != 0)
+      printf("%d %g %g %g     %g %g\n",j,sum0,sum1,sum2,sum1/sum0,sum2/sum0);
+  }
+}
