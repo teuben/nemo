1e85e99dca7fab35848c4b10a56846f11651c198
Peter Teuben <teuben@gmail.com>
2003-12-06 17:15:08 +0000

    made it official ansi

--- src/tutor/hello/bye.c
+++ src/tutor/hello/bye.c
@@ -1,14 +1,16 @@
 /*
  *	a very simple (ANSI) C program, no NEMO interface
+ *      ANSI C requires main() to be of type int !!!
  */
 
 #include <stdio.h>
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int i;
 
-    for (i=0; i<argc; i++)
+    for (i=0; i<argc; i++)      /* report all command line arguments */
         printf("%s ",argv[i]);
-    printf("\nBye.\n");
+    printf("\nBye.\n");         /* bye */
+    return 0;                   /* note return is NOT a function */
 }
