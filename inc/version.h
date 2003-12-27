4d29c5a3db96aef3de3e036789f42f49716a6eb7
Peter Teuben <teuben@gmail.com>
2003-12-27 17:26:30 +0000

    new version

--- inc/version.h
+++ inc/version.h
@@ -1,7 +1,5 @@
 /* do not edit - created by the version script */
-
 #if defined(GETPARAM_VERSION_ID)
-static char *nemo_version_h = "$Id$";
+static char *version_h = "$Id$";
 #endif
-
-#define NEMO_VERSION "3.1.1"
+#define NEMO_VERSION "3.1.2"
