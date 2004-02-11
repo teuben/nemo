665fcc29b1c4b8fe4262ab0421a6dd9aaf02d91f
Peter Teuben <teuben@gmail.com>
2004-02-11 05:30:25 +0000

    added reporting

--- src/tools/tcppipe/tcppipe_nemo.c
+++ src/tools/tcppipe/tcppipe_nemo.c
@@ -17,6 +17,8 @@
  *
  *        2001   Original version                  Grischa Weberstaedt
  *  2-jul-2001   Adapted for NEMO                  Peter Teuben
+ * 10-feb-2004   added report= and changed host= to better reflect its use PJT
+ *  
  */ 
 
 #include <nemo.h>
@@ -24,15 +26,17 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/socket.h>
+#include <sys/times.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
 string defv[] = {    /* NEMO's command line keyword, and their defaults */
-  "host=\n                 hostname, only needed in send mode",
+  "host_sender=\n          hostname, only needed in sender mode",
   "port=2801\n             TCP/IP port to communicate with",
   "bufsize=1024\n          buffersize to use",
-  "VERSION=0.3\n           2-jul-01 PJT",
+  "report=f\n              Report I/O speed info (to stderr)",
+  "VERSION=0.4\n           10-feb-04 PJT",
   NULL,
 };
 
@@ -45,7 +49,7 @@ string usage = "create TCP connection to send data from/to pipe";
 
 int do_listen(int port, char *rhost);
 int do_connect(int port, char *rhost);
-void do_transfer(int from, int to, int buffer_size);
+void do_transfer(int from, int to, int buffer_size, bool Qreport, int port);
 int get_ip(char *string, struct in_addr *ip);
 
 int nemo_main() {
@@ -54,11 +58,12 @@ int nemo_main() {
       port = getiparam("port"),
       buffer_size = getiparam("bufsize");
     string rhost = NULL;
+    bool Qreport = getbparam("report");
 
-    if (hasvalue("host")) {
+    if (hasvalue("host_sender")) {
       mode = CONNECT;
       direction = RECEIVE;
-      rhost = getparam("host");
+      rhost = getparam("host_sender");
     } else {
       mode = LISTEN;
       direction = SEND;
@@ -73,9 +78,9 @@ int nemo_main() {
                         break;
     }
     switch (direction) {
-	case SEND:	do_transfer(STDIN_FILENO, sock, buffer_size);
+        case SEND:	do_transfer(STDIN_FILENO, sock, buffer_size, FALSE, port);
 			break;
-	case RECEIVE:	do_transfer(sock, STDOUT_FILENO, buffer_size);
+        case RECEIVE:	do_transfer(sock, STDOUT_FILENO, buffer_size, Qreport, port);
 			break;
         default:	error("bad direction");
 			break;
@@ -118,19 +123,35 @@ int do_connect(int port, char *rhost) {
     return data_socket;
 }
 
-void do_transfer(int from, int to, int buffer_size) {
+void do_transfer(int from, int to, int buffer_size, bool Qreport, int port) {
     char *buffer;
     int used_length;
+    clock_t ct0, ct1;
+    int n = 0;
+    struct tms buf;
+    long clk_tck;
 
     if ((buffer=malloc(buffer_size)) == NULL) error("malloc-3");
-    while (used_length=read(from, buffer, buffer_size)) {
+    if (Qreport) ct0 = times(&buf);
+    while ( (used_length=read(from, buffer, buffer_size)) ) {
 	int written_length=0;
 	if (used_length == -1) error("read-3");
 	while (used_length - written_length > 0) {
 	    int write_ret;
-	    if ((write_ret=write(to, buffer+written_length, used_length-written_length)) == -1) error("write-3");
+	    if ((write_ret=write(to, buffer+written_length, used_length-written_length)) == -1) 
+	      error("write-3");
 	    written_length += write_ret;
 	}
+	n += used_length;
+    }
+    if (Qreport) {
+      double r1, r2;
+      ct1 = times(&buf);
+      clk_tck = sysconf(_SC_CLK_TCK);
+      r1 = (double)n / (double)(ct1-ct0);
+      r2 = r1/1024/1024*clk_tck;
+      dprintf(0,"tcppipe(port %d) : %d bytes/ %ld ticks => %g bytes/clock_tick = %g MB/sec\n",
+	      port,n,ct1-ct0,r1,r2);
     }
 }
 
