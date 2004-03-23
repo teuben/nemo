e990ff0fdd14bcffea7f32668840d127d0868811
Peter Teuben <teuben@gmail.com>
2004-03-23 14:57:34 +0000

    oops, forgotten header

---
+++ usr/boily/magalie/params.h
@@ -0,0 +1,8 @@
+
+        INTEGER nbodsmax,maxtabh,maxtab,mdisc, Nd
+
+c++ Reduced maxtab, was 210 ; maxtabh, was 3000 cp buicmb 24.08.2k 
+
+        PARAMETER(mdisc=7000)
+        PARAMETER(nbodsmax=100000,maxtabh=1000,maxtab=105)
+
