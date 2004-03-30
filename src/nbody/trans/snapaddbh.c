dbcb9849d6cef931e2b11534260e3a4a0e4a50f2
Peter Teuben <teuben@gmail.com>
2004-03-30 04:57:41 +0000

    added mode=0 and 1

--- src/nbody/trans/snapaddbh.c
+++ src/nbody/trans/snapaddbh.c
@@ -2,6 +2,10 @@
  * SNAPADDBH.C:    Add a black hole to the center of a snapshot, 
  *                 and modify the velocities of the stars accordingly
  *
+ * mode 0:  the sqrt(M/r) is in the same plane as (r,v) but pure centrifugal
+ * mode 1:  sqrt(M/r) is added to the original velocity the star had
+ * 
+ *
  *      22-mar-04:  after Nelly Mouawad's ideas originally in a modified mkplummer
  */
 
@@ -9,8 +13,9 @@
 #include <getparam.h>
 #include <vectmath.h>
 #include <filestruct.h>
+#include <history.h>
 #include <snapshot/snapshot.h>
-#include <snapshot/barebody.h>
+#include <snapshot/body.h>
 #include <snapshot/get_snap.c>
 #include <snapshot/put_snap.c>
 
@@ -18,31 +23,35 @@ string defv[] = {	                 	/* DEFAULT INPUT PARAMETERS */
     "in=???\n	    Input snapshot file",
     "out=???\n      Output snapshot file",
     "bh=1\n         Mass of the BH to add",
-    "VERSION=1.0\n  22-mar-04 PJT",
+    "mode=0\n       Mode how to add",
+    "VERSION=1.0\n  29-mar-04 PJT",
     NULL,
 };
 
 string usage = "add a black hole to a snapshot";
 
 
-nemo_main()
+local void add_bh(Body *btab, int nbody, real bh, int mode);
+
+void nemo_main(void)
 {
     stream instr, outstr;
     Body *btab = NULL;
-    int i, nbody, bits;
+    int nbody, bits,mode;
     real tsnap, bh;
 
     instr = stropen(getparam("in"), "r");           /* open input file */
     outstr = stropen(getparam("out"), "w");
 
     bh = getdparam("bh");
+    mode = getiparam("mode");
 
     get_history(instr);
     while (get_tag_ok(instr, SnapShotTag)) {
 	get_snap(instr, &btab, &nbody, &tsnap, &bits);
         if (bits & PhaseSpaceBit) {
             dprintf(1,"Processing time %g bits=0x%x\n",tsnap,bits);
-	    add_bh(btab, nbody, bh);
+	    add_bh(btab, nbody, bh, mode);
 	    put_snap(outstr, &btab, &nbody, &tsnap, &bits);
 	}
     }
@@ -50,7 +59,27 @@ nemo_main()
     strclose(outstr);
 }
 
-add_bh(Body *btab, int nbody, real bh)
+local void add_bh(Body *btab, int nbody, real bh, int mode)
 {
-  warning("this doesn't do a thing yet");
+  vector r,v,j,w;
+  real vlen, rlen, factor;
+  Body *bp;
+
+  for (bp=btab; bp<btab+nbody; bp++) {
+    SETV(r,Pos(bp));
+    SETV(v,Vel(bp));
+    CROSSVP(j,r,v);
+    CROSSVP(w,j,r);
+    vlen = absv(v);
+    rlen = absv(r);
+    factor = sqrt(bh/rlen) / (rlen*rlen*vlen);
+    SMULVS(w,factor);
+    if (mode==0) {
+      SETV(Vel(bp),w);
+    } else if (mode == 1) {
+      SADDV(Vel(bp),w);
+    } else {
+      error("Unknown mode %d",mode);
+    }
+  }
 }
