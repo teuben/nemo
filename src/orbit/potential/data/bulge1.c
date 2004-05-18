aa0afd49e2080927795b13812128154861d7fd43
Peter Teuben <teuben@gmail.com>
2004-05-18 17:20:35 +0000

    float/double version

--- src/orbit/potential/data/bulge1.c
+++ src/orbit/potential/data/bulge1.c
@@ -1,6 +1,8 @@
 /*
  * bulge1:	homogeneous bulge with certain axis ratio
  *
+ *        18-may-04    made a float/double, for Alan Peyaud    PJT
+ *
  */
 
 /*CTEX
@@ -20,10 +22,7 @@ static double ecc, rbul2;
 
 #define G  1.0
 
-void inipotential (npar, par, name)
-int    *npar;
-double par[];
-char *name;
+void inipotential (int *npar, double *par, char *name)
 {
     int i;
 
@@ -54,9 +53,7 @@ char *name;
 }
 
 
-void potential (ndim,pos,acc,pot,time)
-int    *ndim;
-double pos[], acc[], *pot,*time;
+void potential_double (int *ndim, double *pos,double *acc,double *pot,double *time)
 {
     int    i;
     double r,x,f;
@@ -66,7 +63,7 @@ double pos[], acc[], *pot,*time;
     dprintf(3,"r = %f",sqrt(r));
     if (ecc>0.0) {
         if (*ndim>2 && pos[2] != 0.0) 
-    	    warning("bulge1	doesn't work outside xy-plane");
+    	    warning("bulge1_double: doesn't work outside xy-plane");
         r = sqrt(r);
         x = ecc / MAX(1.0, r/rbul);
         f = r * 3*G*bumass * (asin(x)-x*sqrt(1-x*x)) /
@@ -90,4 +87,36 @@ double pos[], acc[], *pot,*time;
     dprintf(3," f = %f acc=%f %f %f\n",f,acc[0],acc[1],acc[2]);
     
 }
-
+void potential_float (int *ndim, float *pos, float *acc, float *pot, float *time)
+{
+    int    i;
+    float  r,x,f;
+        
+    for (i=0, r=0; i<*ndim; i++)    /* find radius */
+        r += sqr(pos[i]);
+    dprintf(3,"r = %f",sqrt(r));
+    if (ecc>0.0) {
+        if (*ndim>2 && pos[2] != 0.0) 
+    	    warning("bulge1_float   doesn't work outside xy-plane");
+        r = sqrt(r);
+        x = ecc / MAX(1.0, r/rbul);
+        f = r * 3*G*bumass * (asin(x)-x*sqrt(1-x*x)) /
+                    (2*rbul*rbul*rbul*ecc*ecc*ecc);
+    } else {
+        if (r>rbul2) {                 /* outside bulge */
+            f = G * bumass / r;        /* Good old Newton */
+            r = sqrt(r);
+        } else {                       /* inside bulge */
+            r = sqrt(r);
+            f = G * bumass / (rbul*rbul*rbul) * r;
+        }
+    }
+    if (r>0.0) {
+        for (i=0; i<*ndim; i++) 
+            acc[i] = -(pos[i]/r) * f;        /* radial force to cartesian */
+    } else 
+        for (i=0; i<*ndim; i++) 
+            acc[i] = 0.0;
+    *pot = 0.0;
+    dprintf(3," f = %f acc=%f %f %f\n",f,acc[0],acc[1],acc[2]);
+}
