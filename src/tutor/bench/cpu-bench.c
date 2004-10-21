e4e1b69adc28c3553d87d564858895803d3ff612
Peter Teuben <teuben@gmail.com>
2004-10-21 00:08:07 +0000

    updates

--- src/tutor/bench/cpu-bench.c
+++ src/tutor/bench/cpu-bench.c
@@ -5,7 +5,7 @@
  */
 
 #include <nemo.h>
-
+#include <timers.h>
 
 typedef int (*action)(int);
 
@@ -72,7 +72,7 @@ string defv[] = {
   "n=30000\n         max size of arrays",
   "vlen=0,3,10,30,100,300,1000,3000,30000\n     Vector lengths to try",
   "count=300000\n    how often shoudl the largest vlen be run",
-  "VERSION=3.0\n     4-apr-2004 PJT",
+  "VERSION=3.1\n     24-apr-2004 PJT",
   NULL,
 };
 
@@ -179,7 +179,7 @@ int dummy(int n)
   return n;
 }
 
-void time_action(action a, int factor, char *name, int ntmax)
+void new_time_action(action a, int factor, char *name, int ntmax)
 {
   static int ntest=MAXTEST;
   real dtime0, dtime[MAXTEST], t_start, t_end;
@@ -213,16 +213,28 @@ void time_action(action a, int factor, char *name, int ntmax)
     real sum;
 
     t_start = cpu_time();
+    stamp_timers(i);
     for (j = 0; j < count[i]; j++) {
       s1 = 1./s1;
       a(vlen[i]);
     }
+    stamp_timers(i+1);
     t_end = cpu_time();
     dtime[i] = (t_end - t_start) / count[i] - dtime0;
-
-    printf("vlen = %6d, time = %.3e, time/loop = %.3e, Mflops = %.4f\n",
+#if 1
+    printf("vlen = %6d, time = %.3e, time/loop = %.3e, Mflops = %.4f Ticks = %Ld\n",
 	   vlen[i], t_end - t_start, dtime[i],
-	   vlen[i]*1.0e-6 / dtime[i] * factor);
+	   vlen[i]*1.0e-6 / dtime[i] * factor,
+	   diff_timers(i,i+1));
+#endif
+  } /* i */
+  
+  for (i = 0; i <= maxtst; i++) {
+    printf("vlen = %6d,  time/loop = %.3e, Mflops = %.4f Ticks = %Ld\n",
+	   vlen[i], dtime[i],
+	   vlen[i]*1.0e-6 / dtime[i] * factor,
+	   diff_timers(i,i+1));
+
   }
 
   printf("function overhead = %.3e/call\n", dtime0);
@@ -231,7 +243,119 @@ void time_action(action a, int factor, char *name, int ntmax)
 
 }
 
+void time_action(action a, int factor, char *name, int ntmax)
+{
+  static int ntest=MAXTEST;
+  real dtime0, dtime[MAXTEST], t_start, t_end;
+  int i, j, maxtst;
+
+  int vlen[MAXTEST]  = {0, 3, 10, 30, 100, 300, 1000, 3000, 30000, 300000, 3000000};
+  int count[MAXTEST] = {10000000, 3000000, 1000000, 300000, 100000,
+		      30000, 10000, 3000, 300, 30, 3};
+
+  printf("\n%s\n", name);
+
+  if (ntest==0) {
+    error("new code");
+    ntest = nemoinpi(getparam("vlen"),vlen,MAXTEST);
+    if (ntest<=0) error("parsing");
+    
+  }
+
+  for (i = 0; i < ntest; i++)
+    if (vlen[i] <= ntmax) maxtst = i;
+
+  /* Function call timing. */
+
+  i = 0;
+  t_start = cpu_time();
+  for (j = 0; j < count[i]; j++) dummy(vlen[i]);
+  t_end = cpu_time();
+  dtime0 = (t_end - t_start) / count[i];
+
+  for (i = 0; i <= maxtst; i++) {
+    real sum;
+
+    stamp_timers(i);
+    for (j = 0; j < count[i]; j++) {
+      s1 = 1./s1;
+      a(vlen[i]);
+    }
+    stamp_timers(i+1);
+  } /* i */
+
+  for (i = 0; i <= maxtst; i++) {
+    printf("vlen = %6d,  Ticks = %Ld\n",
+	   vlen[i], diff_timers(i,i+1));
+
+  }
+
+
+}  
+
+void old_time_action(action a, int factor, char *name, int ntmax)
+{
+  static int ntest=MAXTEST;
+  real dtime0, dtime[MAXTEST], t_start, t_end;
+  int i, j, maxtst;
+
+  int vlen[MAXTEST]  = {0, 3, 10, 30, 100, 300, 1000, 3000, 30000, 300000, 3000000};
+  int count[MAXTEST] = {10000000, 3000000, 1000000, 300000, 100000,
+		      30000, 10000, 3000, 300, 30, 3};
+
+  printf("\n%s\n", name);
+
+  if (ntest==0) {
+    error("new code");
+    ntest = nemoinpi(getparam("vlen"),vlen,MAXTEST);
+    if (ntest<=0) error("parsing");
+    
+  }
+
+  for (i = 0; i < ntest; i++)
+    if (vlen[i] <= ntmax) maxtst = i;
+
+  /* Function call timing. */
+
+  i = 0;
+  t_start = cpu_time();
+  for (j = 0; j < count[i]; j++) dummy(vlen[i]);
+  t_end = cpu_time();
+  dtime0 = (t_end - t_start) / count[i];
+
+  for (i = 0; i <= maxtst; i++) {
+    real sum;
+
+    t_start = cpu_time();
+    stamp_timers(i);
+    for (j = 0; j < count[i]; j++) {
+      s1 = 1./s1;
+      a(vlen[i]);
+    }
+    stamp_timers(i+1);
+    t_end = cpu_time();
+    dtime[i] = (t_end - t_start) / count[i] - dtime0;
+#if 1
+    printf("vlen = %6d, time = %.3e, time/loop = %.3e, Mflops = %.4f Ticks = %Ld\n",
+	   vlen[i], t_end - t_start, dtime[i],
+	   vlen[i]*1.0e-6 / dtime[i] * factor,
+	   diff_timers(i,i+1));
+#endif
+  } /* i */
+  
+  for (i = 0; i <= maxtst; i++) {
+    printf("vlen = %6d,  Ticks = %Ld\n",
+	   vlen[i], diff_timers(i,i+1));
+
+  }
+
+
+  fflush(stdout);
+
+}
+
 int do_test(a,b) {
+  stamp_timers(b);
   if (a==0 || b==0) return 1;
   if (a==b) return 1;
   return 0;
@@ -245,7 +369,7 @@ int do_test(a,b) {
 
 nemo_main()
 {
-    int n = NMAX;
+    int i, n = NMAX;
     int test = getiparam("test");
 
     if (hasvalue("n")) n = getiparam("n");
@@ -257,57 +381,62 @@ nemo_main()
 #endif
 
     initialize(n);
-
+    init_timers(200);
+    stamp_timers(0);
     if (do_test(test,1)) time_action(itor, 1, "vr = vi", n);
     if (do_test(test,2)) time_action(rtoi, 1, "vi = vr", n);
     if (do_test(test,3)) time_action(iadd, 1, "vi1 = vi2 + vi3", n);
 
     if (do_test(test,4)) time_action(vmove, 1, "v1 = v2", n);
 
-    s1 = 1.0; if (do_test(test,4)) time_action(ssum1, 1, "s += v", n);
-    s1 = 1.0; if (do_test(test,5)) time_action(ssum2, 2, "s += v + v", n);
-    s1 = 1.0; if (do_test(test,6)) time_action(ssum3, 2, "s += v * v", n);
+    s1 = 1.0; if (do_test(test,5)) time_action(ssum1, 1, "s += v", n);
+    s1 = 1.0; if (do_test(test,6)) time_action(ssum2, 2, "s += v + v", n);
+    s1 = 1.0; if (do_test(test,7)) time_action(ssum3, 2, "s += v * v", n);
 
     s1 = 1.00000123;
     reset_v1(n);
-    if (do_test(test,7)) time_action(vsadd1, 1, "v += s", n);
+    if (do_test(test,8)) time_action(vsadd1, 1, "v += s", n);
     reset_v1(n);
-    if (do_test(test,8)) time_action(vsmul1, 1, "v *= s", n);
+    if (do_test(test,9)) time_action(vsmul1, 1, "v *= s", n);
 
     reset_v1(n);
-    if (do_test(test,9)) time_action(vsmul1a, 1, "v *= s (partly unrolled)", n);
-    if (do_test(test,10)) time_action(vsadd2, 1, "v1 = v2 + s", n);
-    if (do_test(test,11)) time_action(vsmul2, 1, "v1 = v2 * s", n);
-    if (do_test(test,12)) time_action(vsdiv2, 1, "v1 = v2 / s", n);
+    if (do_test(test,10)) time_action(vsmul1a, 1, "v *= s (partly unrolled)", n);
+    if (do_test(test,11)) time_action(vsadd2, 1, "v1 = v2 + s", n);
+    if (do_test(test,12)) time_action(vsmul2, 1, "v1 = v2 * s", n);
+    if (do_test(test,13)) time_action(vsdiv2, 1, "v1 = v2 / s", n);
 
     reset_v1(n);
-    if (do_test(test,13)) time_action(vsum1, 1, "v1 += v2", n);
-    if (do_test(test,14)) time_action(vsum2, 1, "v1 = v2 + v3", n);
+    if (do_test(test,14)) time_action(vsum1, 1, "v1 += v2", n);
+    if (do_test(test,15)) time_action(vsum2, 1, "v1 = v2 + v3", n);
 
     reset_v1(n);
-    if (do_test(test,15)) time_action(vmul1, 1, "v1 *= v2", n);
-    if (do_test(test,16)) time_action(vmul2, 1, "v1 = v2 * v3", n);
+    if (do_test(test,16)) time_action(vmul1, 1, "v1 *= v2", n);
+    if (do_test(test,17)) time_action(vmul2, 1, "v1 = v2 * v3", n);
     reset_v1(n);
-    if (do_test(test,17)) time_action(vdiv1, 1, "v1 /= v2", n);
-    if (do_test(test,18)) time_action(vdiv2, 1, "v1 = v3 / v2", n);
+    if (do_test(test,18)) time_action(vdiv1, 1, "v1 /= v2", n);
+    if (do_test(test,19)) time_action(vdiv2, 1, "v1 = v3 / v2", n);
 
 /*     {int i; for (i = 0; i < n; i+=1000) printf("%d %f\n", i, v1[i]);} */
 
-    if (do_test(test,19)) time_action(saxpy1, 2, "v1 = s*v2 + s", n);
-    if (do_test(test,20)) time_action(saxpy2, 2, "v1 = s*v2 + v3", n);
-    if (do_test(test,21)) time_action(saxpy3, 2, "v1 = v2*v3 + v4", n);
-    if (do_test(test,22)) time_action(vforce, 18, "v1 = pseudo_force(v2, v3, v4)", n);
+    if (do_test(test,20)) time_action(saxpy1, 2, "v1 = s*v2 + s", n);
+    if (do_test(test,21)) time_action(saxpy2, 2, "v1 = s*v2 + v3", n);
+    if (do_test(test,22)) time_action(saxpy3, 2, "v1 = v2*v3 + v4", n);
+    if (do_test(test,23)) time_action(vforce, 18, "v1 = pseudo_force(v2, v3, v4)", n);
 
-    if (do_test(test,23)) time_action(vsqrt, 1, "v1 = sqrt(v2)", n);
-    if (do_test(test,24)) time_action(vabs, 1, "v1 = abs(v2)", n);
-    if (do_test(test,25)) time_action(vsin, 1, "v1 = sin(v2)", n);
-    if (do_test(test,26)) time_action(vexp, 1, "v1 = exp(v2)", n);
-    if (do_test(test,27)) time_action(vpow, 1, "v1 = pow(v2, s)", n);
+    if (do_test(test,24)) time_action(vsqrt, 1, "v1 = sqrt(v2)", n);
+    if (do_test(test,25)) time_action(vabs, 1, "v1 = abs(v2)", n);
+    if (do_test(test,26)) time_action(vsin, 1, "v1 = sin(v2)", n);
+    if (do_test(test,27)) time_action(vexp, 1, "v1 = exp(v2)", n);
+    if (do_test(test,28)) time_action(vpow, 1, "v1 = pow(v2, s)", n);
 
-    if (do_test(test,28)) time_action(scatter1, 1, "v1[iv] = v2", n);
-    if (do_test(test,29)) time_action(scatter2, 1, "v1[iv] = v2", n);
-    if (do_test(test,30)) time_action(gather1, 1, "v1 = v2[iv]", n);
-    if (do_test(test,31)) time_action(gather2, 1, "v1 = v2[iv]", n);
-
-    if (do_test(test,32)) time_action(vif, 1, "if (v2 > 0) v1 = v2", n);
+    if (do_test(test,29)) time_action(scatter1, 1, "v1[iv] = v2", n);
+    if (do_test(test,30)) time_action(scatter2, 1, "v1[iv] = v2", n);
+    if (do_test(test,31)) time_action(gather1, 1, "v1 = v2[iv]", n);
+    if (do_test(test,32)) time_action(gather2, 1, "v1 = v2[iv]", n);
+    if (do_test(test,33)) time_action(vif, 1, "if (v2 > 0) v1 = v2", n);
+    if (test)
+        printf("Ticks: %Ld\n",diff_timers(test,test+1));
+    else
+	for (i=0; i<33; i++)
+	    printf("Ticks %d %Ld:\n",i+1,diff_timers(i,i+1));
 }
