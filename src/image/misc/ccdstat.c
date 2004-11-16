97d2a3b6696a012c5b3bfc9c72999735cda58d1d
Peter Teuben <teuben@gmail.com>
2004-11-16 04:08:47 +0000

    1 too many

--- src/image/misc/ccdstat.c
+++ src/image/misc/ccdstat.c
@@ -92,7 +92,7 @@ nemo_main()
       }
     }
     if (npar > 0) {
-      nu = n_moment(&m)/nppb - npar - 1;
+      nu = n_moment(&m)/nppb - npar;
       if (nu < 1) error("%g: No degrees of freedom",nu);
       printf("chi2= %g\n", show_moment(&m,2)/nu/nppb);
       printf("df= %g\n", nu);
