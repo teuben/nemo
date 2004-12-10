5c6fe0375b718c35498e0b4a6f490f0e3423d2e6
Jean Charles Lambert <jcl@oamp.fr>
2004-12-10 10:26:51 +0000

    About form

---
+++ src/nbody/glnemo/src/about_form.ui.h
@@ -0,0 +1,25 @@
+// ============================================================================
+// Copyright Jean-Charles LAMBERT - 2004                                       
+// e-mail:   Jean-Charles.Lambert@oamp.fr                                      
+// address:  Dynamique des galaxies                                            
+//           Laboratoire d'Astrophysique de Marseille                          
+//           2, place Le Verrier                                               
+//           13248 Marseille Cedex 4, France                                   
+//           CNRS U.M.R 6110                                                   
+// ============================================================================
+// See the complete license in LICENSE and/or "http://www.cecill.info".        
+// ============================================================================
+#include "images/glnemo35.xpm"
+
+// Automatically called by constructor
+void AboutForm::init()
+{
+    // Form's icon
+    setIcon( QPixmap( glnemo35_xpm ) );
+    // glnemo's pixmap
+    pixmapLabel3->setPixmap( QPixmap( glnemo35_xpm ) );
+}
+// Automatically called by destructor
+void AboutForm::destroy()
+{
+}
