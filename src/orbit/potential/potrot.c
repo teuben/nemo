41e8c6eb648e8dd76c946fd1531662279da8d077
Peter Teuben <teuben@gmail.com>
2005-02-08 04:44:18 +0000

    working version for Mousumi to try

--- src/orbit/potential/potrot.c
+++ src/orbit/potential/potrot.c
@@ -1,23 +1,26 @@
 /*
  * POTROT: query a potential to derive a rotation curve
  *
- *	Peter Teuben		7-feb-05       Created
+ *	Peter Teuben		7-feb-05       cloned off potq
  */
 
 #include <stdinc.h>
 #include <getparam.h>
 #include <potential.h>
 #include <vectmath.h>
+#include <moment.h>
 
 string defv[] = {
     "potname=???\n  Name of potential",
     "potpars=\n     Parameters for potential (1st one is pattern speed)",
     "potfile=\n     Any optional data file associated with potential",
     "r=0:2:0.1\n    Radii to sample",
-    "p=0\n          Angles to sample",
+    "p=0\n          Angles to sample (degrees)",
     "t=0.0\n        Time to test potential at, if relevant",
     "omega=\n       Use this instead of any returned pattern speed",
     "format=%g\n    Format used to print numbers",
+    "niter=10\n     Iteration max",
+    "eps=0.001\n    Accuracy",
     "VERSION=0.1\n  7-feb-05 PJT",
     NULL,
 };
@@ -33,6 +36,7 @@ string cvsid = "$Id$";
 local potproc_double mypotd;     /* pointer to potential calculator function : double */
 local potproc_float  mypotf;     /* pointer to potential calculator function : float */
 local void do_potential(bool,int *, double *, double *, double *, double *);
+local double force (double rad, double phi);
 
 local double report_q(int,double);
 
@@ -40,16 +44,18 @@ void nemo_main(void)
 {
   int    i, j, k, nr,np, nstep;
   double pos[3],acc[3],pot, time;
-  real   radii[MAXPT],phi[MAXPT],xarr[MAXPT],yarr[MAXPT],zarr[MAXPT];
-  double dr, dphi, phi;
+  real   radii[MAXPT],phis[MAXPT],xarr[MAXPT],yarr[MAXPT],zarr[MAXPT];
+  double dr, dphi, phi, fr, vrot;
   double fourpi = 4*PI;
   double omega, qt, qt0, dq;
   char *fmt, s[20], pfmt[256];
   int niter = getiparam("niter");
   double eps = getdparam("eps");
+  Moment m;
   
   nr = nemoinpr(getparam("r"), radii, MAXPT);  /* get radii */
-  np = nemoinpr(getparam("p"), phi,   MAXPT);  /* get angles */
+  np = nemoinpr(getparam("p"), phis,  MAXPT);  /* get angles */
+  for (i=0; i<np; i++) phis[i] *= PI/180.0;    /* convert to radian */
   
   time = getdparam("t");
   
@@ -67,9 +73,27 @@ void nemo_main(void)
   strcpy(s,fmt);    /* use format from command line */
   if (strchr(fmt,' ')==NULL && strchr(fmt,',')==NULL)
     strcat(s," ");      /* append separator if none specified */
-  
+  ini_moment(&m,2,0);
+
+#if 1
   for (i=0;i<nr;i++) {
+    reset_moment(&m);
+    for (j=0; j<np; j++) {
+      fr = force(radii[i],phis[j]);
+      accum_moment(&m,fr,1.0);
+    }
+    fr = mean_moment(&m);
+    vrot = sqrt(-fr*radii[i]);            /* force better be inward bound ! */
+    printf("%g %g %g\n",radii[i],fr,vrot); 
+  }
+#endif
+
+
+#if 0
+  for (i=0;i<nr;i++) {
+
     reset_q();
+
     add_q(radii[i], 0);
     add_q(radii[i], HALF_PI);
     add_q(radii[i], HALF_PI/2.0);
@@ -94,6 +118,69 @@ void nemo_main(void)
     }
     qt = report_q(0,qt0);
   }
+#endif
+
+#if 0
+  for (i=0;i<nr;i++) {
+
+    reset_q();
+
+    add_q(radii[i], 0);
+    add_q(radii[i], HALF_PI);
+    add_q(radii[i], HALF_PI/2.0);
+    qt0 = report_q(1,-1.0);
+    
+    dphi  = HALF_PI/2.0;
+    nstep = 2;
+    for (j=0; j<niter; j++) {
+      for (k=0, phi = dphi/2.0; k<nstep-1; k++, phi += dphi) {
+	add_q(radii[i], phi);
+      }
+      qt0 = qt;
+      qt = report_q(1,qt0);
+      dq = (qt-qt0)/qt;
+      dq = ABS(dq);
+      if (dq < eps && j>5) {
+	// warning("Early convergence after %d iterations",j);
+	break;
+      }
+      nstep *= 2;
+      dphi  /= 2.0;
+    }
+    qt = report_q(0,qt0);
+  }
+#endif
+
+}
+
+/* radial force */
+
+double force (double rad, double phi)
+{
+  double pos[3], acc[3], pot, r, fr, ft, time = 0.0;
+  int ndim = 3;
+
+  pos[0] = rad * cos(phi);
+  pos[1] = rad * sin(phi);
+  pos[2] = 0.0;
+
+  (*mypotd)(&ndim, pos, acc, &pot, &time);
+
+  r  = pos[0]*pos[0] + pos[1]*pos[1];
+  fr = (pos[0]*acc[0] + pos[1]*acc[1])/sqrt(r);
+
+  return fr;
+#if 0
+  ft = sqrt( (acc[0]*acc[0] + acc[1]*acc[1]) - fr*fr);
+  dprintf(2,"r,fr,ft=%g %g %g\n",r,fr,ft);
+
+  sum_fr += fr;
+  nsum++;
+  if (ft > max_ft) max_ft = ft;
+
+  rad_q = rad;
+  iter++;
+#endif
 }
 
 
