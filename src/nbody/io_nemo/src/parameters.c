93161028798b436544f86c03d03cd7bcea3f4599
Jean Charles Lambert <jcl@oamp.fr>
2005-03-14 15:58:18 +0000

    New file Header. cvs release number removed. a lot of code cleaning. happy gcc 3.4

--- src/nbody/io_nemo/src/parameters.c
+++ src/nbody/io_nemo/src/parameters.c
@@ -1,57 +1,65 @@
-/* -------------------------------------------------------------- *\
-|* $Id$
-|*
-|* Manage io_nemo's parameters
-|*
-\* -------------------------------------------------------------- */
+/* =================================================================
+|  Copyright Jean-Charles LAMBERT - 2005                            
+|  e-mail:   Jean-Charles.Lambert@oamp.fr                           
+|  address:  Dynamique des galaxies                                 
+|            Laboratoire d'Astrophysique de Marseille               
+|            2, place Le Verrier                                    
+|            13248 Marseille Cedex 4, France                        
+|            CNRS U.M.R 6110                                        
+| ==================================================================
+|* Manage io_nemo's parameters                                      
+| ==================================================================
+| 03-Mars-05: happy gcc 3.4                                         
++----------------------------------------------------------------- */
 
-/* -------------------------------------------------------------- *\
-|* Standard include files
-\* -------------------------------------------------------------- */
+/* -----------------------------------------------------------------
+|  Standard include files                                           
++----------------------------------------------------------------- */ 
 #include <stdinc.h>
 
-/* -------------------------------------------------------------- *\
-|* Nemo include files
-\* -------------------------------------------------------------- */
+/* -----------------------------------------------------------------
+|  Nemo include files                                               
++----------------------------------------------------------------- */
 #include "parameters.h"
 
 /* extern variables */
 #include "flags_data.h"
 
-/* -------------------------------------------------------------- *\ 
-|* get_case :
-|* Return the code of the fields if it exist otherwise 0.
-\* -------------------------------------------------------------- */
+/* -----------------------------------------------------------------
+|  get_case :                                                       
+|  Return the code of the fields if it exist otherwise 0.           
++----------------------------------------------------------------- */
 int get_case(char * field)
 {
   struct ch_num
     { char * ch;
       int    num;
     } tab_ch_num[] ={
-    "n"     ,   1, "nbody"    ,   1,   /* nbody */
-    "t"     ,   2, "time"     ,   2,   /* time  */
-    "m"     ,   3, "mass"     ,   3,   /* mass  */
-    "x"     ,   4, "pos"      ,   4,   /* pos   */
-    "v"     ,   5, "vel"      ,   5,   /* vel   */
-    "p"     ,   6, "pot"      ,   6,   /* pot   */
-    "a"     ,   7, "acc"      ,   7,   /* acc   */
-    "k"     ,   8, "key"      ,   8,   /* keys  */
-    "xv"    ,   9, "phase"    ,   9,   /* phase */
-    "e"     ,  10, "eps"      ,   10,  /* eps   */
-    "f"     ,  50, "n3"       ,  50,   /* n3    */
-    "c"     ,  51, "3n"       ,  51,   /* 3n    */
-    "s"     ,  52, "save"     ,  52,   /* save  */
-    "r"     ,  53, "read"     ,  53,   /* read  */
-    "single",  54, "float"    ,  54, 
-    "real4" ,  54,                     /* float type         */
-    "double",  55, "double"   ,  55,
-    "real8" ,  55,                     /* double type        */
-    "info"  ,  56, "diag"     ,  56,   /* print information  */
-    "st"    ,  57,                     /* selected time      */
-    "sp"    ,  58,                     /* Selected particles */
-    "h"     ,  59, "history"  ,  59,   /* history            */
-    "close" ,  60,                     /* close              */
-    NULL
+    {"n"     ,   1}, {"nbody"    ,   1},   /* nbody              */
+    {"t"     ,   2}, {"time"     ,   2},   /* time               */
+    {"m"     ,   3}, {"mass"     ,   3},   /* mass               */
+    {"x"     ,   4}, {"pos"      ,   4},   /* pos                */
+    {"v"     ,   5}, {"vel"      ,   5},   /* vel                */
+    {"p"     ,   6}, {"pot"      ,   6},   /* pot                */
+    {"a"     ,   7}, {"acc"      ,   7},   /* acc                */
+    {"k"     ,   8}, {"key"      ,   8},   /* keys               */
+    {"xv"    ,   9}, {"phase"    ,   9},   /* phase              */
+    {"e"     ,  10}, {"eps"      ,  10},   /* eps                */
+    {"b"     ,  11}, {"bits"     ,  11},   /* bits               */
+    {"f"     ,  50}, {"n3"       ,  50},   /* n3                 */
+    {"c"     ,  51}, {"3n"       ,  51},   /* 3n                 */
+    {"s"     ,  52}, {"save"     ,  52},   /* save               */
+    {"r"     ,  53}, {"read"     ,  53},   /* read               */
+    {"single",  54}, {"float"    ,  54},   /* float type         */
+    {"real4" ,  54},                       /* float type         */
+    {"double",  55}, {"double"   ,  55},   /* double type        */
+    {"real8" ,  55},                       /* double type        */
+    {"info"  ,  56}, {"diag"     ,  56},   /* print information  */
+    {"st"    ,  57},                       /* selected time      */
+    {"sp"    ,  58},                       /* Selected particles */
+    {"h"     ,  59}, {"history"  ,  59},   /* history            */
+    {"close" ,  60},                       /* close              */
+    { NULL   ,  -1}
   };   
   struct ch_num * p;
   int find = '\000';
@@ -68,10 +76,10 @@ int get_case(char * field)
   return find;
 }
 
-/* -------------------------------------------------------------- *\ 
-|* get_field :
-|* Return the field between two comma.
-\* -------------------------------------------------------------- */
+/* -----------------------------------------------------------------
+|  get_field :                                                      
+|  Return the field between two comma.                              
++----------------------------------------------------------------- */
 char * get_field(char **p)
 { 
   char * chaine="", * x, * y;
@@ -121,10 +129,10 @@ char * get_field(char **p)
 
   return(chaine);
 }
-/* -------------------------------------------------------------- *\ 
-|* chk_parameters :
-|* Print out selected parameters.
-\* -------------------------------------------------------------- */
+/* -----------------------------------------------------------------
+|  chk_parameters :                                                 
+|  Print out selected parameters.                                   
++----------------------------------------------------------------- */
 int chk_parameters(bool io_op,int size_array, int rtype)
 { 
 
@@ -166,8 +174,10 @@ int chk_parameters(bool io_op,int size_array, int rtype)
   }
   else                  /* 'io_nemo' mode (C)          */
     fprintf(stderr," <%s> ]\n",tab_info_real[rtype-1]);
+
+  return 1;
 }
 
-/* -------------------------------------------------------------- *\
-|* End of parameters.c
-\* -------------------------------------------------------------- */
+/* -----------------------------------------------------------------
+|  End of parameters.c                                              
++----------------------------------------------------------------- */
--- src/nbody/io_nemo/src/parameters.h
+++ src/nbody/io_nemo/src/parameters.h
@@ -1,10 +1,17 @@
-/* --------------------------------------------------------- *\
-|* $Id$
-|*
-|* Functions prototypes
-|*
-\* --------------------------------------------------------- */
+/* =================================================================
+|  Copyright Jean-Charles LAMBERT - 2005                            
+|  e-mail:   Jean-Charles.Lambert@oamp.fr                           
+|  address:  Dynamique des galaxies                                 
+|            Laboratoire d'Astrophysique de Marseille               
+|            2, place Le Verrier                                    
+|            13248 Marseille Cedex 4, France                        
+|            CNRS U.M.R 6110                                        
+| ==================================================================
+|* Functions prototypes                                             
++----------------------------------------------------------------  */
 
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -19,3 +26,8 @@ int chk_parameters(bool ,int , int );
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* PARAMETERS_H */
+/* ----------------------------------------------------------------
+|  End of parameters.h                                             
++---------------------------------------------------------------- */
