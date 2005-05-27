0beb45184b9cf345803ce7e58ff2c113c4b0ac69
Peter Teuben <teuben@gmail.com>
2005-05-27 04:46:20 +0000

    allow dx<0 for log steps

--- src/kernel/tab/tabint.c
+++ src/kernel/tab/tabint.c
@@ -2,6 +2,7 @@
  *
  *
  *      13-may-05    Q&D first version, cloned off tabfilter
+ *      26-may-05    allow step<0 for logsteps (xmin must be > 0)
  */
 
 #include <stdinc.h> 
@@ -12,11 +13,11 @@
 
 string defv[] = {
   "in=???\n         Input table file",
-  "xcol=1\n         Column with X coordinate (better be sorted in that column",
+  "xcol=1\n         Column with X coordinate (must be sorted in that column)",
   "ycol=2\n         Column with Y coordinate of function",
-  "step=\n          Integration step if resampling used",
+  "step=\n          Integration step if resampling used (<0 for logarithmic steps)",
   "normalize=f\n    Normalize integral",
-  "VERSION=0.1\n    13-may-05 PJT",
+  "VERSION=0.3\n    26-may-05 PJT",
   NULL,
 
 };
@@ -26,20 +27,19 @@ string usage="integrate a sorted table";
 string cvsid="$Id$";
 
 
-#define MAXDATA  16384
+extern int minmax(int, real *, real *, real *);
 
-extern string *burststring(string, string);
-extern void freestrings(string *);
-
+
 void nemo_main()
 {
-  int colnr[2], i, n, nmax;
-  real *coldat[2], *xdat, *ydat, xmin, xmax, ymin, ymax;
-  real x, y, xold, yold, dx, sum, sum0, *sdat;
+  int colnr[2], i, n, nmax, nsteps;
+  real *coldat[2], *xdat, *ydat, xmin, xmax, ymin, ymax, zmin, zmax;
+  real x, y, z, s, xold, yold, zold, dx, dz, sum, sum0, *sdat;
   stream instr;
   string spectrum = getparam("in");
   bool Qnorm = getbparam("normalize");
-  
+
+  /* read the data */
   nmax = nemo_file_lines(spectrum,MAXLINES);
   xdat = coldat[0] = (real *) allocate(nmax*sizeof(real));
   ydat = coldat[1] = (real *) allocate(nmax*sizeof(real));
@@ -49,53 +49,64 @@ void nemo_main()
   n = get_atable(instr,2,colnr,coldat,nmax);
   strclose(instr);
   
-  for(i=0; i<n; i++) {
-    dprintf(2,"%g %g\n",xdat[i],ydat[i]);
-    if (i==0) {
-      xmin = xmax = xdat[0];
-      ymin = ymax = ydat[0];
-    } else {
-      if (xdat[i] <= xdat[i-1]) 
-	error("Column %d must be sorted",colnr[0]);
-      xmax = MAX(xmax,xdat[i]);
-      ymax = MAX(ymax,ydat[i]);
-      xmin = MIN(xmin,xdat[i]);
-      ymin = MIN(ymin,ydat[i]);
-    }
-  }
+  /* figure out some min/max */
+  minmax(n,xdat,&xmin,&xmax);
+  minmax(n,ydat,&ymin,&ymax);
   dprintf(1,"X range: %g : %g\n",xmin,xmax);
   dprintf(1,"Y range: %g : %g\n",ymin,ymax);
 
   sum = sum0 = 0.0;
-
-  if (hasvalue("step")) {     /* integration via summation of a resampled function using splines */
+  nsteps = 0;
+  if (hasvalue("step")) {     /* resample the function using splines */
     dx = getdparam("step");
 
     /* setup a spline interpolation table into the function */
     sdat = (real *) allocate(sizeof(real)*n*3);
     spline(sdat,xdat,ydat,n);
-
-    yold = ydat[0];
-    xold = xmin;
-    for (x=xmin+dx; x<xmax; x += dx) {
-      y = seval(x,xdat,ydat,sdat,n);
-      dprintf(1,"%g -> %g\n",x,y);
-
-      sum  += 0.5*(yold+y)*(x-xold);
-      sum0 += (x-xold);
-      yold = y;
-      xold = x;
-    }
-    y = ydat[n-1];
-    sum  += 0.5*(yold+y)*(xmax-xold);
-    sum0 += (xmax-xold);
-  } else {              /* simple summation via the datapoints itself */
+    if (dx > 0) {                        /* dx > 0 : linear steps from xmin->xmax */
+        yold = ydat[0];
+      xold = xmin;
+      for (x=xmin+dx; x<xmax; x += dx) { /* loop over the sampled interval */
+	nsteps++;
+	y = seval(x,xdat,ydat,sdat,n);
+	sum  += 0.5*(yold+y)*(x-xold);
+	sum0 += (x-xold);
+	yold = y;
+	xold = x;
+      }
+      y = ydat[n-1];
+      sum  += 0.5*(yold+y)*(xmax-xold);     /* it's possible we never */
+      sum0 += (xmax-xold);                  /* closed the interval */
+    } else if (xmin > 0 || xmax < 0) {     /* dx < 0: log steps */
+      dz = -dx;
+      s = xmin > 0 ? 1.0 : -1.0;
+      zmin = log10(s*xmin);
+      zmax = log10(s*xmax);
+      yold = ydat[0];
+      xold = xmin;
+      for (z=zmin+dz; z<zmax; z += dz) {
+	nsteps++;
+	x = s*pow(10.0,z);
+	y = seval(x,xdat,ydat,sdat,n);
+	sum  += 0.5*(yold+y)*(x-xold);
+	sum0 += (x-xold);
+	yold = y;
+	xold = x;
+      }
+      y = ydat[n-1];
+      sum  += 0.5*(yold+y)*(xmax-xold);     /* it's possible we never */
+      sum0 += (xmax-xold);                  /* closed the interval */
+    } else
+      error("Cannot handle log steps with dx=%g xmin=%g xmax=%g",dx,xmin,xmax);
+  } else {  /* use the datapoints itself */
+    dx = 0.0;
     for (i=1; i<n; i++) {
       sum += 0.5*(ydat[i]+ydat[i-1])*(xdat[i]-xdat[i-1]);
       sum0 += (xdat[i]-xdat[i-1]);
-      dprintf(1,"%d %g %g %g\n",i,ydat[i],sum,sum0);
     }
   }
+  dprintf(1,"xmin=%g xmax=%g dx=%g sum=%g sum0=%g nsteps=%d\n",
+	  xmin,xmax,dx,sum,sum0,nsteps);
   if (Qnorm)
     sum /= sum0;
   printf("%g\n",sum);
