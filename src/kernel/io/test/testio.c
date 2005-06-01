21eead96834a12e8fb201a0abaaf02bc22661e70
Peter Teuben <teuben@gmail.com>
2005-06-01 14:55:22 +0000

    cleaned up prototypes

--- src/kernel/io/test/testio.c
+++ src/kernel/io/test/testio.c
@@ -9,6 +9,8 @@
 #include <stdinc.h>
 #include <getparam.h>
 #include <filestruct.h>
+#include <history.h>
+
 
 string defv[] = {
     "in=\n         Input file (if provided)",
@@ -16,37 +18,19 @@ string defv[] = {
     "real=1.234\n  Real number to write (if out=)",
     "count=1\n     Number of reals to write (if out=)",
     "random=1\n    Number of times to reverse (random) write count",
-    "VERSION=1.2a\n 27-feb-94 PJT",
+    "VERSION=1.3\n 31-may-05 PJT",
     NULL,
 };
+
 string usage = "testing NEMO's I/O";
 
-nemo_main()
-{
-    real x,y = 0;
-    int count, random;
-    string name;
+string cvsid="$Id$";
 
-    x = getdparam("real");    
-    count = getiparam("count");
-    random = getiparam("random");
-    
-    name = getparam("in");
-    if (*name)
-        get_number(name,&y);
-    else
-        dprintf(1,"No input file specified\n");
-    
-    name = getparam("out");
-    if (*name)
-        put_number(name,y+x,count,random);
-    else
-        dprintf(1,"No output file specified\n");
-}
 
-get_number(name,x)
-string name;
-real *x;
+extern void get_nanf(float *);
+extern void get_nand(double *);
+
+int get_number(string name, real *x)
 {
     stream str;
     real *buf;
@@ -68,13 +52,10 @@ real *x;
     *x = *buf;
     dprintf(1,"Read number %f from file %s\n",*x,name);
     free( (char *) buf);
-    return(n);
+    return n;
 }
 
-put_number(name,x,n,r)
-string name;
-real x;
-int n,r;
+void put_number(string name, real x, int n, int r)
 {
     stream str;
     real *buf;
@@ -83,23 +64,71 @@ int n,r;
     str = stropen(name,"w");
     put_history(str);
     nout = n*r;
+    //put_set(str,"Testing");
     put_data(str,"n",IntType,&nout,0);
     buf = (real *) allocate(n*sizeof(real));
     buf[0] = x;   /* only set first and third number */
     if (n>2) buf[2] = x;
-    if (r==1)
-        put_data(str,"x",RealType,buf,n,0);
-    else {
+    if (r==1) {
+        put_data(str,"x",RealType,buf,0,n);
+        put_data(str,"y",RealType,buf,0,n);
+    } else {
         put_data_set(str,"x",RealType,nout,0);
         for (i=r;i>0;i--) {
-            buf[1] = -1.0 * i;
-            if (n>3) get_nand(&buf[3]);
-	    if (n>4) get_nanf(&buf[4]);
+            buf[i] = -1.0 * i;
+            if (n>3) get_nand((double*)&buf[3]);
+	    if (n>4) get_nanf((float *)&buf[4]);
             put_data_ran(str,"x",buf,(i-1)*n,n);
         }
         put_data_tes(str,"x");
+
+        put_data_set(str,"y",RealType,nout,0);
+        for (i=r;i>0;i--) {
+            buf[i] = -1.0 * i;
+            if (n>3) get_nand((double *)&buf[3]);
+	    if (n>4) get_nanf((float  *)&buf[4]);
+            put_data_ran(str,"y",buf,(i-1)*n,n);
+        }
+        put_data_tes(str,"y");
+
+#if 1
+    dprintf(0,"nout=%d\n",nout);
+    for (i=0; i<nout; i++) buf[i]=10+i;
+    put_data_set(str,"z1",RealType,nout,0);
+
+    //    for (i=nout-1; i>=0; i--) put_data_ran(str,"z1",&buf[i],i,1);
+    //    for (i=0; i<nout; i++) put_data_ran(str,"z1",&buf[i],i,1);
+    put_data_ran(str,"z1",buf,0,1);
+    put_data_tes(str,"z1");
+#endif
     }
+    //put_tes(str,"Testing");
     strclose(str);
     dprintf(1,"Wrote number %f to file %s\n",x,name);
     free( (char *) buf);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void nemo_main()
+{
+    real x,y = 0;
+    int count, random;
+    string name;
+
+    x = getdparam("real");    
+    count = getiparam("count");
+    random = getiparam("random");
+    
+    name = getparam("in");
+    if (*name)
+        get_number(name,&y);
+    else
+        dprintf(1,"No input file specified\n");
+    
+    name = getparam("out");
+    if (*name)
+        put_number(name,y+x,count,random);
+    else
+        dprintf(1,"No output file specified\n");
 }
