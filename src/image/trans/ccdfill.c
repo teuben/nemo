f45ff850f6486bf4691d8956e162eff65ab8ab19
Peter Teuben <teuben@gmail.com>
2005-08-01 21:26:41 +0000

    make it work on cubes, but ignore a single pixel edge for problems with linear fits

--- src/image/trans/ccdfill.c
+++ src/image/trans/ccdfill.c
@@ -7,6 +7,7 @@
  *      19-feb-02       1.2 added all=, m=			pjt
  *       2-may-03       1.3 added iter=                         pjt
  *       2-jun-03       1.4 trying out xmirror/ymirror          pjt
+ *       1-aug-05       1.5 make it work on cubes too           pjt
  *                      
  */
 
@@ -27,7 +28,7 @@ string defv[] = {
 	"iter=1\n       Number of iterations",
 	"xmirror=f\n    Use points mirrored in X to get near a border",
 	"ymirror=f\n    Use points mirrored in Y to get near a border",
-	"VERSION=1.4\n  2-jun-03 PJT",
+	"VERSION=1.5\n  1-aug-05 PJT",
 	NULL,
 };
 
@@ -46,7 +47,7 @@ void nemo_main(void)
   stream   instr, outstr;
   int      m, n, nx, ny, nz;        /* size of scratch map */
   int      ngood, ntry, nlin;
-  int      i,j, di, dj;
+  int      i,j,k, di, dj;
   imageptr iptr=NULL, iptr1=NULL;      /* pointer to images */
   real     crit = getdparam("bad");
   bool     Qall = getbparam("all");
@@ -63,51 +64,54 @@ void nemo_main(void)
   
   nx = Nx(iptr);	
   ny = Ny(iptr);
-  create_image(&iptr1,nx,ny);
+  nz = Nz(iptr);
+  create_image(&iptr1,nx,ny);     /* single temp plane to work in */
   
   outstr = stropen(getparam("out"), "w");
   
-  for (iter=0; iter < niter; iter++) {
-
-    for (j=0; j<ny; j++) {                  /* make copy of image */
+  for (k=0; k<nz; k++) {
+    for (iter=0; iter < niter; iter++) {
+      
+      for (j=0; j<ny; j++) {                  /* make copy of image */
     	for (i=0; i<nx; i++) {
-    	    MapValue(iptr1,i,j) = MapValue(iptr,i,j);
+	  MapValue(iptr1,i,j) = CubeValue(iptr,i,j,k);
 	}
-    }
-
-    ntry = ngood = nlin = 0;
-    for (j=0; j<ny; j++) {                  /* loop over image and patch */
-    	for (i=0; i<nx; i++) {
-            if (MapValue(iptr1,i,j) == crit || Qall) {   /* try and patch up */
-                ntry++;
-                ini_fit();
-                for (dj=-n; dj<=n; dj++) {
-		  if ((j+dj<0 || j+dj>=ny)) continue;
-                    for (di=-n; di<=n; di++) {
-                        if ((i+di<0 || i+di>=nx)) continue;
-                        if (MapValue(iptr1,i+di,j+dj) != crit)
-                            accum_fit(di,dj,MapValue(iptr1,i+di,j+dj));
-                    }
-                }
-                if (good_fit(m)) {
-                    MapValue(iptr,i,j) = best_fit();
-                    ngood++;
-                } else if (lin_fit(n)) {
-		  if (MapValue(iptr1,i+1,j) != crit && MapValue(iptr1,i-1,j) != crit) {
-		    nlin++;
-		    MapValue(iptr,i,j) = 0.5 * (MapValue(iptr1,i+1,j) + MapValue(iptr1,i-1,j));
-		  }
-		  if (MapValue(iptr1,i,j+1) != crit && MapValue(iptr1,i,j-1) != crit) {
-		    nlin++;
-		    MapValue(iptr,i,j) = 0.5 * (MapValue(iptr1,i,j+1) + MapValue(iptr1,i,j-1));
-		  }
-		}
-            }
-	}
-    }
-    dprintf(0,"Found %d values to patch, successfull with %d, %d linear?\n",ntry,ngood,nlin);
-    if (ngood == 0 || ntry==ngood) break;
-  }
+      }
+      
+      ntry = ngood = nlin = 0;
+      for (j=1; j<ny-1; j++) {                  /* loop over image and patch */
+    	for (i=1; i<nx-1; i++) {
+	  if (MapValue(iptr1,i,j) == crit || Qall) {   /* try and patch up */
+	    ntry++;
+	    ini_fit();
+	    for (dj=-n; dj<=n; dj++) {
+	      if (j+dj<0 || j+dj>=ny) continue;
+	      for (di=-n; di<=n; di++) {
+		if (i+di<0 || i+di>=nx) continue;
+		if (MapValue(iptr1,i+di,j+dj) != crit)
+		  accum_fit(di,dj,MapValue(iptr1,i+di,j+dj));
+	      }
+	    }
+	    if (good_fit(m)) {
+	      CubeValue(iptr,i,j,k) = best_fit();
+	      ngood++;
+	    } else if (lin_fit(n)) {
+	      if (MapValue(iptr1,i+1,j) != crit && MapValue(iptr1,i-1,j) != crit) {
+		nlin++;
+		CubeValue(iptr,i,j,k) = 0.5 * (MapValue(iptr1,i+1,j) + MapValue(iptr1,i-1,j));
+	      }
+	      if (MapValue(iptr1,i,j+1) != crit && MapValue(iptr1,i,j-1) != crit) {
+		nlin++;
+		CubeValue(iptr,i,j,k) = 0.5 * (MapValue(iptr1,i,j+1) + MapValue(iptr1,i,j-1));
+	      }
+	    }
+	  }
+	} /* i */
+      } /* j */
+      dprintf(0,"Found %d values to patch, successfull with %d, %d linear?\n",ntry,ngood,nlin);
+      if (ngood == 0 || ntry==ngood) break;
+    } /* iter */
+  } /* k */
   write_image(outstr, iptr);
 }
 
@@ -118,18 +122,18 @@ local real lmat[NLSQ*NLSQ], lvec[NLSQ], lsol[NLSQ], la[NLSQ+1];
 
 local void ini_fit(void)
 {
-    nsum = 0;
-    lsq_zero(NLSQ,lmat,lvec);
+  nsum = 0;
+  lsq_zero(NLSQ,lmat,lvec);
 }
 
 local void accum_fit(int di, int dj, real val)
 {
-    nsum++;
-    la[0] = 1.0;
-    la[1] = (real) di;
-    la[2] = (real) dj;
-    la[3] = val;
-    lsq_accum(NLSQ,lmat,lvec,la,1.0);
+  nsum++;
+  la[0] = 1.0;
+  la[1] = (real) di;
+  la[2] = (real) dj;
+  la[3] = val;
+  lsq_accum(NLSQ,lmat,lvec,la,1.0);
 }
 
 local bool good_fit(int m)
@@ -145,6 +149,6 @@ local bool lin_fit(int n)
 
 local real best_fit(void)
 {
-    lsq_solve(NLSQ,lmat,lvec,lsol);
-    return lsol[0];
+  lsq_solve(NLSQ,lmat,lvec,lsol);
+  return lsol[0];
 }
