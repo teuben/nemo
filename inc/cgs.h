66f54593b7663dc527181a0f2faa1bbf27315168
Peter Teuben <teuben@gmail.com>
2005-12-02 21:03:30 +0000

    reorder and align with mks

--- inc/cgs.h
+++ inc/cgs.h
@@ -6,12 +6,13 @@
  */
 
 
-#define G_CGS 6.67e-8		/* gravitional constant */
-#define Msol_CGS 1.99e33	/* solar mass in grams */
-#define mH_CGS 1.67e-24		/* mass of hydrogen atom in grams */
 #define c_CGS 299792458000.0    /* speed of light [cm/sec] */
 #define k_CGS 1.380658e-16      /* Boltzmann  [erg/deg] */
 #define h_CGS 6.6260755e-27     /* Planck [erg-sec] */
+#define G_CGS 6.67259e-8	/* gravitional constant */
+
+#define Msol_CGS 1.99e33	/* solar mass in grams */
+#define mH_CGS 1.67e-24		/* mass of hydrogen atom in grams */
 
 #define KPCCM 3.085678e+21	/* kiloparsec in centimeters */
 #define GYRSEC 3.155693e+16	/* gigayear in seconds */
