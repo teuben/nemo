f020acd710155774ed9ae1417948a7837b9b88eb
Peter Teuben <teuben@gmail.com>
2006-02-20 02:46:05 +0000

    implement sequential memory for the arrays

--- src/kernel/misc/mdarray.c
+++ src/kernel/misc/mdarray.c
@@ -1,26 +1,44 @@
 /*
  * mdarray: a simple multi-dimensional array (allocator)
+ *          cannot be used in a multi-threaded environment
  *
- * Note:  MDMAXDIM defines the highest dimension we've implemented (currenlty 7)
+ * Note:  MDMAXDIM defines the highest dimension we've implemented (currently 7)
+ *        see mdarray.h
  *
- *  5-may-2003   Created           Peter Teuben
+ *  5-may-2003   Created                                 Peter Teuben
+ * 18-feb-2006   use sequential memory for the data      PJT
  *
  */
 
 #include <nemo.h>
 #include <mdarray.h>
 
+static int top = 0;    /* highest mdarray will have this at 0, each call increases top */
+static real *p = 0;    /* keep pointer to the big alloc incrementing by n1 */
+
+
 mdarray1 allocate_mdarray1(int n1)
 {
-  return (mdarray1 )allocate(n1*sizeof(real));
+  real *p1;
+  if (top) {
+    dprintf(7,"  top @ 0x%x\n",p);
+    p1 = p;
+    p += n1;
+    return p1;
+  } else
+    return (mdarray1 )allocate(n1*sizeof(real));
 }
 
 mdarray2 allocate_mdarray2(int n2,int n1)
 {
   mdarray2 x = (mdarray2)allocate(sizeof(mdarray2)*n2);
   int i;
+  if (top == 0) 
+    p = (real *) allocate(sizeof(real)*n1*n2);
+  top++;
   for (i=0;i<n2;i++)
     x[i] = allocate_mdarray1(n1);
+  top--;
   return x;
 }
 
@@ -28,8 +46,12 @@ mdarray3 allocate_mdarray3(int n3,int n2,int n1)
 {
   mdarray3 x = (mdarray3)allocate(sizeof(mdarray3)*n3);
   int i;
+  if (top == 0)
+    p = (real *) allocate(sizeof(real)*n1*n2*n3);
+  top++;
   for (i=0;i<n3;i++)
     x[i] = allocate_mdarray2(n2,n1);
+  top--;
   return x;
 }
 
@@ -37,8 +59,12 @@ mdarray4 allocate_mdarray4(int n4,int n3,int n2,int n1)
 {
   mdarray4 x = (mdarray4)allocate(sizeof(mdarray4)*n4);
   int i;
+  if (top == 0)
+    p = (real *) allocate(sizeof(real)*n1*n2*n3*n4);
+  top++;
   for (i=0;i<n4;i++)
     x[i] = allocate_mdarray3(n3,n2,n1);
+  top--;
   return x;
 }
 
@@ -46,8 +72,12 @@ mdarray5 allocate_mdarray5(int n5,int n4,int n3,int n2,int n1)
 {
   mdarray5 x = (mdarray5)allocate(sizeof(mdarray5)*n5);
   int i;
+  if (top == 0)
+    p = (real *) allocate(sizeof(real)*n1*n2*n3*n4*n5);
+  top++;
   for (i=0;i<n5;i++)
     x[i] = allocate_mdarray4(n4,n3,n2,n1);
+  top--;
   return x;
 }
 
@@ -55,8 +85,12 @@ mdarray6 allocate_mdarray6(int n6,int n5,int n4,int n3,int n2,int n1)
 {
   mdarray6 x = (mdarray6)allocate(sizeof(mdarray6)*n6);
   int i;
+  if (top == 0)
+    p = (real *) allocate(sizeof(real)*n1*n2*n3*n4*n5*n6);
+  top++;
   for (i=0;i<n6;i++)
     x[i] = allocate_mdarray5(n5,n4,n3,n2,n1);
+  top--;
   return x;
 }
 
@@ -64,61 +98,83 @@ mdarray7 allocate_mdarray7(int n7,int n6,int n5,int n4,int n3,int n2,int n1)
 {
   mdarray7 x = (mdarray7)allocate(sizeof(mdarray7)*n7);
   int i;
+  if (top == 0)
+    p = (real *) allocate(sizeof(real)*n1*n2*n3*n4*n5*n6*n7);
+  top++;
   for (i=0;i<n7;i++)
     x[i] = allocate_mdarray6(n6,n5,n4,n3,n2,n1);
+  top--;
   return x;
 }
 
 void free_mdarray1(mdarray1 x, int n1)
 {
-  free(x);
+  if (top==0) free(x);
 }
 
 void free_mdarray2(mdarray2 x, int n2, int n1)
 {
   int i;
+  if (top==0) free(x[0]);
+  top++;
   for (i=0;i<n2;i++)
     free_mdarray1(x[i],n1);
+  top--;
   free(x);
 }
 
 void free_mdarray3(mdarray3 x, int n3, int n2, int n1)
 {
   int i;
+  if (top==0) free(x[0][0]);
+  top++;
   for (i=0;i<n3;i++)
-    free_mdarray2(x[i],n2,n1);
+    free_mdarray2(x[i],n2,n1); 
+  top--;
   free(x);
 }
 
 void free_mdarray4(mdarray4 x, int n4, int n3, int n2, int n1)
 {
   int i;
+  if (top==0) free(x[0][0][0]);
+  top++;
   for (i=0;i<n4;i++)
     free_mdarray3(x[i],n3,n2,n1);
+  top--;
   free(x);
 }
 
 void free_mdarray5(mdarray5 x, int n5, int n4, int n3, int n2, int n1)
 {
   int i;
+  if (top==0) free(x[0][0][0][0]);
+  top++;
   for (i=0;i<n5;i++)
     free_mdarray4(x[i],n4,n3,n2,n1);
+  top--;
   free(x);
 }
 
 void free_mdarray6(mdarray6 x, int n6, int n5, int n4, int n3, int n2, int n1)
 {
   int i;
+  if (top==0) free(x[0][0][0][0][0]);
+  top++;
   for (i=0;i<n6;i++)
     free_mdarray5(x[i],n5,n4,n3,n2,n1);
+  top--;
   free(x);
 }
 
 void free_mdarray7(mdarray7 x, int n7, int n6, int n5, int n4, int n3, int n2, int n1)
 {
   int i;
+  if (top==0) free(x[0][0][0][0][0][0]);
+  top++;
   for (i=0;i<n7;i++)
     free_mdarray6(x[i],n6,n5,n4,n3,n2,n1);
+  top--;
   free(x);
 }
 
@@ -131,8 +187,9 @@ string defv[] = {
   "dim=10,20,5\n    Dimensions of array A[dim1][dim2][dim3]....",
   "flip=f\n         Reverse traversal through array, for benchmarking",
   "iter=1\n         Number of times to do the work, for benchmarking",
-  "free=f\n         Free-Allocate the array each iter? [not implemented]",
-  "VERSION=1.0\n    5-may-03 PJT",
+  "free=t\n         Free-Allocate the array each iter?",
+  "layout=f\n       Show memory layout",
+  "VERSION=2.0\n    18-feb-06 PJT",
   NULL,
 };
 
@@ -336,6 +393,45 @@ work7(int *dim,mdarray7 x, bool flip)
   }
 }
 
+static real p1 = 1;
+static real p2 = 2;
+static real p3[2][2][2] = {11,12,13,14,15,16,17,18};
+static real p4 = 20;
+static real p5[2][2] = {101,102,103,104};
+static real p6 = 201;
+
+void try()
+{
+  real *t;
+  t = &p1;
+
+  printf("static C arrays:\n");
+  printf("p1:\n");
+  printf("0x%x\n",&p1);
+  printf("p2:\n");
+  printf("0x%x\n",&p2);
+  printf("p3:\n");
+  printf("0x%x\n",p3);
+  printf("0x%x\n",p3[0]);
+  printf("0x%x\n",p3[0][0]);
+  printf("0x%x\n",&p3[0][0][0]);
+  printf("p4:\n");
+  printf("0x%x\n",&p4);
+  printf("p5:\n");
+  printf("0x%x\n",p5);
+  printf("0x%x\n",p5[0]);
+  printf("0x%x\n",&p5[0][0]);
+  printf("p6:\n");
+  printf("0x%x\n",&p6);
+
+  printf("t=%g\n",*t++);
+  printf("t=%g\n",*t++);
+  printf("t=%g\n",*t++);
+  printf("t=%g\n",*t++);
+  printf("t=%g\n",*t++);
+  printf("t=%g\n",*t++);
+}
+
 #define MAXDIM 100
 
 nemo_main()
@@ -344,7 +440,12 @@ nemo_main()
   int ndim = nemoinpi(getparam("dim"),dim,MAXDIM);
   bool flip = getbparam("flip"), free=getbparam("free");
   int iter=getiparam("iter");
-  
+  bool layout = getbparam("layout");
+  real a2[2][2];
+  real a3[4][2][3];
+  real *p;
+  int i1,i2,i3,i4, nd;
+
   mdarray1 x1;
   mdarray2 x2;
   mdarray3 x3;
@@ -352,6 +453,63 @@ nemo_main()
   mdarray5 x5;
   mdarray6 x6;
   mdarray7 x7;
+
+  if (layout) {
+    try();
+    printf("a2 static\n");
+    printf("0x%x\n",a2);
+    printf("0x%x\n",a2[0]);
+    printf("0x%x\n",&a2[0][0]);
+    for (i1=0; i1<2; i1++)
+      for (i2=0; i2<2; i2++)
+	printf("  0x%x\n",&a2[i1][i2]);
+
+    nd = 2;
+
+    printf("x2 dynamic %d\n",nd);
+    x2 = allocate_mdarray2(nd,nd);
+    printf("0x%x\n",x2);
+    printf("0x%x\n",x2[0]);
+    printf("0x%x\n",&x2[0][0]);
+    p = &x2[0][0];
+    for (i1=0; i1<nd; i1++)
+      for (i2=0; i2<nd; i2++) {
+	printf("  0x%x  0x%d\n",&x2[i1][i2],&x2[i1][i2]-p);
+	p = &x2[i1][i2];
+      }
+    
+    printf("x3 dynamic %d\n",nd);
+    x3 = allocate_mdarray3(nd,nd,nd);
+    printf("0x%x\n",x3);
+    printf("0x%x\n",x3[0]);
+    printf("0x%x\n",x3[0][0]);
+    printf("0x%x\n",&x3[0][0][0]);
+    p = &x3[0][0][0];
+    for (i1=0; i1<nd; i1++)
+      for (i2=0; i2<nd; i2++)
+	for (i3=0; i3<nd; i3++) {
+	  printf("  0x%x 0x%x\n",&x3[i1][i2][i3],&x3[i1][i2][i3]-p);
+	  p = &x3[i1][i2][i3];
+	}
+
+    printf("x4 dynamic %d\n",nd);
+    x4 = allocate_mdarray4(nd,nd,nd,nd);
+    printf("0x%x\n",x4);
+    printf("0x%x\n",x4[0]);
+    printf("0x%x\n",x4[0][0]);
+    printf("0x%x\n",x4[0][0][0]);
+    printf("0x%x\n",&x4[0][0][0][0]);
+    p = &x4[0][0][0][0];
+    for (i1=0; i1<nd; i1++)
+      for (i2=0; i2<nd; i2++)
+	for (i3=0; i3<nd; i3++)
+	  for (i4=0; i4<nd; i4++) {
+	    printf("  0x%x 0x%x\n",&x4[i1][i2][i3][i4],&x4[i1][i2][i3][i4]-p);
+	    p = &x4[i1][i2][i3][i4];
+	  }
+
+    stop(0); 
+  }
 
   printf("Working with ndim=%d MDArray",ndim);
   for (i=ndim-1; i>=0; i--)
