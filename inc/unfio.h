8dea03816fe0a64ccf4f9ecdf08408121a87bb24
Peter Teuben <teuben@gmail.com>
2006-03-01 10:21:22 +0000

    add (optional) size setting

--- inc/unfio.h
+++ inc/unfio.h
@@ -2,6 +2,7 @@
  * unformatted fortran I/O using C
  */
 
+extern int unfsize(int);
 extern int unfswap(bool);
 extern int unfscan(stream fp);
 extern int unfread(stream fp, char *buf, int bufsize);
