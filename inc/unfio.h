5ddbae328494ce46040535ac86f63111f1abd843
Peter Teuben <teuben@gmail.com>
2006-03-01 12:19:53 +0000

    todo

--- inc/unfio.h
+++ inc/unfio.h
@@ -6,3 +6,6 @@ extern int unfsize(int);
 extern int unfswap(bool);
 extern int unfscan(stream fp);
 extern int unfread(stream fp, char *buf, int bufsize);
+/* TODO::                                ^^^
+                                         int should become size_t
+ */
