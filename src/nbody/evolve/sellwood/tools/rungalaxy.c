79dd44aec02991b963e12f3aae61186d1ae57d24
Peter Teuben <teuben@gmail.com>
2006-03-08 01:18:27 +0000

    no snap2ini dependancy

--- src/nbody/evolve/sellwood/tools/rungalaxy.c
+++ src/nbody/evolve/sellwood/tools/rungalaxy.c
@@ -9,48 +9,55 @@
  *	24-jun-1997		written
  *	18-jul-2001		default is galaxy.exe
  *      18-mar-2004             finally made it work
+ *       7-mar-2006             no need for snap2ini
  *
- * Todo:  incoorporate some kind of snap2ini script in the code.
  */
 
 #include <stdinc.h>
 #include <getparam.h>
+#include <vectmath.h>
+#include <filestruct.h>
+#include <history.h>
 
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <snapshot/snapshot.h>
+#include <snapshot/body.h>
+#include <snapshot/get_snap.c>
+
 string defv[] = {
-    "in=???\n        input snapshot (nemo format)",
-    "out=???\n       directory in which GALAXY will be run",
-    "scale=15.0\n    number of grid cells per unit length",
-    "dt=0.05\n       integration time step",
-    "dtout=0.5\n     time between particle outputs",
-    "dtlog=0.1\n     time between integral checks",
-    "tstop=1.0\n     end time",
+    "in=???\n         input snapshot (nemo format)",
+    "outdir=???\n     output directory in which GALAXY will be run",
+    "scale=15.0\n     number of grid cells per unit length",
+    "dt=0.05\n        integration time step",
+    "dtout=0.5\n      time between particle outputs",
+    "dtlog=0.1\n      time between integral checks",
+    "tstop=1.0\n      end time",
+    "format=%g\n      format for pos,vel for galaxy.ini",
     "grid=33,33,33\n  number of grid cells in (x,y,z,)",
     "exe=galaxy.exe\n name of GALAXY executable",
-    "VERSION=1.3\n    18-jul-01 PJT",
+    "VERSION=2.0\n    7-mar-06 PJT",
     NULL,
 };
 
-string usage = "run galaxy";
+string usage = "frontend to run Sellwood's galaxy code";
 
-#define MAXCARDS 256
-
-static char *database[MAXCARDS];    /* full lines */
-static char *names[MAXCARDS];       /* individual namelist */
-static int ncards = 0;
+string cvsid="$Id$";
 
 int nemo_main()
 {
-    int i, n, ngrid[3];
-    real scale, dt, dtout, dtlog, tstop;
+    int i, n, nbody, bits,ngrid[3];
+    real scale, dt, dtout, dtlog, tstop, tsnap, mass;
     string exefile = getparam("exe");
-    string rundir = getparam("out");
-    stream datstr;
+    string rundir = getparam("outdir");
+    string fmt = getparam("format");
+    stream datstr, instr;
     char fullname[256];
     char command[256];
+    char fmt6[256];
+    Body *btab,*bp;
 
     n = nemoinpi(getparam("grid"),ngrid,3);
     if (n>0 && n<=3) {
@@ -64,6 +71,7 @@ int nemo_main()
     dtout = getdparam("dtout");
     dtlog = getdparam("dtlog");
     tstop = getdparam("tstop");
+    sprintf(fmt6,"%s %s %s %s %s %s\n",fmt,fmt,fmt,fmt,fmt,fmt);
 
     make_rundir(rundir);
 
@@ -78,18 +86,24 @@ int nemo_main()
     fprintf(datstr,"%g\n",tstop);
     strclose(datstr);
 
-#if 0
+    instr = stropen(getparam("in"),"r");
+    get_history(instr);
+    if (!get_tag_ok(instr, SnapShotTag)) error("no snapshot");
+    get_snap(instr, &btab, &nbody, &tsnap, &bits);
+    if ( (bits & PhaseSpaceBit) == 0) error("no phasespace");
+    for (bp=btab;bp<btab+nbody; bp++)
+      mass += Mass(bp);
+    
     sprintf(fullname,"%s/%s",rundir,"galaxy.ini");
     datstr = stropen(fullname,"w");
+    fprintf(datstr,"%g %g %d\n",tsnap,mass,nbody);
+    for (bp=btab;bp<btab+nbody; bp++)
+      fprintf(datstr,fmt6,
+	      Pos(bp)[0],Pos(bp)[1],Pos(bp)[2],
+	      Vel(bp)[0],Vel(bp)[1],Vel(bp)[2]);
     strclose(datstr);
-#else
-    sprintf(command,"snap2ini %s > %s/%s",
-	    getparam("in"),rundir,"galaxy.ini");
-    dprintf(1,"COMMAND: %s\n",command);
-    system(command);
-#endif
-    goto_rundir(rundir);
 
+    goto_rundir(rundir);
     run_program(exefile);
 }
 
@@ -105,22 +119,6 @@ make_rundir(string name)
     if (mkdir(name, 0755))
         error("Run directory %s already exists",name);
 }
-
-write_namelist(char *name)
-{
-    int i;
-    FILE *fp;
-
-    fp = fopen(name,"w");
-    if (fp==NULL) {
-        fprintf(stderr,"File %s could not be written\n",name);
-        exit(1);
-    }
-    for (i=0; i<ncards; i++)
-        fprintf(fp,"%s",database[i]);
-    fclose(fp);
-}
-
 
 
 run_program(string exe)
