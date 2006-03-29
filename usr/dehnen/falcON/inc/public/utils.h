ef56be1719fa8bc9d6e1a19a2855b5beede51bd5
Peter Teuben <teuben@gmail.com>
2006-03-29 11:37:28 +0000

    new for V3.0.5

---
+++ usr/dehnen/falcON/inc/public/utils.h
@@ -0,0 +1,78 @@
+// -*- C++ -*-                                                                  
+////////////////////////////////////////////////////////////////////////////////
+///                                                                             
+/// \file    inc/public/utils.h                                                 
+///                                                                             
+/// \author  Walter Dehnen                                                      
+///                                                                             
+/// \date    2005                                                               
+///                                                                             
+/// \brief   includes utilities from WDutils into namespace falcON              
+///                                                                             
+////////////////////////////////////////////////////////////////////////////////
+//                                                                              
+// Copyright (C) 2005  Walter Dehnen                                            
+//                                                                              
+// This program is free software; you can redistribute it and/or modify         
+// it under the terms of the GNU General Public License as published by         
+// the Free Software Foundation; either version 2 of the License, or (at        
+// your option) any later version.                                              
+//                                                                              
+// This program is distributed in the hope that it will be useful, but          
+// WITHOUT ANY WARRANTY; without even the implied warranty of                   
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU            
+// General Public License for more details.                                     
+//                                                                              
+// You should have received a copy of the GNU General Public License            
+// along with this program; if not, write to the Free Software                  
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                    
+//                                                                              
+////////////////////////////////////////////////////////////////////////////////
+#ifndef falcON_included_utils_h
+#define falcON_included_utils_h 1
+
+// include WDutils
+#ifndef WDutils_included_traits_h
+#  include <traits.h>
+#endif
+#ifndef WDutils_included_Pi_h
+#  include <Pi.h>
+#endif
+#ifndef WDutils_included_inline_h
+#  include <inline.h>
+#endif
+#ifndef WDutils_included_inline_io_h
+#  include <inline_io.h>
+#endif
+#ifndef WDutils_included_tupel_h
+#  include <tupel.h>
+#endif
+#ifndef WDutils_included_exception_h
+#  include <exception.h>
+#endif
+#ifndef WDutils_included_memory_h
+#  include <memory.h>
+#endif
+// declare WDutils in namespace falcON
+namespace falcON {
+  using namespace WDutils;
+  using WDutils::pow;
+  using WDutils::exception;
+  void error(const char*, ...);
+  void warning(const char*, ...);
+  void debug_info(int, const char*, ...);
+  using falcON::error;
+  using falcON::warning;
+  using falcON::debug_info;
+  namespace meta {
+    using namespace WDutils::meta;
+  }
+}
+// resolve ambiguities for the anonymous namespace
+namespace {
+  using falcON::error;
+  using falcON::warning;
+  using falcON::debug_info;
+}
+////////////////////////////////////////////////////////////////////////////////
+#endif // falcON_included_utils_h
