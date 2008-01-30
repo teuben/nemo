fed842860c545c6a5ff8278978cabce7101e81e2
Peter Teuben <teuben@gmail.com>
2008-01-30 03:20:25 +0000

    new for rotcurshape and rotcurtab

---
+++ inc/rotcurshape.h
@@ -0,0 +1,19 @@
+#include <nemo.h>
+
+typedef real (*rcproc)(real, int, real *, real *);
+
+extern real rotcur_flat    (real r, int n, real *p, real *d);
+extern real rotcur_linear  (real r, int n, real *p, real *d);
+extern real rotcur_poly    (real r, int n, real *p, real *d);
+extern real rotcur_core    (real r, int n, real *p, real *d);
+extern real rotcur_core1   (real r, int n, real *p, real *d);
+extern real rotcur_core2   (real r, int n, real *p, real *d);
+extern real rotcur_plummer (real r, int n, real *p, real *d);
+extern real rotcur_tanh    (real r, int n, real *p, real *d);
+extern real rotcur_iso     (real r, int n, real *p, real *d);
+extern real rotcur_nfw     (real r, int n, real *p, real *d);
+extern real rotcur_moore   (real r, int n, real *p, real *d);
+extern real rotcur_brand   (real r, int n, real *p, real *d);
+extern real rotcur_power   (real r, int n, real *p, real *d);
+extern real rotcur_disk1   (real r, int n, real *p, real *d);
+
