f5e6d57cf4dc7d75ba407655d98a37952b46efca
Walter Dehnen <wd11@leicester.ac.uk>
2008-05-01 14:10:34 +0000

    updated date stamp

--- usr/dehnen/falcON/src/public/exe/s2s.cc
+++ usr/dehnen/falcON/src/public/exe/s2s.cc
@@ -4,11 +4,11 @@
 /// \file   src/public/exe/s2s.cc                                               
 ///                                                                             
 /// \author Walter Dehnen                                                       
-/// \date   2007                                                                
+/// \date   2007-2008                                                           
 ///                                                                             
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                              
-// Copyright (C) 2007 Walter Dehnen                                             
+// Copyright (C) 2007-2008 Walter Dehnen                                        
 //                                                                              
 // This program is free software; you can redistribute it and/or modify         
 // it under the terms of the GNU General Public License as published by         
