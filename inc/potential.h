c57ba6888e1aca9e546a3b165c3732724feee6ec
Walter Dehnen <wd11@leicester.ac.uk>
2008-09-18 14:40:19 +0000

    removed potproc and introduced potproc_real == return type for get_potential()

--- inc/potential.h
+++ inc/potential.h
@@ -19,20 +19,23 @@ typedef struct a_potential {
 
 /* should we make args 1,2 and 5 const ?? */
 
-typedef void (*potproc)       (const int *, const double *, double *, double *, const double *);
 typedef void (*potproc_double)(const int *, const double *, double *, double *, const double *);
 typedef void (*potproc_float) (const int *, const float *,  float *,  float *,  const float *);
-
+#ifdef SINGLEPREC
+typedef potproc_float potproc_real;
+#else
+typedef potproc_double potproc_real;
+#endif
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
 
-potproc        get_potential        (const string, const string, const string);
+potproc_real   get_potential        (const string, const string, const string);
 potproc_float  get_potential_float  (const string, const string, const string);
 potproc_double get_potential_double (const string, const string, const string);
-potproc        get_inipotential     (void);
+proc           get_inipotential     (void);
 real           get_pattern          (void);
 
 #if defined(__cplusplus)
