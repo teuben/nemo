4dfc074d8beef6a964e75eac69aa5ed689741dd3
Walter Dehnen <wd11@leicester.ac.uk>
2008-11-07 17:55:47 +0000

    appending file only computing profile if density is up-to-date (dropped 2nd parameter)

--- usr/dehnen/falcON/src/public/manip/densprof.cc
+++ usr/dehnen/falcON/src/public/manip/densprof.cc
@@ -1,38 +1,39 @@
-// -*- C++ -*-                                                                  
+// -*- C++ -*-
 ////////////////////////////////////////////////////////////////////////////////
-///                                                                             
-/// \file   src/public/manip/densprof.cc                                        
-///                                                                             
-/// \author Walter Dehnen                                                       
-/// \date   2006,2008                                                           
-///                                                                             
+///
+/// \file   src/public/manip/densprof.cc
+///
+/// \author Walter Dehnen
+/// \date   2006-2008
+///
 ////////////////////////////////////////////////////////////////////////////////
-//                                                                              
-// Copyright (C) 2006,2008 Walter Dehnen                                        
-//                                                                              
-// This program is free software; you can redistribute it and/or modify         
-// it under the terms of the GNU General Public License as published by         
-// the Free Software Foundation; either version 2 of the License, or (at        
-// your option) any later version.                                              
-//                                                                              
-// This program is distributed in the hope that it will be useful, but          
-// WITHOUT ANY WARRANTY; without even the implied warranty of                   
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU            
-// General Public License for more details.                                     
-//                                                                              
-// You should have received a copy of the GNU General Public License            
-// along with this program; if not, write to the Free Software                  
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                    
-//                                                                              
+//
+// Copyright (C) 2006-2008 Walter Dehnen
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or (at
+// your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
 ////////////////////////////////////////////////////////////////////////////////
-//                                                                              
-// history:                                                                     
-//                                                                              
-// v 0.0    02/05/2006  WD created                                              
-// v 0.1    07/07/2006  WD using bodies in_subset()                             
-// v 0.2    27/07/2006  WD made public                                          
-// v 0.3    13/05/2008  WD debugged error with minor & major axes               
-// v 0.4    11/09/2008  WD erased direct use of nemo functions                  
+//
+// history:
+//
+// v 0.0    02/05/2006  WD created
+// v 0.1    07/07/2006  WD using bodies in_subset()
+// v 0.2    27/07/2006  WD made public
+// v 0.3    13/05/2008  WD debugged error with minor & major axes
+// v 0.4    11/09/2008  WD erased direct use of nemo functions
+// v 0.5    05/11/2008  WD using 'trho' instead of 2nd parameter
 ////////////////////////////////////////////////////////////////////////////////
 #include <public/defman.h>
 #include <public/io.h>
@@ -105,30 +106,29 @@ namespace falcON { namespace Manipulate {
   //////////////////////////////////////////////////////////////////////////////
   const int    W_default = 1000;
   // ///////////////////////////////////////////////////////////////////////////
-  //                                                                            
-  // class densprof                                                             
-  //                                                                            
-  /// manipulator: measures profiles of density bins                            
+  //
+  // class densprof
+  //
+  /// manipulator: measures profiles of density bins
   ///                                                                           
-  /// This manipulator uses a pre-computed mass-density (see manipulator        
-  /// Manipulate::density) to compute the centre and profile of shells with     
-  /// a small range in rho. Only bodies in_subset() are used.                   
-  ///                                                                           
-  /// Meaning of the parameters:\n                                              
-  /// par[0]: # bodies per density shell (window size, default: 1000)\n         
-  /// par[1]: delta time between analyses (default: 0)\n                        
-  /// file  : format string for output table files\n                            
-  ///                                                                           
-  /// Usage of pointers: none\n                                                 
-  /// Usage of flags:    uses in_subset()\n                                     
-  ///                                                                           
+  /// This manipulator uses a pre-computed mass-density (see manipulator
+  /// Manipulate::density) to compute the centre and profile of shells with a
+  /// small range in rho. Only bodies in_subset() are used.\n
+  ///
+  /// If 'trho' is given with the snapshot and does not match the actual
+  /// simulation time, nothing is done.\n  	
+  ///
+  /// Meaning of the parameters:\n
+  /// par[0]: # bodies per density shell (window size, default: 1000)\n
+  /// file  : format string for output table files\n
+  ///
+  /// Usage of pointers: 'trho'\n
+  /// Usage of flags:    uses in_subset()\n
+  ///
   // ///////////////////////////////////////////////////////////////////////////
   class densprof : public manipulator {
   private:
     const int        W;         ///< window size
-    double           STEP;      ///< delta time between manipulations
-    mutable double   TMAN;      ///< time for next manipulation
-    mutable bool     FST;       ///< first call to manipulate() ?
     mutable int      I;         ///< index of manipulation
     mutable output   OUT;       ///< file for output table
     char*  const     FILE;      ///< format for file name
@@ -159,27 +159,24 @@ namespace falcON { namespace Manipulate {
 		     int          npar,
 		     const char  *file) falcON_THROWING
   : W    ( npar>0?     int(pars[0])    : W_default ),
-    STEP ( npar>1?         pars[1]     : 0. ),
     I    ( 0 ),
-    FST  ( true ),
     FILE ( (file && file[0])? falcON_NEW(char,strlen(file)+1) : 0 ),
     PS   ( 3 )
   {
-    if(debug(2) || file==0 || file[0]==0 || npar>2)
+    if(debug(2) || file==0 || file[0]==0 || npar>1 || (debug(1) && npar<1))
       std::cerr<<
 	" Manipulator \""<<name()<<"\":\n"
 	" uses estimated density to compute centre and radial profiles\n"
 	" par[0]: # bodies per density shell (window size, def: "
 	       <<W_default<<")\n"
-	" par[1]: delta time between analyses (def: 0)\n"
 	" file  : format string for output table files\n";
     if(FILE) strcpy(FILE,file);
     if(file==0 || file[0]==0)
       falcON_THROW("Manipulator \"densprof\": no file given");
     if(W<=2) falcON_THROW("Manipulator \"%s\": W = %d <2\n",name(),W);
-    if(npar > 2)
+    if(npar > 1)
       falcON_WarningN("Manipulator \"%s\": "
-		      "skipping parameters beyond 2\n",name());
+		      "skipping parameters beyond 1\n",name());
   }
   //////////////////////////////////////////////////////////////////////////////
   inline void densprof::print_line() const
@@ -194,27 +191,23 @@ namespace falcON { namespace Manipulate {
   //////////////////////////////////////////////////////////////////////////////
   bool densprof::manipulate(const snapshot*SHOT) const
   {
-    // 0. preliminaries
-    // 0.1 first call ever:
-    if(FST) {
-      TMAN = SHOT->initial_time();
-      FST  = false;
-    }
-    // 0.2 is it time for a manipulation?
-    if(SHOT->time() < TMAN) return false;
-    TMAN += STEP;
-    // 0.3 are data sufficient?
+    const double dt=1.e-8;
+    // 0  check whether density is up-to-date
+    const double*Trho = SHOT->pointer<double>("trho");
+    if(Trho && abs(*Trho - SHOT->time()) > dt)
+      return false;
+    // 1  are data sufficient?
     if(!SHOT->have_all(need()))
       falcON_Error("densprof::manipulate(): need %s, but got %s\n",
 		   word(need()), word(SHOT->all_data()));
-    // 1. sort bodies in descending density
+    // 2  sort bodies in descending density
     Array<bodies::index> T;
     SHOT->sorted(T,&neg_density);
     const unsigned Nb = T.size();
     if(Nb < W)
       falcON_THROW("densprof::manipulate(): "
 		   "fewer (%d) bodies than window (%d)\n",Nb,W);
-    // 2. open output file and write header
+    // 3  open output file and write header
     if(OUT.reopen(FILE,I++,1)) {
       print_line();
       OUT << "#\n"
@@ -246,11 +239,11 @@ namespace falcON { namespace Manipulate {
     print_line();
     Array<real> Rq(W+W);
     Array<int>  Ir(W+W);
-    // 3. loop windows of W
+    // 4  loop windows of W and compute properties
     for(unsigned ib=0,kb=W+W>Nb? Nb:W; ib!=Nb;
 	ib=kb,kb=kb+W+W > Nb? Nb : kb+W) {
       const unsigned N = kb-ib;
-      // 1st body loop: measure total mass, mean density, and centre
+      // 4.1  measure total mass, mean density, and centre
       double M(0.);
       vect_d X0(0.), V0(0.);
       double Rho(0.);
@@ -265,7 +258,7 @@ namespace falcON { namespace Manipulate {
       X0  *= iM;
       V0  *= iM;
       Rho *= iM;
-      // 2nd body loop: measure moment of inertia, rotation and median radius
+      // 4.2  measure moment of inertia, rotation and median radius
       vect_d Mvp(0.);
       double Mxx[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
       for(unsigned i=ib,j=0; i!=kb; ++i,++j) {
@@ -292,7 +285,7 @@ namespace falcON { namespace Manipulate {
 	  break;
 	}
       }
-      // 3rd body loop: measure mean velocities and dispersion
+      // 4.3  measure mean velocities and dispersion
       vect_d erot = norm(Mvp)>0.? normalized(Mvp) : vect_d(0.,0.,1.);
       double Mvr(0.), Mvrq(0.), Mvpq(0.), Mvtq(0.);
       for(unsigned i=ib; i!=kb; ++i) {
@@ -312,7 +305,7 @@ namespace falcON { namespace Manipulate {
       double sr = sqrt(M*Mvrq-Mvr*Mvr)*iM;
       double st = sqrt(Mvtq*iM);
       double sp = sqrt(M*Mvpq-Mvp*Mvp)*iM;
-      // finally print out the data
+      // 4.4  finally print out the data
       OUT << print(X0 ,9,3)               <<' '  // centre position
 	  << print(V0 ,9,3)               <<' '  // centre velocity
 	  << print(mR ,9,3)               <<' '  // median radius
