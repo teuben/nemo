2b6078acfe88bcf81309fbebb13e5a1cfd63f696
Peter Teuben <teuben@gmail.com>
2009-02-26 14:03:23 +0000

    process multiple files

--- src/nbody/io/modysnap.c
+++ src/nbody/io/modysnap.c
@@ -3,6 +3,7 @@
  *
  *    23-feb-09  PJT  created ; 
  *    25-feb-09  PJT  added pin= (but not processing) , wrote out few more pars
+ *    26-feb-09  PJT  process multiple MOUT files
  */
  
 #define SinglePrec
@@ -20,7 +21,7 @@ string defv[] = {
     "pin=\n         Input mbody poutXX.bin potential file (optional)",
     "swap=f\n       Swap bytes in each float if endianism differs",
     "headline=\n    Random verbiage",
-    "VERSION=1.1\n  25-feb-09 pjt",
+    "VERSION=1.2\n  26-feb-09 pjt",
     NULL,
 };
 
@@ -50,40 +51,42 @@ void nemo_main()
       warning("pin=%s not yet processed",getparam("pin"));
 
 
-    nread = unfread(instr, (char *)ipar, 5*sizeof(int));
-    dprintf(0,"ipar: nbody=%d mods=%d\n",
+    while (1) {
+      nread = unfread(instr, (char *)ipar, 5*sizeof(int));
+      if (nread == 0) break;
+      dprintf(0,"ipar: nbody=%d mods=%d\n",
 	    ipar[0],ipar[1]);
 
-    nread = unfread(instr, (char *)rpar, 5*sizeof(float));
-    dprintf(0,"rpar: totms=%f tnow=%f tdyn=%f vir=%f\n",
+      nread = unfread(instr, (char *)rpar, 5*sizeof(float));
+      dprintf(0,"rpar: totms=%f tnow=%f tdyn=%f vir=%f\n",
 	    rpar[0],rpar[1],rpar[2],rpar[3]);
 
-    nbody = ipar[0];
-    model = ipar[1];
-    mond_ind = ipar[2];
-    /* ipar[3,4] still unused */
+      nbody = ipar[0];
+      model = ipar[1];
+      mond_ind = ipar[2];
+      /* ipar[3,4] still unused */
 
-    mass0 = rpar[0];
-    mass1 = mass0/nbody;
-    tsnap = rpar[1];
-    tdyn  = rpar[2];
-    /* rpar[3,4] still unused */
+      mass0 = rpar[0];
+      mass1 = mass0/nbody;
+      tsnap = rpar[1];
+      tdyn  = rpar[2];
+      /* rpar[3,4] still unused */
 
-    /* fbuf holds the array of posvel's
-     * mbuf holds the (all the same ) masses 
-     */
-    fbuf = (real *) allocate(nbody*6*sizeof(real)); 
-    mbuf = (real *) allocate(nbody*sizeof(real));   
+      /* fbuf holds the array of posvel's
+       * mbuf holds the (all the same ) masses 
+       */
+      fbuf = (real *) allocate(nbody*6*sizeof(real)); 
+      mbuf = (real *) allocate(nbody*sizeof(real));   
 
-    for (i=0; i<nbody; i++) {
-      mbuf[i] = mass1;                              
-      nread = unfread(instr, (char *) &fbuf[i*6], 6*sizeof(real)); 
-      if (nread<1) error("Early EOF for i=%d",i);
-    }
+      for (i=0; i<nbody; i++) {
+	mbuf[i] = mass1;                              
+	nread = unfread(instr, (char *) &fbuf[i*6], 6*sizeof(real)); 
+	if (nread<1) error("Early EOF for i=%d",i);
+      }
     
-    /* write out snapshot for NEMO */
+      /* write out snapshot for NEMO */
 
-    put_set(outstr,SnapShotTag);
+      put_set(outstr,SnapShotTag);
       put_set(outstr,ParametersTag);
        put_data(outstr, NobjTag, IntType, &nbody, 0);
        put_data(outstr, TimeTag, RealType, &tsnap, 0);
@@ -97,7 +100,11 @@ void nemo_main()
         put_data(outstr,MassTag,RealType,mbuf,nbody,0);
         put_data(outstr,PhaseSpaceTag,RealType,fbuf,nbody,2,NDIM,0);
       put_tes(outstr,ParticlesTag);
-    put_tes(outstr,SnapShotTag);
+      put_tes(outstr,SnapShotTag);
+
+      free(fbuf);
+      free(mbuf);
+    }
 
     strclose(instr);
     strclose(outstr);
