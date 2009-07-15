5d2a6bd78e6359a3edf58caf03e4932142573e90
Jean Charles Lambert <jcl@oamp.fr>
2009-07-15 13:42:37 +0000

    fix a bug on list of files

--- usr/jcl/glnemo2/plugins/snapshotlist.cc
+++ usr/jcl/glnemo2/plugins/snapshotlist.cc
@@ -130,7 +130,7 @@ bool SnapshotList::openFile()
     std::string header(magic);
     if (line == header) status=true;
     else {
-#if 0
+#if 1
       fi.seekg(0, std::ios::beg); // go back to the beginning
       if (getLine(true)) { // read the first file
         SnapshotInterface * test_data = plugins->getObject(snapshot);
