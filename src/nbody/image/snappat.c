253e1d88082b102ce8b22e8714fa07a2114852ca
Peter Teuben <teuben@gmail.com>
2009-08-08 14:25:38 +0000

    new

---
+++ src/nbody/image/snappat.c
@@ -0,0 +1,97 @@
+/*
+ *  SNAPPAT: create a regular Particle-Attribute-Time cube from a snapshot
+ *
+ *       8-aug-2009    0.1 written                                    PJT
+ */
+
+#include <stdinc.h>
+#include <getparam.h>
+#include <math.h>
+#include <vectmath.h>
+#include <filestruct.h>
+#include <history.h>
+#include <image.h>
+
+#include <snapshot/snapshot.h>	
+#include <snapshot/body.h>
+#include <snapshot/get_snap.c>
+
+string defv[] = {		/* DEFAULT INPUT PARAMETERS */
+    "in=???\n			Input file (snapshot)",
+    "out=???\n                  Output file (image)",
+    "options=x,y,z,vx,vy,vz\n	Things to output",
+    "times=all\n		Times to select snapshot",
+    "ntime=\n                   if used, pre-allocate this number of snapshots",
+    "VERSION=0.1\n		8-aug-09 PJT",
+    NULL,
+};
+
+string usage="convert snapshot to a regular Particle-Attribute-Time image";
+
+string cvsid="$Id$";
+
+#define MAXOPT    64
+
+void nemo_main()
+{
+    stream instr, outstr;
+    real   tsnap, dr, aux;
+    string times;
+    Body *btab = NULL, *bp, *bq;
+    int i, j, k, n, nbody, bits, nopt, ParticlesBit, ntime;
+    char fmt[20],*pfmt;
+    string *burststring(), *opt;
+    rproc btrtrans(), fopt[MAXOPT];
+    imageptr iptr = NULL; 
+
+    ParticlesBit = (MassBit | PhaseSpaceBit | PotentialBit | AccelerationBit |
+            AuxBit | KeyBit | DensBit | EpsBit);
+    instr = stropen(getparam("in"), "r");	
+    outstr = stropen(getparam("out"), "w");
+
+    opt = burststring(getparam("options"),", ");
+    nopt = 0;					/* count options */
+    while (opt[nopt]) {				/* scan through options */
+        fopt[nopt] = btrtrans(opt[nopt]);
+        nopt++;
+        if (nopt==MAXOPT) {
+            dprintf(0,"\n\nMaximum number of options = %d exhausted\n",MAXOPT);
+            break;
+        }
+    }
+    times = getparam("times");
+    ntime = (hasvalue("ntime") ? getiparam("ntime") : 1);
+    
+    for(;;) {                /* repeating until first or all times are read */
+	get_history(instr);
+        if (!get_tag_ok(instr, SnapShotTag))
+            break;                                  /* done with work */
+        get_snap(instr, &btab, &nbody, &tsnap, &bits);
+        if (!streq(times,"all") && !within(tsnap,times,0.0001))
+            continue;                   /* skip work on this snapshot */
+        if ( (bits & ParticlesBit) == 0)
+            continue;                   /* skip work, only diagnostics here */
+	dprintf(0,"Time=%g\n",tsnap);
+	if (iptr == NULL) {
+	  create_cube(&iptr,nbody,nopt,ntime);
+	  k=0;
+	} 
+	for (j=0; j<nopt; j++) {
+	  for (bp = btab, i=0; bp < btab+nbody; bp++, i++) {
+	    CubeValue(iptr,i,j,k) = fopt[j](bp,tsnap,i);
+	  }
+	}
+	k++;
+
+	if (k==ntime)  { /* cube is full */
+	  write_image(outstr,iptr);
+	  free_image(iptr);
+	  iptr = NULL;
+	  k = 0;
+	}
+    }
+    if (k) warning("something not written");
+    strclose(instr);
+    strclose(outstr);
+}
+
