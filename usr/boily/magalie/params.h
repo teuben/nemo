677893f99d71ae09aa817af8da0b7b050cf9b4a7
Peter Teuben <teuben@gmail.com>
2009-08-25 18:34:53 +0000

    allow 1e6 particles

--- usr/boily/magalie/params.h
+++ usr/boily/magalie/params.h
@@ -4,5 +4,5 @@
 c++ Reduced maxtab, was 210 ; maxtabh, was 3000 cp buicmb 24.08.2k 
 
         PARAMETER(mdisc=7000)
-        PARAMETER(nbodsmax=100000,maxtabh=1000,maxtab=105)
+        PARAMETER(nbodsmax=1000000,maxtabh=1000,maxtab=105)
 
