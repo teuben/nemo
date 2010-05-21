360d205f610e71bcaf1c9e8329acb87c710bad67
Jean Charles Lambert <jcl@oamp.fr>
2010-05-21 13:41:56 +0000

    Detect automatically fortran compiler used during NEMO installation

--- usr/jcl/unsio/build/FindNEMO.cmake
+++ usr/jcl/unsio/build/FindNEMO.cmake
@@ -3,6 +3,33 @@ SET(NEMO_INSTALLED FALSE)
 FILE(GLOB GLOB_TEMP_VAR $ENV{NEMO})
 IF(GLOB_TEMP_VAR)
   SET(NEMO_INSTALLED TRUE)
+
+  # Detect fortran compiler from ${NEMOLIB}/makedefs
+  # files (variable FC  = )
+
+  FILE(READ $ENV{NEMOLIB}/makedefs MAKEDEFS)
+  STRING(REGEX MATCH "FC  = gfortran" GFORTRAN_IS_SET ${MAKEDEFS}) 
+
+  
+  IF ("FC  = gfortran" STREQUAL "${GFORTRAN_IS_SET}")
+     # gfortran stuffs
+     MESSAGE("GFortran compiler Detected......")
+     #SET (FC_COMPILER gfortran)
+     SET (FC_LIB_PATH "")
+     SET (FC_LIB "")
+
+  ELSE("FC  = gfortran" STREQUAL "${GFORTRAN_IS_SET}")
+     # g77 stuffs     
+     MESSAGE("Assuming G77 compiler")
+     #SET (FC_COMPILER g77)
+     execute_process(COMMAND g77 -print-file-name=libg2c.a 
+	OUTPUT_VARIABLE G2CFPATH OUTPUT_STRIP_TRAILING_WHITESPACE)
+     execute_process(COMMAND dirname ${G2CFPATH} 
+	OUTPUT_VARIABLE FC_LIB_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
+     SET (FC_LIB g2c)
+     MESSAGE( STATUS "FC LIB PATH : " ${FC_LIB_PATH})
+
+  ENDIF  ("FC  = gfortran" STREQUAL "${GFORTRAN_IS_SET}")
 ENDIF(GLOB_TEMP_VAR)
 
 if (NOT NEMO_INSTALLED)
