aca0ea7b6777a596a870ab08bd671e251ccc2d70
Peter Teuben <teuben@gmail.com>
2010-06-03 13:18:01 +0000

    add dubinski's version (Leiden release really)

--- src/nbody/init/mkh41.c
+++ src/nbody/init/mkh41.c
@@ -7,6 +7,7 @@
  *
  *   15-jul-2009     1.0   Created at PiTP - Alar Toomre & Peter Teuben
  *   22-jul-2009     1.1   added sign=
+ *   13-apr-2010     1.2   added Dubinski's version as an option
  *
  */
 
@@ -26,8 +27,9 @@ string defv[] = {
     "mass=1.0,1.0,1.0,0.7,0.3\n   masses of each particle",
     "phi=0,0,0,0,0\n              angles of first particle (deg)",
     "sign=1\n                     Sign of angular momentum",
+    "mode=0\n                     0=nemo 1=dubinski",
     "headline=\n	          verbiage for output",
-    "VERSION=1.1\n                22-jul-09 PJT+AT",
+    "VERSION=1.2\n                13-apr-10 PJT",
     NULL,
 };
 
@@ -54,10 +56,24 @@ local string headline;
 local bool Qgrow;
 
 
+typedef struct {
+	float m, x, y, z, vx, vy, vz, ax, ay, az;
+} phase;
+
+local phase r[37], r0[38], r1[37];
+
+
+
 
 void nemo_main()
 {
     int i, nrad, n;
+    int mode = getiparam("mode");
+
+    if (mode==1) {
+      dubinski();
+      return;
+    }
 
     sign_L = getdparam("sign");
 
@@ -131,4 +147,207 @@ writesnap(int n)
      put_tes(outstr, ParticlesTag);
     put_tes(outstr, SnapShotTag);
     ntot += n;
+}
+
+dubinski(void)
+{
+  int i, j, nobj;
+  double a, da, rad;
+  double fx, fy, fz, fr;
+  double dx, dy, dz, dr2, dr, vc;
+  double mtot, epssq=0.25;
+  
+  nobj = 37;
+
+  r[0].x = 0; r[0].y = 0; r[0].z = 0;
+  r[0].vx = 0; r[0].vy = 0; r[0].vz = 0;
+
+  a = M_PI/2.0;
+  da = 2.0*M_PI/6.;
+  rad = 1.0;
+  for(i=1; i<7; i++) {
+    r[i].x = rad*cos(a);
+    r[i].y = rad*sin(a);
+    a += da;
+  }
+  a = M_PI/2.0;
+  da = 2.0*M_PI/8.;
+  rad = 2.0;
+  for(i=7; i<15; i++) {
+    r[i].x = rad*cos(a);
+    r[i].y = rad*sin(a);
+    a += da;
+  }
+  a = M_PI/2.0;
+  da = 2.0*M_PI/10.;
+  rad = 3.0;
+  for(i=15; i<25; i++) {
+    r[i].x = rad*cos(a);
+    r[i].y = rad*sin(a);
+    a += da;
+  }
+  a = M_PI/2.0;
+  da = 2.0*M_PI/12.;
+  rad = 4.0;
+  for(i=25; i<37; i++) {
+    r[i].x = rad*cos(a);
+    r[i].y = rad*sin(a);
+    a += da;
+  }
+  for(i=0; i<37; i++) {
+    r[i].m = 1.0/nobj;
+    r[i].z = 0;
+    r[i].vx = 0;
+    r[i].vy = 0;
+    r[i].vz = 0;
+  }
+  mtot = 0;
+  for(i=0; i<15; i++) {
+    r[i].m = 1.0;
+    mtot += r[i].m;
+  }
+  for(i=15; i<25; i++) {
+    r[i].m = 0.7;
+    mtot += r[i].m;
+  }
+  for(i=25; i<37; i++) {
+    r[i].m = 0.3;
+    mtot += r[i].m;
+  }
+  for(i=0; i<37; i++) {
+    r[i].m /= mtot;
+  }
+  
+  for(i=0; i<nobj; i++) {
+    fx = fy = fz = 0;
+    for(j=0; j<nobj; j++) {
+      if( j == i ) continue;
+      dx = r[j].x - r[i].x;
+      dy = r[j].y - r[i].y;
+      dz = r[j].z - r[i].z;
+      dr2 = dx*dx + dy*dy + dz*dz + epssq;
+      dr = sqrt(dr2);
+      fx += r[j].m*dx/(dr*dr2);
+      fy += r[j].m*dy/(dr*dr2);
+      fz += r[j].m*dz/(dr*dr2);
+    }
+    r[i].ax = fx; r[i].ay = fy; r[i].az = fz;
+  }
+  r[0].vx = r[0].vy = r[0].vz = 0;
+  
+  fr = 0;
+  for(i=1; i<7; i++) {
+    fx = r[i].ax; fy = r[i].ay; fz = r[i].az;
+    dr = sqrt(r[i].x*r[i].x + r[i].y*r[i].y + r[i].z*r[i].z);
+    fr += (fx*r[i].x + fy*r[i].y + fz*r[i].z)/dr;
+  }
+  fr /= 6;
+  vc = sqrt(-fr);
+  for(i=1; i<7; i++) {
+    r[i].vx = -vc*r[i].y/dr;
+    r[i].vy = vc*r[i].x/dr;
+    r[i].vz = 0.0;
+  }
+  
+  fr = 0;
+  for(i=7; i<15; i++) {
+    fx = r[i].ax; fy = r[i].ay; fz = r[i].az;
+    dr = sqrt(r[i].x*r[i].x + r[i].y*r[i].y + r[i].z*r[i].z);
+    fr += (fx*r[i].x + fy*r[i].y + fz*r[i].z)/dr;
+  }
+  fr /= 8;
+  vc = sqrt(-2*fr);
+  for(i=7; i<15; i++) {
+    r[i].vx = -vc*r[i].y/dr;
+    r[i].vy = vc*r[i].x/dr;
+    r[i].vz = 0.0;
+  }
+  
+  fr = 0;
+  for(i=15; i<25; i++) {
+    fx = r[i].ax; fy = r[i].ay; fz = r[i].az;
+    dr = sqrt(r[i].x*r[i].x + r[i].y*r[i].y + r[i].z*r[i].z);
+    fr += (fx*r[i].x + fy*r[i].y + fz*r[i].z)/dr;
+  }
+  fr /= 10;
+  vc = sqrt(-3*fr);
+  for(i=15; i<25; i++) {
+    r[i].vx = -vc*r[i].y/dr;
+    r[i].vy = vc*r[i].x/dr;
+    r[i].vz = 0.0;
+  }
+  
+  fr = 0;
+  for(i=25; i<37; i++) {
+    fx = r[i].ax; fy = r[i].ay; fz = r[i].az;
+    dr = sqrt(r[i].x*r[i].x + r[i].y*r[i].y + r[i].z*r[i].z);
+    fr += (fx*r[i].x + fy*r[i].y + fz*r[i].z)/dr;
+  }
+  fr /= 12;
+  vc = sqrt(-4*fr);
+  for(i=25; i<37; i++) {
+    r[i].vx = -vc*r[i].y/dr;
+    r[i].vy = vc*r[i].x/dr;
+    r[i].vz = 0.0;
+  }
+  
+/*
+	for(i=0; i<37; i++) {
+		fprintf(stdout,"%.8g %.8g %.8g %.8g %.8g %.8g %.8g\n",
+			r[i].m,r[i].x,r[i].y,r[i].z,r[i].vx,r[i].vy,r[i].vz);
+	}
+*/
+
+
+  
+  {
+    float m1, m2, mu1, mu2, vp, b, x, y, vx, vy, rsep;
+    
+    m1 = m2 = 1;
+    rsep = 12;
+    b = 9;
+    mu1 = m2/(m1+m2);
+    mu2 = -m1/(m1+m2);
+    vp = sqrt(2*(m1 + m2)/b);
+    
+    x = (2*b - rsep);  y = 2*sqrt(b*(rsep-b));
+    vx = -sqrt(b*(rsep-b))*vp/rsep; vy = +b*vp/rsep;
+    fprintf(stderr,"%g %g %g %g\n",x,y,vx,vy);
+    
+    for(i=0; i<nobj; i++) {
+      r0[i] = r[i];
+      r1[i] = r[i];
+      r0[i].x = r[i].x - mu1*x;
+      r0[i].y = r[i].y + mu1*y;
+      r0[i].vx = r[i].vx + mu1*vx;
+      r0[i].vy = r[i].vy - mu1*vy;
+      r1[i].x = -r[i].x - mu2*x;
+      r1[i].y = -r[i].y + mu2*y;
+      r1[i].vx = -r[i].vx + mu2*vx;
+      r1[i].vy = -r[i].vy - mu2*vy;
+    }
+  }
+  
+/*
+	fprintf(stdout,"74\n");
+*/
+  for(i=0; i<37; i++)
+/*
+		fwrite(r0+i,28,1,stdout);
+*/
+    fprintf(stdout,"%.8g %.8g %.8g %.8g %.8g %.8g %.8g\n",
+	    r0[i].m,r0[i].x,r0[i].y,r0[i].z,r0[i].vx,r0[i].vy,r0[i].vz);
+  for(i=0; i<37; i++) 
+/*
+		fwrite(r1+i,28,1,stdout);
+*/
+    fprintf(stdout,"%.8g %.8g %.8g %.8g %.8g %.8g %.8g\n",
+	    r1[i].m,r1[i].x,r1[i].y,r1[i].z,r1[i].vx,r1[i].vy,r1[i].vz);
+/*
+	for(i=0; i<37; i++)
+		fwrite(r0+i,28,1,stdout);
+	for(i=0; i<37; i++)
+		fwrite(r1+i,28,1,stdout);
+*/
+  
 }
