04357a75c492776e1363c187e7aee6ba02b448b1
Walter Dehnen <wd11@leicester.ac.uk>
2010-07-02 09:00:01 +0000

    avoid using nemo's sqr(), which caused linkage problems

--- src/orbit/potential/data/harmonic.c
+++ src/orbit/potential/data/harmonic.c
@@ -4,6 +4,7 @@
  *	7-mar-92  happy gcc2.0		pjt
  *	  oct-93  get_pattern		pjt
  * 	  sep-04  double/float		PJT
+ *        aug-08  get rid of sqr        WD  (cvs committed jul-2010)
  */
 
 /*CTEX
@@ -26,7 +27,8 @@
 static double omega = 0.0;           /* just put to zero until implemented */
 static double h[3] = {1.0,1.0,1.0};  /* default parameters harmonic potential */
 
-#define SQR(x)  ((x)*(x))
+/* have our own square (to avoid linkage problems) */
+inline double square(double x) { return x*x; }
 
 void inipotential (int *npar, double *par, string name)
 {
@@ -34,7 +36,7 @@ void inipotential (int *npar, double *par, string name)
 
     if (*npar>0) omega = par[0];
     for (i=1; i<(*npar); i++)
-       h[i-1] = sqr(par[i]);
+       h[i-1] = square(par[i]);      /* WD; original: h[i-1] = sqr(par[i]); */
 
     dprintf (1,"INI_POTENTIAL Harmonic\n");
     dprintf (1,"  Parameters : Pattern Speed = %f\n",omega);
