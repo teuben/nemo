dd428e0aedb17dbbfac444dd040f4523e2cd046f
Peter Teuben <teuben@gmail.com>
2011-02-05 00:37:25 +0000

    end of day checkin; most things work, but not final.

--- src/image/rotcur/velfit.c
+++ src/image/rotcur/velfit.c
@@ -15,6 +15,7 @@
  *   31aug02  pjt   Ieck, rms calculation wrong, arrays not reset to 0
  *    5-oct-2003  Created, embedding in rotcur was too painful    PJT
  *   22-oct-2005  Optional output map with rotation velocities  SNV/PJT
+ *    3-feb-2011  more options to output map, added tab=           PJT
  *
  *
  */
@@ -33,9 +34,11 @@ string defv[] = {
   "frang=0\n      free angle around minor axis (2*frang is the total)",
   "blank=0.0\n    Value of the blank pixel to be ignored",
   "coswt=1\n      power of cos(theta) weighting",
-  "wwb73=t\n      use the classic WWB73 method",
+  "wwb73=t\n      use the classic WWB73 method (fixed)",
+  "mode=vtan\n    Output mode (vtan, vres, ome)",
   "out=\n         Optional output map of converted rotation speeds",
-  "VERSION=1.1\n  22-oct-2005 PJT",
+  "tab=\n         Optional output table of radii, velocities etc.",
+  "VERSION=1.2\n  4-feb-2011 PJT",
   NULL,
 };
 
@@ -50,6 +53,7 @@ string cvsid="$Id$";
 bool Qwwb73;
 bool Qden = FALSE;
 bool Qout = FALSE;
+bool Qtab = FALSE;
 imageptr denptr = NULL, velptr = NULL, outptr = NULL;
 
 real rad[MAXRING];
@@ -60,6 +64,20 @@ real vrot[MAXRING];
 
 real pa, inc, vsys, xpos, ypos;
 real undf;
+
+string outmodes[] = {"vtan","vmod","vres","vtan/r", "ome", NULL};
+
+
+int string_index(string *sa, string s)
+{
+  int i=0;
+  while (sa[i]) {
+    if (streq(sa[i],s)) return i;
+    i++;
+  }
+	   
+  return -1;
+}
 
 int ring_index(int n, real *r, real rad)
 {
@@ -74,11 +92,13 @@ int ring_index(int n, real *r, real rad)
 
 nemo_main()
 {
-  stream denstr, velstr, outstr;
+  stream denstr, velstr, outstr, tabstr;
   real center[2], cospa, sinpa, cosi, sini, cost, costmin, x, y, xt, yt, r, den;
   real vr, wt, frang, dx, dy, xmin, ymin, rmin, rmax, fsum, ave, tmp, rms;
-  real sincosi, cos2i, tga, dmin, dmax, dval;
+  real sincosi, cos2i, tga, dmin, dmax, dval, vmod;
   int i, j, k, nx, ny, ir, nring, nundf, nout, nang, nsum, coswt;
+  string outmode;
+  int mode = -1;
 
   velstr = stropen(getparam("in"),"r");
 
@@ -93,11 +113,19 @@ nemo_main()
   } 
 
   if (hasvalue("out")) {
-    warning("New out= option not well tested yet");
+    outmode = getparam("mode");
+    mode = string_index(outmodes, outmode);
+    if (mode < 0) error("Illegal mode=%s",outmode);    
+    warning("New out= mode mode=%s [%d]",outmode,mode);
     Qout = TRUE;
     outstr = stropen(getparam("out"),"w");
     copy_image(velptr,&outptr);
   } 
+
+  if (hasvalue("tab")) {
+    Qtab = TRUE;
+    tabstr = stropen(getparam("tab"),"w");
+  }
 
   nrad = nemoinpd(getparam("radii"),rad,MAXRING);
   if (nrad < 2) error("got no rings, use radii=");
@@ -118,13 +146,13 @@ nemo_main()
   frang = getdparam("frang");
   coswt = getiparam("coswt");
 
-  cospa = cos(pa*PI/180.0);
-  sinpa = sin(pa*PI/180.0);
-  sini = sin(inc*PI/180.0);
-  cosi = cos(inc*PI/180.0);
+  cospa   = cos(pa*PI/180.0);
+  sinpa   = sin(pa*PI/180.0);
+  sini    = sin(inc*PI/180.0);
+  cosi    = cos(inc*PI/180.0);
   costmin = sin(frang*PI/180.0);
   sincosi = sini*cosi;
-  cos2i = cosi*cosi;
+  cos2i   = cosi*cosi;
     
   for (i=0; i<nring; i++)
     pixe[i] = flux[i] = vsum[i] = vsqu[i] = wsum[i] = 0.0;
@@ -134,7 +162,7 @@ nemo_main()
   xmin = Xmin(velptr);
   dx = Dx(velptr);       dx = ABS(dx);    dx = -dx;
   dy = Dy(velptr);       dy = ABS(dy);
-  rmin = nx*dx*10.0;
+  rmin = -nx*dx*10.0;
   rmax = 0.0;
 
   dmin = dmax = vsys;
@@ -152,17 +180,19 @@ nemo_main()
       x = (i-xpos)*dx;
       yt = x*sinpa + y*cospa;
       xt = x*cospa - y*sinpa;
-      if (Qout) {
-	if (xt==0.0) {
-	  MapValue(outptr,i,j) = undf;
+      if (Qout) {      /* record the circular speed in diskplane */
+	dval = MapValue(velptr,i,j)-vsys;
+	if (dval < 0) dval = -dval;
+	if (yt==0.0) {
+	  dval = undf;
 	} else {
-	  tga = xt/yt; /* X and Y are reversed  */
-	  dval = MapValue(velptr,i,j)-vsys;
-	  if (dval < 0) dval = -dval;
-	  dval = vsys + dval*sqrt(cos2i+tga*tga)/sincosi;
+	  tga = xt/yt;        /* X and Y are reversed  */
+	  dval *= sqrt(cos2i+tga*tga)/sincosi;
+	}
+	if (mode==0)                      /* mode=vtan */
 	  MapValue(outptr,i,j) = dval;
-	  if (dval > dmax) dmax = dval;
-	  if (dval < dmin) dmin = dval;
+	else if (mode==3 || mode==4) {    /* mode=vtan/r or mode=ome */
+	  MapValue(outptr,i,j) = dval/sqrt(sqr(xt/cosi)+sqr(yt));
 	}
       }
       xt /= cosi;
@@ -177,7 +207,7 @@ nemo_main()
 	continue;
       }
       cost = yt/r;
-      vr = (MapValue(velptr,i,j)-vsys)/cost/sini;
+      vr = (MapValue(velptr,i,j)-vsys)/cost/sini;  /* rotation speed */
       den = Qden ? MapValue(denptr,i,j) : 1.0;
       wt = den;
       for (k=0; k<coswt; k++)  wt *= cost;
@@ -193,17 +223,7 @@ nemo_main()
     }
   }
 
-  /* write output map(s), if needed */
-  if (Qout) {
-    dprintf(0,"Data min/max = %g %g\n",dmin,dmax);    
-    MapMin(outptr) = dmin;
-    MapMax(outptr) = dmax;
-    write_image(outstr,outptr);
-  }
-
-
   /* find the rotation curve */
-
 
   for (i=0; i<nring; i++) {
     if (pixe[i] < 1) continue;
@@ -231,22 +251,34 @@ nemo_main()
       ir = ring_index(nrad,rad,r);
       if (ir < 0) continue;
       cost = yt/r;
+      if (ABS(cost) < costmin) {
+	if (Qout) MapValue(outptr,i,j) = undf;
+	continue;
+      }
       den = Qden ? MapValue(denptr,i,j) : 1.0;
       wt = den;
       for (k=0; k<coswt; k++)  wt *= cost;
       wt = ABS(wt);
       if (ABS(cost) > costmin) {
-	vr = MapValue(velptr,i,j)-vsys-vrot[ir]*cost*sini;
+	vmod = vsys + vrot[ir]*cost*sini;     /* model */
+	vr = MapValue(velptr,i,j)-vmod;       /* residual:   vobs-vmod */
+	if (Qout) {
+	  if (mode==1)
+	    MapValue(outptr,i,j) = vmod;
+	  else if (mode==2)
+	    MapValue(outptr,i,j) = vr;
+	}
 	wsum[ir] += wt;
 	vsum[ir] += wt*vr;
 	vsqu[ir] += wt*vr*vr;
 	dprintf(1,"%d %g %g %g %g\n",ir,xt,yt,vr,wt);
-      }
+      } 
     }
   }
 
   /* report */
 
+  if (Qtab) fprintf(tabstr,"# r v rms N tmp sqrt(F/N),  ave, vsum, wsum\n");
   fsum = 0.0;
   nsum = 0;
   for (i=0; i<nring; i++) {
@@ -256,16 +288,34 @@ nemo_main()
     tmp = flux[i]/pixe[i];
     ave = vsum[i]/wsum[i];
     rms = vsqu[i]/wsum[i]-ave*ave;
-    if (rms <  0) rms=0.0;
+    if (pixe[i]==0 || rms <  0) rms=0.0;
     rms = sqrt(rms);
     fsum += rms*rms;
-    printf("%g %g %g %g %g %g ;; %g %g %g\n",
+    if (Qtab) fprintf(tabstr,"%g %g %g %g %g %g  %g %g %g\n",
 	   r,vrot[i],rms,pixe[i],tmp,sqrt(fsum/nsum),    ave,vsum[i],wsum[i]);
   }
 
   dprintf(0,"Nundf=%d/%d Nout=%d Nang=%d (sum=%d)\n",
 	  nundf,nx*ny,nout,nang,nout+nundf+nang);
   dprintf(0,"Rmin/max = %g %g\n",rmin,rmax);
+
+
+  /* write output map(s), if needed */
+  if (Qout) {
+    dmin = dmax = MapValue(outptr,0,0); 
+    for (j=0; j<ny; j++) {
+      for (i=0; i<nx; i++) {
+	dval = MapValue(outptr,i,j);
+	if (dval > dmax) dmax = dval;
+	if (dval < dmin) dmin = dval;
+      }
+    }
+    dprintf(0,"Data min/max = %g %g\n",dmin,dmax);    
+    MapMin(outptr) = dmin;
+    MapMax(outptr) = dmax;
+    write_image(outstr,outptr);
+  }
+
 
 }
 
--- src/image/rotcur/velmap.c
+++ src/image/rotcur/velmap.c
@@ -22,31 +22,37 @@ string defv[] = {
   "den=\n         input density image, unity if left blank",
   "frang=0\n      free angle around minor axis (2*frang is the total)",
   "blank=0.0\n    Value of the blank pixel to be ignored",
-  "coswt=1\n      power of cos(theta) weighting",
-  "VERSION=0.1\n  3-feb-2011 PJT",
+  "mode=v\n       output mode (v, v/r, ..)",
+  "tab=\n         Optional output table",
+  "VERSION=0.3\n  4-feb-2011 PJT",
   NULL,
 };
+
 
 string usage="various mapping functions on velocity fields";
 
 string cvsid="$Id$";
 
 
-#define MAXRING    1000
-
+#ifndef MAXRING
+#define MAXRING    2048
+#endif
 
 bool Qden = FALSE;
 bool Qout = FALSE;
+bool Qtab = FALSE;
 imageptr denptr = NULL, velptr = NULL, outptr = NULL;
 
 real rad[MAXRING];
 int nrad;
 
-real pixe[MAXRING], flux[MAXRING], vsum[MAXRING], vsqu[MAXRING], wsum[MAXRING];
+real pixe[MAXRING], vsum[MAXRING], vsqu[MAXRING], wsum[MAXRING];
 real vrot[MAXRING];
 
 real pa, inc, vsys, xpos, ypos;
 real undf;
+
+string outmodes[] = { "vtan", "vres", "ome", "vtan/r", NULL};
 
 int ring_index(int n, real *r, real rad)
 {
@@ -61,11 +67,12 @@ int ring_index(int n, real *r, real rad)
 
 nemo_main()
 {
-  stream denstr, velstr, outstr;
+  stream denstr, velstr, outstr, tabstr;
+  string outmode;
   real center[2], cospa, sinpa, cosi, sini, cost, costmin, x, y, xt, yt, r, den;
   real vr, wt, frang, dx, dy, xmin, ymin, rmin, rmax, fsum, ave, tmp, rms;
-  real sincosi, cos2i, tga, dmin, dmax, dval;
-  int i, j, k, nx, ny, ir, nring, nundf, nout, nang, nsum, coswt;
+  real sincosi, cos2i, tga, dmin, dmax, dval, dr, area;
+  int i, j, k, nx, ny, ir, nring, nundf, nout, nang, nsum;
 
   velstr = stropen(getparam("in"),"r");
 
@@ -86,43 +93,53 @@ nemo_main()
     copy_image(velptr,&outptr);
   } 
 
+  if (hasvalue("tab")) {
+    Qtab = TRUE;
+    tabstr = stropen(getparam("tab"),"w");
+  } 
+
   nrad = nemoinpd(getparam("radii"),rad,MAXRING);
-  if (nrad < 2) error("got no rings, use radii=");
+  if (nrad < 2) error("got no rings (%d), use radii=",nrad);
   nring = nrad-1;
   if (hasvalue("center")) {
     if (nemoinpd(getparam("center"),center,2) != 2)
-      error("not enuf for center=");
+      error("not enuf values for center=, need 2");
     xpos = center[0];
     ypos = center[1];
   } else {
     xpos = (Nx(velptr)-1.0)/2.0;
     ypos = (Ny(velptr)-1.0)/2.0;
   }
-  pa = getdparam("pa");
-  inc = getdparam("inc");
-  vsys = getdparam("vsys");
-  undf = getdparam("blank");
+  pa    = getdparam("pa");
+  inc   = getdparam("inc");
+  vsys  = getdparam("vsys");
+  undf  = getdparam("blank");
   frang = getdparam("frang");
-  coswt = getiparam("coswt");
 
-  cospa = cos(pa*PI/180.0);
-  sinpa = sin(pa*PI/180.0);
-  sini = sin(inc*PI/180.0);
-  cosi = cos(inc*PI/180.0);
+  outmode = getparam("mode");
+
+
+  cospa   = cos(pa*PI/180.0);
+  sinpa   = sin(pa*PI/180.0);
+  sini    = sin(inc*PI/180.0);
+  cosi    = cos(inc*PI/180.0);
   costmin = sin(frang*PI/180.0);
   sincosi = sini*cosi;
-  cos2i = cosi*cosi;
+  cos2i   = cosi*cosi;
     
   for (i=0; i<nring; i++)
-    pixe[i] = flux[i] = vsum[i] = vsqu[i] = wsum[i] = 0.0;
+    pixe[i] = vsum[i] = vsqu[i] = wsum[i] = 0.0;
   nundf = nout = nang = 0;
 
   ymin = Ymin(velptr);
   xmin = Xmin(velptr);
   dx = Dx(velptr);       dx = ABS(dx);    dx = -dx;
   dy = Dy(velptr);       dy = ABS(dy);
-  rmin = nx*dx*10.0;
+  rmin = -nx*dx*10.0;
   rmax = 0.0;
+  dprintf(0,"Map %d x %d pixels, size %g x %g\n",
+	  Nx(velptr), Ny(velptr), -dx*Nx(velptr), dy*Ny(velptr));
+  dprintf(0,"Pixel size: %g x %g\n",-dx, dy);
 
   dmin = dmax = vsys;
 
@@ -137,25 +154,10 @@ nemo_main()
 	continue;
       }
       x = (i-xpos)*dx;
-      yt = x*sinpa + y*cospa;
-      xt = x*cospa - y*sinpa;
-#if 0
-      if (Qout) {
-	if (xt==0.0) {
-	  MapValue(outptr,i,j) = undf;
-	} else {
-	  tga = xt/yt; /* X and Y are reversed  */
-	  dval = MapValue(velptr,i,j)-vsys;
-	  if (dval < 0) dval = -dval;
-	  dval = vsys + dval*sqrt(cos2i+tga*tga)/sincosi;
-	  MapValue(outptr,i,j) = dval;
-	  if (dval > dmax) dmax = dval;
-	  if (dval < dmin) dmin = dval;
-	}
-      }
-#endif
-      xt /= cosi;
-      r  = sqrt(xt*xt+yt*yt);
+      yt = x*sinpa + y*cospa;      /* major axis now along Y  */
+      xt = x*cospa - y*sinpa;      /* minor axis along X      */
+      xt /= cosi;                  /* deproject to the circle */
+      r  = sqrt(xt*xt+yt*yt);      /* radius in the disk      */
       rmin = MIN(r,rmin);
       rmax = MAX(r,rmax);
       ir = ring_index(nrad,rad,r);
@@ -166,27 +168,34 @@ nemo_main()
 	continue;
       }
       cost = yt/r;
-      vr = (MapValue(velptr,i,j)-vsys)/cost/sini;
+      dval = MapValue(velptr,i,j);
 
-      dval = vr/r;
-      MapValue(outptr,i,j) = dval;
-      if (dval > dmax) dmax = dval;
-      if (dval < dmin) dmin = dval;
+      vr = dval;
+
+      if (outptr) {
+	if  (ABS(cost) > costmin) {
+	  MapValue(outptr,i,j) = dval;
+	  if (dval > dmax) dmax = dval;
+	  if (dval < dmin) dmin = dval;
+	} else {
+	  MapValue(outptr,i,j) = undf;
+	}
+      }
+
+      /* now some ring accumulation, remnant of the velfit fitting */
 
       den = Qden ? MapValue(denptr,i,j) : 1.0;
       wt = den;
-      for (k=0; k<coswt; k++)  wt *= cost;
       wt = ABS(wt);
       if (ABS(cost) > costmin) {
 	pixe[ir] += 1.0;
-	flux[ir] += den;
+	wsum[ir] += wt;
 	vsum[ir] += wt*vr;
 	vsqu[ir] += wt*vr*vr;
-	wsum[ir] += wt;
       } else
 	nang++;
-    }
-  }
+    } /* i */
+  } /* j */
 
   /* write output map(s), if needed */
   if (Qout) {
@@ -196,51 +205,7 @@ nemo_main()
     write_image(outstr,outptr);
   }
 
-
-  /* find the rotation curve */
-
-
-  for (i=0; i<nring; i++) {
-    if (pixe[i] < 1) continue;
-    if(wsum[i] != 0)
-      vrot[i] = vsum[i]/wsum[i];
-    else
-      vrot[i] = 0.0;
-    /* reset arrays for next round of accumulations */
-    wsum[i] = vsum[i] = vsqu[i] = 0.0;
-  }
-
-
-  /* loop over the map again, computing the residuals */
-
-  for (j=0; j<ny; j++) {
-    y = (j-ypos)*dy;
-    for (i=0; i<nx; i++) {
-      if (MapValue(velptr,i,j) == undf) continue;
-      x = (i-xpos)*dx;
-      yt =  x*sinpa + y*cospa;
-      xt = (x*cospa - y*sinpa)/cosi;
-      r  = sqrt(xt*xt+yt*yt);
-      rmin = MIN(r,rmin);
-      rmax = MAX(r,rmax);
-      ir = ring_index(nrad,rad,r);
-      if (ir < 0) continue;
-      cost = yt/r;
-      den = Qden ? MapValue(denptr,i,j) : 1.0;
-      wt = den;
-      for (k=0; k<coswt; k++)  wt *= cost;
-      wt = ABS(wt);
-      if (ABS(cost) > costmin) {
-	vr = MapValue(velptr,i,j)-vsys-vrot[ir]*cost*sini;
-	wsum[ir] += wt;
-	vsum[ir] += wt*vr;
-	vsqu[ir] += wt*vr*vr;
-	dprintf(1,"%d %g %g %g %g\n",ir,xt,yt,vr,wt);
-      }
-    }
-  }
-
-  /* report */
+  /* report on the rings */
 
   fsum = 0.0;
   nsum = 0;
@@ -248,15 +213,17 @@ nemo_main()
     if (wsum[i] == 0.0) continue;
     nsum++;
     r = 0.5*(rad[i] + rad[i+1]);
-    tmp = flux[i]/pixe[i];
+    dr = rad[i+1] - rad[i];
+    area = PI*(sqr(rad[i+1]) - sqr(rad[i]));
+    tmp = wsum[i]/pixe[i];
     ave = vsum[i]/wsum[i];
     rms = vsqu[i]/wsum[i]-ave*ave;
     if (rms <  0) rms=0.0;
     rms = sqrt(rms);
     fsum += rms*rms;
 
-    dprintf(1,"%g %g %g %g %g %g ;; %g %g %g\n",
-	   r,vrot[i],rms,pixe[i],tmp,sqrt(fsum/nsum),    ave,vsum[i],wsum[i]);
+    if (Qtab) fprintf(tabstr,"%g %g %g %g %g %g ;; %g %g\n",
+	   r,ave,rms,pixe[i],tmp,sqrt(fsum/nsum),    vsum[i],wsum[i]);
   }
 
   dprintf(0,"Nundf=%d/%d Nout=%d Nang=%d (sum=%d)\n",
