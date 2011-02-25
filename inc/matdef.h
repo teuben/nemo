daeaa62aa88ea2895d62af081c030089ed2c33a6
Peter Teuben <teuben@gmail.com>
2011-02-25 03:51:43 +0000

    mixup

--- inc/matdef.h
+++ inc/matdef.h
@@ -3,13 +3,13 @@
  *		This include file is automatically included by image.h
  *		and perhaps by a few others, e.g. contour.c
  *
- *	#define CDEF			c-definition        = column major
- *	#define FORDEF			fortran-definition  = row major
+ *	#define CDEF			c-definition        = row major
+ *	#define FORDEF			fortran-definition  = column major
  *
  *	The default is CDEF
  * 
- *   C:  data[row][col]     column major,  column after column
- *   F:  data(row,col)      row major,     row after row
+ *   C:  data[row][col]     row major,     row after row
+ *   F:  data(row,col)      column major,  column after column
  *
  */
 
