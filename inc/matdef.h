ae71edf1590154e5d02b08321079e395fee166dd
Peter Teuben <teuben@gmail.com>
2011-02-25 03:48:33 +0000

    clarify

--- inc/matdef.h
+++ inc/matdef.h
@@ -3,13 +3,18 @@
  *		This include file is automatically included by image.h
  *		and perhaps by a few others, e.g. contour.c
  *
- *	#define CDEF			c-definition
- *	#define FORDEF			fortran-definition
+ *	#define CDEF			c-definition        = column major
+ *	#define FORDEF			fortran-definition  = row major
  *
  *	The default is CDEF
+ * 
+ *   C:  data[row][col]     column major,  column after column
+ *   F:  data(row,col)      row major,     row after row
  *
  */
+
 #define CDEF 
+
 #if !defined(FORDEF) && !defined(CDEF)
 # define FORDEF
 #endif
