674ba8f0970b69d1e169d267353cf03572d6e314
Peter Teuben <teuben@gmail.com>
2011-08-11 16:09:46 +0000

    added a reference and solar radius/lum

--- inc/cgs.h
+++ inc/cgs.h
@@ -3,6 +3,10 @@
  *
  *  See also: "The Fundamental Physical Constants" by E. Richard
  *     Cohen and Barry N. Taylor (PHYICS TODAY, August 1989).
+ * 
+ *  A recent discussion:
+ *  Harmanec & Prsa, PASP 123, 976 (2011)
+ *      http://www.jstor.org/stable/10.1086/661258
  */
 
 
@@ -11,7 +15,10 @@
 #define h_CGS 6.6260755e-27     /* Planck [erg-sec] */
 #define G_CGS 6.67259e-8	/* gravitional constant */
 
-#define Msol_CGS 1.99e33	/* solar mass in grams */
+#define Msol_CGS 1.988547e33    /* solar mass in grams */
+#define Rsol_CGS 6.95508e10     /* solar radius in cm */
+#define Lsol_CGS 3.846e33       /* solar lum in ergs/sec */
+
 #define mH_CGS 1.67e-24		/* mass of hydrogen atom in grams */
 
 #define KPCCM 3.085678e+21	/* kiloparsec in centimeters */
