73b3625cc012a9ac1520cbee63663952028f82e7
Peter Teuben <teuben@gmail.com>
2012-01-10 17:19:20 +0000

    implemented the laplacian

--- src/image/trans/ccdsharp3.c
+++ src/image/trans/ccdsharp3.c
@@ -41,116 +41,120 @@ extern int match(string, string, int *);
 
 void nemo_main()
 {
-    stream  instr, outstr;
-    int     nx, ny, nz, mode;
-    int     i,j,k;
-    imageptr iptr1=NULL, iptr2=NULL, optr;      /* pointer to images */
-    real    d1, d2, d3, d4, dx, dy;
-    bool    Qsym = TRUE;            /* symmetric derivates w.r.t. pixel point */
-
-    match(getparam("mode"),valid_modes,&mode);
-    if (mode==0) error("Not a valid mode; valid:%s",valid_modes);
-    dprintf(0,"Image sharpening method #%d\n",mode);
-
-    instr = stropen(getparam("in"), "r");
-    read_image( instr, &iptr1);
-    nx = Nx(iptr1);	
-    ny = Ny(iptr1);
-    nz = Nz(iptr1);
-    dx = Dx(iptr1);
-    dy = Dy(iptr1);
-    if (mode & MODE_DIV || mode & MODE_VORT) {
-        if (read_image(instr,&iptr2) == 0)
-            error("No second image found in %s\n",getparam("in"));
-        if (nx != Nx(iptr2))  
-            error("Second image doesn't match in NX: %d <> %d\n",Nx(iptr2),nx);
-        if (ny != Ny(iptr2))  
-            error("Second image doesn't match in NY: %d <> %d\n",Ny(iptr2),ny);
-        if (nz != Nz(iptr2))  
-            error("Second image doesn't match in NZ: %d <> %d\n",Nz(iptr2),nz);
+  stream  instr, outstr;
+  int     nx, ny, nz, mode;
+  int     i,j,k;
+  imageptr iptr1=NULL, iptr2=NULL, optr;      /* pointer to images */
+  real    d1, d2, d3, d4, d5, d6, dx, dy, dz;
+  bool    Qsym = TRUE;            /* symmetric derivates w.r.t. pixel point */
+  
+  match(getparam("mode"),valid_modes,&mode);
+  if (mode==0) error("Not a valid mode; valid:%s",valid_modes);
+  dprintf(0,"Image sharpening method #%d\n",mode);
+  
+  instr = stropen(getparam("in"), "r");
+  read_image( instr, &iptr1);
+  nx = Nx(iptr1);	
+  ny = Ny(iptr1);
+  nz = Nz(iptr1);
+  dx = Dx(iptr1);
+  dy = Dy(iptr1);
+  dz = Dz(iptr1);
+  if (mode & MODE_DIV || mode & MODE_VORT) {
+    if (read_image(instr,&iptr2) == 0)
+      error("No second image found in %s\n",getparam("in"));
+    if (nx != Nx(iptr2))  
+      error("Second image doesn't match in NX: %d <> %d\n",Nx(iptr2),nx);
+    if (ny != Ny(iptr2))  
+      error("Second image doesn't match in NY: %d <> %d\n",Ny(iptr2),ny);
+    if (nz != Nz(iptr2))  
+      error("Second image doesn't match in NZ: %d <> %d\n",Nz(iptr2),nz);
+  }
+  
+  outstr = stropen(getparam("out"), "w");
+  create_cube(&optr,nx,ny,nz);
+  Dx(optr) = Dx(iptr1);
+  Dy(optr) = Dy(iptr1);
+  Dz(optr) = Dz(iptr1);
+  Xmin(optr) = Xmin(iptr1);
+  Ymin(optr) = Ymin(iptr1);
+  Zmin(optr) = Zmin(iptr1);
+  
+  if (mode & MODE_LAPLACE) {
+    for (k=1; k<nz-1; k++) {
+      for (j=1; j<ny-1; j++) {
+	for (i=1; i<nx-1; i++) {
+	  d1 = CV1(i,j,k) - CV1(i-1,j,k);
+	  d2 = CV1(i,j,k) - CV1(i+1,j,k);
+	  d3 = CV1(i,j,k) - CV1(i,j-1,k);
+	  d4 = CV1(i,j,k) - CV1(i,j+1,k);
+	  d5 = CV1(i,j,k) - CV1(i,j,k-1);
+	  d6 = CV1(i,j,k) - CV1(i,j,k+1);
+	  CVO(i,j,k) = sqrt(d1*d1+d2*d2+d3*d3+d4*d4+d5*d5+d6*d6);
+	}
+	CVO(0,j,k)    = 0.0;
+	CVO(nx-1,j,k) = 0.0;
+      }
+      for (i=0; i<nx; i++)
+	CVO(i,0,k)    = CVO(i,ny-1,k) = 0.0;
     }
-
-    outstr = stropen(getparam("out"), "w");
-    create_cube(&optr,nx,ny,nz);
-    Dx(optr) = Dx(iptr1);
-    Dy(optr) = Dy(iptr1);
-    Dz(optr) = Dz(iptr1);
-    Xmin(optr) = Xmin(iptr1);
-    Ymin(optr) = Ymin(iptr1);
-    Zmin(optr) = Zmin(iptr1);
-    /* should do the others too */
-
-    if (mode & MODE_LAPLACE) {
-        for (k=0; k<nz; k++) {
-            for (j=1; j<ny-1; j++) {
-                for (i=1; i<nx-1; i++) {
-                    d1 = CV1(i,j,k) - CV1(i-1,j,k);
-                    d2 = CV1(i,j,k) - CV1(i+1,j,k);
-                    d3 = CV1(i,j,k) - CV1(i,j-1,k);
-                    d4 = CV1(i,j,k) - CV1(i,j+1,k);
-                    CVO(i,j,k) = sqrt(d1*d1+d2*d2+d3*d3+d4*d4);
-                }
-                CVO(0,j,k) = 0.0;
-                CVO(nx-1,j,k) = 0.0;
-            }
-            for (i=0; i<nx; i++) {
-                CVO(i,0,k) = 0.0;
-                CVO(i,ny-1,k) = 0.0;
-            }
-        }
-    } else if (mode & MODE_DIV || mode & MODE_VORT) {
-        for (k=0; k<nz; k++) {
-            for (j=0; j<ny-1; j++) {
-                for (i=0; i<nx-1; i++) {
-                    if (Qsym) {
-                        if (i>0 && j>0) {
-                            d1 = 0.5*(CV1(i+1,j,k) - CV1(i-1,j,k));         /* dv_x/dx */
-                            d2 = 0.5*(CV1(i,j+1,k) - CV1(i,j-1,k));         /* dv_x/dy */
-                            d3 = 0.5*(CV2(i+1,j,k) - CV2(i-1,j,k));         /* dv_y/dx */
-                            d4 = 0.5*(CV2(i,j+1,k) - CV2(i,j-1,k));         /* dv_y/dy */
-                        } else
-                            d1 = d2 = d3 = d4 = 0.0;
-                    } else {
-                        d1 = CV1(i+1,j,k) - CV1(i,j,k);         /* dv_x/dx */
-                        d2 = CV1(i,j+1,k) - CV1(i,j,k);         /* dv_x/dy */
-                        d3 = CV2(i+1,j,k) - CV2(i,j,k);         /* dv_y/dx */
-                        d4 = CV2(i,j+1,k) - CV2(i,j,k);         /* dv_y/dy */
-                    }
-                    if (mode&MODE_DIV)
-                        CVO(i,j,k) = d1/dx + d4/dy;
-                    else if (mode&MODE_VORT)
-                        CVO(i,j,k) = d3/dx - d2/dy;
-                }
-                CVO(nx-1,j,k) = 0.0;
-            }
-            for (i=0; i<nx; i++) {
-                CVO(i,ny-1,k) = 0.0;
-            }
-        }
-    } else if (mode & MODE_AREGAN || mode & MODE_PREGAN) {
-        for (k=0; k<nz; k++) {
-            for (j=0; j<ny-1; j++) {
-                for (i=0; i<nx-1; i++) {
-                    d1 = CV1(i,j,k)   - CV1(i+1,j,k);
-                    d2 = CV1(i,j+1,k) - CV1(i+1,j+1,k);
-                    d3 = CV1(i,j,k)   - CV1(i,j+1,k);
-                    d4 = CV1(i+1,j,k) - CV1(i+1,j+1,k);
-                    if (mode&MODE_AREGAN)
-                        CVO(i,j,k) = sqrt(sqr(d1+d2)+sqr(d3+d4))/2;
-                    else {
-                        if (d3+d4==0.0 && d1+d2==0.0)
-                            CVO(i,j,k) = 0.0;
-                        else
-                            CVO(i,j,k) = atan2(d3+d4,d1+d2) * 180 / PI;
-                    }
-                }
-                CVO(nx-1,j,k) = 0.0;
-            }
-            for (i=0; i<nx; i++) {
-                CVO(i,ny-1,k) = 0.0;
-            }
-        }
+    for(j=0; j<ny; j++)
+      for(i=0; i<nx; i++)
+	CVO(i,j,0)    = CVO(i,j,nz-1) = 0.0;
+  } else if (mode & MODE_DIV || mode & MODE_VORT) {
+    warning("only 2D implemented");
+    for (k=0; k<nz; k++) {
+      for (j=0; j<ny-1; j++) {
+	for (i=0; i<nx-1; i++) {
+	  if (Qsym) {
+	    if (i>0 && j>0) {
+	      d1 = 0.5*(CV1(i+1,j,k) - CV1(i-1,j,k));         /* dv_x/dx */
+	      d2 = 0.5*(CV1(i,j+1,k) - CV1(i,j-1,k));         /* dv_x/dy */
+	      d3 = 0.5*(CV2(i+1,j,k) - CV2(i-1,j,k));         /* dv_y/dx */
+	      d4 = 0.5*(CV2(i,j+1,k) - CV2(i,j-1,k));         /* dv_y/dy */
+	    } else
+	      d1 = d2 = d3 = d4 = 0.0;
+	  } else {
+	    d1 = CV1(i+1,j,k) - CV1(i,j,k);         /* dv_x/dx */
+	    d2 = CV1(i,j+1,k) - CV1(i,j,k);         /* dv_x/dy */
+	    d3 = CV2(i+1,j,k) - CV2(i,j,k);         /* dv_y/dx */
+	    d4 = CV2(i,j+1,k) - CV2(i,j,k);         /* dv_y/dy */
+	  }
+	  if (mode&MODE_DIV)
+	    CVO(i,j,k) = d1/dx + d4/dy;
+	  else if (mode&MODE_VORT)
+	    CVO(i,j,k) = d3/dx - d2/dy;
+	}
+	CVO(nx-1,j,k) = 0.0;
+      }
+      for (i=0; i<nx; i++) {
+	CVO(i,ny-1,k) = 0.0;
+      }
     }
-    write_image(outstr, optr);
-
+  } else if (mode & MODE_AREGAN || mode & MODE_PREGAN) {
+    warning("only 2D implemented");
+    for (k=0; k<nz; k++) {
+      for (j=0; j<ny-1; j++) {
+	for (i=0; i<nx-1; i++) {
+	  d1 = CV1(i,j,k)   - CV1(i+1,j,k);
+	  d2 = CV1(i,j+1,k) - CV1(i+1,j+1,k);
+	  d3 = CV1(i,j,k)   - CV1(i,j+1,k);
+	  d4 = CV1(i+1,j,k) - CV1(i+1,j+1,k);
+	  if (mode&MODE_AREGAN)
+	    CVO(i,j,k) = sqrt(sqr(d1+d2)+sqr(d3+d4))/2;
+	  else {
+	    if (d3+d4==0.0 && d1+d2==0.0)
+	      CVO(i,j,k) = 0.0;
+	    else
+	      CVO(i,j,k) = atan2(d3+d4,d1+d2) * 180 / PI;
+	  }
+	}
+	CVO(nx-1,j,k) = 0.0;
+      }
+      for (i=0; i<nx; i++) {
+	CVO(i,ny-1,k) = 0.0;
+      }
+    }
+  }
+  write_image(outstr, optr);
 }
