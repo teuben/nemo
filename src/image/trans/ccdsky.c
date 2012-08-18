ecc0f9c91097d6d11786461b4d74092793a86ee1
Peter Teuben <teuben@gmail.com>
2012-08-18 01:21:01 +0000

    cleanup

--- src/image/trans/ccdsky.c
+++ src/image/trans/ccdsky.c
@@ -21,7 +21,7 @@ string defv[] = {
   "d=1,pc\n       Distance to object",
   "r=1,AU\n       Length scale of object",
   "v=1,km/s\n     Velocity scale of object",
-  "iscale=1       Scale image values [not implemented]",
+  "scale=1        Scale image values [not implemented]",
   "VERSION=1.0\n  17-aug-2012 PJT",
   NULL,
 };
@@ -30,14 +30,17 @@ string usage = "lazy sky scaling calculator";
 
 string cvsid = "$Id$";
 
-
 
 #define CVI(x,y,z)  CubeValue(iptr,x,y,z)
-#define CVO(x,y,z)  CubeValue(optr,x,y,z)
 
 
 #define MAXU 64
 
+
+
+/*
+ *     get_nu :   parse a   NUMBER,UNIT    string
+ */
 
 void get_nu(string kv,real *value,string unit, string defunit)
 {
@@ -46,19 +49,19 @@ void get_nu(string kv,real *value,string unit, string defunit)
   int nv;
 
   *value = 1.0;
-  strcpy(unit,"");
+  strcpy(unit,defunit);
   if (nsp == 0) return;
 
   nv = nemoinpr(sp[0],value,1);
   if (nv != 1) error("error parsing %s",sp[0]);
-  if (nsp == 1) {
-    strcpy(unit,defunit);
-    return;
-  }
+  if (nsp == 1) return;
 
   strcpy(unit,sp[1]);
-
 }
+
+/*
+ * efactor:  scaling factor between two common units
+ */
 
 real efactor(string u1, string u2)
 {
@@ -68,6 +71,8 @@ real efactor(string u1, string u2)
       s = 1.0;
     else if (streq(u2,"pc")) 
       s = PC/AU;
+    else if (streq(u2,"km")) 
+      s = 1000.0/AU;
     else if (streq(u2,"Kpc") || streq(u2,"kpc"))
       s = 1e3*PC/AU;
     else if (streq(u2,"Mpc") || streq(u2,"mpc"))
@@ -87,15 +92,6 @@ real efactor(string u1, string u2)
       s = AU/PC;
     else
       warning("Comparison unit %s not understood for %s",u2,u1);
-  } else if (streq(u1,"pc")) {
-    warning("u1=pc not implemented");
-  } else if (streq(u1,"km/s")) {
-    if (streq(u2,"km/s")) 
-      s = 1;
-    else if  (streq(u2,"m/s")) 
-      s = 1e-3;
-    else
-      warning("Comparison unit %s not understood for %s",u2,u1);      
   } else if (streq(u1,"m/s")) {
     if (streq(u2,"m/s")) 
       s = 1;
@@ -105,17 +101,19 @@ real efactor(string u1, string u2)
       warning("Comparison unit %s not understood for %s",u2,u1);      
   } else
     warning ("Comparison unit %s not understood",u1);
-  dprintf(0,"u1=%s u2=%s  s=%g\n",u1,u2,s);
+  dprintf(1,"u1=%s u2=%s  s=%g\n",u1,u2,s);
   return s;
 }
-
+
 void nemo_main()
 {
     stream  instr, outstr;
     int     nx, ny, nz;
     imageptr iptr=NULL;
-    real d,r,v,  rscale, vscale;
+    real d,r,v,  rscale, vscale, iscale = getrparam("scale");
     char ds[MAXU], rs[MAXU], vs[MAXU];
+
+    /*  get the distance and scales      */
 
     get_nu(getparam("d"),&d,ds,"pc");
     get_nu(getparam("r"),&r,rs,"AU");
@@ -125,11 +123,12 @@ void nemo_main()
     printf("r=%g %s\n",r,rs);
     printf("v=%g %s\n",v,vs);
 
+
     /* angles: convert to deg for FITS */
     rscale = (r * AU) / ( d * PC) * 180.0/PI;   
     rscale *= efactor("AU",rs);
     rscale /= efactor("pc",ds);
-    printf("rscale=%g\n",rscale);
+    printf("rscale=%g  (%g arcsec)\n",rscale,rscale*3600.0);
 
     /* velocities:  convert to m/s for FITS */
     vscale = v;
@@ -137,7 +136,7 @@ void nemo_main()
     printf("vscale=%g\n",vscale);
     
 
-    if (hasvalue("in") && hasvalue("out")) {
+    if (hasvalue("in") && hasvalue("out")) {      /* patch image if needed */
       instr = stropen(getparam("in"), "r");
       read_image( instr, &iptr);
       Xmin(iptr) *= rscale;
@@ -146,6 +145,9 @@ void nemo_main()
       Dx(iptr) *= rscale;
       Dy(iptr) *= rscale;
       Dz(iptr) *= vscale;
+      if (iscale != 1.0) {
+	warning("scale=%g not implemented yet",iscale);
+      }
 
       outstr = stropen(getparam("out"), "w");
       write_image(outstr, iptr);
