49aca1bcff9d4e0b987352b518c00cba7e89e3cb
Peter Teuben <teuben@gmail.com>
2012-08-22 19:31:36 +0000

    add another median method

--- src/kernel/misc/median.c
+++ src/kernel/misc/median.c
@@ -1,7 +1,11 @@
 /*
- * Median: find the median of an array
+ * Median: find the median of an array (various implementations)
+ *
+ *    see also discussions in: 
+ *    http://ndevilla.free.fr/median/median/index.html
  *
  *	20-jun-01	gcc3
+ *       6-aug-2012     added median_torben
  */
 
 #include <stdinc.h>
@@ -81,4 +85,90 @@ void finis_median(void)
     nix = 0;
     last_n = 0;
     last_x = NULL;
+}
+
+
+/*
+ *
+ */
+
+
+real median_torben(int n, real *x, real xmin, real xmax)
+{
+  real guess, max_lt_guess, min_gt_guess;
+  int i, k=0, lt_count, gt_count, eq_count;
+  int n2 = (n+1)/2;
+  
+  while(++k) {           /* iterate, and keep a convenience counter */
+    guess = 0.5*(xmin+xmax);
+    lt_count = gt_count = eq_count = 0;
+    max_lt_guess = xmin;
+    min_gt_guess = xmax;
+    for (i=0; i<n; i++) {
+      if (x[i] < guess) {
+	lt_count++;
+	if (x[i] > max_lt_guess) max_lt_guess = x[i];
+      } else if (x[i] > guess) {
+	gt_count++;
+	if (x[i] < min_gt_guess) min_gt_guess = x[i];
+      } else
+	eq_count++;
+    }
+    if (lt_count <= n2 && gt_count <= n2)
+      break;
+    else if (lt_count > gt_count)
+      xmax = max_lt_guess;
+    else
+      xmin = min_gt_guess;
+  }
+  dprintf(1,"median_torben: iter k=%d\n",k);
+  if (lt_count >= n2)
+    return max_lt_guess;
+  else if (lt_count + eq_count >= n2)
+    return guess;
+  else
+    return min_gt_guess;
+  /* will never get here */
+  return guess;  
+}
+
+/*
+ * wirth's algorithm
+ */
+
+#define ELEM_SWAP(a,b) { register real t=(a);(a)=(b);(b)=t; }
+
+real kth_smallest(int n, real  *x,  int k)
+{
+  register int i,j,l,m;
+  register real xi;
+
+  l=0 ; m=n-1 ;
+  while (l<m) {
+    xi=x[k] ;
+    i=l ;
+    j=m ;
+    do {
+      while (x[i]<xi) i++ ;
+      while (xi<x[j]) j-- ;
+      if (i<=j) {
+	ELEM_SWAP(x[i],x[j]) ;
+	i++ ; j-- ;
+      }
+    } while (i<=j) ;
+    if (j<k) l=i ;
+    if (k<i) m=j ;
+  }
+  return x[k] ;
+}
+
+
+real median_wirth(int n, real  *x) 
+{
+  if (n&1) 
+    return kth_smallest(n, x,   n/2);      /* odd, picks the middle */
+  else
+    return kth_smallest(n, x,   n/2 - 1);  /* even, a slight cheat */
+  /* never reached */
+  return x[0];
 }
