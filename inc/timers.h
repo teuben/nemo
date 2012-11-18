55d592e66984ba8fda3be740c5329d8bbafb6c76
Peter Teuben <teuben@gmail.com>
2012-11-18 18:20:51 +0000

    new cycle.h that timers should use

--- inc/timers.h
+++ inc/timers.h
@@ -1,6 +1,8 @@
 
 /* timers.c */
 
+/* @todo:  should use the new more portable cycle.h  */
+
 long long readTSC(void);	/* not sure if we keep this public */
 void init_timers(int n);
 void stamp_timers(int i);
