976892ceac483821a8761881fa0e3f1d80c138d9
Peter Teuben <teuben@gmail.com>
2013-04-24 02:54:47 +0000

    allow output per Z plane

--- src/image/misc/ccdstat.c
+++ src/image/misc/ccdstat.c
@@ -12,6 +12,8 @@
  *      24-may-06   1.8 add mmcount=
  *       6-nov-06   1.9 output the total mass/luminosity as well as sum of densities
  *      13-feb-13   2.0 ignore axis if N=1 
+ *      17-apr-13   3.0 allow each Z plane to be treated seperately
+ *      23-apr-13       half= option, use compute_robust_moment()
  *
  */
 
@@ -27,15 +29,17 @@ string defv[] = {
     "max=\n         Maximum overrride",
     "bad=\n         Use this as masking value to ignore data",
     "win=\n         Optional input map for weights",
-    "npar=0\n       Number of fitting parameters assumed for chi2 calc",
+    "npar=0\n       Number of fitting parameters assumed for chi2 calc (full data only)",
     "nppb=1\n       Optional correction 'number of points per beam' for chi2 calc",
     "median=f\n     Optional display of the median value",
     "robust=f\n     Compute robust median",
     "mmcount=f\n    Count occurances of min and max",
+    "half=f\n       Only use half (negative) values and symmetrize them",
     "maxmom=4\n     Control how many moments are computed",
     "ignore=t\n     (for summing) Ignore cell width when N=1 (assumed infinity)",
     "sort=qsort\n   Sorting routine (not activated yet)",
-    "VERSION=2.0\n  13-feb-2013 PJT",
+    "planes=-1\n    -1: whole cube in one      0=all planes   start:end:step = selected planes",
+    "VERSION=3.0\n  17-apr-2013 PJT",
     NULL,
 };
 
@@ -54,22 +58,25 @@ int    nx,ny,nz,nsize;			/* actual size of map */
 double xmin,ymin,zmin,dx,dy,dz;
 double size;				/* size of frame (square) */
 double cell;				/* cell or pixel size (square) */
+int *planes = NULL;                     /* selected planes , if used */
 
 real get_median(int n, real *x);
 
 
 nemo_main()
 {
-    int  i, j, k;
+    int  i, j, k, ki;
     real x, xmin, xmax, mean, sigma, skew, kurt, median, bad, w, *data;
     real sum, sov;
     Moment m;
     bool Qmin, Qmax, Qbad, Qw, Qmedian, Qrobust, Qmmcount = getbparam("mmcount");
-    bool Qx, Qy, Qz, Qone = getbparam("ignore");
+    bool Qx, Qy, Qz, Qone, Qall, Qign = getbparam("ignore");
+    bool Qhalf = getbparam("half");
     real nu, nppb = getdparam("nppb");
     int npar = getiparam("npar");
-    int ngood = 0;
+    int ngood;
     int ndat = 0;
+    int nplanes;
     int min_count, max_count;
     int maxmom = getiparam("maxmom");
     char slabel[32];
@@ -77,21 +84,40 @@ nemo_main()
     instr = stropen (getparam("in"), "r");
     read_image (instr,&iptr);
     strclose(instr);
+    nx = Nx(iptr);	
+    ny = Ny(iptr);
+    nz = Nz(iptr);
+#if 0
+    dprintf(1,"# data order debug:  %f %f\n",Frame(iptr)[0], Frame(iptr)[1]);
+#endif    
 
+    planes = (int *) allocate((nz+1)*sizeof(int));
+    nplanes = nemoinpi(getparam("planes"),planes,nz+1);
+    Qall = (planes[0]==-1);
+    if (planes[0]==0) {
+      Qone = FALSE;
+      nplanes = nz;
+      for (k=0; k<nz; k++)
+	planes[k] = k;
+    } else if (!Qall) {
+      Qone = (!Qall && nplanes==1);
+      for (k=0; k<nplanes; k++) {
+	if (planes[k]<1 || planes[k]>nz) error("%d is an illegal plane [1..%d]",planes[k],nz);
+	planes[k]--;
+      }
+    }
+ 
     if (hasvalue("win")) {
       instr = stropen (getparam("win"), "r");
       read_image (instr,&wptr);
       strclose(instr);
-      if (Nx(iptr) != Nx(wptr)) error("X sizes of in/win don't match");
-      if (Ny(iptr) != Ny(wptr)) error("X sizes of in/win don't match");
-      if (Nz(iptr) != Nz(wptr)) error("X sizes of in/win don't match");
+      if (Nx(iptr) != Nx(wptr)) error("X sizes of in=/win= don't match");
+      if (Ny(iptr) != Ny(wptr)) error("Y sizes of in=/win= don't match");
+      if (Nz(iptr) != Nz(wptr)) error("Z sizes of in=/win= don't match");
       Qw = TRUE;
     } else
       Qw = FALSE;
 
-    nx = Nx(iptr);	
-    ny = Ny(iptr);
-    nz = Nz(iptr);
     Qmin = hasvalue("min");
     if (Qmin) xmin = getdparam("min");
     Qmax = hasvalue("max");
@@ -105,12 +131,10 @@ nemo_main()
       data = (real *) allocate(ndat*sizeof(real));
     }
 
-
-    sov = 1.0;
-
-    Qx = Qone && Nx(iptr)==1;
-    Qy = Qone && Ny(iptr)==1;
-    Qz = Qone && Nz(iptr)==1;
+    sov = 1.0;       /* volume of a pixel/voxel */
+    Qx = Qign && Nx(iptr)==1;
+    Qy = Qign && Ny(iptr)==1;
+    Qz = Qign && Nz(iptr)==1;
     sov *= Qx ? 1.0 : Dx(iptr);
     sov *= Qy ? 1.0 : Dy(iptr);
     sov *= Qz ? 1.0 : Dz(iptr);
@@ -123,11 +147,98 @@ nemo_main()
       warning("No work done, maxmom<0");
       stop(0);
     }
-    ini_moment(&m,maxmom,ndat);
-#if 1
-    for (i=0; i<nx; i++) {
-      for (j=0; j<ny; j++) {
-        for (k=0; k<nz; k++) {
+
+    if (Qall) {
+
+      ini_moment(&m,maxmom,ndat);
+      ngood = 0;
+      for (k=0; k<nz; k++) {
+#if 0
+	if (Qone && k != planes[0]) continue;
+#endif
+	for (j=0; j<ny; j++) {
+	  for (i=0; i<nx; i++) {
+            x =  CubeValue(iptr,i,j,k);
+	    if (Qhalf && x>=0.0) continue;
+            if (Qmin && x<xmin) continue;
+            if (Qmax && x>xmax) continue;
+            if (Qbad && x==bad) continue;
+	    w = Qw ? CubeValue(wptr,i,j,k) : 1.0;
+            accum_moment(&m,x,w);
+	    if (Qhalf && x<0) accum_moment(&m,-x,w);
+	    if (Qmedian) data[ngood++] = x;
+	  }
+	}
+      }
+      
+      if (npar > 0) {
+	nu = n_moment(&m)/nppb - npar;
+	if (nu < 1) error("%g: No degrees of freedom",nu);
+	printf("chi2= %g\n", show_moment(&m,2)/nu/nppb);
+	printf("df= %g\n", nu);
+      } else {
+	nsize = nx * ny * nz;
+	sum = mean = sigma = skew = kurt = 0;
+	if (maxmom > 0) {
+	  mean = mean_moment(&m);
+	  sum = show_moment(&m,1);
+	}
+	if (maxmom > 1)
+	  sigma = sigma_moment(&m);
+	if (maxmom > 2)
+	  skew = skewness_moment(&m);
+	if (maxmom > 3)
+	  kurt = kurtosis_moment(&m);
+	
+	printf ("Min and Max           : %f %f\n",min_moment(&m), max_moment(&m));
+	printf ("Number of points      : %d\n",n_moment(&m));
+	printf ("Mean and dispersion   : %f %f\n",mean,sigma);
+	printf ("Skewness and kurtosis : %f %f\n",skew,kurt);
+	printf ("Sum and Sum*%s        : %f %f\n",sum, slabel, sum*sov);
+	if (Qmedian) {
+	  printf ("Median                : %f\n",get_median(ngood,data));
+	  if (ndat>0)
+	    printf ("Median                : %f\n",median_moment(&m));
+	}
+	if (Qrobust) {
+	  printf ("Mean Robust           : %f\n",mean_robust_moment(&m));
+	  printf ("Sigma Robust          : %f\n",sigma_robust_moment(&m));
+	  printf ("Median Robust         : %f\n",median_robust_moment(&m));
+	}
+
+	if (Qmmcount) {
+	  min_count = max_count = 0;
+	  xmin = min_moment(&m);
+	  xmax = max_moment(&m);
+	  for (i=0; i<nx; i++) {
+	    for (j=0; j<ny; j++) {
+	      for (k=0; k<nz; k++) {
+		x =  CubeValue(iptr,i,j,k);
+		if (x==xmin) min_count++;
+		if (x==xmax) max_count++;
+	      }
+	    }
+	  } /* i */
+	  printf("Min_Max_count         : %d %d\n",min_count,max_count);
+	}
+	printf ("%d/%d out-of-range points discarded\n",nsize-n_moment(&m), nsize);
+      }
+
+    } else { 
+
+      /* tabular output, one line per (selected) plane */
+
+      printf("# p  min  max  N  mean sigma skew kurt sum sum ");
+      if (Qmedian) printf(" [med med]");
+      if (Qrobust) printf(" robust[N mean sig med]");
+      printf("\n");
+
+      for (ki=0; ki<nplanes; ki++) {
+	k = planes[ki];
+	ini_moment(&m,maxmom,ndat);
+	ngood = 0;
+	for (j=0; j<ny; j++) {
+	  for (i=0; i<nx; i++) {
             x =  CubeValue(iptr,i,j,k);
             if (Qmin && x<xmin) continue;
             if (Qmax && x>xmax) continue;
@@ -135,78 +246,51 @@ nemo_main()
 	    w = Qw ? CubeValue(wptr,i,j,k) : 1.0;
             accum_moment(&m,x,w);
 	    if (Qmedian) data[ngood++] = x;
-        }
-      }
-    }
-#else
-	/* for test/benchmark */
-    w = 1.0 ;
-    for (i=0; i<nx; i++) {
-      for (j=0; j<ny; j++) {    
-         for (k=0; k<nz; k++) {
-            x =  CubeValue(iptr,i,j,k);
-            //if (Qmin && x<xmin) continue;
-            //if (Qmax && x>xmax) continue;
-            //if (Qbad && x==bad) continue;
-	    // Qw ? CubeValue(wptr,i,j,k) : 1.0;            
-            accum_moment(&m,x,w);
-	    // if (Qmedian) data[ngood++] = x;
-        }
-      }
-    }
+	  }
+	}
 
-#endif
-    if (npar > 0) {
-      nu = n_moment(&m)/nppb - npar;
-      if (nu < 1) error("%g: No degrees of freedom",nu);
-      printf("chi2= %g\n", show_moment(&m,2)/nu/nppb);
-      printf("df= %g\n", nu);
-    } else {
-      nsize = nx * ny * nz;
-      sum = mean = sigma = skew = kurt = 0;
-      if (maxmom > 0) {
-	mean = mean_moment(&m);
-        sum = show_moment(&m,1);
-      }
-      if (maxmom > 1)
-	sigma = sigma_moment(&m);
-      if (maxmom > 2)
-	skew = skewness_moment(&m);
-      if (maxmom > 3)
-	kurt = kurtosis_moment(&m);
-
-      printf ("Min=%f  Max=%f\n",min_moment(&m), max_moment(&m));
-      printf ("Number of points      : %d\n",n_moment(&m));
-      printf ("Mean and dispersion   : %f %f\n",mean,sigma);
-      printf ("Skewness and kurtosis : %f %f\n",skew,kurt);
-      printf ("Sum and Sum*%s        : %f %f\n",sum, slabel, sum*sov);
-      if (Qmedian) {
-	printf ("Median                : %f\n",get_median(ngood,data));
-	if (ndat>0)
-	  printf ("Median                : %f\n",median_moment(&m));
-      }
-      if (Qrobust) {
-	  printf ("Mean Robust           : %f\n",mean_robust_moment(&m));
-	  printf ("Sigma Robust          : %f\n",sigma_robust_moment(&m));
-	  printf ("Median Robust         : %f\n",median_robust_moment(&m));
-      }
-
-      if (Qmmcount) {
-	min_count = max_count = 0;
-	xmin = min_moment(&m);
-	xmax = max_moment(&m);
-	for (i=0; i<nx; i++) {
-	  for (j=0; j<ny; j++) {
-	    for (k=0; k<nz; k++) {
+	nsize = nx * ny * nz;
+	sum = mean = sigma = skew = kurt = 0;
+	if (maxmom > 0) {
+	  mean = mean_moment(&m);
+	  sum = show_moment(&m,1);
+	}
+	if (maxmom > 1)
+	  sigma = sigma_moment(&m);
+	if (maxmom > 2)
+	  skew = skewness_moment(&m);
+	if (maxmom > 3)
+	  kurt = kurtosis_moment(&m);
+	printf("%d %f %f %d  %f %f %f %f  %f %f",
+	       k+1, min_moment(&m), max_moment(&m), n_moment(&m),
+	       mean,sigma,skew,kurt,sum,sum*sov);
+	if (Qmedian) {
+	  printf ("   %f",get_median(ngood,data));
+	  if (ndat>0) printf (" %f",median_moment(&m));
+	}
+	if (Qrobust) {
+	  compute_robust_moment(&m);
+	  printf ("   %d %f %f %f",n_robust_moment(&m), mean_robust_moment(&m),
+		  sigma_robust_moment(&m), median_robust_moment(&m));
+	}
+#if 0	  
+	if (Qmmcount) {
+	  min_count = max_count = 0;
+	  xmin = min_moment(&m);
+	  xmax = max_moment(&m);
+	  for (i=0; i<nx; i++) {
+	    for (j=0; j<ny; j++) {
 	      x =  CubeValue(iptr,i,j,k);
 	      if (x==xmin) min_count++;
 	      if (x==xmax) max_count++;
 	    }
-	  }
-	} /* i */
-	printf("Min_Max_count         : %d %d\n",min_count,max_count);
+	  } /* i,j */
+	  printf(" %d %d",min_count,max_count);
+	}
+	printf ("%d/%d out-of-range points discarded\n",nsize-n_moment(&m), nsize);
+#endif
+	printf("\n");
       }
-      printf ("%d/%d out-of-range points discarded\n",nsize-n_moment(&m), nsize);
     }
 }
 
