81b66d1164b71cb4cfb1184e637ffada87e44b51
Peter Teuben <teuben@gmail.com>
2013-05-29 13:28:13 +0000

    reporting

--- src/image/trans/ccdmask.c
+++ src/image/trans/ccdmask.c
@@ -2,6 +2,7 @@
  * CCDMASK: mask one or more maps into a bitmask
  *
  *     21-may-2013    Initial version, cloned off ccdmath      PJT
+ *     29             report final mask count
  *                      
  */
 
@@ -17,11 +18,11 @@ string defv[] = {
   "in=???\n        Input file(s), separated by comma's (optional)",
   "out=???\n       Output file",
   "clip=0.0\n      Clip level(s) for each input file",
-  "VERSION=0.1\n   21-may-2013 PJT",
+  "VERSION=0.2\n   29-may-2013 PJT",
   NULL,
 };
 
-string usage = "image masking";
+string usage = "image masking using clip levels";
 
 string cvsid="$Id$";
 
@@ -29,43 +30,28 @@ string cvsid="$Id$";
 # define HUGE 1.0e20
 #endif
 
-#define MAXIMAGE 20
-
-imageptr iptr;	/* pointers to (input) images */
-imageptr optr;	/* pointers to (input) images */
-
-int      nimage;                /* actual number of input images */
-
-#define MAXNAX 3
-
-double crval[MAXNAX], crpix[MAXNAX], cdelt[MAXNAX];
-int nwcs = 0;
-
-
-
-extern  int debug_level;		/* see initparam() */
-extern    void    dmpfien();
-extern    int     inifien();
-extern    void    dofien();
+#define MAXIMAGE 64
 
 extern string *burststring(string,string);
+
+imageptr iptr, optr;              /* images */
 
 
 void nemo_main ()
 {
-  string *fnames, fie;
-  stream  instr;                /* input files */
-  stream  outstr;                     /* output file */
-  int     i,j,k,l,nx,ny,nz;     /* size of scratch map */
-  int     noper;                      /* number of images needed from oper */
-  int     nclip;
-  real    cv, clip[MAXIMAGE];
-  bool    Qclip = hasvalue("clip");
-  int     imask,nmask;
+  string  *fnames;
+  stream   instr, outstr;                /* files */
+  int      i,j,k,l,nx,ny,nz;    
+  int      nclip, nimage;
+  real     cv, clip[MAXIMAGE];
+  bool     Qclip = hasvalue("clip");
+  int      imask,nmask;
 
   fnames = burststring(getparam("in"), ", ");  /* input file names */
   nimage = xstrlen(fnames, sizeof(string))-1;  /* number of files */
+  if (nimage > MAXIMAGE) error("cannot handle %d images, MAXIMAGE=%d",nimage,MAXIMAGE);
 
+  /* read clip levels, one per file. if not enough, last one repeats */
   nclip = nemoinpr(getparam("clip"),clip,MAXIMAGE);
   if (nclip<1) error("error parsing clip=%s",getparam("clip"));
   for (l=nclip; l<nimage; l++) clip[l] = clip[l-1];
@@ -95,7 +81,7 @@ void nemo_main ()
       }
     }
   }
-  dprintf(0,"%d/%d masked\n",nmask,nx*ny*nz);
+  dprintf(0,"%d/%d masked in map 1 @ clip %g\n",nmask,nx*ny*nz,clip[0]);
   strclose(instr);
 
   for (l=1; l<nimage; l++) {
@@ -114,9 +100,16 @@ void nemo_main ()
 	}
       }
     }
-    dprintf(0,"%d/%d masked\n",nmask,nx*ny*nz);
+    dprintf(0,"%d/%d masked in map %d @ clip %g\n",nmask,nx*ny*nz,l+1,clip[l]);
     strclose(instr);    
   }
+
+  nmask = 0;
+  for (k=0; k<nz; k++)
+      for (j=0; j<ny; j++)
+	for (i=0; i<nx; i++) 
+	  if (CubeValue(iptr,i,j,k) > 0) nmask++;
+  dprintf(0,"%d/%d masked in final map\n",nmask,nx*ny*nz);
   
   write_image (outstr,optr);         /* write image to file */
   strclose(outstr);
