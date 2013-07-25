e7aaec5ec58c1e64cd5a28c2971d9d1794e1f79a
Peter Teuben <teuben@gmail.com>
2013-07-25 14:27:53 +0000

    add key support and multiple outputs

--- src/orbit/misc/stoo.c
+++ src/orbit/misc/stoo.c
@@ -10,6 +10,7 @@
  *	 7-mar-92	     make gcc happy
  *	25-may-92	V2.5 need <potential.h> now
  *	18-apr-95       V2.6 
+ *      25-jul-2013     V3.0 allow multiple orbits output
  */
 
 #include <stdinc.h>
@@ -21,16 +22,17 @@
 #include <potential.h>
 #include <orbit.h>
 
-string defv[] = {		/* DEFAULT INPUT PARAMETERS */
-    "in=???\n			snapshot input file name",
-    "out=???\n			orbit output file name",
-    "ibody=0\n			which particle to take",
-    "nsteps=1\n			alloctation default",
-    "VERSION=2.6\n		18-apr-95 PJT",
-    NULL,
+string defv[] = {
+  "in=???\n			snapshot input file name",
+  "out=???\n			orbit output file name",
+  "ibody=0\n			which particles to take (-1=all, 0=first)",
+  "nsteps=10000\n		allocation default",
+  "VERSION=3.0\n		25-jul-2013 PJT",
+  NULL,
 };
 
 string usage="convert a snapshot file to an orbit";
+string cvsid="$Id$";
 
 string iname, oname;		/* file names */
 stream instr,outstr;		/* file pointers */
@@ -39,12 +41,13 @@ string headline;
  	/* global snapshot stuff */
 
 #define MOBJ 	8000
-int    nobj;
+int    nobj, norb;
 real   tsnap;			/* time of snapshot */
 real   mass[MOBJ];
 real   phase[MOBJ][2][NDIM];
-bool   Qtime;	
-bool   Qmass;
+int    key[MOBJ];
+int    isel[MOBJ];            /* select for output orbit?*/
+bool   Qtime, Qmass, Qkey;
 	/* global orbit stuff */
 
 int    ibody, nsteps;		/* allocation */
@@ -55,53 +58,59 @@ int read_snap();
 
 void nemo_main()
 {
-    int i;
+  int i,j;
     
-    setparams();
+  setparams();
     
-    instr  = stropen(iname,"r");		/* read from snapshot */
-    outstr = stropen(oname,"w");		/* write to orbit */
+  instr  = stropen(iname,"r");		/* read from snapshot */
+  outstr = stropen(oname,"w");		/* write to orbit */
 
-    optr=NULL;
-    allocate_orbit (&optr,NDIM,nsteps);
+  optr=NULL;
+  allocate_orbit (&optr,NDIM,nsteps);
+  
+  for (j=0; j<norb; j++) {
+    ibody = isel[j];
     i = 0;				/* counter of timesteps */
+    if (j>0) rewind(instr);
     while (read_snap()) {		/* read until exausted */
-	if (i==0) {			/* first time around */
-	   Masso(optr) = mass[ibody];
-	   I1(optr) = I2(optr) = I3(optr) = 0.0;
-	}
-	if (ibody>=nobj) {
-	    printf ("request to output ibody=%d, however nobj=%d\n",ibody,nobj);
-	    exit(1);
-	}
-	if (i>=nsteps) {
-	    printf ("Warning: too many timesteps requested, stopped at %d",i);
-	    break;
-	}
-/*	printf ("Outputting istep=%d time=%f\n",i,tsnap);	*/
-	Torb(optr,i) = tsnap;
-	Xorb(optr,i) = phase[ibody][0][0];	
-	Yorb(optr,i) = phase[ibody][0][1];	
-	Zorb(optr,i) = phase[ibody][0][2];	
-	Uorb(optr,i) = phase[ibody][1][0];	
-	Vorb(optr,i) = phase[ibody][1][1];	
-	Worb(optr,i) = phase[ibody][1][2];
-	i++;
+      if (i==0) {			/* first time around */
+	Masso(optr) = mass[ibody];
+	I1(optr) = I2(optr) = I3(optr) = 0.0;
+      }
+      if (ibody>=nobj) {
+	printf ("request to output ibody=%d, however nobj=%d\n",ibody,nobj);
+	exit(1);
+      }
+      if (i>=nsteps) {
+	printf ("Warning: too many timesteps requested, stopped at %d",i);
+	break;
+      }
+      Torb(optr,i) = tsnap;
+      Key(optr)    = key[ibody];
+      Xorb(optr,i) = phase[ibody][0][0];	
+      Yorb(optr,i) = phase[ibody][0][1];	
+      Zorb(optr,i) = phase[ibody][0][2];	
+      Uorb(optr,i) = phase[ibody][1][0];	
+      Vorb(optr,i) = phase[ibody][1][1];	
+      Worb(optr,i) = phase[ibody][1][2];
+      i++;
     }
     Nsteps(optr) = i;			/* record actual number */
-    put_history(outstr);
+    if (j==0) put_history(outstr);
     write_orbit(outstr,optr);		/* write orbit to file */
-    strclose(outstr);			/* close files */
-    strclose(instr);
+  }
+  strclose(outstr);			/* close files */
+  strclose(instr);
 }
 
 void setparams()
 {
-    iname = getparam("in");
-    oname = getparam("out");
-    ibody = getiparam("ibody");
-    nsteps = getiparam("nsteps");
-
+  iname = getparam("in");
+  oname = getparam("out");
+  nsteps = getiparam("nsteps");
+  norb = nemoinpi(getparam("ibody"),isel,MOBJ);
+  if (norb < 0) error("ibody=%s bad",getparam("ibody"));
+  
 }
 
 /*
@@ -110,8 +119,8 @@ void setparams()
 
 int read_snap()
 {				
-    int    i, cs;
-
+  int i, cs;
+    
   for(;;) {  /* loop until one snapshot found */
     get_history(instr);
 
@@ -150,6 +159,13 @@ int read_snap()
          }
          
          get_data(instr, PhaseSpaceTag, DoubleType, phase, nobj, 2, NDIM, 0);
+	 /* need additional Qkey here ? */
+	 if (get_tag_ok(instr,KeyTag))
+	   get_data(instr, KeyTag, IntType, key, nobj, 0);
+	 else {
+	   dprintf(0,"added ordinal keys\n");
+	   for (i=0; i<nobj; i++) key[i] = i;
+	 }
       get_tes(instr,ParticlesTag);
     get_tes(instr,SnapShotTag);
 
