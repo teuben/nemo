550f05b4aec2369845d411c627bc1fd23d682f66
Peter Teuben <teuben@gmail.com>
2013-10-11 14:26:39 +0000

    cleanup

--- src/kernel/tab/fit/gauss3_hcn.c
+++ src/kernel/tab/fit/gauss3_hcn.c
@@ -71,62 +71,102 @@ static real av3 = A11;
 static debug_first = 1;
 
 
-real func_gauss3_hcn_10(real *x, real *p, int np)
+/* parameters: a simple HCN triplet, only continuum, middle amp, middle vel
+ *             and dispersion given
+ *
+ */
+real func_gauss3_hcn(real *x, real *p, int np)
 {
-  real a,b,arg1,arg2,arg3;
+  real a,b,arg1,arg2,arg3,val;
+  int ic,nc, ia,iv,id;
+
+
+  if ( (np-1)%3) error("gauss3_hcn: need 1+3*Ncomp parameters: %d",(np-1)%3);
+  nc = (np-1)/3;
 
   if (debug_first) {
-    dprintf(0,"gauss3_hcn: a,b1,c1,d1,b2,c2,d2,b3,c3,d3\n");
+    dprintf(1,"gauss3_hcn: np=%d nc=%d\n",np,nc);
+    dprintf(1,"gauss3_hcn: a,b,c,d:");
+    for (ic=0; ic<np; ic++) dprintf(1," %g",p[ic]);
+    dprintf(1,"\n");
+    dprintf(1,"gauss3_hcn: dv=%g,%g,%g\n",dv1,dv2,dv3);
+    dprintf(1,"gauss3_hcn: av=%g,%g,%g\n",av1,av2,av3);
     debug_first = 0;
   }
 
-  a = p[2]-x[0];
-  b = p[3];
-  arg1 = a*a/(2*b*b);
+  val = p[0];
+  for (ic=0; ic<nc; ic++) {
+    ia = 1+ic*3;            /* offsets into parameter array */
+    iv = 2+ic*3;
+    id = 3+ic*3;
+    
+    a = p[iv]-x[0]+dv1;
+    b = p[id];
+    arg1 = a*a/(2*b*b);
 
-  a = p[5]-x[0];
-  b = p[6];
-  arg2 = a*a/(2*b*b);
+    a = p[iv]-x[0]+dv2;
+    b = p[id];
+    arg2 = a*a/(2*b*b);
 
-  a = p[8]-x[0];
-  b = p[9];
-  arg3 = a*a/(2*b*b);
+    a = p[iv]-x[0]+dv3;
+    b = p[id];
+    arg3 = a*a/(2*b*b);
 
-  return p[0] + p[1]*exp(-arg1) + p[4]*exp(-arg2) + p[7]*exp(-arg3);
+    val += p[ia]*(av1*exp(-arg1)+av2*exp(-arg2)+av3*exp(-arg3));
+  }
+  return val;
 
 }
- 
-void derv_gauss3_hcn_10(real *x, real *p, real *e, int np)
+
+void derv_gauss3_hcn(real *x, real *p, real *e, int np)
 {
   real a1,b1,arg1,a2,b2,arg2,a3,b3,arg3;
+  real earg1,earg2,earg3;
+  int ic,nc, ia,iv,id;
 
-  a1 = p[2]-x[0];
-  b1 = p[3];
-  arg1 = a1*a1/(2*b1*b1);
+  if ( (np-1)%3) error("Need 1+3*Ncomponents parameters: %d",(np-1)%3);
+  nc = (np-1)/3;
 
-  a2 = p[5]-x[0];
-  b2 = p[6];
-  arg2 = a2*a2/(2*b2*b2);
-
-  a3 = p[8]-x[0];
-  b3 = p[9];
-  arg3 = a3*a3/(2*b3*b3);
-  
   e[0] = 1.0;
+  for (ic=1; ic<np; ic++) e[ic] = 0.0;
 
-  e[1] = exp(-arg1);
-  e[2] = -p[1]*e[1] *   a1  /  (b1*b1);
-  e[3] =  p[1]*e[1] * a1*a1 / (b1*b1*b1);
+  for (ic=0; ic<nc; ic++) {
+    ia = 1+ic*3;            /* offsets into parameter array */
+    iv = 2+ic*3;
+    id = 3+ic*3;    
 
-  e[4] = exp(-arg2);
-  e[5] = -p[4]*e[4] *   a2  /  (b2*b2);
-  e[6] =  p[4]*e[4] * a2*a2 / (b2*b2*b2);
+    a1 = p[iv]-x[0]+dv1;
+    b1 = p[id];
+    arg1 = a1*a1/(2*b1*b1);
+    earg1 = exp(-arg1);
 
-  e[7] = exp(-arg3);
-  e[8] = -p[7]*e[7] *   a3  /  (b3*b3);
-  e[9] =  p[7]*e[7] * a3*a3 / (b3*b3*b3);
+    a2 = p[iv]-x[0]+dv2;
+    b2 = p[id];
+    arg2 = a2*a2/(2*b2*b2);
+    earg2 = exp(-arg2);
+    
+    a3 = p[iv]-x[0]+dv3;
+    b3 = p[id];
+    arg3 = a3*a3/(2*b3*b3);
+    earg3 = exp(-arg3);
+    
+    e[ia] +=  av1*earg1 + av2*earg2 + av3*earg3 ;
+
+    e[iv] += -p[ia]*av1*earg1*a1/(b1*b1)
+             -p[ia]*av2*earg2*a2/(b2*b2)
+             -p[ia]*av3*earg3*a3/(b3*b3);
+
+    e[id] += p[ia] * av1*earg1 * a1*a1 / (b1*b1*b1) +
+             p[ia] * av2*earg2 * a2*a2 / (b2*b2*b2) +
+             p[ia] * av3*earg3 * a3*a3 / (b3*b3*b3);
+  }
 }
 
+/*  the remainder of this code is for less restricted fits, where
+ *    amplitudes or velocity dispersions are kept free for each wing
+ *  for all free, use gaussn
+ */
+
 /* parameters:
  *      a,c,b1,d1,b2,d2,b3,d3
  *      0 1 2  3  4  5  6  7
@@ -255,101 +295,3 @@ void derv_gauss3_hcn_2(real *x, real *p, real *e, int np)
          p[4] * e[4] * a2 * a2 / (b2*b2*b2) +
          p[5] * e[5] * a3 * a3 / (b3*b3*b3) ;
 }
-
-/* parameters: fix all widths to be the same as well as DV's as well as all Amp's
- *             to a pre-described ratio
- * 
- *      a,b5,c5,d
- *      0 1  2  3 
- *
- */
-real func_gauss3_hcn(real *x, real *p, int np)
-{
-  real a,b,arg1,arg2,arg3,val;
-  int ic,nc, ia,iv,id;
-
-
-  if ( (np-1)%3) error("Need 1+3*Ncomponents parameters: %d",(np-1)%3);
-  nc = (np-1)/3;
-
-  if (debug_first) {
-    dprintf(0,"gauss3_hcn: np=%d nc=%d\n",np,nc);
-    dprintf(0,"gauss3_hcn: a,b5,c5,d: %g %g %g %g\n",p[0],p[1],p[2],p[3]);
-    dprintf(0,"gauss3_hcn: dv=%g,%g,%g\n", dv1,dv2,dv3);
-    dprintf(0,"gauss3_n2hp: av=%g,%g,%g\n",av1,av2,av3);
-    debug_first = 0;
-  }
-
-  val = p[0];
-  for (ic=0; ic<nc; ic++) {
-    ia = 1+ic*3;            /* offsets into parameter array */
-    iv = 2+ic*3;
-    id = 3+ic*3;
-    
-    a = p[iv]-x[0]+dv1;
-    b = p[id];
-    arg1 = a*a/(2*b*b);
-
-    a = p[iv]-x[0]+dv2;
-    b = p[id];
-    arg2 = a*a/(2*b*b);
-
-    a = p[iv]-x[0]+dv3;
-    b = p[id];
-    arg3 = a*a/(2*b*b);
-
-    val += p[ia]*(av1*exp(-arg1)+av2*exp(-arg2)+av3*exp(-arg3));
-  }
-  return val;
-
-}
-
-/* parameters:
- *      a,c,d,b1,b2,b3
- *      0 1 2 3  4  5
- */
-
-void derv_gauss3_hcn(real *x, real *p, real *e, int np)
-{
-  real a1,b1,arg1,a2,b2,arg2,a3,b3,arg3;
-  real earg1,earg2,earg3;
-  int ic,nc, ia,iv,id;
-
-  if ( (np-1)%3) error("Need 1+3*Ncomponents parameters: %d",(np-1)%3);
-  nc = (np-1)/3;
-
-  e[0] = 1.0;
-  for (ic=1; ic<np; ic++) e[ic] = 0.0;
-
-  for (ic=0; ic<nc; ic++) {
-    ia = 1+ic*3;            /* offsets into parameter array */
-    iv = 2+ic*3;
-    id = 3+ic*3;    
-
-    a1 = p[iv]-x[0]+dv1;
-    b1 = p[id];
-    arg1 = a1*a1/(2*b1*b1);
-    earg1 = exp(-arg1);
-
-    a2 = p[iv]-x[0]+dv2;
-    b2 = p[id];
-    arg2 = a2*a2/(2*b2*b2);
-    earg2 = exp(-arg2);
-    
-    a3 = p[iv]-x[0]+dv3;
-    b3 = p[id];
-    arg3 = a3*a3/(2*b3*b3);
-    earg3 = exp(-arg3);
-    
-    e[ia] +=  av1*earg1 + av2*earg2 + av3*earg3 ;
-
-    e[iv] += -p[ia]*av1*earg1*a1/(b1*b1)
-             -p[ia]*av2*earg2*a2/(b2*b2)
-      -p[ia]*av3*earg3*a3/(b3*b3);
-
-    e[id] += p[ia] * av1*earg1 * a1*a1 / (b1*b1*b1) +
-             p[ia] * av2*earg2 * a2*a2 / (b2*b2*b2) +
-             p[ia] * av3*earg3 * a3*a3 / (b3*b3*b3);
-  }
-}
-
