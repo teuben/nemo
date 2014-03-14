60b8a0df0b7372cf038df3d39e1191a2c1976a71
Peter Teuben <teuben@gmail.com>
2014-03-14 12:09:18 +0000

    CDEF

--- src/image/io/ccdtest.c
+++ src/image/io/ccdtest.c
@@ -7,6 +7,7 @@
  *		    and  fixep various old style coding
  *	 14-sep-93  V1.2 Added index mode keyword offset=	pjt
  *	 14-oct-99  V1.2b fixed bug printing WCS labels		pjt
+ *       26-sep-2013  V1.3  print out 2D and 3D  correct for CDEF   PJT
  *
  */
 
@@ -18,7 +19,7 @@
 
 string defv[] = {
 	"in=???\n        Input filename",
-	"VERSION=1.2b\n     14-oct-99 PJT",
+	"VERSION=1.3\n   26-sep-2013 PJT",
 	NULL,
 };
 
@@ -35,7 +36,7 @@ nemo_main()
     imageptr iptr=NULL;			      /* allocated dynamically */
     string   fmt, label;
     real     scale_factor, x, y, z;
-    real     **a;
+    real     **a, ***b;
     
     instr = stropen (getparam("in"), "r");
     if (read_image (instr,&iptr) == 0)
@@ -43,11 +44,22 @@ nemo_main()
     
     nx = Nx(iptr);	                        /* cube dimensions */
     ny = Ny(iptr);
-    a = map2_image(iptr);
-    for (j=0; j<ny; j++)
-    for (i=0; i<nx; i++)
-        printf(" %d %d -> %g\n",i,j,a[i][j]);
-
-    strclose(instr);
+    nz = Nz(iptr);
+    if (nz>1) {
+      printf("# nx,ny,nz: %d %d %d\n",nx,ny,nz);
+      b = map3_image(iptr);                       /* get the first slice */
+      for (k=0; k<nz; k++)
+	for (j=0; j<ny; j++)
+	  for (i=0; i<nx; i++)
+	    printf(" B[%d][%d][%d] -> %g\n",k,j,i,b[k][j][i]); // CDEF
+    } else {
+      printf("# nx,ny %d %d\n",nx,ny);
+      a = map2_image(iptr);                       /* get the first slice */
+      for (j=0; j<ny; j++)
+	for (i=0; i<nx; i++)
+	  //printf(" A[%d][%d] -> %g\n",j,i,a[i][j]); // CDEF
+	  printf(" A[%d][%d] -> %g\n",j,i,a[j][i]); // FORDEF
+    }
+    //strclose(instr);
 }
 
