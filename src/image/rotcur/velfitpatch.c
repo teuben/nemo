3f32e3059b879992c5067cd9c1822a0656138238
Peter Teuben <teuben@gmail.com>
2015-07-26 17:54:38 +0000

    implemented out=, some cleanup from old code

--- src/image/rotcur/velfitpatch.c
+++ src/image/rotcur/velfitpatch.c
@@ -21,7 +21,7 @@ string defv[] = {
   "blank=0.0\n    Blank Value in velocity field",
   "mode=xy\n      Output mode (xy derivates, vs. gp (gradient/positionangle)",
   "tab=\n         If given, tabular output file name",
-  "VERSION=0.2\n  26-jul-2015 PJT",
+  "VERSION=0.3\n  26-jul-2015 PJT",
   NULL,
 };
 
@@ -32,33 +32,52 @@ string cvsid="$Id$";
 
 #define MAXRING    4096
 
-void fit_patch(int npt, real *x, real *y, real *v, real *sol);
+void fit_patch(int npt, real *x, real *y, real *v, real *sol, real scale);
 void xy2rt(real *sol);
 
-imageptr denptr = NULL, velptr = NULL, outptr = NULL;
+imageptr velptr = NULL, outptr0 = NULL, outptr1 = NULL, outptr2 = NULL;
 
 
 nemo_main()
 {
-  bool Qsample, Qtab, Qxy;
-  stream curstr, velstr, outstr, tabstr;
-  real center[2], cospa, sinpa, cosi, sini, sint, cost, costmin, 
-    xt, yt, r, den, vrot_s, dvdr_s;
-  real vr, wt, frang, dx, dy, xmin, ymin, rmin, rmax, fsum, ave, tmp, rms;
-  real sincosi, cos2i, tga, dmin, dmax, dval, vmod;
+  bool Qsample, Qtab, Qout, Qxy;
+  stream velstr, tabstr, outstr0, outstr1, outstr2;
+  real scale;
   real *x, *y, *v, sol[3];
   int i, j, id, jd, nx, ny, nd, d, d2;
   string outmode = getparam("mode");
+  string bname, oname;
   int mode = -1;
   int npt = 0;
 
-  velstr = stropen(getparam("in"),"r");
+  /* output mode */
+
+  Qtab = hasvalue("tab");
+  Qxy  = (*outmode == 'x' || *outmode == 'X');
+
+  scale = getdparam("scale");
 
   /* read velocity field */
 
+  velstr = stropen(getparam("in"),"r");
   read_image(velstr,&velptr);
   nx = Nx(velptr);
   ny = Ny(velptr);
+
+  Qout = hasvalue("out");
+  if (Qout) {
+    copy_image(velptr, &outptr0);
+    copy_image(velptr, &outptr1);
+    copy_image(velptr, &outptr2);
+    bname = getparam("out");
+    oname = (string) allocate(strlen(bname) + 5);
+    sprintf(oname,"%s.%s", bname, "vc");
+    outstr0 = stropen(oname,"w");
+    sprintf(oname,"%s.%s", bname, Qxy ? "ox" : "om");
+    outstr1 = stropen(oname,"w");
+    sprintf(oname,"%s.%s", bname, Qxy ? "oy" : "pa");
+    outstr2 = stropen(oname,"w");
+  }
 
   /* needed arrays */
 
@@ -67,11 +86,6 @@ nemo_main()
   x = (real *) allocate(d2*d2*sizeof(real));
   y = (real *) allocate(d2*d2*sizeof(real));
   v = (real *) allocate(d2*d2*sizeof(real));
-
-  /* output mode */
-
-  Qtab = hasvalue("tab");
-  Qxy  = (*outmode == 'x' || *outmode == 'X');
 
   for (i=d; i<nx-d; i++) {
     for (j=d; j<ny-d; j++) {
@@ -84,15 +98,23 @@ nemo_main()
 	  if (v[nd] != 0.0) nd++;
 	}
       }
-      fit_patch(nd, x, y, v, sol);
+      fit_patch(nd, x, y, v, sol, scale);
       if (!Qxy) xy2rt(sol);
       if (Qtab) printf("%d %d   %g %g %g\n",i,j,sol[0],sol[1],sol[2]);
       npt++;
+      if (Qout) {
+	MapValue(outptr0,i,j) = sol[0];
+	MapValue(outptr1,i,j) = sol[1];
+	MapValue(outptr2,i,j) = sol[2];
+      }
     }
   }
   dprintf(0,"Processed %d points with patch area %d x %d\n",npt,d2,d2);
-
-
+  if (Qout) {
+    write_image(outstr0,outptr0);
+    write_image(outstr1,outptr1);
+    write_image(outstr2,outptr2);
+  }
 }
 
 /*
@@ -111,7 +133,7 @@ void xy2rt(real *sol)
 
 #define MAXCOL 4
 
-void fit_patch(int npt, real *x, real *y, real *v, real *sol)
+void fit_patch(int npt, real *x, real *y, real *v, real *sol, real scale)
 { 
   real mat[(MAXCOL+1)*(MAXCOL+1)], vec[MAXCOL+1], a[MAXCOL+2];
   bool Qpoly = FALSE;
@@ -126,5 +148,9 @@ void fit_patch(int npt, real *x, real *y, real *v, real *sol)
     lsq_accum(order+1,mat,vec,a,1.0);
   }
   lsq_solve(order+1,mat,vec,sol);
+  if (scale != 1.0) {
+    sol[1] *= scale;
+    sol[2] *= scale;
+  }
 }
 
