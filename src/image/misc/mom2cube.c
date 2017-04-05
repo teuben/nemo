c29f3306d705c64cc0574e52caeda6282f28c8f1
Peter Teuben <teuben@gmail.com>
2017-04-05 19:05:59 +0000

    added clip; norm different default

--- src/image/misc/mom2cube.c
+++ src/image/misc/mom2cube.c
@@ -20,8 +20,8 @@ string defv[] = {
   "sigma=1.0\n        Optional mom2 (if constant)",
   "cube=\n            Optional cube to describe 3rd axis",
   "z=-1.0,1.0,0.1\n   Zmin,Zmax,dZ (or use cube)",
-  "norm=t\n           Normalization?",
-  "clip=0.0           Clipvalue for mom0",
+  "norm=f\n           Normalization?",
+  "clip=0.0\n         Clipvalue for mom0",
   "VERSION=0.1\n      5-apr-2017 PJT",
   NULL,
 };
@@ -29,13 +29,6 @@ string defv[] = {
 string usage = "Use moments to reconstruct a cube";
 string cvsid="$Id$";
 
-local real peak_spectrum(int n, real *spec, int p);
-local real peak_mom(int n, real *spec, int *smask, int peak, int mom);
-local real peak_axis(imageptr iptr, int i, int j, int k, int axis);
-local int  peak_find(int n, real *data, int *mask, int npeak);
-local int  peak_assign(int n, real *data, int *mask);
-local bool out_of_range(real);
-local void image_oper(imageptr ip1, string oper, imageptr ip2);
 
 
 
@@ -46,19 +39,25 @@ void nemo_main()
     real    z[3];     /* replaces cubestr */
     bool    Qnorm;
     int     i,j,k,nx,ny,nz;
-    real    m0, m1, m2, v;
-    
+    real    m0, m1, m2, v, clip, sfactor, sum0, sumc;
 
     outstr  = stropen(getparam("out"), "w");
     mom0str = stropen(getparam("mom0"), "r");
-    mom1str = stropen(getparam("mom1"), "r");    
-    sigma = getrparam("sigma");              /* or mom2str */
+    mom1str = stropen(getparam("mom1"), "r");
+    if (hasvalue("mom2")) {
+      dprintf(0,"Using mom2, skipping sigma\n");
+      mom2str = stropen(getparam("mom2"), "r");
+      sigma = -1;
+    } else
+      sigma = getrparam("sigma");
     nz    = nemoinpd(getparam("z"),z,3);    /* or cubestr */
     Qnorm = getbparam("norm");
+    clip  = getrparam("clip");
     imageptr mom0=NULL, mom1=NULL, mom2=NULL, cube=NULL;
 
     read_image( mom0str, &mom0);
     read_image( mom1str, &mom1);
+    if (sigma < 0) read_image( mom2str, &mom2);
     
     nx = Nx(mom0);	
     ny = Ny(mom1);
@@ -67,6 +66,12 @@ void nemo_main()
 
     create_cube(&cube,nx,ny,nz);
 
+    if (sigma > 0) {
+      sfactor = sqrt(TWO_PI) * z[2] / sigma;
+      dprintf(0,"Scaling factor %g\n",sfactor);
+    }
+ 
+    sumc = sum0 = 0.0;
     for (j=0; j<ny; j++) {
       for (i=0; i<nx; i++) {
 	m0 = MapValue(mom0,i,j);
@@ -75,13 +80,23 @@ void nemo_main()
 	  m2 = MapValue(mom2,i,j);
 	else
 	  m2 = sigma;
+	if (m0 > clip)sum0 += m0;
+	if (Qnorm)
+	  sfactor = sqrt(TWO_PI) * z[2] / m2;
+	else
+	  sfactor = 1.0;
 	for (k=0; k<nz; k++) {
-	  v = z[0] + k*z[2] - m1;
-	  v = 0.5*v*v/(m2*m2);
-	  CubeValue(cube,i,j,k) = m0 * exp(-v); 
+	  if (m0 > clip) {
+	    v = z[0] + k*z[2] - m1;
+	    v = 0.5*v*v/(m2*m2);
+	    CubeValue(cube,i,j,k) = sfactor * m0 * exp(-v);
+	    sumc += CubeValue(cube,i,j,k);
+	  } else
+	    CubeValue(cube,i,j,k) = 0.0;
 	}
       }
     }
+    dprintf(0,"MOM0 sum=%g  CUBE sum=%g\n",sum0,sumc);
     
     write_image(outstr, cube);
     
