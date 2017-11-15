ec4df12aa1911d82c44c4411ebf73fc9704a3212
Peter Teuben <teuben@gmail.com>
2017-11-15 08:14:18 +0000

    more options to deal with ra/dec tables

--- src/kernel/tab/tabdist.c
+++ src/kernel/tab/tabdist.c
@@ -2,6 +2,8 @@
  *  Compare distance between points in two tables
  *
  *      13-feb-2013     0.1    Created, Q&D
+ *      24-jun-2016     0.7    allow radec in degrees; fix C language issues, introduce dmin=,
+ *                             faster precompute
  *
  *  See also the SIRTF/map2 project in 'c2d'
  *
@@ -11,6 +13,8 @@
  *  @todo   ID column
  *
  *  @todo   Faster lookup using 2D grid with linked list?
+ *
+ *  @todo   Generalize to N tables?
  */
 
 #include <stdinc.h>
@@ -25,9 +29,11 @@ string defv[] = {
     "id1=0\n            Column in first data reprenting the ID",
     "id2=0\n            Column in second data reprenting the ID",
     "radec=false\n      Interpret X and Y as RA/DEC angles on sky",
+    "units=r\n          radians or degree",
     "xgrid=\n           Grid to limit nearest neighbors for special 2D case",
     "ygrid=\n           Grid to limit nearest neighbors for special 2D case",
-    "VERSION=0.6\n	7-mar-2014 PJT",
+    "dmin=-1\n          If positive, only print distances less than this value",
+    "VERSION=0.7\n	24-jun-2016 PJT",
     NULL,
 };
 
@@ -36,36 +42,51 @@ string cvsid="$Id$";
 
 #define MAXGRID  1024
 
-void compare_1d(int npt1, int npt2, real *x1, real *x2);
-void compare_2d(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2);
-void compare_2da(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, string *name1, string *name2);
-void compare_2dg(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, Grid *g);
-void compare_3d(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real *z1, real *z2);
+void compare_1d (int npt1, int npt2, real *x1, real *x2, real dmin);
+void compare_2d (int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real dmin);
+void compare_2da(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real scale, string *name1, string *name2, real dmin);
+void compare_2dg(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, Grid *g, real dmin);
+void compare_3d (int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real *z1, real *z2, real dmin);
 
 void get_atable_a(stream instr, int id, int npt, string *name);
 
-inline real dist1(real x1,real x2) {
+inline static real dist1(real x1,real x2) {
   real d = x1-x2;
   if (d > 0.0) return d;
   return -d;
 }
 
-inline real dist2(real x1,real y1,real x2,real y2) {
+inline static real dist2(real x1,real y1,real x2,real y2) {
   return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
 }
 
-inline real dist3(real x1,real y1,real z1,real x2,real y2,real z2) {
+inline static real dist3(real x1,real y1,real z1,real x2,real y2,real z2) {
   return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2);
 }
 
-inline real dist2a(real x1,real y1,real x2,real y2) {
+/* slow version : compute sin() and cos() in the fly */
+
+inline static real dist2a(real x1,real y1,real x2,real y2) {
   return sin(y1)*sin(y2)+cos(y1)*cos(y2)*cos(x1-x2);
 }
+
+/* fast version : stores and precomputes */
+   
+static real *X1, *X2;
+static real *Sinx1, *Sinx2, *Siny1, *Siny2;
+static real *Cosx1, *Cosx2, *Cosy1, *Cosy2;
+
+inline static real dist2ai(i,j) {
+  return Siny1[i]*Siny2[j]+Cosy1[i]*Cosy2[j]*cos(X1[i]-X2[j]);
+}
+
+
 
 void key2grid(string key, Grid *g)
 {
 
 }
+
 
 #define NDIM   3
 
@@ -77,14 +98,18 @@ nemo_main()
   real *x1 = NULL, *x2 = NULL;
   real *y1 = NULL, *y2 = NULL;
   real *z1 = NULL, *z2 = NULL;
+  real ascale = 1.0;
+  real dmin = getrparam("dmin");
   string input1 = getparam("in1");
   string input2 = getparam("in2");
   stream instr1, instr2;
   string *name1, *name2;
   bool Qradec = getbparam("radec");
+  string units = getparam("units");
   Grid gx, gy;
 
   if (Qradec) warning("new radec mode ");
+  if (*units == 'd') ascale = PI/180.0;
 
   ncol1 = nemoinpi(getparam("col1"),colnr1,NDIM);
   ncol2 = nemoinpi(getparam("col2"),colnr2,NDIM);
@@ -123,6 +148,34 @@ nemo_main()
 
   dprintf(0,"Npt1=%d Npt2=%d\n",npt1,npt2);
 
+  if (Qradec) {   /* fast precompute for angular case */
+    warning("precompute, ascale=%g",ascale);
+    X1 = (real *) allocate(npt1 * sizeof(real));
+    X2 = (real *) allocate(npt2 * sizeof(real));
+    Sinx1 = (real *) allocate(npt1 * sizeof(real));
+    Siny1 = (real *) allocate(npt1 * sizeof(real));
+    Cosx1 = (real *) allocate(npt1 * sizeof(real));
+    Cosy1 = (real *) allocate(npt1 * sizeof(real));
+    Sinx2 = (real *) allocate(npt2 * sizeof(real));
+    Siny2 = (real *) allocate(npt2 * sizeof(real));
+    Cosx2 = (real *) allocate(npt2 * sizeof(real));
+    Cosy2 = (real *) allocate(npt2 * sizeof(real));
+    for (i=0; i<npt1; i++) {
+      X1[i]    = x1[i]*ascale;
+      Sinx1[i] = sin(x1[i]*ascale);
+      Siny1[i] = sin(y1[i]*ascale);
+      Cosx1[i] = cos(x1[i]*ascale);
+      Cosy1[i] = cos(y1[i]*ascale);
+    }
+    for (i=0; i<npt2; i++) {
+      X2[i]    = x2[i]*ascale;
+      Sinx2[i] = sin(x2[i]*ascale);
+      Siny2[i] = sin(y2[i]*ascale);
+      Cosx2[i] = cos(x2[i]*ascale);
+      Cosy2[i] = cos(y2[i]*ascale);
+    }
+  }
+
   id1 = getiparam("id1");
   id2 = getiparam("id2");
   if (id1 > 0) {
@@ -142,13 +195,13 @@ nemo_main()
   strclose(instr2);
 
   if (Qradec && ncol1==2)
-    compare_2da(npt1, npt2, x1, x2, y1, y2, name1, name2);
+    compare_2da(npt1, npt2, x1, x2, y1, y2, ascale, name1, name2, dmin);
   else if (ncol1 == 1) 
-    compare_1d(npt1, npt2, x1, x2);
+    compare_1d(npt1, npt2, x1, x2, dmin);
   else if (ncol1 == 2) 
-    compare_2d(npt1, npt2, x1, x2, y1, y2);
+    compare_2d(npt1, npt2, x1, x2, y1, y2, dmin);
   else if (ncol1 == 3)
-    compare_3d(npt1, npt2, x1, x2, y1, y2, z1, z2);
+    compare_3d(npt1, npt2, x1, x2, y1, y2, z1, z2, dmin);
   else
     warning("%d columns not supported",ncol1);
 }
@@ -156,7 +209,7 @@ nemo_main()
 
 
 
-void compare_1d(int npt1, int npt2, real *x1, real *x2)
+void compare_1d(int npt1, int npt2, real *x1, real *x2, real dmin_out)
 {
   int i, j, jmin;
   real d, dmin;
@@ -171,11 +224,12 @@ void compare_1d(int npt1, int npt2, real *x1, real *x2)
 	jmin = j;
       }
     }
-    printf("%d %g   %d %g   %g\n",i+1,x1[i],jmin+1,x2[jmin], dmin);
+    if (dmin_out>0 && dmin<dmin_out)
+      printf("%d %g   %d %g   %g\n",i+1,x1[i],jmin+1,x2[jmin], dmin);
   }
 }
 
-void compare_2d(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2)
+void compare_2d(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real dmin_out)
 {  
   int i, j, jmin;
   real d, dmin;
@@ -191,11 +245,20 @@ void compare_2d(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2)
       }
     }
     dmin = sqrt(dmin);
-    printf("%d %g %g   %d %g %g   %g\n",i+1,x1[i],y1[i],jmin+1,x2[jmin], y2[jmin],dmin);
+    if (dmin_out>0 && dmin<dmin_out)
+      printf("%d %g %g   %d %g %g   %g\n",i+1,x1[i],y1[i],jmin+1,x2[jmin], y2[jmin],dmin);
   }
 }
 
-void compare_2da(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, string *name1, string *name2)
+/* fast version - doesn't work yet 
+ * Npt1=1493 Npt2=12681
+ * cartesian:  0.2"
+ * slow:       8.0"
+ * fast:       2.1"
+ */
+
+/* fast */
+void compare_2da(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real s, string *name1, string *name2, real dmin_out)
 {  
   int i, j, jmin;
   real d, dmax, dmin;
@@ -205,16 +268,16 @@ void compare_2da(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, str
   
   for (i=0; i<npt1; i++) {
     if (name1) dprintf(2,"NAME1: %s\n",name1[i]);
-    dmax = dist2a(x1[i],y1[i],x2[0],y2[0]);
+    dmax = dist2ai(i,0);
     jmin = 0;
     for (j=1; j<npt2; j++) {
-      d = dist2a(x1[i],y1[i],x2[j],y2[j]);
+      d = dist2ai(i,j);
       if (d>dmax) {
 	dmax = d;
 	jmin = j;
       }
     }
-    dmin = acos(dmax)*206265;  /* in arcsec */
+    dmin = acos(dmax)*DR2AS;  /* radians to arcsec */
 
     if (name1==NULL) {
       sprintf(ord1,"%d",i+1);
@@ -228,11 +291,51 @@ void compare_2da(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, str
     } else
       n2 = name2[jmin];
 
-    printf("%s %g %g   %s %g %g   %g\n",n1,x1[i],y1[i],n2,x2[jmin],y2[jmin],dmin);
+    if (dmin_out>0 && dmin<dmin_out)
+      printf("%s %g %g   %s %g %g   %g\n",n1,x1[i],y1[i],n2,x2[jmin],y2[jmin],dmin);
   }
 }
 
-void compare_2g(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, Grid *gx, Grid *gy)
+/* slow */
+void compare_2da_slow(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real s, string *name1, string *name2, real dmin_out)
+{  
+  int i, j, jmin;
+  real d, dmax, dmin;
+  string n1, n2;
+  char ord1[16], ord2[16];
+
+  
+  for (i=0; i<npt1; i++) {
+    if (name1) dprintf(2,"NAME1: %s\n",name1[i]);
+    dmax = dist2a(x1[i]*s,y1[i]*s,x2[0]*s,y2[0]*s);
+    jmin = 0;
+    for (j=1; j<npt2; j++) {
+      d = dist2a(x1[i]*s,y1[i]*s,x2[j]*s,y2[j]*s);
+      if (d>dmax) {
+	dmax = d;
+	jmin = j;
+      }
+    }
+    dmin = acos(dmax)*DR2AS;  /* radians to arcsec */
+
+    if (name1==NULL) {
+      sprintf(ord1,"%d",i+1);
+      n1 = ord1;
+    } else
+      n1 = name1[i];
+
+    if (name2==NULL) {
+      sprintf(ord2,"%d",jmin+1);
+      n2 = ord2;
+    } else
+      n2 = name2[jmin];
+
+    if (dmin_out>0 && dmin<dmin_out)
+      printf("%s %g %g   %s %g %g   %g\n",n1,x1[i],y1[i],n2,x2[jmin],y2[jmin],dmin);
+  }
+}
+
+void compare_2g(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, Grid *gx, Grid *gy, real dmin_out)
 {  
   int i, j, jmin;
   real d, dmin;
@@ -248,12 +351,13 @@ void compare_2g(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, Grid
       }
     }
     dmin = sqrt(dmin);
-    printf("%d %g %g   %d %g %g   %g\n",i+1,x1[i],y1[i],jmin+1,x2[jmin], y2[jmin],dmin);
+    if (dmin_out>0 && dmin<dmin_out)
+      printf("%d %g %g   %d %g %g   %g\n",i+1,x1[i],y1[i],jmin+1,x2[jmin], y2[jmin],dmin);
   }
 }
 
 
-void compare_3d(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real *z1, real *z2)
+void compare_3d(int npt1, int npt2, real *x1, real *x2, real *y1, real *y2, real *z1, real *z2, real dmin_out)
 {  
   int i, j, jmin;
   real d, dmin;
