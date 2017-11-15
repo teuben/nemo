1edbc996a2c3e6ef5621aec842045d473f2920bb
Peter Teuben <teuben@gmail.com>
2017-11-15 08:21:43 +0000

    fix sign error, remove unused code

--- src/image/misc/ccdborder.c
+++ src/image/misc/ccdborder.c
@@ -18,7 +18,7 @@ string defv[] = {
   "y=0,0\n	  Y pixels to add before and after",
   "z=0,0\n	  Z pixels to add before and after",
   "value=0\n      value in the border region",
-  "VERSION=0.1\n  19-apr-2017 PJT",
+  "VERSION=0.2\n  19-apr-2017 PJT",
   NULL,
 };
 
@@ -51,6 +51,8 @@ void nemo_main()
     if (nemoinpi(getparam("y"),yb,2) != 2) error("Need 2 values for y");
     if (nemoinpi(getparam("z"),zb,2) != 2) error("Need 2 values for z");
 
+    /* TODO: make sure no negative values for the borders */
+
     read_image( instr, &iptr);
     outstr = stropen(getparam("out"), "w");
 
@@ -66,13 +68,13 @@ void nemo_main()
     LOOP(k,nz)  LOOP(j,ny)  LOOP(i,nx)  CV(optr,i+xb[0],j+yb[0],k+zb[0]) = CV(iptr,i,j,k);
 
 
-    Xmin(optr) = Xmin(iptr) + xb[0] * Dx(iptr);
+    Xmin(optr) = Xmin(iptr) - xb[0] * Dx(iptr);
     Dx(optr)   = Dx(iptr);
 
-    Ymin(optr) = Ymin(iptr) + yb[0] * Dy(iptr);
+    Ymin(optr) = Ymin(iptr) - yb[0] * Dy(iptr);
     Dy(optr)   = Dy(iptr);
 
-    Zmin(optr) = Zmin(iptr) + zb[0] * Dz(iptr);
+    Zmin(optr) = Zmin(iptr) - zb[0] * Dz(iptr);
     Dz(optr)   = Dz(iptr);
     dprintf(0,"WCS Corner: %g %g %g\n",Xmin(optr),Ymin(optr),Zmin(optr));
 
@@ -82,45 +84,4 @@ void nemo_main()
     
     write_image(outstr, optr);
 
-}
-
-/*
- * either initialize idx array, if not done, or normalize to 0..n-1
- */
- 
-int ax_index(string name, int n, int n1, int *idx)
-{
-    int i;
-    
-    if (n1==0) {		/* copy array */
-        n1=n;
-        for (i=0; i<n; i++) idx[i] = i;
-    } else {
-        for (i=0; i<n1; i++) {
-            if (idx[i] < 1 || idx[i] > n)
-                error("Index %d illegal in %s axis; max %d",idx[i],name,n);
-            idx[i] -= 1;
-        }
-    }
-    return n1;
-}
-
-
-void ax_copy(imageptr i0, imageptr i1)
-{
-  Dx(i1) = Dx(i0);
-  Dy(i1) = Dy(i0);
-  Dz(i1) = Dz(i0);
-  Xmin(i1) = Xmin(i0);
-  Ymin(i1) = Ymin(i0);
-  Zmin(i1) = Zmin(i0);
-  Xref(i1) = Xref(i0);
-  Yref(i1) = Yref(i0);
-  Zref(i1) = Zref(i0);
-  Beamx(i1) = Beamx(i0);
-  Beamy(i1) = Beamy(i0);
-  Beamz(i1) = Beamz(i0);
-  if (Namex(i0))  Namex(i1) = strdup(Namex(i0));
-  if (Namey(i0))  Namey(i1) = strdup(Namey(i0));
-  if (Namez(i0))  Namez(i1) = strdup(Namez(i0));
 }
