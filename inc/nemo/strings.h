5d20eb89b2f872eb3206ab5025faf6c2f6a0c707
Peter Teuben <teuben@gmail.com>
2017-12-27 16:02:09 +0100

    historic note

--- inc/nemo/strings.h
+++ inc/nemo/strings.h
@@ -1,6 +1,8 @@
 /* 
  *   strings.h
  *	for BSD diehards:   strings.h is really supposed to be string.h 
+ *      this used to be <strings.h> and is now <nemo/strings.h>
+ *      but is simply a dummy
  */
 
 #include <string.h>
