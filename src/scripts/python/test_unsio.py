6a47d8e615564549adeef08898ade98553ed587d
Jean-Charles Lambert <34373231+jcldc@users.noreply.github.com>
2019-09-13 16:14:45 +0200

    Update test_unsio.py
    more fixes

--- src/scripts/python/test_unsio.py
+++ src/scripts/python/test_unsio.py
@@ -25,9 +25,11 @@ print(my_in.getFileStructure())
 while my_in.nextFrame(): 
   s1,pos   = my_in.getData("all","pos")
   s2,timex = my_in.getData("time")
-  n1 = len(pos)
-  nbody = n1//3                         #   why not something like this my_in.getData("nbody")
-  pos=pos.reshape(nbody,3)
-  print("Time=%g %d %d %s" % (timex,s1,s2,str(pos.shape)))
-  print("Pos[0]: %s" % str(pos[0]))
+  s0,nsel = my_in.getData("nsel")
+  if s1 and s2 and s0:
+      n1 = len(pos)
+      nbody = n1//3                         #   why not something like this my_in.getData("nbody")
+      pos=pos.reshape(nbody,3)
+      print("Time=%g %d %d %s nbody=" % (timex,s1,s2,str(pos.shape),nsel))
+      print("Pos[0]: %s" % str(pos[0]))
   # be aware that snapshots with no PhaseSpace will not be flagged as such, the same pos array is returned
