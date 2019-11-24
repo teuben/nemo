6010588648a7e6f32a2a3ef5cd14afefffbb140c
Peter Teuben <teuben@gmail.com>
2019-11-24 18:17:21 -0500

    add fmt=

--- src/kernel/io/bsf.c
+++ src/kernel/io/bsf.c
@@ -17,8 +17,9 @@
 #include <moment.h>
 
 string defv[] = {
-  "in=???\n              input file name ",
-  "VERSION=0.1\n         24-nov-2019 PJT ",
+  "in=???\n              input file name",
+  "fmt=%g\n              output format for floating point values",
+  "VERSION=0.2\n         24-nov-2019 PJT ",
   NULL,
 };
 
@@ -40,17 +41,22 @@ string find_name    (string);
 void nemo_main()
 {
   string *tags;
+  string fmt = getparam("fmt");
+  char fmt4[32];
 
   dprintf(2,"TSF: MaxSetLen = %d\n",MaxSetLen);
   ini_moment(&m,2,0);
-  
+  sprintf(fmt4,"%s %s %s %s %%d\n",fmt,fmt,fmt,fmt);
+  dprintf(1,"%s\n",fmt4);
+    
   instr = stropen(getparam("in"), "r");
   while ((tags = list_tags(instr)) != NULL) {
     accum_item(*tags);
     free(*tags);
     free((char *)tags);
   }
-  printf("%g %g %g %g %d\n",
+  
+  printf(fmt4,
 	 mean_moment(&m), sigma_moment(&m),
 	 min_moment(&m), max_moment(&m),
 	 n_moment(&m));
@@ -87,30 +93,34 @@ void accum_data(string tag, string type, int *dims)
 {
   size_t dlen;
   byte *dat, *dp;
+  real rv;
 
   dlen = get_dlen(instr, tag);
   dat = (byte*) allocate(dlen+1);
   get_data_sub(instr, tag, type, dat, dims, FALSE);
   
-  for (dp = dat; dp < dat + dlen; ) {		/* loop over data array     */
-    if (streq(type, AnyType)) {             /*   output generic data?   */
+  for (dp = dat; dp < dat + dlen; ) {	/* loop over data array     */
+    if (streq(type, AnyType)) {             /*   generic data?   */
       dp += sizeof(byte);
-    } else if (streq(type, CharType)) {     /*   output readable chars? */
+    } else if (streq(type, CharType)) {     /*   readable chars? */
       dp += sizeof(char);
-    } else if (streq(type, ByteType)) {     /*   output bytes of data?  */
+    } else if (streq(type, ByteType)) {     /*   bytes of data?  */
       dp += sizeof(byte);
-    } else if (streq(type, ShortType)) {    /*   output short integers? */
+    } else if (streq(type, ShortType)) {    /*   short integers? */
       dp += sizeof(short);
-    } else if (streq(type, IntType)) {      /*   output standard ints?  */
+    } else if (streq(type, IntType)) {      /*   standard ints?  */
       dp += sizeof(int);
-    } else if (streq(type, LongType)) {     /*   output long integers?  */
+    } else if (streq(type, LongType)) {     /*   long integers?  */
       dp += sizeof(long);
-    } else if (streq(type, HalfpType)) {	/*   output short int? */
+    } else if (streq(type, HalfpType)) {    /*   short int? */
       dp += sizeof(short);
-    } else if (streq(type, FloatType)) {	/*   output floating point? */
+    } else if (streq(type, FloatType)) {    /*   floating point? */
+      rv = (real) *((float *) dp);
       dp += sizeof(float);
-    } else if (streq(type, DoubleType)) {	/*   output double numbers? */
-      accum_moment(&m, *((double *) dp),1.0);
+      accum_moment(&m,rv,1.0);
+    } else if (streq(type, DoubleType)) {   /*   double numbers? */
+      rv = (real) *((double *) dp);      
+      accum_moment(&m,rv,1.0);
       dp += sizeof(double);
     } else
       error("accum_data: type %s unknown", type);
