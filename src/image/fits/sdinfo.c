f48c7098479d85210fad165de3d18ae021e2c1e0
Peter Teuben <teuben@gmail.com>
2019-11-24 23:18:52 -0500

    still experimenting

--- src/image/fits/sdinfo.c
+++ src/image/fits/sdinfo.c
@@ -16,11 +16,12 @@
 string defv[] = {
     "in=???\n            Input SDFITS  fits file(s)",
     "stats=t\n           Doing simple stats",
+    "mom=2\n             Highest moment for stats",
     "VERSION=0.2\n       23-nov-2019 PJT",
     NULL,
 };
 
-string usage = "sdfits info";
+string usage = "sdfits info and bench";
 
 #if defined(HAVE_LIBCFITSIO)
 # include <fitsio.h>  
@@ -29,6 +30,7 @@ string usage = "sdfits info";
 #error CFITSIO not available or properly configured
 #endif
 
+  // inline
 float muladd(float x, float a, float b) {
     return x*a + b;
 }
@@ -44,6 +46,7 @@ void nemo_main(void)
     char template[64];
     char keyword[FLEN_KEYWORD], colname[FLEN_VALUE], data_fmt[FLEN_VALUE];
     bool Qstats = getbparam("stats");
+    int mom = getiparam("mom");
 
     fnames = burststring(fname,", ");
     nfiles = xstrlen(fnames, sizeof(string)) -1;
@@ -54,6 +57,12 @@ void nemo_main(void)
       status = 0;         /* initialize status before calling fitsio routines */
       fmode = READONLY;   /* from fitsio.h */
       fits_open_table(&fptr, fname, fmode, &status);
+      if (status) {
+	dprintf(0,"status=%d\n",status);
+	fits_close_file(fptr, &status);     /* close the file, if that even makes sense */
+	fits_report_error(stderr, status);  /* print out any error messages */	
+	continue;
+      }
       fits_get_num_rows(fptr, &nrows, &status);
       fits_get_num_cols(fptr, &ncols, &status);
       dprintf(1,"%s : Nrows: %d   Ncols: %d\n",fname,nrows,ncols);
@@ -75,27 +84,53 @@ void nemo_main(void)
       }
       dprintf(0,"%s : Nrows: %d   Ncols: %d  Nchan: %d\n",fname,nrows,ncols,nchan);      
       int anynul = 0;
-#if 1   
-      float *data = (float *) allocate(nchan*nrows*sizeof(float));
+
+#define ONEDIM
+      
+#ifdef ONEDIM
+      float *data1 = (float *) allocate(nchan*nrows*sizeof(float));
       float nulval = 0.0;
-      fits_read_col(fptr, TFLOAT, data_col, 1, 1, nchan, &nulval, data, &anynul, &status);
+      fits_read_col(fptr, TFLOAT, data_col, 1, 1, nchan, &nulval, data1, &anynul, &status);
+      dprintf(0,"DATA1 %g %g %g\n",data1[0],data1[1],data1[nchan]);
 #else
-      mdarray2 data = allocate_mdarray2(nrows,nchan);
+      mdarray2 data2 = allocate_mdarray2(nrows,nchan);
       double nulval = 0.0;
-      fits_read_col(fptr, TDOUBLE, data_col, 1, 1, nchan, &nulval, data, &anynul, &status);
-    
+      fits_read_col(fptr, TDOUBLE, data_col, 1, 1, nchan, &nulval, &data2[0][0], &anynul, &status);
+      dprintf(0,"DATA2 %g %g %g\n",data2[0][0], data2[0][1], data2[1][0]);
 #endif
       fits_close_file(fptr, &status);            /* close the file */
-      fits_report_error(stderr, status);  /* print out any error messages */
+      fits_report_error(stderr, status);     /* print out any error messages */
 
       if (Qstats) {
 	float sum = 0.0;
 	int nmax = nchan * nrows;
-	for (i=0; i<nmax; i++) {
-	  sum += muladd(data[i], 1.0, 0.0);
-	  //sum += data[0][i];	
+	Moment m;
+
+	ini_moment(&m,mom,0);
+#ifdef ONEDIM
+	for (i=0; i<nmax; i++) {                  // empty loop: 1.1"
+	  //sum += muladd(data[i], 1.0, 0.0);     // 2.1"
+	  //sum += data2[0][i];
+	  //sum += data1[i];                          // 1.6"
+	  //accum_moment(&m, data1[i], 1.0);         // 7.9" (6.6 if mom=1)
 	}
+#else	
+	for (ii=0; ii<nrows; ii++) {                  // empty loop: 1.1"
+	  for (jj=0; jj<nchan; jj++) {
+	    sum += data2[ii][jj];                         // 3.1"
+	    //accum_moment(&m, data2[ii][jj], 1.0);       // 8.8" (7.3 if mom=1)
+	  }
+	}
+#endif	
 	dprintf(0,"mean: %g\n", sum / nmax);
+	if (mom > 1)
+	  printf("Mean: %g   Dispersion: %g Min: %g Max: %g\n",
+	       mean_moment(&m), sigma_moment(&m),
+	       min_moment(&m), max_moment(&m));
+	else if (mom > 0)
+	  printf("Mean: %g   Dispersion: %g Min: %g Max: %g\n",
+	       mean_moment(&m), -1.0,
+	       min_moment(&m), max_moment(&m));
       }
     }
 }
