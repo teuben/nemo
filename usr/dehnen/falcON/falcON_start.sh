f04c294113a1f3f1f4090d069fb2557b48f14360
Peter Teuben <teuben@gmail.com>
2019-12-08 21:19:13 -0500

    shorter falcON start

---
+++ usr/dehnen/falcON/falcON_start.sh
@@ -0,0 +1,32 @@
+#! /bin/csh
+#
+# FALCON bash startup, needs to be sourced in the shell
+# Written for NEMO V4.1
+#
+# $FALCON must be set.
+#
+if [ -z $FALCON ]; then
+  echo "Environment variable FALCON is not set, falcON cannot be set up"
+  echo "add something like: export FALCON=/xxx/yyy"
+  echo "to denote the root directory of falcON to your .bashrc file"
+
+  # set enviroment variables FALCONLIB and ACCPATH
+  export  FALCONLIB=$FALCON/lib
+  export WDUTILSLIB=$FALCON/utils/lib
+
+  if [ -z $ACCPATH ]; then
+    export ACCPATH=$FALCON/acc/
+  else    
+    export ACCPATH=$FALCON/acc/:$ACCPATH
+  fi
+
+  # add FALCON/bin to PATH
+  export PATH=$FALCON/bin:$PATH
+
+  # add FALCONLIB to LD_LIBRARY_PATH and DYLD_LIBRARY_PATH
+  if [ -z "$LD_LIBRARY_PATH" ]; then
+    export LD_LIBRARY_PATH=$FALCONLIB:$WDUTILSLIB
+  else
+    export LD_LIBRARY_PATH=$FALCONLIB:$WDUTILSLIB:$LD_LIBRARY_PATH
+  fi
+fi
