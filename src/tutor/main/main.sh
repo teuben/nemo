1c469285755aad57127c9590a4d4f29a1ec5cdcc
Peter Teuben <teuben@gmail.com>
2019-12-27 16:27:17 -0500

    add a nice yapp function for scripts that need yapp flexibility

--- src/tutor/main/main.sh
+++ src/tutor/main/main.sh
@@ -5,9 +5,22 @@
 a=1
 b=2
 c=3
+yapp=xs
 
 for arg in $*; do\
    export $arg
 done
 
-echo a=$a b=$b c=$c
+# a useful YAPP_PGPLOT and YAPP_PS device function
+yapp() {
+  if test $yapp = "xs"; then
+     echo $1/$yapp
+  elif test $yapp = "_ps"; then
+     echo fig$1.ps
+  else
+     echo fig$1.$yapp/$yapp
+  fi
+}
+
+
+echo a=$a b=$b c=$c yapp=$(yapp 10)
