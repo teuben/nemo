062ccfe99dbed06808fe7aca74d377d25a926c2e
Peter Teuben <teuben@gmail.com>
2019-12-30 10:46:37 -0500

    trying a zeno in nemo

---
+++ usr/zeno/zeno_start.sh
@@ -0,0 +1,17 @@
+#   this is the ZENO rc file assuming you have installed 
+#   this via NEMO in $NEMO/usr/zeno/zeno
+
+export ZENOPATH="$NEMO/usr/zeno/zeno"
+export ZCC="gcc"
+if test $NEMOSYS = "Linux"; then
+    export ZCCFLAGS="-std=gnu99 -DLINUX  -I$ZENOPATH/inc"
+else
+    export ZCCFLAGS="-std=gnu99 -DMACOSX -I$ZENOPATH/inc"
+fi
+
+export ZLDFLAGS="-L$ZENOPATH/lib"
+export ZENO_SAFE_SELECT="true"
+export ZENO_MSG_OPTION="all"
+
+
+export PATH=$ZENOPATH:$PATH
