893f0ec4c80eca81afec358d47a66f925d6569ba
Peter Teuben <teuben@gmail.com>
2020-01-11 20:34:58 -0500

    more sanity during the install

--- docs/install_nemo.sh
+++ docs/install_nemo.sh
@@ -1,13 +1,36 @@
 #! /bin/bash
 #
-#  new V4.1 install, very few bells and whistles, just commented lines
-#  to suggest another approach
+#  new V4.1+ install
 #
 #  the old (csh) script "nemo_install" is still available to guide you for some problems
+#  but this is the recommended more cleaner procedure.   You can also use this script
+#  to shortcut network installs in clone off a local nemo.git tree and use caching of
+#  optional tar files.
+#
+#  url=file:///home/teuben/NEMO/redo/nemo.git
+#
+#  opt=1          hdf4 hdf5 cfitsio fftw wcslib gsl
+#  python=1       uses an anaconda3 install within $NEMO
+#
+#  In full version (opt=1 python=1) this script takes about 13 mins
+#  A simple version without falcON and any benchmarks takes about 2 mins
+
+echo "install_nemo.sh -- Version 11-jan-2020"
 
 opt=1
 nemo=nemo
 python=0
+url=https://github.com/teuben/nemo
+
+# local git repo via nemo.git for faster testing
+if [ -d nemo.git ]; then
+  (cd nemo.git; git pull)
+  nemo=nemo42
+  url=file://`pwd`/nemo.git
+else
+  echo "Using a direct git clone. You can can activate local hacking with:"
+  echo "      git clone $url nemo.git"
+fi  
 
 for arg in $*; do\
    export $arg
@@ -17,40 +40,43 @@ echo "Using: "
 echo "  opt=$opt"
 echo "  nemo=$nemo"
 echo "  python=$python"
+echo "  url=$url"
 echo ""
 
-# safet
-echo Sleep 5 seconds....;     sleep 5
+# safety
+echo Sleep 5 seconds before removing nemo=$nemo ....;     sleep 5
 
 
 date0=$(date)
 
 rm -rf $nemo
-git clone https://github.com/teuben/nemo $nemo
+git clone $url $nemo
 cd $nemo
 
 
 #                        when opt=1 it will first compile these packages in $NEMO/opt and use them
+#                        it needs a bootstrap configure/build1
 if test $opt = 1; then
-    ./configure
-    make hdf4
-    make hdf5
-    make cfitsio
-    ./configure --with-opt
+    ./configure 
+    make build1 MKNEMOS="hdf4 hdf5 cfitsio fftw wcslib gsl"
     opt="--with-opt"
 else
+    echo Skipping optional installs
     opt=""
 fi
 
 if test $python = 1; then
     make python
     source python_start.sh
+else
+    echo No python install
 fi    
 
-# pick a configure (remember to use $opt)
+# pick a configure
 
 #./configure $opt
-#./configure $opt --with-yapp=pgplot --disable-falcon
+./configure $opt --with-yapp=ps
+#./configure $opt --with-yapp=pgplot
 #./configure $opt --enable-debug --with-yapp=pgplot --with-pgplot-prefix=/usr/lib     # ok
 #./configure $opt --enable-debug --with-yapp=pgplot --with-pgplot-prefix=/usr/lib   --enable-pedantic
 #./configure $opt --enable-debug --with-yapp=pgplot
@@ -58,10 +84,12 @@ fi
 #./configure $opt --with-yapp=pgplot --with-pgplot-prefix=/usr/lib 
 #./configure $opt --enable-native
 #./configure $opt --disable-falcon
-./configure $opt --with-openmp --enable-native
+#./configure $opt --disable-falcon --with-yapp=ps
+#./configure $opt --with-openmp --enable-native
 #./configure $opt --enable-cfitsio --with-cfitsio-prefix=/usr
 #./configure $opt --with-yapp=plplot --disable-falcon
 #./configure $opt --with-opt
+
 
 
 # on a mac:
