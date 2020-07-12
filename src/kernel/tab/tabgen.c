e450fb0daae4b96f49fb05b0768c9024c409fd28
Peter Teuben <teuben@gmail.com>
2020-07-12 10:05:57 -0400

    new program, random table

---
+++ src/kernel/tab/tabgen.c
@@ -0,0 +1,37 @@
+/*
+ *  TABGEN:     table with random numbers
+ */
+
+#include <nemo.h>
+
+string defv[] = {
+    "out=???\n     Output table",
+    "nr=10\n       Number of rows",
+    "nc=10\n       Number of columns",
+    "mode=0\n      Mode",
+    "seed=123\n    Random seed",
+    "VERSION=0.1\n 12-Jul-2020 XYZ",
+    NULL,
+};
+
+string usage="Create a table with random numbers";
+
+string cvsid="$Id:$";
+
+void nemo_main()
+{
+  stream ostr = stropen(getparam("out"),"w");
+  int i, nr = getiparam("nr");
+  int j, nc = getiparam("nc");
+  int mode = getiparam("mode");
+  int seed = init_xrandom(getparam("seed"));
+
+  dprintf(1,"seed=%d\n",seed);
+
+  for (i=0; i<nr; i++) {
+    for (j=0; j<nc; j++)
+      fprintf(ostr,"%g ", xrandom(0.0, 1.0));
+    fprintf(ostr,"\n");
+  }
+
+}
