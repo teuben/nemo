2b70499d5e91b4d59e3dec9fab002d6397a6170b
Sathvik Ravi <sathvikrav@gmail.com>
2020-07-24 13:59:28 -0400

    tabtranspose pull request mod

--- src/kernel/tab/tabtranspose.c
+++ src/kernel/tab/tabtranspose.c
@@ -9,10 +9,12 @@
 #include <extstring.h>
 #include <ctype.h>
 #include <string.h>
+#include <strings.h>
 
 string defv[] = { 
     "in=???\n           input file name(s)",
     "out=???\n          output file name",
+    "align=n\n        ask for alignment",
     "nmax=10000\n       max space (needed if data in pipe)",
     "VERSION=1.0\n      5-oct-02 PJT",
     NULL
@@ -21,6 +23,7 @@ string defv[] = {
 string usage = "transpose a table";
 
 string  input, output;			/* file names */
+string alignment;           /* determines if tabtranspose should/shouldn't align */
 stream  instr, outstr;			/* file streams */
 int     nmax;                           /* # lines in file */
 int     kmin;                           /* # columns to transpsse */
@@ -50,6 +53,8 @@ local void setparams(void)
     output = getparam("out");
     outstr = stropen(output,"w");
 
+    alignment = getparam("align");
+
     nmax = nemo_file_lines(input,getiparam("nmax"));
     lines = (string *) allocate (nmax * sizeof(string));
     words = (string **) allocate (nmax * sizeof(string *));
@@ -78,15 +83,23 @@ local void do_output()
   int i, j, max_spaces = 0, count_spaces;
   int max_space[nmax];
 
-  for (j = 0; j < nmax; ++j) {
-    for (i = 0; i < kmin; ++i) {
-      if ((int)strlen(words[j][i]) > max_spaces) {
-        max_spaces = (int)strlen(words[j][i]);
+  if (strcmp(alignment, "n") == 0) {
+    dprintf(0, "The alignment parameter is %s\n", alignment); 
+  } else {
+    dprintf(0, "The alignment parameter is %s\n", alignment);
+  }
+
+  if (strcasecmp(alignment, "y") == 0) {
+    for (j = 0; j < nmax; ++j) {
+      for (i = 0; i < kmin; ++i) {
+        if ((int)strlen(words[j][i]) > max_spaces) {
+          max_spaces = (int)strlen(words[j][i]);
+        }
       }
-    }
     /* fprintf(outstr, "%d\n", max_spaces); */
-    max_space[j] = max_spaces;
-    max_spaces = 0;
+      max_space[j] = max_spaces;
+      max_spaces = 0;
+    }
   }
 
   /* for (j = 0; j < nmax; ++j) {
@@ -96,8 +109,10 @@ local void do_output()
   for (i=0; i<kmin; i++) {
     for (j=0; j<nmax; j++) {
       fprintf(outstr,"%s ",words[j][i]);
-      for (count_spaces = 0; count_spaces < max_space[j] - (int)strlen(words[j][i]); ++count_spaces) {
-        fprintf(outstr, " ");
+      if (strcasecmp(alignment, "y") == 0) {
+        for (count_spaces = 0; count_spaces < max_space[j] - (int)strlen(words[j][i]); ++count_spaces) {
+          fprintf(outstr, " ");
+        }
       }
     }
     fprintf(outstr,"\n");
