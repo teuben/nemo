fe92d0319df5be2ba59a92a7c85256bb3de4ad54
Peter Teuben <teuben@gmail.com>
2020-09-13 23:03:11 -0400

    add a dprintf()

--- src/tutor/hello/hello2.c
+++ src/tutor/hello/hello2.c
@@ -1,5 +1,6 @@
 /*
- *  HELLO2:  Hello World, but without any initial nemo definitions
+ *  HELLO2:  Hello World, probably the most simple NEMO main(),
+ *           without any extraneous nemo definitions.
  *
  */
 
@@ -7,5 +8,8 @@
 
 void nemo_main() 
 {
+    // hello world as we always do
     printf("Hello2.\n");
+    // NEMO's debug level printf(); use debug=1 or higher to see this (and more)
+    dprintf(1,"Hello debug=1 World\n");
 }
