1801bc4c8dea59249e31b7a841e4818aec125972
Peter Teuben <teuben@gmail.com>
2020-11-21 11:49:19 -0500

    example script

---
+++ src/scripts/nemo_merging.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+#
+#      create two NEMO trees, nemo1 and nemo2, and prepare some merging scenarios
+#      preferably do this in a clean directory
+#      set the mode= to controlr
+
+
+#    Set the two repo's
+url1=https://github.com/teuben/nemo
+url2=https://github.com/psanth/nemo
+
+#    Set the branche names in these repos to compare/merge
+b1=c99
+b2=c++_nemo
+
+#    Select a mode how to prepare the merging
+#
+#       0. do nothing
+#       1. merge in your given branch
+#       2. make a local branch of the remote with the same name
+#
+mode=0
+
+
+#   ---                  grab both repos
+git clone $url1 nemo1
+git clone $url2 nemo2
+
+#   ---                  add the other as a remote, and switch to their respective branches
+cd nemo1
+git remote add nemo2 $url2
+git fetch nemo2
+if test $mode = 1; then
+    git checkout $b1
+    git merge nemo2/$b2
+elif test $mode = 2; then
+    git checkout -b $b2    
+    git merge nemo2/$b2
+fi
+cd ..
+
+cd nemo2
+git remote add nemo1 $url1
+git fetch nemo1
+if test $mode = 1; then
+    git checkout $b2
+    git merge nemo1/$b1
+elif test $mode = 2; then
+    git checkout -b $b1
+    git merge nemo1/$b1
+fi
+    
+cd ..
