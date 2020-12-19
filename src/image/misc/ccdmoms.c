279196734a006c91182402604723a857f447cdaa
Peter Teuben <teuben@gmail.com>
2020-12-19 14:24:34 -0500

    make sigma work, but win= not working yet

--- src/image/misc/ccdmoms.c
+++ src/image/misc/ccdmoms.c
@@ -23,7 +23,7 @@ string defv[] = {
   "axis=\n        WCS value per image, if needed",
   "win=\n         Input image weights, one per input (not implemented)",
   "sigma=f\n      Are the weights still SIGMA (t), or straight weights (f)",
-  "VERSION=0.2\n  18-dec-2020 PJT",
+  "VERSION=0.3\n  18-dec-2020 PJT",
   NULL,
 };
 
@@ -43,7 +43,7 @@ real iwt[MAXIMAGE];             /* scalar weight per image */
 real iax[MAXIMAGE];             /* WCS axis value per image */
 int      nimage;                /* actual number of input images */
 int      mom;                   /* special moment to take */
-
+bool     Qsigma;
 
 local void do_combine(void);
 
@@ -51,58 +51,85 @@ local void do_combine(void);
 
 void nemo_main ()
 {
-    string *fnames, fie;
-    stream  instr[MAXIMAGE];            /* input files */
+    string *fnames, *wnames;
+    stream  instr;                      /* input files */
     stream  outstr;                     /* output file */
     int     size[3], nx, ny, nz;        /* size of scratch map */
     int     noper;                      /* number of images needed from oper */
     int     i, j, k, l, n;
-    bool    Qsigma = getbparam("sigma");
+    bool    Qwin = hasvalue("win");
 
     mom = getiparam("mom");
+    Qsigma = getbparam("sigma");    
 
     fnames = burststring(getparam("in"), ", ");  /* input file names */
     nimage = xstrlen(fnames, sizeof(string)) - 1;
     if (nimage > MAXIMAGE) error("Too many images %d > %d", nimage, MAXIMAGE);
+    dprintf(0,"Using %d images\n",nimage);
+
+    if (Qwin) {
+	wnames = burststring(getparam("win"), ", ");	
+	n = xstrlen(wnames, sizeof(string)) - 1;
+	if (n != nimage) error("Not the correct number of win= images (%d != %d)",n,nimage);
+	if (n>0)
+	  warning("The win= keyword is currently not implemented");
+    }
 
     n = nemoinpr(getparam("weight"), iwt, nimage);
     if (n<0)
       error("Parsing %s", getparam("weight"));
     else if (n==0)
       for (l=0; l<nimage; l++)  iwt[l] = 1.0;
-    else 
+    else if (n!=nimage)
       error("Cannot handle %d values for weight=",n);
     if (Qsigma)
-      for (l=0; l<nimage; l++)  iwt[l] = 1/sqrt(iwt[l]);
+      for (l=0; l<nimage; l++)  iwt[l] = 1/(iwt[l]*iwt[l]);
     
-    dprintf(0,"weight=%g %g ...\n", iwt[0], iwt[1]);    
+    dprintf(0,"weight=%g %g ...\n", iwt[0], iwt[1]);
+    
 
     n = nemoinpr(getparam("axis"), iax, nimage);
     if (n<0)
       error("Parsing %s", getparam("axis"));
     else if (n==0)
       for (l=0; l<nimage; l++)  iax[l] = l;
-    else 
+    else if (n!=nimage)
       error("Cannot handle %d values for axis=",n);
     dprintf(0,"axis=%g %g ...\n", iax[0], iax[1]);
+    if (n>0)
+      warning("The axis= keyword is currently not implemented");
 
 
     outstr = stropen (getparam("out"),"w");  /* open output file first ... */
 
     for (l=0; l<nimage; l++) {
-      instr[l] = stropen(fnames[l],"r");    /* open file */
+        instr   = stropen(fnames[l],"r");    /* open file */
         iptr[l] = NULL;        /* make sure to init it right */
-        read_image (instr[l], &iptr[l]);
+        wptr[l] = NULL;        /* make sure to init it right */	
+        read_image (instr, &iptr[l]);
         dprintf (2,"Image %d read in\n",l);
         if (l>0) {                   /* check size consistency */
-            if (Nx(iptr[l]) != Nx(iptr[l-1]))
+            if (Nx(iptr[l]) != Nx(iptr[0]))
                 error ("Input map %d does have different Nx\n",l);
-            if (Ny(iptr[l]) != Ny(iptr[l-1]))
+            if (Ny(iptr[l]) != Ny(iptr[0]))
                 error ("Input map %d does have different Ny\n",l);
-            if (Nz(iptr[l]) != Nz(iptr[l-1]))
+            if (Nz(iptr[l]) != Nz(iptr[0]))
                 error ("Input map %d does have different Nz\n",l);
         }
-        strclose(instr[l]);        /* close input file */
+        strclose(instr);        /* close input file */
+	if (Qwin) {
+	  dprintf(1,"win=%s weight map\n",wnames[l]);
+	  instr = stropen(wnames[l],"r"); 
+	  read_image (instr, &wptr[l]);
+	  if (Nx(wptr[l]) != Nx(iptr[0]))
+	    error ("Input map %d does have different Nx\n",l);
+	  if (Ny(wptr[l]) != Ny(iptr[0]))
+	    error ("Input map %d does have different Ny\n",l);
+	  if (Nz(wptr[l]) != Nz(iptr[0]))
+	    error ("Input map %d does have different Nz\n",l);
+	  strclose(instr);
+	}
+	
     }
     do_combine();
 
@@ -142,8 +169,12 @@ local void do_combine()
 	  reset_moment(&m);
 	  for (l=0; l<nimage; l++) {
 	    fin[l] = CubeValue(iptr[l],ix,iy,iz);
+	    if (wptr[l]) {
+	      iwt[l] = CubeValue(wptr[l],ix,iy,iz);
+	      if (Qsigma) iwt[l] = 1.0/(iwt[l]*iwt[l]);
+	    }
 	    accum_moment(&m, fin[l], iwt[l]);
-	  }
+	  } // l
 	  if (mom==0)
 	    fout = sum_moment(&m);
 	  else if (mom==1)
@@ -166,7 +197,7 @@ local void do_combine()
     MapMin(iptr[0]) = m_min;
     MapMax(iptr[0]) = m_max;
 
-    dprintf(1,"New min and max in map are: %f %f\n",m_min,m_max);
+    dprintf(0,"New min and max in map are: %f %f\n",m_min,m_max);
     if (badvalues)
     	warning("There were %d bad operations in dofie",badvalues);
     
