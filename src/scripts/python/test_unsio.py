abb542eb6ba28835fc2b3f154abe7f7ab9d1943d
Peter Teuben <teuben@gmail.com>
2021-01-25 20:12:18 -0500

    add tools

--- src/scripts/python/test_unsio.py
+++ src/scripts/python/test_unsio.py
@@ -6,9 +6,11 @@
 #
 # Created on Wicked Friday (friday Sep 13, 2019) in a commit war between PJ and JC
 #
+import os, sys
 
 import unsio.input as uns_in
-import os, sys
+import unsiotools.simulations.cfalcon as falcon
+
 
 if len(sys.argv) > 1:
     myfile = sys.argv[1]
@@ -16,7 +18,9 @@ else:
     myfile = "p100"
 
 if not os.path.exists(myfile):
-    os.system('hackcode1 out=%s' % myfile)
+    os.system('hackcode1 out=%s freqout=1' % myfile)
+else:
+    print("Re-using existing %s" % myfile)
 
 
 my_in=uns_in.CUNS_IN(myfile,"all")
@@ -31,10 +35,19 @@ while my_in.nextFrame():
   s0,nsel  = my_in.getData("nsel")
   s1,pos   = my_in.getData("all","pos")
   s2,timex = my_in.getData("time")
+  s3,mass  = my_in.getData("all","mass")
+  
   if s0 and s1 and s2:
       n1 = len(pos)
       nbody = n1//3          
-      pos=pos.reshape(nbody,3)
-      print("Time=%g %s nbody=%d" % (timex,str(pos.shape),nsel))
-      print("Pos[0]: %s" % str(pos[0]))
+      pos1=pos.reshape(nbody,3)
+      print("Time=%g %s nbody=%d" % (timex,str(pos1.shape),nsel))
+      print("Pos[0]: %s" % str(pos1[0]))
   # be aware that snapshots with no PhaseSpace will not be flagged as such, the same pos array is returned
+
+
+cf = falcon.CFalcon()
+ok,rho,hsml=cf.getDensity(pos,mass)
+print('pos ',pos[0],pos[1],pos[2])
+print('mass',mass[0])
+print('rho ',rho[0])
