2e01cb2e64046b65ea745d8093d62c630ecc2022
Peter Teuben <teuben@gmail.com>
2021-03-10 21:42:36 -0500

    bench

---
+++ src/scripts/bench/ccdgen.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#
+
+# set -x
+
+
+size=1000
+tmp=tmp$$
+
+
+for arg in $*; do
+  export $arg
+done
+
+
+/usr/bin/time -f "CPU %e" ccdgen "" .      size=$size,$size,$size   > $tmp.log 2>&1
+/usr/bin/time -f "CPU %e" ccdgen "" $tmp.1 size=$size,$size,$size  >> $tmp.log 2>&1
+
+dt=$(grep ^CPU $tmp.log  | sed s/CPU// | tabtrend -  debug=-1 | awk '{print $1}')
+
+nemoinp "($size*$size*$size)/$dt/(1000*1000)"
+
+
+rm -f $tmp.*
