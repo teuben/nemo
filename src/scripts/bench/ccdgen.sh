e80e97e1536ac57022bee64bbfe53a56b87d6c78
Peter Teuben <teuben@gmail.com>
2021-03-11 09:14:19 -0500

    debug option

--- src/scripts/bench/ccdgen.sh
+++ src/scripts/bench/ccdgen.sh
@@ -6,18 +6,25 @@
 
 size=1000
 tmp=tmp$$
-
+debug=0
 
 for arg in $*; do
   export $arg
 done
 
+if [ $debug = 1 ]; then
+    set -x
+fi
 
-/usr/bin/time -f "CPU %e" ccdgen "" .      size=$size,$size,$size   > $tmp.log 2>&1
-/usr/bin/time -f "CPU %e" ccdgen "" $tmp.1 size=$size,$size,$size  >> $tmp.log 2>&1
+/usr/bin/time -f "CPU %e %U %S" ccdgen "" .      size=$size,$size,$size   > $tmp.log 2>&1
+/usr/bin/time -f "CPU %e %U %S" ccdgen "" $tmp.1 size=$size,$size,$size  >> $tmp.log 2>&1
+
+if [ $debug = 1 ]; then
+    cat $tmp.log
+fi
 
 ts=$(grep ^CPU $tmp.log  | sed s/CPU// | tr -d '\n')
-dt=$(grep ^CPU $tmp.log  | sed s/CPU// | tabtrend -  debug=-1 | awk '{print $1}')
+dt=$(grep ^CPU $tmp.log  | sed s/CPU// | tabtrend -  xcol=1 debug=-1 | awk '{print $1}')
 
 echo "$ts " `nemoinp "($size*$size*$size)/$dt/(1000*1000)"`
 
