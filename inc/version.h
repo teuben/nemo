a2d4159d15408c369143c6cfa02c32cc4f2ca105
Peter Teuben <teuben@gmail.com>
2022-01-07 21:29:56 -0500

    new version

--- inc/version.h
+++ inc/version.h
@@ -1,5 +1,5 @@
 /* do not edit - created by the src/scripts/version script */
 #if defined(GETPARAM_VERSION_ID)
-static char *version_h = "git_9479";
+static char *version_h = "git_9813";
 #endif
-#define NEMO_VERSION "4.3.0"
+#define NEMO_VERSION "4.3.1"
