88447c39d1295c759082767db59207ad7fdaf765
Peter Teuben <teuben@gmail.com>
2022-10-28 09:49:10 -0400

    better bash syntax, add the #--HELP

--- scripts/templates/ex0.sh
+++ scripts/templates/ex0.sh
@@ -3,22 +3,32 @@
 #     can also try:    /bin/bash
 #
 
+#--HELP
 #       set defaults
 aaa=1
 bbb=2
 debug=0
+#--HELP
+
+#       give help?
+if [ "$1" == "--help" ];then
+    set +x
+    awk 'BEGIN{s=0} {if ($1=="#--HELP") s=1-s;  else if(s) print $0; }' $0
+    exit 0
+fi
 
 #       parse the commandline
-for arg in $*; do
-    export $arg
+for arg in "$@"; do
+    export "$arg"
 done
 
-#       some debugging
+#       some debugging?
 if [ $debug != 0 ]; then
     set -x
     set -e
+    set -u
 fi
 
 #       do the work
 echo "Computing with aaa=${aaa} and bbb=${bbb}"
-echo "And ccc=${ccc}"
+echo "And a possibly non-existing ccc=${ccc}"
