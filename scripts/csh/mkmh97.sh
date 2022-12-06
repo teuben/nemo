f8912cd846c2e944106eaf7950beed0af0f3c022
Peter Teuben <teuben@gmail.com>
2022-12-05 19:58:21 -0500

    bonsai fixes

--- scripts/csh/mkmh97.sh
+++ scripts/csh/mkmh97.sh
@@ -145,13 +145,20 @@ if [ $restart = 1 ]; then
     elif [ $code = 2 ]; then
 	# See $NEMO/usr/bonsai
 	snaptipsy  $run.3 $run.3t
+	ulimit -s unlimited
 	bonsai2 -i $run.3t --snapname $run. --logfile $run.gpu --dev 0 --snapiter $step -T $tstop -t $(nemoinp 1/2**$kmax)  1>$run.4.log 2>$run.4.err
-	grep Etot= run0.4.log  |tabcols - 4,6 > $run.4.etot
+	grep Etot= $run.4.log  |tabcols - 4,6 > $run.4.etot
+	# since bonsai writes a new snapshot (in tipsy format) for each time, need to convert them
+	# to single NEMO snapshot for the same analysis as the other codes.
 	set +x
 	for f in $run._*; do
 	    echo Processing $f
 	    (tipsysnap $f - | csf - - item=SnapShot 1>>$run.4 ) 2>>$run.4.t2s
 	done
+	rm -f $run._*
+	if [ $debug == 1 ]; then
+	    set -x
+	fi	    
 	echo Wrote final combined snapshot in $run.4
     else
 	set +x
@@ -169,8 +176,12 @@ fi
 #  compute the path of G1 and G2.   G2 needs a special treatment if mass G2 << G1
 
 if [ ! -e $run.xv.tab ]; then
-    snapcopy $run.4 - i=0 | snapprint - t                           > $run.4.t.tab
-    snapcenter $run.4 . "weight=i<$nbody?-phi*phi*phi:0" report=t > $run.4.g1.tab
+    snapcopy $run.4 - i=0 | snapprint - t                         > $run.4.t.tab
+    if [ $code == 2 ]; then
+	snapcopy $run.4 - "select=i<$nbody?1:0" | hackforce - - debug=-1 | snapcenter - . "-phi*phi*phi" report=t >$run.4.g1.tab
+    else
+	snapcenter $run.4 . "weight=i<$nbody?-phi*phi*phi:0" report=t > $run.4.g1.tab
+    fi
     snapcopy $run.4 - "select=i>=$nbody?1:0" | hackforce - - debug=-1 | snapcenter - . "-phi*phi*phi" report=t >$run.4.g2.tab
     paste  $run.4.t.tab $run.4.g1.tab $run.4.g2.tab | awk '{print $1,$2,$5,$8,$11}' > $run.xv.tab
     
