275b7a2279d380baecd40bc343369340faa02f3e
Peter Teuben <teuben@gmail.com>
2023-01-22 14:13:07 -0500

    add npeak=

--- src/kernel/tab/tabpeak.c
+++ src/kernel/tab/tabpeak.c
@@ -5,6 +5,7 @@
  *   30-may-2013   0.2 Also search for valleys
  *    1-jun-2013   0.3 Allow intensity weighted mean
  *   23-mar-2022   0.4 3pt vs. 5 pt
+ *   22-jan-2023   0.6 added npeak= following ccdmom - PJT
  *
  *                        
  * 
@@ -33,8 +34,9 @@ string defv[] = {
     "edge=1\n                     Edge to ignore (1 or higher)",
     "valley=f\n                   Also find the valleys?",
     "mean=f\n                     Intensity weighted mean",
+    "npeak=0\n                    extract the Nth peak (N>0)",
     "nmax=100000\n                max size if a pipe",
-    "VERSION=0.5\n		  25-mar-2012 PJT",
+    "VERSION=0.6\n		  22-jan-2023 PJT",
     NULL
 };
 
@@ -59,10 +61,12 @@ local stream instr;			/* input file */
 local int col[2], ncol;
 
 real *xcol, *ycol, *coldat[2];
+int *smask;
 local int    nmax;			/* lines to use at all */
 local int    npt;			/* actual number of points */
 local int    pmin;
 local int    edge;
+local int    npeak;
 real clip;
 bool  Qvalley, Qmean;
 
@@ -71,16 +75,33 @@ local void read_data(void);
 local void peak_data(void);
 local void peak_fit(void);
 local void mean_data(void);
-
+//local void extract_peak(void);
+local int  peak_find(int n, real *data, int *mask, int npeak);
 
 
 /****************************** START OF PROGRAM **********************/
 
 void nemo_main()
 {
+    int ipeak;
+  
     setparams();			/* read the parameters */
     read_data();
-    if (Qmean)
+    
+    if (npeak > 0) {
+      // initialize mask
+      (void) peak_find(npt, ycol, smask, 0);
+      for (int n=1; n<=npeak; n++)
+	ipeak = peak_find(npt, ycol, smask, n);
+      if (ipeak >= 0)
+	dprintf(0,"peak %d found near %g\n", npeak,xcol[ipeak]);
+      else
+	error("ipeak %d?", ipeak);
+      for (int i; i<npt; i++) {
+	if (smask[i]==npeak)
+	  printf("%g %g %d\n",xcol[i],ycol[i],smask[i],i);
+      }
+    } else if (Qmean)
       mean_data();
     else if (pmin == 3)
       peak_data();
@@ -101,6 +122,7 @@ local void setparams()
     Qvalley = getbparam("valley");
     Qmean = getbparam("mean");
     if (Qmean && Qvalley) warning("Valley fitting not supported in mean mode");
+    npeak = getiparam("npeak");
     
     nmax = nemo_file_lines(input,getiparam("nmax"));
     if (nmax<1) error("Problem reading from %s",input);
@@ -126,6 +148,7 @@ local void read_data()
     	warning("Could only read %d data",nmax);
     	npt = nmax;
     }
+    smask = (int *) allocate(npt * sizeof(int));
 }
 
 
@@ -264,3 +287,91 @@ local void mean_data(void)
 }
 
 
+
+/* 
+ * this routine can be called multiple times
+ * each time it will find a peak, and then walk down the peak
+ * mask[] contains 0 if not part of a peak, 1 for the first peak
+ * 2 for the 2nd etc.
+ *
+ * During the first call, the mask[] array is set to all 0's.
+ * i.e. unassigned to a peak, you will need npeak=0
+ *
+ * Returns: peak location (an integer)  and mask[] was modified
+ *
+ * TODO:    after all peaks have been find, need to resolve the issue
+ *          of possibly re-assigning membership to the neighbor peak
+ */
+
+
+local int peak_find(int n, real *data, int *mask, int npeak)
+{
+  int i, ipeak=0, apeak=-1;
+  real peakvalue=0, oldvalue=0;  // fool compiler
+
+  dprintf(1,"peak_find %d\n",n);
+  if (npeak==0) {               /* initialize by resetting the mask */
+    for(i=0; i<n; i++)
+      mask[i] = 0;              /* 0 means no peak assigned */
+    return -1;
+  }
+
+  while (1) {                   /* iterate until a good peak found ? */
+    ipeak++;
+    dprintf(1,"iter ipeak=%d npeak=%d\n",ipeak,npeak);
+    if (ipeak > npeak+3) break;
+
+    for(i=0; i<n; i++) {        /* go over all good points and find the peak */
+      if (mask[i]==0) {         /* for all good data not yet assigned to a peak */
+	if (apeak<0) {          /* make sure we initialize first good value as peak */
+	  peakvalue = data[i];   
+	  apeak = i; 
+	  continue;
+	}
+	if (data[i] > peakvalue) {   /* find largest values and remember where it was */
+	  peakvalue = data[i];
+	  apeak = i;
+	}
+      }
+    }
+    dprintf(1,"apeak=%d\n",apeak);
+    if (apeak==0) {             /* never allow first point to be the peak ... */
+      mask[apeak] = 0;
+      apeak = -1;
+      for (i=1, oldvalue=peakvalue; mask[i]==0 && i<n; i++) {   /* walk down, and mask out */
+	if (data[i] > oldvalue) break;                         /* util data increase again */
+	oldvalue = data[i];
+	mask[i] = npeak;
+      }
+      continue;
+    }
+    if (apeak==(n-1)) {         /* ...or last point, since they have no neighbors */
+      mask[apeak]= 0;
+      apeak = -1;
+      // compiler: peakvalue may be used uninitialized
+      for (i=n-2; oldvalue=peakvalue, mask[i]==0 && i>0; i--) {  /* walk down, and mask out */
+	if (data[i] > oldvalue) break;                        /* until data increase again */
+	oldvalue = data[i];
+	mask[i] = npeak;
+      }
+      // break;
+      continue;
+    }
+    if (apeak > 0) {            /* done, found a peak, but mask down the peak */
+      mask[apeak] = npeak;
+      for (i=apeak+1, oldvalue=peakvalue; mask[i]==0 && i<n;  i++) {
+	if (data[i] > oldvalue) break;
+	oldvalue = data[i];
+	mask[i] = npeak;
+      }
+      for (i=apeak-1, oldvalue=peakvalue; mask[i]==0 && i>=0; i-- ) {
+	if (data[i] > oldvalue) break;
+	oldvalue = data[i];
+	mask[i] = npeak;
+      }
+      break;
+    }
+
+  } /* while() */
+  return apeak;
+}
