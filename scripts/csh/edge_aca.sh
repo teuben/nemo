be524361cc5db68b17c5ff5319e13c8cdf506ee1
Peter Teuben <teuben@gmail.com>
2023-09-06 23:10:02 -0400

    various

--- scripts/csh/edge_aca.sh
+++ scripts/csh/edge_aca.sh
@@ -5,33 +5,40 @@
 #
 #
 _script=edge_aca.sh
-_version=3-sep-2023
+_version=6-sep-2023
 _pars=nemopars.rc
 _date=$(date +%Y-%m-%dT%H:%M:%S)
 
 #--HELP
+#
+#  edge_aca.sh   simulate an EDGE ACA cube at CO(2-1)
+#
 run=model1             # identification, and basename for all files     
 nbody=1000000          # number of bodies per model                       #> RADIO 1000,10000,100000,1000000
-r0=10                  # turnover radius                                  #> SCALE 1:100:1
-v0=200                 # peak velocity                                    
+r0=10                  # turnover radius (arcsec)                         #> SCALE 1:100:1
+v0=200                 # peak velocity (km/s)                             #> SCALE 50:400:10
 re=20                  # exponential scalelength of disk                  #> SCALE 1:100:1
 rmax=60                # edge of disk                                     
 
-pa=90                  # PA of disk on sky (E through N)                  
-inc=60                 # INC of disk                                      #> SCALE 0:90:2
+pa=90                  # PA of receding side disk (E through N)           #> SCALE 0:360:1               
+inc=60                 # INC of disk                                      #> SCALE 0:90:1
 
-beam=5                 # FWHM of spatial smoothing beam                   #> SCALE 1:20:1
+beam=5                 # FWHM of spatial smoothing beam (arcsec)          #> SCALE 1:20:1
 vbeam=5                # FWHM of spectral smoothing beam                  #> SCALE 1:40:2
 range=80               # gridding from -range:range                       
 vrange=320             # velocity gridding -vrange:vrange
-nsize=160              # number of pixels in gridding (px=py=2*range/nx)
-nvel=120               # number of pixels in velocity
+nsize=160              # number of spatial pixels (px=py=2*range/nx)
+nvel=120               # number of spectral pixels 
 
 seed=0                 # random seed
-frac=0                 # fraction random motion
+frac=0                 # fraction random motion                           #> SCALE 0:1:0.01
 
-noise=0                # add optional noise to cube                       #> ENTRY
-clip=0                 # clipping level for cube                          #> ENTRY
+noise=0.1              # add optional noise to cube                       #> ENTRY
+clip=0.1               # clipping level for cube                          #> ENTRY
+
+refmap=0               # reference map for WCS and masking                #> IFILE 0
+
+show=1                 # display some results (ds9, plots)                #> RADIO 0,1
 #--HELP
 
 
@@ -51,6 +58,7 @@ done
 grid_pars="xrange=-${range}:${range} yrange=-${range}:${range} nx=$nsize ny=$nsize"
 cell=`nemoinp "2*$range/$nsize*60"`
 cen=`nemoinp $nsize/2-0.5`
+restfreq=230.53800     # CO(2-1) in GHz
 
 #  keep a log, incase we call this routine multiple times
 echo `date` :: $* >> $run.history
@@ -85,7 +93,6 @@ ccdmath $run.20v,$run.21 $run.21v "ifgt(%2,0,%1,0)"
 ccdmath $run.20s,$run.21 $run.21s "ifgt(%2,0,%1,0)"
 
 
-
 echo "Creating the beam"
 mkplummer - 1 | snapgrid - $run.p1 $grid_pars
 ccdsmooth $run.p1 $run.beam $beam dir=xy
@@ -104,28 +111,45 @@ ccdmom $run.32 $run.33d axis=3 mom=0 clip=$clip
 ccdmom $run.32 $run.33v axis=3 mom=1 clip=$clip rngmsk=true
 ccdmom $run.32 $run.33s axis=3 mom=2 clip=$clip
 
+# see if we need WCS from a reference map
 
-xpaset -p ds9 frame frameno 1
-nds9 $run.33d
-xpaset -p ds9 frame frameno 2
-nds9 $run.33v
-xpaset -p ds9 frame frameno 3
-nds9 $run.33s
-xpaset -p ds9 frame frameno 4
-nds9 $run.32
-
-# export for barolo or so, in decent units (could also use ccdsky)
-# this way the input spatial scale is in arcmin and km/s
-ccdfits $run.32 $run.fits radecvel=t scale=1/60.0,1/60.0,1000.0 vsys=1000
-# rotcur
-
-
+if [ -e $refmap ]; then
+    ra=$(fitshead $refmap | grep CRVAL1 | awk '{print $3}')
+   dec=$(fitshead $refmap | grep CRVAL2 | awk '{print $3}')
+  vref=$(fitshead $refmap | grep CRVAL3 | awk '{print $3}')
+  echo "REFMAP: $ra $dec $vref"
+  # @todo figure out of m/s,km/s or freq
+  vscale=1000
+  vlsr=$(nemoinp $vref/$vscale)
+  crval=$ra,$dec,$vlsr 
+else
+  vlsr=0    
+  crval=180,0,0
+fi
+     
 # single dish profile
 ccdmom $run.32 - axis=1 mom=0 |\
     ccdmom - - axis=2 mom=0 |\
     ccdprint - x= y= z= label=z newline=t |\
     tabcomment - - punct=f delete=t > $run.spec
-tabplot $run.spec line=1,1
+
+# export for barolo or so, in decent units (could also use ccdsky)
+# this way the input spatial scale is in arcsec and km/s
+ccdfits $run.32 $run.fits radecvel=t scale=1/3600.0,1/3600.0,1.0 crval=$crval restfreq=$restfreq
+
+if [ $show = 1 ]; then
+    xpaset -p ds9 frame frameno 1
+    nds9 $run.33d
+    xpaset -p ds9 frame frameno 2
+    nds9 $run.33v
+    xpaset -p ds9 frame frameno 3
+    nds9 $run.33s
+    xpaset -p ds9 frame frameno 4
+    nds9 $run.fits
+
+    tabplot $run.spec line=1,1  headline="Spectrum around VLSR=$vlsr"
+fi
+
 
 echo "CCDSTAT"
 ccdstat $run.32
