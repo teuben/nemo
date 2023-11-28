115cba5e55e8d6ee19b46c4f8a6f5fb3b97d45ed
Peter Teuben <teuben@gmail.com>
2023-11-28 09:05:12 -0500

    upd

--- src/scripts/progress_bar.sh
+++ src/scripts/progress_bar.sh
@@ -5,13 +5,13 @@
 #
 #  progress=0     none
 #           1     zenity
-#           2     qt5
+#           2     qt5 (not implemented yet)
 
 pro=$1
 tab=$2
 col=$3
 max=$4
-
+sleep=1
 
 if [ $# -ne 4 ]; then
     echo "$0: need 4 arguments, got $#:"
@@ -27,10 +27,10 @@ echo "$0: $pro $tab $col $max"
 #  some programs take some startup time to create the progress file
 for i in $(seq 5); do
     if [ ! -e $tab ]; then
-	echo SLEEPING for $tab
-	sleep 1
+	echo "SLEEPING for $tab"
+	sleep $sleep
     else
-	echo Finally saw $tab
+	echo "Finally saw $tab"
 	break
     fi
 done
@@ -42,14 +42,14 @@ if [ ! -e $tab ]; then
 fi
 
 #    another minor sleep
-sleep 2
+sleep $sleep
 
 #    looping to check progress, passing on the percentage completeness to zenity
 (while [ 1 = 1 ]; do
  now=$(tail -1 $tab | tabcols - $col);
  p=$(nemoinp "100*${now}/${max}" format=%d);
  printf "%d\n"  $p;
- sleep 1;
+ sleep $sleep;
 done)   | zenity --progress --auto-close --time-remaining --title "NEMO: $tab" --text "$tab" --percentage=0 
  
 
