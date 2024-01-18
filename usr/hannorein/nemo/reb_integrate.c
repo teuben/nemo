4e4cdcaf344712e31fce777748e6d492c025ac10
Peter Teuben <teuben@gmail.com>
2024-01-17 21:10:11 -0500

    improved order of keywords, added gravity type

--- usr/hannorein/nemo/reb_integrate.c
+++ usr/hannorein/nemo/reb_integrate.c
@@ -14,22 +14,25 @@
 string defv[] = {
     "in=???\n                     input snapshot",
     "out=???\n                    output snapshot",
-    "server=1234\n                Use web based server on this port (if > 0)",
-    "eps=0.01\n                   Softening in terms of r0",
-    "dt=2e-5\n                    Integration step, in terms of t0",
-    "dtout=\n                     Optional interval to save simulation",
-    "tstop=\n                     Set, or let go forever",
-    "box=20\n                     Simulation box in terms or r0",
+    "tstop=10\n                   Set, or let go forever",
+    "dtout=1\n                    Optional interval to save simulation",
+    "dt=0.01\n                    Integration Step",
+    "eps=0.01\n                   Gravitational Softening Length",
     "integrator=leapfrog\n        rebound integrator",
-    "VERSION=0.1\n                16-jan-2024 PJT",
+    "gravity=basic\n              none, basic, compensated or tree",
+    "tolerance=0.25\n             tree opening angle - in radians?",
+    "box=\n                       Optional Simulation Box size - in terms or r0",
+    "G=1\n                        Gravitational Constant",
+    "server=-1\n                  Use web based server on this port (if > 0)",
+    "VERSION=0.3\n                17-jan-2024 PJT",
     NULL,
 };
 
-string usage="rebound+NEMO nbody integrator (toy model)";
+string usage="Rebound nbody integrator for NEMO";
 
 void heartbeat(struct reb_simulation* r);
     
-int integrator(string ri)
+int integrator_type(string ri)
 {
   if (streq(ri,"ias15"))     return REB_INTEGRATOR_IAS15;      // 0 - IAS15 integrator, 15th order, non-symplectic (default)
   if (streq(ri,"whfast"))    return REB_INTEGRATOR_WHFAST;     // 1 - WHFast integrator, symplectic, 2nd order, up to 11th order correctors
@@ -42,7 +45,17 @@ int integrator(string ri)
   if (streq(ri,"eos"))       return REB_INTEGRATOR_EOS;        // 11 - Embedded Operator Splitting (EOS) integrator family (Rein 2019)
   if (streq(ri,"bs"))        return REB_INTEGRATOR_BS;         // 12 - Gragg-Bulirsch-Stoer 
   if (streq(ri,"whfast512")) return REB_INTEGRATOR_WHFAST512;  // 21 - WHFast integrator, optimized for AVX512
-  error("Invalid integrator %s", ri);
+  error("Invalid integrate=%s", ri);
+  return -1;
+}
+
+int gravity_type(string ti)
+{
+  if (streq(ti,"none"))         return REB_GRAVITY_NONE;        // 0
+  if (streq(ti,"basic"))        return REB_GRAVITY_BASIC;       // 1
+  if (streq(ti,"compensated"))  return REB_GRAVITY_COMPENSATED; // 2
+  if (streq(ti,"tree"))         return REB_GRAVITY_TREE;        // 3
+  error("Invalid gravity=%s", ti);  
   return -1;
 }
 
@@ -50,53 +63,54 @@ int integrator(string ri)
 void nemo_main()
 {
     string infile = getparam("in");
+    string outfile = getparam("out");
     int port = getiparam("server");
-    int nbody = 100;
     double eps = getdparam("eps");
     double box = getdparam("box");
     double dt = getdparam("dt");
-    int reb_integrator = integrator(getparam("integrator"));
+    double dtout = getdparam("dtout");
+    double G = getdparam("G");
+    double tolerance = getdparam("tolerance");
+    int reb_integrator = integrator_type(getparam("integrator"));
+    int reb_gravity = gravity_type(getparam("gravity"));
     double tstop = INFINITY;
+    double tsnap;
+    int nbody;
     if (hasvalue("tstop")) tstop = getdparam("tstop");      
-    dprintf(0,"Integrating with %d\n",reb_integrator);
     
-    struct reb_simulation* r = reb_simulation_create();
+    struct reb_simulationarchive *sa = reb_simulationarchive_create_from_file(infile);
+    if (!sa) error("bad simulation archive %s", infile);
+    struct reb_simulation *r = reb_simulation_create_from_simulationarchive(sa, -1);
+    if (!r) error("cannot find snapshot");
+    reb_simulationarchive_free(sa);
+    nbody = r->N;
+    tsnap = r->t;
+    dprintf(0,"Read snapshot N=%d at t=%g\n", nbody, tsnap);
+    dprintf(0,"Integrator:     %d\n", reb_integrator);
+    dprintf(0,"Gravity Solver: %d\n", reb_gravity);
     
     // Start the REBOUND visualization server. This
     // allows you to visualize the simulation by pointing 
-    // your web browser to http://localhost:1234
+    // your web browser to http://localhost:<port>
     if (port > 0)
-      reb_simulation_start_server(r, 1234);
-
-    if (hasvalue("in"))
-      error("NEMO snapshot input from %s not yet supported",infile);
-
-    // Setup system characteristics : a Plummer sphere in nbody (virial) units
-    int _N = nbody;                 // Number of particles
-    double G = 1;                   // Gravitational constant
-    double M = 1;                   // Total mass of the cluster
-    double R = 1;                   // Radius of the cluster
-    double E = 3./64.*M_PI*M*M/R;   // Energy of the cluster
-    double r0 = 16./(3.*M_PI)*R;    // Characteristic length scale
-    double t0 = r->G*pow(M,5./2.)*pow(4.*E,-3./2.)*(double)_N/log(0.4*(double)_N); // Relaxation time
-    printf("Characteristic size:              %f\n", r0);
-    printf("Characteristic time (relaxation): %f\n", t0);
+      reb_simulation_start_server(r, port);
 
     // Setup constants
-    r->G             = G;        
-    r->integrator    = REB_INTEGRATOR_LEAPFROG;
-    r->dt            = dt*t0;       // timestep
-    r->softening     = eps*r0;      // Softening parameter
-    r->heartbeat     = heartbeat;
-    
-    reb_simulation_configure_box(r, box*r0, 1, 1, 1);
-    reb_simulation_add_plummer(r, _N, M, R);             // Adds particles
-    reb_simulation_move_to_com(r);                       // force center of mass at 0
-    if (hasvalue("dtout"))
-      reb_simulation_save_to_file_interval(r, getparam("out"), getdparam("dtout"));
+    r->G              = G;        
+    r->integrator     = reb_integrator;
+    r->gravity        = reb_gravity;
+    r->dt             = dt;
+    r->softening      = eps;
+    r->opening_angle2 = tolerance;
+    r->heartbeat      = heartbeat;
+
+    if (hasvalue("box"))
+      reb_simulation_configure_box(r, box, 1, 1, 1);
+    reb_simulation_save_to_file_interval(r, outfile, dtout);
+    reb_simulation_synchronize(r);   // note docs are wrong on name
     reb_simulation_integrate(r, tstop);
-    if (hasvalue("out"))
-      reb_simulation_save_to_file(r, getparam("out"));
+    //reb_simulation_save_to_file(r, outfile);
+    reb_simulation_free(r);
 }
 
 void heartbeat(struct reb_simulation* r){
