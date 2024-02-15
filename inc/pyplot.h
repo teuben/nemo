8a52b1df203a6ceea13c68545908e1d290550711
Peter Teuben <teuben@gmail.com>
2024-02-15 11:06:14 -0500

    add plot2 option

--- inc/pyplot.h
+++ inc/pyplot.h
@@ -5,4 +5,5 @@
 stream pyplot_init(string fname);
 void  pyplot_close(stream str);
 void  pyplot_plot(stream str, string tabname, int *xcol, int *ycol, int *dycol, real *xrange, real *yrange);
+void  pyplot_plot2(stream str, string tabname1, string tabname2, int *xcol, int *ycol, real *xrange, real *yrange);
 void  pyplot_hist(stream str, string tabname, int *xcol, real *xrange, int bins);
