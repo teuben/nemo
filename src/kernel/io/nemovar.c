1fcab6efbd146d76faf7667fbbace621012f9a01
Peter Teuben <teuben@gmail.com>
2024-03-07 21:14:18 -0500

    add show=t option

--- src/kernel/io/nemovar.c
+++ src/kernel/io/nemovar.c
@@ -3,11 +3,15 @@
  */
 
 #include <nemo.h>
+#include <table.h>
+#include <extstring.h>
+
 
 string defv[] = {
-  "var=???\n          Name of  variable(s)",
+  "var=\n             Name of  variable(s)",
   "value=\n           Optional new value",
-  "VERSION=0.1\n      7-mar-2024 PJT",
+  "show=f\n           Show all variables",
+  "VERSION=0.2\n      7-mar-2024 PJT",
   NULL,
 };
 
@@ -17,16 +21,33 @@ string usage="display NEMO variable(s)";
 void nemo_main()
 {
   string var = getparam("var");
-  string value = getparam("value");  // could be blank
+  string value = getparam("value");  
   bool  Qset = hasvalue("value");
+  bool Qshow = getbparam("show");
   string nemovar = getenv("NEMOVAR");
+  stream tstr = stropen(nemovar,"r");
+  table *t = table_open(tstr, -1);      // read table as a dumb set of lines
+  int i, nrows = t->nr;
+  string s;
 
-  dprintf(0,"nemovar: %s\n", nemovar);
+  dprintf(1,"nemovar: %s\n", nemovar);
+  
 
-  if (Qset) {
+  if (Qshow) {
+    for (i=0; i<nrows; i++)
+	printf("%s\n", table_row(t, i));
+    return;
+  }
+
+
+
+  if (Qset) { 
+    // writing code might need flock() to lock file
     warning("setting not allowed yet");
     return;
   }
+
+  // loop over lines and find the keyword, then display it
   
   printf("%s= TBD\n",var);
 
