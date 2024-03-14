def5cedc39fe3efbabd953b83f75ba6ea45a84ea
Shubham Bhatnagar <shubhambhatnagar2017@gmail.com>
2024-03-14 19:04:10 -0400

    implemented basic nemovar saving/loading/searching capabilities

--- src/kernel/io/nemovar.c
+++ src/kernel/io/nemovar.c
@@ -21,9 +21,9 @@ string usage="display NEMO variable(s)";
 void nemo_main()
 {
   string var = getparam("var");
+  bool hasVar = hasvalue("var");
   string value = getparam("value");  
-  bool  Qset = hasvalue("value");
-  bool Qshow = getbparam("show");
+  bool hasVal = hasvalue("value");
   string nemovar = getenv("NEMOVAR");
   stream tstr = stropen(nemovar,"r");
   table *t = table_open(tstr, -1);      // read table as a dumb set of lines
@@ -33,22 +33,56 @@ void nemo_main()
   dprintf(1,"nemovar: %s\n", nemovar);
   
 
-  if (Qshow) {
+  if (!hasVar && !hasVal) { 
     for (i=0; i<nrows; i++)
-	printf("%s\n", table_row(t, i));
+	    printf("%s\n", table_row(t, i));
     return;
   }
 
-
-
-  if (Qset) { 
+  //@TODO - Should use hash in the future?
+  if (hasVar && !hasVal) { 
     // writing code might need flock() to lock file
-    warning("setting not allowed yet");
-    return;
+    // loop over lines and find the keyword, then display it
+    bool found_var = FALSE;
+    for (i=0; i<nrows; i++) {
+      s = table_row(t, i);
+      if (strncmp(s,var,strlen(var))==0) {
+          printf("%s\n",s+strlen(var)+1);      
+          found_var = TRUE;  
+      }    
+    }
+
+    if (!found_var) {
+      error("Variable %s not found\n",var);
+    }
+  }
+
+
+  if (hasVar && hasVal) {
+    stream write_stream = stropen(nemovar,"w!");
+    bool found_var = FALSE;
+    
+    for (i=0; i<nrows; i++) {
+      s = table_row(t, i);
+      if (strncmp(s,var,strlen(var))==0) {
+        fprintf(write_stream,"%s=\"%s\"\n",var,value);    
+        found_var = TRUE;  
+      } else {
+        fprintf(write_stream,"%s\n",s);
+      }
+    }
+
+    if (!found_var) {
+      fprintf(write_stream,"%s=\"%s\"\n",var,value);    
+    }
+
+    
+
+    
   }
 
   // loop over lines and find the keyword, then display it
   
-  printf("%s= TBD\n",var);
+  // printf("%s= TBD\n",var);
 
 }
