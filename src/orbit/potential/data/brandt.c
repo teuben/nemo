00fa979ee27497c59af587713b21985235d0ea1a
Peter Teuben <teuben@gmail.com>
2024-06-19 14:32:39 -0400

    normalize to v0, make v0 the first parameter

--- src/orbit/potential/data/brandt.c
+++ src/orbit/potential/data/brandt.c
@@ -2,14 +2,15 @@
  * brandt.c: potential & forces as defined by a Brandt rotation curve
  *
  *	20-jun-2019	derived from rotcurm
+ *      19-jun-2024     add norm=1 as default now; switched order or (v,r)  v is first now
  */
 
 /*CTEX
- *  {\bf potname=rotcurm
- *       potpars={\it $\Omega,r_0,m$}}
+ *  {\bf potname=brand1,
+ *       potpars={\it $\Omega,v_0,r_0,m$}}
  *	 
  * The forces returned are the axisymmetric forces as defined by
- * a parameterized rotation curve as defined by the turnover point $r_0,v_0$.
+ * a parameterized rotation curve as defined by max $v_0$ around $r_0$
  *
  * See also Brandt, J.C. 1960, ApJ 131, 293. eq. (26)
  */
@@ -21,9 +22,10 @@
 #include <potential_float.h>
 
 local double omega = 0.0;
+local double v0    = 1.0;   /* this is not the vmax if norm=0 (v0*r0 if norm=0) */
 local double r0    = 1.0;
-local double v0    = 1.0;
 local double m     = 2.0;
+local int    norm  =   1;  /* non-zero will normalize v0 so it is the vmax */
 
 /* the 1/3 and 2/3 are from Greisen 2009, but Brandt 1960 eq.26 seems to use 1 and 1 */
 
@@ -41,14 +43,17 @@ void inipotential (int *npar, double *par, string name)
 
     n = *npar;
     if (n>0) omega = par[0];
-    if (n>1) r0    = par[1];
-    if (n>2) v0    = par[2];
+    if (n>1) v0    = par[1];
+    if (n>2) r0    = par[2];
     if (n>3) m     = par[3];
-    if (n>4) warning("Rotcurm potential: only 4 parameters usable");
+    if (n>4) norm  = (int) par[4];
+    if (n>5) warning("Brandt potential: only 4 parameters usable");
     
-    dprintf (1,"INIPOTENTIAL Rotcurm potential %s\n",name);
+    dprintf (1,"INIPOTENTIAL Brandt potential %s (%g,%g)\n",name,C1,C2);
     dprintf (1,"  Parameters : Pattern Speed = %f\n",omega);
-    dprintf (1,"  R0 = %g  V0 = %g m=%g\n", r0, v0, m);
+    if (norm != 0)
+      v0 = v0 / r0;
+    dprintf (1,"  V0 = %g  R0 = %g m=%g  norm=%d\n", v0, r0, m, norm);
 
     par[0] = omega;     /* return pattern speed again */
 }
