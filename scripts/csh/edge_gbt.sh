89acccbab37d50b6effa09bfe27b9bf9946076ee
Peter Teuben <teuben@gmail.com>
2024-06-19 20:27:44 -0400

    various

--- scripts/csh/edge_gbt.sh
+++ scripts/csh/edge_gbt.sh
@@ -13,36 +13,29 @@ _date=$(date +%Y-%m-%dT%H:%M:%S)
 #  edge_gbt.sh   simulate an EDGE GBT spectrum
 #
 run=model1             # identification, and basename for all files       #> ENTRY    
-nbody=1000000          # number of bodies per model                       #> RADIO 1000,10000,100000,1000000
-r0=10                  # turnover radius (arcsec)                         #> SCALE 1:100:1
-v0=200                 # peak velocity (km/s)                             #> SCALE 50:400:10
-m0=0.5                 # brandt power                                     #> SCALE 0:10:0.1
+logn=6                 # log of number of bodies per model                #> SCALE 1:8:0.2
+r0=10                  # characteristic radius (arcsec)                   #> SCALE 1:100:1
+v0=200                 # characteristic velocity (km/s)                   #> SCALE 10:500:5
+m0=0.5                 # brandt power  (0=flat)                           #> SCALE 0:10:0.1
 r1=0.1                 # central unresolved bulge, bar or black hole
 v1=0                   # representative rotation speed at r1
 re=20                  # exponential scalelength of disk  (arcsec)        #> SCALE 1:100:1
 rmax=60                # edge of disk  (arcsec)                           #> SCALE 1:80:1
-
-pa=90                  # PA of receding side disk (E through N)           #> SCALE 0:360:1               
 inc=60                 # INC of disk                                      #> SCALE 0:90:1
+z0=0                   # scaleheight [@todo buggy]                        #> SCALE 0:10:0.5
 
 vbeam=5                # FWHM of spectral smoothing beam   (arcsec)       #> SCALE 1:40:2
-vrange=320             # velocity gridding -vrange:vrange (km/s)
-nvel=120               # number of spectral pixels
-
-z0=0                   # scaleheight [buggy]                              #> SCALE 0:10:0.5
+vrange=400             # velocity gridding -vrange:vrange (km/s)
+nvel=200               # number of spectral pixels
 
 seed=0                 # random seed
 sigma=0                # random motion in plane  (km/s)                   #> SCALE 0:20:0.1
 
 noise=0                # add optional noise to cube                       #> ENTRY
-
 vlsr=0                 # optional VLSR if non-zero                        #> ENTRY
 
-plot=profile,rotcur    # show which plots can be made                     #> CHECK rotcur,profile
+plot=profile,rotcur    # show which plots can be made                     #> CHECK rotcur,profile,density
 
-# Example for NGC0001:   inc=34 pa=110  z=0.01511 (4530 km/s)
-#                 NED:   inc=39 pa=110  z=0.01518  VHEL=4550  VLSR=4554  VCMB=4212 (62 Mpc)
- 
 #--HELP
 
 #  basic help if --help or -h is given
@@ -59,9 +52,10 @@ for arg in "$@"; do
 done
 
 
-#  fixed
+#  fixed now compared to edge_aca.sh
 range=$rmax          # gridding size
 nsize=1              # number of spatial pixels (px=py=2*range/nx)
+pa=0                 # PA of receding side disk (E through N)           
 
 
 #  derive some parameters that appear common or logically belong together
@@ -70,6 +64,7 @@ grid_pars="xrange=-${range}:${range} yrange=-${range}:${range} nx=$nsize ny=$nsi
 cell=`nemoinp "2*$range/$nsize*60"`
 cen=`nemoinp $nsize/2-0.5`
 restfreq=230.53800     # CO(2-1) in GHz
+nbody=`nemoinp 10**$logn format=%d`
 
 
 # ================================================================================ START
@@ -91,7 +86,7 @@ if [ $m0 != 0 ]; then
     snapscale - - mscale=$nbody |\
     snaprotate - $run.20 "$inc,$pa" yz
   rotcurves  name1=brandt pars1=0,$v0,$r0,$m0  tab=t radii=0:${rmax}:0.01 plot=f |\
-      tabmath - - %1,1,%2,$sigma all > $run.tab
+      tabmath - - "%1,1,%2,$sigma,exp(-%1/$re)" all  > $run.tab
   vmax=$(tabstat $run.tab 3 | txtpar - p0=max:,1,2)
   echo "Max rotcur: vmax=$vmax m0=$m0"
 elif [ $v1 = 0 ]; then
@@ -151,13 +146,14 @@ ccdmom $run.31 - axis=1 mom=0 |\
 ccdfits $run.31 $run.fits radecvel=t scale=1/3600.0,1/3600.0,1.0 crval=$crval restfreq=$restfreq
 
 
-frameno=0
-
 echo "PLOT: $plot"
 if [[ "$plot" == *"rotcur"* ]]; then
-    tabplot $run.tab 1 3 yapp=1/xs
+    tabplot $run.tab 1 3 headline="Rotation Curve" yapp=1/xs
 fi
 if [[ "$plot" == *"profile"* ]]; then    
     tabplot $run.spec line=1,1  headline="Spectrum around VLSR=$vlsr" yapp=2/xs
+fi
+if [[ "$plot" == *"density"* ]]; then    
+    tabplot $run.tab 1 5 line=1,1  ymin=0 headline="Surface Density" yapp=3/xs
 fi
 
