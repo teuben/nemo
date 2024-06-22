456c2c596e40361b2481c294cc512e42e44338cd
Peter Teuben <teuben@gmail.com>
2024-06-21 21:54:29 -0400

    add pars=, show=

--- src/kernel/tab/tabsmooth.c
+++ src/kernel/tab/tabsmooth.c
@@ -6,6 +6,7 @@
  *      may-13   smooth= added
  *   28-sep-23   0.5 filter= added
  *   29-sep-23   0.6 converted to table V2 format
+ *   21-jan-24   0.8 added show= and pars=
  *
  */
 
@@ -26,9 +27,12 @@ string defv[] = {
     "in=???\n                     Input file name",
     "xcol=1\n			  Column(s) to use (1=first)",
     "filter=0\n                   Select one of the test filters (-1,0,1,2,...)",
+    "pars=\n                      Optional parameters for the smoothing filter",
     "smooth=\n                    Optional explicit smoothing array",
     "tcol=\n                      Optional independant variable ('time')",
-    "VERSION=0.7\n		  29-nov-2023 PJT",
+    "show=f\n                     Show the kernel",
+    "edge=0\n                     How to deal with edge effects (not implemented yet)",
+    "VERSION=0.8\n		  21-jun-2024 PJT",
     NULL
 };
 
@@ -45,7 +49,7 @@ string usage = "smooth columns of a table (Hanning, Savitzky-Golay)";
 #define MAXCOL 256
 #endif
 
-#define MAXSM    9
+#define MAXSM    100
 
 local string input;			/* filename */
 local stream instr;			/* input file */
@@ -56,9 +60,13 @@ local int nxcol;                        /* actual number of columns used */
 local mdarray2  x;                      /* x[col][row] */
 local int    npt;			/* actual number of points (rows) in table */
  
-local real sm[MAXSM];                   /* smoothing array */
+local real sm[MAXSM];                   /* smoothing array (the kernel) */
 local int  nsm;                         /* actual length of smoothing array */
 local bool Qt;                          /* is tcol= used ? */
+local bool Qshow;                       /* show the smoothing kernel */
+
+local real pars[MAXSM];                 /* (optional) smoothing kernel parameters */
+local int  npars;                       /* actual number of parameters given */
 
 
 /* Savitzky-Golay tables for fixed stepsize */
@@ -77,6 +85,9 @@ local cst csts[] = {
   {5,  7, { 2, -1, -2, -1,  2}},          // 3:  SK2 - 2nd der
   {7,231, { 5,-30, 75,131, 75,-30, 5}},   // 4:  SK4 - smooth (7pt)
   {7,252, {22,-67,-58,  0, 58, 67,-22}},  // 5:  SK4
+  // 11:   box         pars=width
+  // 12:   gauss       pars=stdev,nsigma
+  // 13:   trapezoidal pars=width
 };
 
 /* forward references */
@@ -85,6 +96,7 @@ local void setparams(void);
 local void read_data(void);
 local void build_filter(int);
 local void smooth_data(void);
+local void show_kernel(void);
 
 
 
@@ -93,8 +105,12 @@ local void smooth_data(void);
 void nemo_main()
 {
     setparams();
-    read_data();
-    smooth_data();
+    if (Qshow)
+      show_kernel();
+    else {
+      read_data();
+      smooth_data();
+    }
 }
 
 local void setparams()
@@ -103,28 +119,35 @@ local void setparams()
     int i;
     int filter = getiparam("filter");
     int nrows, ncols;
- 
-    input = getparam("in");
 
-    nxcol = nemoinpi(getparam("xcol"),xcol,MAXCOL+1);
-    if (nxcol < 0) error("parsing error xcol=%s",getparam("xcol"));
+    Qshow = getbparam("show");
+     
+    if (!Qshow) {
+      input = getparam("in");
+      nxcol = nemoinpi(getparam("xcol"),xcol,MAXCOL+1);
+      if (nxcol < 0) error("parsing error xcol=%s",getparam("xcol"));
     
-    Qt = hasvalue("tcol");
-    if (Qt)
-      xcol[nxcol] = getiparam("tcol");
+      Qt = hasvalue("tcol");
+      if (Qt)
+	xcol[nxcol] = getiparam("tcol");
 
-    instr = stropen (input,"r");
-    tptr  = table_open(instr, 0);
-    nrows = table_nrows(tptr);
-    ncols = table_ncols(tptr);
-    dprintf(1,"Table: %d x %d\n", nrows, ncols);
+      instr = stropen (input,"r");
+      tptr  = table_open(instr, 0);
+      nrows = table_nrows(tptr);
+      ncols = table_ncols(tptr);
+      dprintf(1,"Table: %d x %d\n", nrows, ncols);
+    }
+    npars = nemoinpr(getparam("pars"), pars, MAXSM);
  
     nsm = nemoinpr(getparam("smooth"),sm,MAXSM);
     if (nsm < 0) error("smooth=%s parsing error",getparam("smooth"));
     if (nsm == 0) 
       build_filter(filter);
-    else
+    else {
       if (nsm % 2 != 1) error("smooth=%s needs an odd number of values",getparam("smooth"));
+      for (i=0, sum=0.0; i<nsm; i++) sum += sm[i];
+      for (i=0; i<nsm; i++) sm[i] /= sum;
+    }
 
     for (i=0, sum=0.0; i<nsm; i++) sum += sm[i];
     dprintf(1,"Smooth sum= %g\n",sum);
@@ -145,6 +168,7 @@ local void read_data()
 local void build_filter(int filter)
 {
   int i;
+  
   if (filter < 0) {
     warning("passthrough");
     nsm = 1;
@@ -152,15 +176,45 @@ local void build_filter(int filter)
     return;
   }
   int maxfilter = sizeof(csts)/sizeof(cst);
-  if (filter >= maxfilter) error("Only %d filters available",maxfilter);
-  
-  cst *c = &csts[filter];
-  nsm = c->n;
-  for (i=0; i<nsm; i++)
-    sm[i] = c->coeff[i]/c->norm;
+  if (filter < maxfilter) {
+    cst *c = &csts[filter];
+    nsm = c->n;
+    for (i=0; i<nsm; i++)
+      sm[i] = c->coeff[i]/c->norm;
+  } else if (filter == 11) {              // box
+    if (npars < 1) error("%d need one pars=",npars);
+    nsm = (int) pars[0];
+    if (nsm >  MAXSM) error("MAXSM=%d too small",MAXSM);
+    for (i=0; i<nsm; i++)
+      sm[i] = 1.0/nsm;
+  } else if (filter == 12) {              // gauss
+    if (npars < 1) error("%d need one pars=",npars);
+    int stdev = (int) pars[0];
+    int nsigma = (npars < 2 ? 4 : (int) pars[1]);
+    nsm = 1+2*nsigma*stdev;
+    if (nsm >  MAXSM) error("MAXSM=%d too small",MAXSM);
+    dprintf(1,"gaussian kernel: stdev=%d nsigma=%d  nsm=%d\n", stdev, nsigma, nsm);
+    double sum = 0.0;
+    for (i=0; i<nsm; i++) {
+      sm[i] = exp(-0.5*sqr((i-nsigma*stdev)/(1.0*stdev)));
+      sum += sm[i];
+    }
+    for (i=0; i<nsm; i++)
+      sm[i] /= sum;
+  } else if (filter == 13) {              // trapezoidal
+    error("trapezoidal not implemented yet");
+  } else if (filter == 14) {              // moffat
+    error("moffat not implemented yet");    
+  } else
+    error("Unknown filter=%d", filter);
 }
 
 
+local void show_kernel(void)
+{
+  for (int i=0; i<nsm; i++)
+    printf("%g\n",sm[i]);
+}
 
 local void smooth_data(void)
 {
@@ -174,7 +228,7 @@ local void smooth_data(void)
       sum[j] = 0.0;
       for (k=-nsmh; k<=nsmh; k++) {
 	ipk = i+k;
-	if (ipk<0 || ipk>=npt) continue;
+	if (ipk<0 || ipk>=npt) continue;   // @todo deal with edge=
 	sum[j] += x[j][ipk] * sm[k+nsmh];
       }
       printf("%g ",sum[j]);
