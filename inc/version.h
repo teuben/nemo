6dd462a3e466eaf213025a5ef9f65d279936a3c6
Peter Teuben <teuben@gmail.com>
2024-12-22 19:45:56 -0500

    new version

--- inc/version.h
+++ inc/version.h
@@ -1,5 +1,5 @@
 /* do not edit - created by the src/scripts/version script */
 #if defined(GETPARAM_VERSION_ID)
-static char *version_h = "git_11690"; // git rev-list --count HEAD
+static char *version_h = "git_12230"; // git rev-list --count HEAD
 #endif
-#define NEMO_VERSION "4.4.3"
+#define NEMO_VERSION "4.4.6"
